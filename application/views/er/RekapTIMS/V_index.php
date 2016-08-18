4a70584e9f35901b598835fdc6c1374fe56e1214
Dony Tri Prasetya <dony_tri_prasetya@quick.com>
2016-08-18 14:04:44 +0700

    Update V_Index.php

--- application/views/er/RekapTIMS/V_index.php
+++ application/views/er/RekapTIMS/V_index.php
@@ -5,15 +5,6 @@
 					<div class="col-md-12">
 						<br />
 						<h1>Dashboard Rekap TIMS</h1>
-
-<?php 
-
-$periode = '2016-05-31';
-
-echo date('Y-m-d 00:00:00', strtotime($periode));
-
-
-?>
 					</div>
 				</div>
 		</section>
