984318c380b16d5568c3c918b71d0bfe770b323c
alfian <alfian@alfian.(none)>
2016-11-18 08:22:23 +0700

    menambahkan model dan view monitoring presensi finger

---
+++ application/models/PresenceManagement/MainMenu/M_monitoring.php
@@ -0,0 +1,207 @@
+<?php
+class M_monitoring extends CI_Model {
+
+        public function __construct()
+        {
+            parent::__construct();
+        }
+		
+		//===========================
+		// 	PRESENCE MANAGEMENT START
+		//===========================
+		
+		//select
+		public function GetDeviceList(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT a.sn,(SELECT lokasi_kerja FROM frontpresensi.tb_lokasikerja AS xx WHERE xx.kd_lokasi=b.lokasi_kerja) AS lokasi_kerja,c.host,a.id_lokasi,(SELECT COUNT(yy.noind) FROM fp_distribusi.tb_fppribadi AS yy WHERE yy.id_lokasi=a.id_lokasi) AS registered,b.lokasi, IF(c.host IS NOT NULL,'1','0') AS status_
+					FROM fp_distribusi.tb_device AS a
+					LEFT JOIN fp_distribusi.tb_lokasi AS b ON a.id_lokasi=b.id_lokasi
+					LEFT JOIN fp_distribusi.tb_mysql AS c ON a.id_lokasi=c.id_lokasi
+					ORDER BY status_ DESC,a.id_lokasi";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		//select
+		public function GetRegisteredPeople($id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT a.*,b.jabatan,(SELECT COUNT(xx.noind) FROM fp_distribusi.tb_jari_tks AS xx WHERE a.noind=xx.noind) AS fp_code 
+					FROM fp_distribusi.tb_fppribadi AS a 
+					LEFT JOIN hrd_khs.tpribadi AS b ON a.noind=b.noind
+					WHERE a.id_lokasi='$id' order by a.noind";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function GetSpesificDeviceList($id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT a.sn,(SELECT lokasi_kerja FROM frontpresensi.tb_lokasikerja AS xx WHERE xx.kd_lokasi=b.lokasi_kerja) AS lokasi_kerja,c.host,a.id_lokasi,(SELECT COUNT(yy.noind) FROM fp_distribusi.tb_fppribadi AS yy WHERE yy.id_lokasi=a.id_lokasi) AS registered,b.lokasi, IF(c.host IS NOT NULL,'1','0') AS status_
+					FROM fp_distribusi.tb_device AS a
+					INNER JOIN fp_distribusi.tb_lokasi AS b ON a.id_lokasi=b.id_lokasi
+					INNER JOIN fp_distribusi.tb_mysql AS c ON a.id_lokasi=c.id_lokasi
+					WHERE a.id_lokasi='$id'";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function GetLocation($id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT * FROM fp_distribusi.tb_lokasi WHERE id_lokasi!='$id' ORDER BY id_lokasi";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function DelRegisteredPerson($loc,$id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "delete from fp_distribusi.tb_fppribadi WHERE noind='$id' AND id_lokasi='$loc'";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function UpdateRegisteredPerson($loc,$id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "UPDATE fp_distribusi.tb_fppribadi SET id_lokasi=NULL WHERE noind='$id' AND id_lokasi='$loc'";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function UpdateNameLocation($loc,$name){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "UPDATE fp_distribusi.tb_lokasi SET lokasi='$name' WHERE id_lokasi='$loc'";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function MutationRegisteredPerson($tgt,$loc,$id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "UPDATE fp_distribusi.tb_fppribadi SET id_lokasi='$tgt' WHERE noind='$id' AND id_lokasi='$loc'";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function getPerson($q,$loc){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql		= "SELECT a.noind,a.nama,a.id_lokasi FROM fp_distribusi.tb_fppribadi AS a WHERE ('$loc' NOT IN (SELECT b.id_lokasi FROM fp_distribusi.tb_fppribadi AS b WHERE a.noind=b.noind) OR a.id_lokasi IS NULL) AND  a.keluar='0' AND ( a.noind LIKE '%$q%') GROUP BY noind";
+			$query		= $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function checkloc($id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql		= "select * from fp_distribusi.tb_fppribadi where noind='$id'";
+			$query		= $quickcom->query($sql);
+			return $query;
+		}
+		
+		public function updatePerson($id,$loc){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql		= "update fp_distribusi.tb_fppribadi set id_lokasi='$loc' where noind='$id'";
+			$query		= $quickcom->query($sql);
+			return;
+		}
+		
+		public function insertPerson($id,$loc){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql		= "INSERT INTO fp_distribusi.tb_fppribadi (noind,nama,jenkel,alamat,nohp,diangkat,masukkerja,kodesie,keluar,tglkeluar,noind_baru,kode_status_kerja,lokasi_kerja,access,id_lokasi)
+							SELECT DISTINCT(noind),nama,jenkel,alamat,nohp,diangkat,masukkerja,kodesie,keluar,tglkeluar,noind_baru,kode_status_kerja,lokasi_kerja,access,'$loc'
+							FROM fp_distribusi.tb_fppribadi
+							WHERE noind='$id'";
+			$query		= $quickcom->query($sql);
+			return;
+		}
+		
+		public function GetAccessablePeople(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT a.*,b.jabatan,(SELECT COUNT(xx.noind) FROM ceklembur.tb_jari_tks AS xx WHERE a.noind=xx.noind) AS fp_code 
+					FROM fp_distribusi.tb_fppribadi AS a 
+					LEFT JOIN hrd_khs.tpribadi AS b ON a.noind=b.noind
+					WHERE a.access='1' order by a.noind";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		//------------------------------ MENU ADD DEVICE
+		
+		public function maxfinger(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT MAX(CAST(SUBSTR(id_lokasi,3) AS UNSIGNED)) as maxfp FROM fp_distribusi.tb_lokasi ORDER BY id_lokasi";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function office(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT * FROM frontpresensi.tb_lokasikerja ORDER BY kd_lokasi";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function checkDevice($sn){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT * FROM fp_distribusi.tb_device WHERE sn='$sn'";
+			$query = $quickcom->query($sql);
+			return $query->num_rows();
+		}
+		
+		public function inserttblokasi($idloc,$loc,$off){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "INSERT INTO fp_distribusi.tb_lokasi (id_lokasi,lokasi,lokasi_kerja) VALUES ('$idloc','$loc','$off')";
+			$query = $quickcom->query($sql);
+			return;
+		}
+
+
+
+		public function inserttbdevice($sn,$vc,$ac,$idloc){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "INSERT INTO fp_distribusi.tb_device (sn,vc,ac,id_lokasi) VALUES ('$sn','$vc','$ac','$idloc')";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function inserttbmysql($idloc,$ip){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "INSERT INTO fp_distribusi.tb_mysql (id_lokasi,host,user,pass,db) VALUES ('$idloc','$ip','$ip','123456','coba_jari')";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function inserttbpostgres($idloc,$ip){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "INSERT INTO fp_distribusi.tb_postgres (id_lokasi,host,port,db,user,pass) VALUES ('$idloc','$ip','5432','presensi','postgres','123456')";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		public function GetSpesificDevice($id){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "SELECT a.*,b.*,c.*
+					FROM fp_distribusi.tb_device AS a
+					LEFT JOIN fp_distribusi.tb_lokasi AS b ON a.id_lokasi=b.id_lokasi
+					LEFT JOIN fp_distribusi.tb_mysql AS c ON a.id_lokasi=c.id_lokasi
+					WHERE a.sn='$id'";
+			$query = $quickcom->query($sql);
+			return;
+		}
+		
+		//--------------------------- Menu Registered person
+		
+		public function GetListPerson(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "select * from fp_distribusi.tb_fppribadi where keluar='0' group by noind order by noind";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		public function getListLocation(){
+			$quickcom	= $this->load->database('quickcom',true);
+			$sql = "select * from fp_distribusi.tb_lokasi order by id_lokasi";
+			$query = $quickcom->query($sql);
+			return $query->result_array();
+		}
+		
+		//=========================
+		// 	PRESENCE MANAGEMENT END
+		//=========================
+		
+}
+?>
---
+++ application/views/PresenceManagement/MainMenu/Monitoring/V_Setting.php
@@ -0,0 +1,128 @@
+<script src="<?php echo base_url('assets/js/ajaxSearch.js');?>"></script>
+
+<section class="content">
+	<div class="inner" >
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="row" >
+				<div class="col-lg-12">
+					<div class="col-lg-11">
+						<div class="text-right">
+						<h1><b>Presence Monitoring</b></h1>
+						</div>
+					</div>
+					<div class="col-lg-1">
+						<div class="text-right hidden-md hidden-sm hidden-xs">
+                            <a class="btn btn-default btn-lg" href="<?php echo site_url('CateringManagement/Receipt');?>">
+                                <i class="icon-wrench icon-2x"></i>
+                                <span><br/></span>	
+                            </a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br/>
+			
+			<div class="row">
+				<div class="col-lg-12">
+				<div class="box box-primary box-solid">
+					<div class="box-header with-border">
+						<b>Add new fingerprint device</b>
+					</div>
+					<div class="box-body">
+					<form method="post" action="<?php echo base_url('PresenceManagement/Monitoring/SaveDevice')?>">
+						<!-- INPUT GROUP 1 ROW 1 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Serial Number</label>
+								<div class="col-lg-4">
+									<input name="txtSN" class="form-control toupper" placeholder="[ Enter the serial number of fingerprint ]" value="" >
+								</div>
+							</div>
+						</div>
+						<!-- INPUT GROUP 1 ROW 2 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Verifivation Code</label>
+								<div class="col-lg-5">
+									<input name="txtVC" class="form-control toupper" placeholder="[ Enter the verification code of fingerprint ]"  >
+								</div>
+							</div>
+						</div>
+						<!-- INPUT GROUP 1 ROW 3 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Activation Code</label>
+								<div class="col-lg-5">
+									<input name="txtAC" class="form-control toupper" placeholder="[ Enter the activation code of fingerprint ]"  >
+								</div>
+							</div>
+						</div>
+						<hr>
+						<!-- INPUT GROUP 2 ROW 1 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Id Location</label>
+								<div class="col-lg-6">
+									<input name="txtIdLocation" class="form-control toupper" value="" placeholder=""  readonly>
+								</div>
+							</div>
+						</div>
+						<!-- INPUT GROUP 2 ROW 1.2 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Location</label>
+								<div class="col-lg-6">
+									<input name="txtLocation" class="form-control toupper" placeholder="[ Enter the location device ]"  >
+								</div>
+							</div>
+						</div>
+						<!-- INPUT GROUP 2 ROW 3 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">Office</label>
+								<div class="col-lg-3">
+									<select class="form-control" name="txtOffice" placeholder="[ Enter the location device ]">
+									<option value="">[ Select Office ]</option>
+										<?php
+											foreach($office as $data_office){
+												echo "<option value='".$data_office['id_lokasi']."'>".$data_office['lokasi_kerja']."</option>";
+											}
+										?>
+									</select>
+								</div>	
+							</div>
+						</div>
+						<hr>
+						
+						
+						<!-- INPUT GROUP 3 ROW 1 -->
+						<div class="row" style="margin: 10px 10px">
+							<div class="form-group">
+								<label class="col-lg-2 control-label">IP Address</label>
+								<div class="col-lg-3">
+									<input id="calc" name="txtIP" class="form-control ip_address" onkeypress="return isNumberKey(event)" placeholder="Calc">
+								</div>								
+							</div>
+						</div>
+						<hr>
+						<!-- submit -->
+						<div class="form-group">
+							<div class="col-lg-12 text-right">
+								<a href="<?php echo site_url('PresenceManagement/Monitoring');?>"  class="btn btn-success btn-lg btn-rect">Back</a>
+								&nbsp;&nbsp;
+								<button type="submit" class="btn btn-success btn-lg btn-rect">Save Data</button>
+							</div>
+						</div>
+						
+					<form>
+					</div>
+				</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+	</div>
+</section>			
+			
+				
