a07bb28c245b425d34a2424789ad3c0552c76f1f
DONY <dony_tri_prasetya@quick.com>
2016-11-26 11:50:24 +0700

    Fix Issues #22

---
+++ application/views/PayrollManagement/RiwayatUpamk/V_index.php
@@ -0,0 +1,82 @@
+<section class="content">
+	<div class="inner" >
+	  <div class="row">
+	    <div class="col-lg-12">
+	      <div class="row">
+	        <div class="col-lg-12">
+	          <div class="col-lg-11">
+	            <div class="text-right">
+	            <h1><b>Riwayat Upamk</b></h1>
+	            </div>
+	          </div>
+	          <div class="col-lg-1">
+	            <div class="text-right hidden-md hidden-sm hidden-xs">
+	            	<a class="btn btn-default btn-lg" href="<?php echo site_url('PayrollManagement/RiwayatUpamk');?>">
+	            		<i class="icon-wrench icon-2x"></i>
+	            		<span><br/></span>
+	            	</a>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	      <br/>
+	      
+	      <div class="row">
+	        <div class="col-lg-12">
+		        <div class="box box-primary box-solid">
+		          <div class="box-header with-border">
+                  <a href="<?php echo site_url('PayrollManagement/RiwayatUpamk/create/') ?>" style="float:right;margin-right:1%;margin-top:-0.5%;" alt="Add New" title="Add New" >
+                      <button type="button" class="btn btn-default btn-sm">
+                        <i class="icon-plus icon-2x"></i>
+                      </button>
+                    </a>
+                    <b>Riwayat Upamk</b>
+		          </div>
+		          <div class="box-body">
+
+		            <div class="table-responsive">
+		              <table class="table table-striped table-bordered table-hover text-left" id="dataTables-riwayatUpamk" style="font-size:12px;">
+		                <thead class="bg-primary">
+		                  <tr>
+		                    <th style="text-align:center; width:30px">NO</th>
+                            <th style='text-align:center'>ACTION</th>
+							<th>Tgl Berlaku</th>
+							<th>Tgl Tberlaku</th>
+							<th>Periode</th>
+							<th>Noind</th>
+							<th>Upamk</th>
+							<th>Kd Petugas</th>
+							<th>Tgl Rec</th>
+
+		                  </tr>
+		                </thead>
+		                <tbody>
+							<?php $no = 1; foreach($riwayatUpamk_data as $row) { ?>
+							<tr>
+							  <td align='center'><?php echo $no++;?></td>
+                              <td align='center' width='200px'>
+                              	<a href="<?php echo base_url('PayrollManagement/RiwayatUpamk/read/'.$row->id_upamk.''); ?>" class="btn btn-xs btn-primary" data-toggle="tooltip" data-placement="bottom" title="Read Data"><span class="fa fa-eye"></span></a>
+                              	<a href="<?php echo base_url('PayrollManagement/RiwayatUpamk/update/'.$row->id_upamk.''); ?>" class="btn btn-xs btn-warning" data-toggle="tooltip" data-placement="bottom" title="Edit Data"><span class="fa fa-pencil-square-o"></span></a>
+                              	<a href="<?php echo base_url('PayrollManagement/RiwayatUpamk/delete/'.$row->id_upamk.''); ?>" class="btn btn-xs btn-danger" data-toggle="tooltip" data-placement="bottom" title="Hapus Data" onclick="return confirm('Are you sure you want to delete this item?');"><span class="fa fa-times"></span></a>
+                              </td>
+							<td><?php echo $row->tgl_berlaku ?></td>
+							<td><?php echo $row->tgl_tberlaku ?></td>
+							<td><?php echo $row->periode ?></td>
+							<td><?php echo $row->noind ?></td>
+							<td><?php echo $row->upamk ?></td>
+							<td><?php echo $row->kd_petugas ?></td>
+							<td><?php echo $row->tgl_rec ?></td>
+
+							</tr>
+							<?php } ?>
+		                </tbody>                                      
+		              </table>
+		            </div>
+		          </div>
+		        </div>
+	        </div>
+	      </div>    
+	    </div>    
+	  </div>
+	</div>
+</section>
