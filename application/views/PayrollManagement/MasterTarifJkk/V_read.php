eae7de5d3c69ca84e3f54a5f92f199b7a156078e
DONY <dony_tri_prasetya@quick.com>
2016-11-26 13:22:25 +0700

    Fix Issues #34

---
+++ application/views/PayrollManagement/MasterTarifJkk/V_read.php
@@ -0,0 +1,68 @@
+<section class="content">
+    <div class="inner" >
+        <div class="row">
+            <form class="form-horizontal">
+            <div class="col-lg-12">
+                <div class="row">
+                        <div class="col-lg-12">
+                            <div class="col-lg-11">
+                                <div class="text-right">
+                                <h1><b>Master Tarif Jkk</b></h1>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-1 ">
+                                <div class="text-right hidden-md hidden-sm hidden-xs">
+                                    <a class="btn btn-default btn-lg" href="<?php echo site_url('PayrollManagement/MasterTarifJkk/');?>">
+                                        <i class="icon-wrench icon-2x"></i>
+                                        <span ><br /></span>
+                                    </a>                             
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <br />
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="box box-primary box-solid">
+                            <div class="box-header with-border">
+                                Read Master Tarif Jkk
+                            </div>
+                        <div class="box-body">
+                            <div class="panel-body">
+                                <div class="row">
+<div class="form-group">
+                                            <label for="txtIdKantorAsal" class="control-label col-lg-4">Id Kantor Asal</label>
+                                            <div class="col-lg-4">
+                                                <input type="text" name="txtIdKantorAsal" id="txtIdKantorAsal" class="form-control" value="<?php echo $id_kantor_asal; ?>" readonly/>
+                                            </div>
+                                        </div>
+<div class="form-group">
+                                            <label for="txtIdLokasiKerja" class="control-label col-lg-4">Id Lokasi Kerja</label>
+                                            <div class="col-lg-4">
+                                                <input type="text" name="txtIdLokasiKerja" id="txtIdLokasiKerja" class="form-control" value="<?php echo $id_lokasi_kerja; ?>" readonly/>
+                                            </div>
+                                        </div>
+<div class="form-group">
+                                            <label for="txtTarifJkk" class="control-label col-lg-4">Tarif Jkk</label>
+                                            <div class="col-lg-4">
+                                                <input type="text" name="txtTarifJkk" id="txtTarifJkk" class="form-control" value="<?php echo $tarif_jkk; ?>" readonly/>
+                                            </div>
+                                        </div>
+</div>
+                                
+                            </div>
+                            <div class="panel-footer">
+                                <div class="row text-right">
+                                    <a href="javascript:history.back(1)" class="btn btn-primary btn-lg btn-rect">Back</a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
+</section>
