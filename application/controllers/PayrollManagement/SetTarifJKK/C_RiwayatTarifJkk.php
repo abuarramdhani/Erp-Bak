68e3106c1444b33a56bbd2e3d41b4712f7c35855
DONY <dony_tri_prasetya@quick.com>
2016-11-29 10:06:55 +0700

    Fix Issues #52

---
+++ application/controllers/PayrollManagement/SetTarifJKK/C_RiwayatTarifJkk.php
@@ -0,0 +1,222 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class C_RiwayatTarifJkk extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        $this->load->helper('url');
+        $this->load->library('csvimport');
+        $this->load->model('SystemAdministration/MainMenu/M_user');
+        $this->load->model('PayrollManagement/SetTarifJKK/M_riwayattarifjkk');
+        if($this->session->userdata('logged_in')!=TRUE) {
+            $this->load->helper('url');
+            $this->session->set_userdata('last_page', current_url());
+            $this->session->set_userdata('Responsbility', 'some_value');
+        }
+    }
+
+	public function index()
+    {
+        $this->checkSession();
+        $user_id = $this->session->userid;
+        
+        $data['Menu'] = 'Payroll Management';
+        $data['SubMenuOne'] = '';
+        $data['SubMenuTwo'] = '';
+
+        $data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+        $data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+        $data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+        $riwayatTarifJkk = $this->M_riwayattarifjkk->get_all();
+
+        $data['riwayatTarifJkk_data'] = $riwayatTarifJkk;
+        $this->load->view('V_Header',$data);
+        $this->load->view('V_Sidemenu',$data);
+        $this->load->view('PayrollManagement/RiwayatTarifJkk/V_index', $data);
+        $this->load->view('V_Footer',$data);
+    }
+
+	public function read($id)
+    {
+        $this->checkSession();
+        $user_id = $this->session->userid;
+        
+        $row = $this->M_riwayattarifjkk->get_by_id($id);
+        if ($row) {
+            $data = array(
+            	'Menu' => 'Payroll Management',
+            	'SubMenuOne' => '',
+            	'SubMenuTwo' => '',
+            	'UserMenu' => $this->M_user->getUserMenu($user_id,$this->session->responsibility_id),
+            	'UserSubMenuOne' => $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id),
+            	'UserSubMenuTwo' => $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id),
+            
+				'id_tarif_jkk' => $row->id_tarif_jkk,
+				'tgl_berlaku' => $row->tgl_berlaku,
+				'tgl_tberlaku' => $row->tgl_tberlaku,
+				'id_kantor_asal' => $row->id_kantor_asal,
+				'id_lokasi_kerja' => $row->id_lokasi_kerja,
+				'tarif_jkk' => $row->tarif_jkk,
+				'kd_petugas' => $row->kd_petugas,
+				'tgl_rec' => $row->tgl_rec,
+				'status_aktif' => $row->status_aktif,
+			);
+
+            $this->load->view('V_Header',$data);
+            $this->load->view('V_Sidemenu',$data);
+            $this->load->view('PayrollManagement/RiwayatTarifJkk/V_read', $data);
+            $this->load->view('V_Footer',$data);
+        }
+        else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+        }
+    }
+
+    public function create()
+    {
+
+        $this->checkSession();
+        $user_id = $this->session->userid;
+
+        $data = array(
+            'Menu' => 'Payroll Management',
+            'SubMenuOne' => '',
+            'SubMenuTwo' => '',
+            'UserMenu' => $this->M_user->getUserMenu($user_id,$this->session->responsibility_id),
+            'UserSubMenuOne' => $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id),
+            'UserSubMenuTwo' => $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id),
+            'action' => site_url('PayrollManagement/RiwayatTarifJkk/save'),
+				'id_tarif_jkk' => set_value(''),
+			'tgl_berlaku' => set_value('tgl_berlaku'),
+			'tgl_tberlaku' => set_value('tgl_tberlaku'),
+			'pr_kantor_asal_data' => $this->M_riwayattarifjkk->get_pr_kantor_asal_data(),
+			'id_kantor_asal' => set_value('id_kantor_asal'),
+			'pr_lokasi_kerja_data' => $this->M_riwayattarifjkk->get_pr_lokasi_kerja_data(),
+			'id_lokasi_kerja' => set_value('id_lokasi_kerja'),
+			'tarif_jkk' => set_value('tarif_jkk'),
+			'kd_petugas' => set_value('kd_petugas'),
+			'tgl_rec' => set_value('tgl_rec'),
+			'status_aktif' => set_value('status_aktif'),
+		);
+
+        $this->load->view('V_Header',$data);
+        $this->load->view('V_Sidemenu',$data);
+        $this->load->view('PayrollManagement/RiwayatTarifJkk/V_form', $data);
+        $this->load->view('V_Footer',$data);
+    }
+
+    public function save()
+    {
+        $this->formValidation();
+
+        
+            $data = array(
+				'tgl_berlaku' => $this->input->post('txtTglBerlaku',TRUE),
+				'tgl_tberlaku' => $this->input->post('txtTglTberlaku',TRUE),
+				'id_kantor_asal' => $this->input->post('cmbIdKantorAsal',TRUE),
+				'id_lokasi_kerja' => $this->input->post('cmbIdLokasiKerja',TRUE),
+				'tarif_jkk' => $this->input->post('txtTarifJkk',TRUE),
+				'kd_petugas' => $this->input->post('txtKdPetugas',TRUE),
+				'tgl_rec' => $this->input->post('txtTglRec',TRUE),
+				'status_aktif' => $this->input->post('txtStatusAktif',TRUE),
+			);
+
+            $this->M_riwayattarifjkk->insert($data);
+            $this->session->set_flashdata('message', 'Create Record Success');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+        
+    }
+
+    public function update($id)
+    {
+
+        $this->checkSession();
+        $user_id = $this->session->userid;
+
+        $row = $this->M_riwayattarifjkk->get_by_id($id);
+
+        if ($row) {
+            $data = array(
+                'Menu' => 'Payroll Management',
+                'SubMenuOne' => '',
+                'SubMenuTwo' => '',
+                'UserMenu' => $this->M_user->getUserMenu($user_id,$this->session->responsibility_id),
+                'UserSubMenuOne' => $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id),
+                'UserSubMenuTwo' => $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id),
+                'action' => site_url('PayrollManagement/RiwayatTarifJkk/saveUpdate'),
+				'id_tarif_jkk' => set_value('txtIdTarifJkk', $row->id_tarif_jkk),
+				'tgl_berlaku' => set_value('txtTglBerlaku', $row->tgl_berlaku),
+				'tgl_tberlaku' => set_value('txtTglTberlaku', $row->tgl_tberlaku),
+				'id_kantor_asal' => set_value('txtIdKantorAsal', $row->id_kantor_asal),
+				'id_lokasi_kerja' => set_value('txtIdLokasiKerja', $row->id_lokasi_kerja),
+				'tarif_jkk' => set_value('txtTarifJkk', $row->tarif_jkk),
+				'kd_petugas' => set_value('txtKdPetugas', $row->kd_petugas),
+				'tgl_rec' => set_value('txtTglRec', $row->tgl_rec),
+				'status_aktif' => set_value('txtStatusAktif', $row->status_aktif),
+                'pr_kantor_asal_data' => $this->M_riwayattarifjkk->get_pr_kantor_asal_data(),
+                'pr_lokasi_kerja_data' => $this->M_riwayattarifjkk->get_pr_lokasi_kerja_data(),
+				);
+            $this->load->view('V_Header',$data);
+            $this->load->view('V_Sidemenu',$data);
+            $this->load->view('PayrollManagement/RiwayatTarifJkk/V_form', $data);
+            $this->load->view('V_Footer',$data);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+        }
+    }
+
+    public function saveUpdate()
+    {
+        $this->formValidation();
+
+            $data = array(
+				'tgl_berlaku' => $this->input->post('txtTglBerlaku',TRUE),
+				'tgl_tberlaku' => $this->input->post('txtTglTberlaku',TRUE),
+				'id_kantor_asal' => $this->input->post('cmbIdKantorAsal',TRUE),
+				'id_lokasi_kerja' => $this->input->post('cmbIdLokasiKerja',TRUE),
+				'tarif_jkk' => $this->input->post('txtTarifJkk',TRUE),
+				'kd_petugas' => $this->input->post('txtKdPetugas',TRUE),
+				'tgl_rec' => $this->input->post('txtTglRec',TRUE),
+				'status_aktif' => $this->input->post('txtStatusAktif',TRUE),
+			);
+
+            $this->M_riwayattarifjkk->update($this->input->post('txtIdTarifJkk', TRUE), $data);
+            $this->session->set_flashdata('message', 'Update Record Success');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+    }
+
+    public function delete($id)
+    {
+        $row = $this->M_riwayattarifjkk->get_by_id($id);
+
+        if ($row) {
+            $this->M_riwayattarifjkk->delete($id);
+            $this->session->set_flashdata('message', 'Delete Record Success');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('PayrollManagement/RiwayatTarifJkk'));
+        }
+    }
+
+    public function checkSession(){
+        if($this->session->is_logged){
+            
+        }else{
+            redirect(site_url());
+        }
+    }
+
+    public function formValidation()
+    {
+	}
+
+}
+
+/* End of file C_RiwayatTarifJkk.php */
+/* Location: ./application/controllers/PayrollManagement/SetTarifJKK/C_RiwayatTarifJkk.php */
+/* Generated automatically on 2016-11-29 09:13:51 */
