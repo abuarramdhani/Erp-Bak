7ea40f8d0f62d8d0fe70df08d74af9b393244690
F2032 <ratno_firmansyah@quick.com>
2016-12-05 12:48:48 +0700

    rapi - rapi tampilan add new service CR

--- application/views/CustomerRelationship/MainMenu/ServiceProducts/V_create.php
+++ application/views/CustomerRelationship/MainMenu/ServiceProducts/V_create.php
@@ -28,109 +28,171 @@
 					<div class="box-body">
 						<div class="row">
 							<div class="col-lg-12">
-								<input type="hidden" value="<?php echo date("Y-m-d H:i:s")?>" name="hdnDate" />
-								<input type="hidden" value="<?php echo $this->session->userid; ?>" name="hdnUser" />
-								<div class="col-lg-6">
-									<div class="form-group">
-											<label for="norm" class="control-label col-lg-4">Activity Type</label>
-											<div class="col-lg-8">
-												<select  name="slcActivityType" id="slcActivityType" class="form-control select4" onchange="getLastActivityNumber('<?php echo site_url();?>');" required>
-													<option value="" ></option>
-													<option value="service_keliling" >Service Keliling</option>
-													<option value="customer_visit" >Customer Visit</option>
-													<option value="visit_us" >Visit Us</option>
-													<option value="kirim_part" >Kirim Part</option>
-													<!--<option value="others">Others</option>-->
-												</select>
-											</div>
-
-									</div>
-									<div class="form-group">
-										<label for="norm" class="control-label col-lg-4">Officer</label>
-										<div class="col-lg-8">
-											<select name="slcEmployeeNum[]" id="slcEmployeeNum" class="form-control jsEmployeeData" data-placeholder="Employee">
-												<option value=""></option>
-											</select>
-										</div>
-									</div>
-									<div class="form-group">
-											<label class="control-label col-lg-4" for="dp2">Activity Date</label>
-
-											<div class="col-lg-8">
-												<input type="text" maxlength="11" placeholder="<?php echo date("d-M-Y")?>" name="txtServiceDate" value="<?php echo date("d-M-Y")?>" class="form-control" data-date-format="dd-M-yyyy" id="dp2" />
-											</div>
-
-									</div>
-									<div class="form-group">
-											<label for="norm" class="control-label col-lg-4">Customer</label>
-											<div class="col-lg-8">
-												<!--<input type="text" placeholder="Customer Name" name="txtCustomerName" id="txtCustomerName" onblur="selectCustomer()" onchange="enadisLineOwner()" class="form-control2"/>-->
-												<div class="input-group">
-													<input type="text" placeholder="Search Customer" name="txtCustomerName" id="txtCustomerName" onfocus="callModal('<?php echo site_url('ajax/ModalCustomer')?>');" onkeypress="return noInput(event)" class="form-control toupper" />
-													<span class="input-group-btn">
-														<a class="btn btn-primary" href="<?php echo site_url('ajax/ModalCustomer')?>" data-toggle="modal" data-target="#myModal"><i class="icon-search"></i></a>
-
-													</span>
-												</div>
-												<input type="hidden" name="hdnCustomerId" id ="hdnCustomerId" />
-												<input type="hidden" name="hdnCategoryId" id ="hdnCategoryId"  />
-											</div>
-									</div>
-									<div class="form-group">
-										<label class="control-label col-lg-4">Condition</label>
-										<div class="col-lg-8">
-											<textarea type="text" class="form-control" name="ConditionClaim" id="ConditionClaim" rows="1" placeholder="Condition"></textarea>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg-6">
-									<div class="form-group">
-											<label for="norm" class="control-label col-lg-4">Activity Number</label>
-											<div class="col-lg-8">
-												<input type="text" name="txtServiceNumber" id="txtServiceNumber" class="form-control" required />
-											</div>
-									</div>
-									<div class="form-group">
-
-											<label for="norm" class="control-label col-lg-4">Activity Status</label>
-											<div class="col-lg-8">
-												<input type="text" placeholder="Service Status" name="txtActivityStatus" id="txtActivityStatus" value="OPEN" readonly="readonly" class="form-control"/>
-											</div>
-									</div>
-									<div class="form-group">
-											<label for="norm" class="control-label col-lg-4">Connect Num</label>
-											<div class="col-lg-8">
-												<select name="slcConnectNum" id="slcConnectNum" class="form-control jsConnectNumber">
-													<option value=""></option>
-												</select>
-											</div>
-										</div>
-									<div class="form-group">
-											<label for="norm" class="control-label col-lg-4">Description</label>
-											<div class="col-lg-8">
-												<textarea placeholder="Description" name="txtDescription" id="txtDescription" class="form-control" rows="1"></textarea>
-											</div>
-									</div>
-									<div class="form-group">
-										<label class="control-label col-lg-4">Qty</label>
-										<div class="col-lg-8">
-											<input type="number" class="form-control" name="QtyClaim" id="QtyClaim" placeholder="Qty">
-										</div>
-									</div>
-									<input type="hidden" id="spanEmployee" />
+								<div class="panel">
+									<ul class="nav nav-tabs" role="tablist">
+										<li role="presentation" class="active">
+											<a href="#general" aria-controls="general" role="tab" data-toggle="tab">General</a>
+										</li>
+										<li role="presentation">
+											<a href="#ClaimDescription" aria-controls="ClaimDescription" role="tab" data-toggle="tab">Claim Description</a>
+										</li>
+										<li role="presentation">
+											<a href="#location" aria-controls="location" role="tab" data-toggle="tab">Location Incident</a>
+										</li>
+										<li role="presentation">
+											<a href="#Condition" aria-controls="Condition" role="tab" data-toggle="tab">Item Condition</a>
+										</li>
+										<li role="presentation">
+											<a href="#Details" aria-controls="Details" role="tab" data-toggle="tab">Details</a>
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
 						<div class="row">
 							<div class="col-lg-12">
-								<div class="col-lg-12">
-									<div class="panel panel-default">
-										<div class="panel-body">
-											<h5><b>Location Incident</b></h5>
+								<div class="tab-content">
+									<div role="tabpanel" class="tab-pane active" id="general">
+										<input type="hidden" value="<?php echo date("Y-m-d H:i:s")?>" name="hdnDate" />
+										<input type="hidden" value="<?php echo $this->session->userid; ?>" name="hdnUser" />
+										<div class="col-lg-6">
 											<div class="form-group">
-												<label class="control-label col-lg-3">Province</label>
-												<div class="col-lg-9">
-													<select class="form-control select4" name="provinceIncident[]" id="provinceIncident">
+													<label for="norm" class="control-label col-lg-4">Activity Type</label>
+													<div class="col-lg-8">
+														<select  name="slcActivityType" id="slcActivityType" class="form-control select4" onchange="getLastActivityNumber('<?php echo site_url();?>');" required>
+															<option value="" ></option>
+															<option value="service_keliling" >Service Keliling</option>
+															<option value="customer_visit" >Customer Visit</option>
+															<option value="visit_us" >Visit Us</option>
+															<option value="kirim_part" >Kirim Part</option>
+															<!--<option value="others">Others</option>-->
+														</select>
+													</div>
+		
+											</div>
+											<div class="form-group">
+												<label for="norm" class="control-label col-lg-4">Officer</label>
+												<div class="col-lg-8">
+													<select name="slcEmployeeNum[]" id="slcEmployeeNum" class="form-control jsEmployeeData" data-placeholder="Employee">
+														<option value=""></option>
+													</select>
+												</div>
+											</div>
+											<div class="form-group">
+													<label class="control-label col-lg-4" for="dp2">Activity Date</label>
+		
+													<div class="col-lg-8">
+														<input type="text" maxlength="11" placeholder="<?php echo date("d-M-Y")?>" name="txtServiceDate" value="<?php echo date("d-M-Y")?>" class="form-control" data-date-format="dd-M-yyyy" id="dp2" />
+													</div>
+		
+											</div>
+											<div class="form-group">
+													<label for="norm" class="control-label col-lg-4">Customer</label>
+													<div class="col-lg-8">
+														<!--<input type="text" placeholder="Customer Name" name="txtCustomerName" id="txtCustomerName" onblur="selectCustomer()" onchange="enadisLineOwner()" class="form-control2"/>-->
+														<div class="input-group">
+															<input type="text" placeholder="Search Customer" name="txtCustomerName" id="txtCustomerName" onfocus="callModal('<?php echo site_url('ajax/ModalCustomer')?>');" onkeypress="return noInput(event)" class="form-control toupper" />
+															<span class="input-group-btn">
+																<a class="btn btn-primary" href="<?php echo site_url('ajax/ModalCustomer')?>" data-toggle="modal" data-target="#myModal"><i class="icon-search"></i></a>
+		
+															</span>
+														</div>
+														<input type="hidden" name="hdnCustomerId" id ="hdnCustomerId" />
+														<input type="hidden" name="hdnCategoryId" id ="hdnCategoryId"  />
+													</div>
+											</div>
+										</div>
+										<div class="col-lg-6">
+											<div class="form-group">
+													<label for="norm" class="control-label col-lg-4">Activity Number</label>
+													<div class="col-lg-8">
+														<input type="text" name="txtServiceNumber" id="txtServiceNumber" class="form-control" required />
+													</div>
+											</div>
+											<div class="form-group">
+		
+													<label for="norm" class="control-label col-lg-4">Activity Status</label>
+													<div class="col-lg-8">
+														<input type="text" placeholder="Service Status" name="txtActivityStatus" id="txtActivityStatus" value="OPEN" readonly="readonly" class="form-control"/>
+													</div>
+											</div>
+											<div class="form-group">
+													<label for="norm" class="control-label col-lg-4">Connect Num</label>
+													<div class="col-lg-8">
+														<select name="slcConnectNum" id="slcConnectNum" class="form-control jsConnectNumber">
+															<option value=""></option>
+														</select>
+													</div>
+												</div>
+											<div class="form-group">
+													<label for="norm" class="control-label col-lg-4">Description</label>
+													<div class="col-lg-8">
+														<textarea placeholder="Description" name="txtDescription" id="txtDescription" class="form-control" rows="1"></textarea>
+													</div>
+											</div>
+											<div class="form-group">
+												<label class="control-label col-lg-4">Qty</label>
+												<div class="col-lg-8">
+													<input type="number" class="form-control" name="QtyClaim" id="QtyClaim" placeholder="Qty">
+												</div>
+											</div>
+											<input type="hidden" id="spanEmployee" />
+										</div>
+									</div>
+									<div role="tabpanel" class="tab-pane" id="ClaimDescription">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label class="control-label col-lg-4">Duration of Use</label>
+												<div class="input-group col-lg-8">
+													<div class="input-group-addon">
+														<i class="glyphicon glyphicon-calendar"></i>
+													</div>
+													<input type="number" class="form-control" name="durationUse" placeholder="Duration of Use" data-toggle="tooltip" data-placement="top" title="Masukkan lama waktu pemakaian" required>
+													<select style="width:100%;" name="durationUseType" data-placeholder="Duration of Use Type" class="form-control select2" required>
+														<option value="" disabled selected><-- PILIH TIPE SATUAN WAKTU --></option>
+														<option value="muach" disabled><-- PILIH TIPE SATUAN WAKTU --></option>
+														<option value="DAYS">DAYS</option>
+														<option value="MONTHS">MONTHS</option>
+														<option value="YEARS">YEARS</option>
+													</select>
+												</div>
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="row">
+												<div class="col-lg-12">
+													<div class="btn-group btn-group-justified" role="group">
+														<div class="btn-group" role="group">
+															<button type="button" class="btn btn-default" id="claimsItem1">
+																Shipment Date
+															</button>
+														</div>
+														<div class="btn-group" role="group">
+															<button type="button" class="btn btn-default" id="claimsItem2">
+																Not Shipped
+															</button>
+														</div>
+														<div class="btn-group" role="group">
+															<button type="button" class="btn btn-default" id="claimsItem3">
+																No Evidence
+															</button>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="row">
+												<div class="col-lg-10 col-lg-offset-1">
+													<div id="showClaimsItem"></div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div role="tabpanel" class="tab-pane" id="location">
+										<div class="col-lg-6">
+											<div class="form-group">
+												<label class="control-label col-lg-4">Province</label>
+												<div class="col-lg-8">
+													<select class="form-control select4" name="provinceIncident" id="provinceIncident" style="width: 100%;">
 														<option value=""></option>
 														<option value="muach" disabled >-- Choose One --</option>
 														<?php foreach ($province as $p) { ?>
@@ -142,26 +204,102 @@
 												</div>
 											</div>
 											<div class="form-group">
-												<label class="control-label col-lg-3">City / Regency</label>
-												<div class="col-lg-9">
-													<select class="form-control select4" name="CityIncident[]" id="CityIncident" disabled></select>
+												<label class="control-label col-lg-4">City / Regency</label>
+												<div class="col-lg-8">
+													<select class="form-control select4" name="CityIncident" id="CityIncident" style="width: 100%;" disabled></select>
 												</div>
 											</div>
 											<div class="form-group">
-												<label class="control-label col-lg-3">District</label>
-												<div class="col-lg-9">
-													<select class="form-control select4" name="DistrictIncident[]" id="DistrictIncident" disabled></select>
+												<label class="control-label col-lg-4">District</label>
+												<div class="col-lg-8">
+													<select class="form-control select4" name="DistrictIncident" id="DistrictIncident" style="width: 100%;" disabled></select>
+												</div>
+											</div>
+										</div>
+										<div class="col-lg-6">
+											<div class="form-group">
+												<label class="control-label col-lg-4">Village</label>
+												<div class="col-lg-8">
+													<select class="form-control select4" name="VillageIncident" id="VillageIncident" style="width: 100%;" disabled></select>
 												</div>
 											</div>
 											<div class="form-group">
-												<label class="control-label col-lg-3">Village</label>
-												<div class="col-lg-9">
-													<select class="form-control select4" name="VillageIncident[]" id="VillageIncident" disabled></select>
+												<label class="control-label col-lg-4">Address</label>
+												<div class="col-lg-8">
+													<textarea class="form-control" rows="4" name="AddressIncident" placeholder="Address"></textarea>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div role="tabpanel" class="tab-pane" id="Condition">
+										<div class="col-md-2 col-md-offset-1">
+											<div class="form-group">
+												<label>Area Category</label>
+												<div class="input-group">
+													<input type="checkbox" name="area[]" value="Basah tidak berair"> Basah tidak berair
+													<br><input type="checkbox" name="area[]" value="Basah berair"> Basah berair
+													<br><input type="checkbox" name="area[]" value="Kering"> Kering
+													<br><input type="checkbox" name="area[]" value="Rawa"> Rawa
+												</div>
+											</div>
+										</div>
+										<div class="col-md-2">
+											<div class="form-group">
+												<label>Type of Soil</label>
+												<div class="input-group">
+													<input type="checkbox" name="Soil[]" value="Lempung"> Lempung
+													<br><input type="checkbox" name="Soil[]" value="Lempung berpasir"> Lempung berpasir
+													<br><input type="checkbox" name="Soil[]" value="Normal"> Normal
+												</div>
+											</div>
+										</div>
+										<div class="col-md-2">
+											<div class="form-group">
+												<label>Depth</label>
+												<div class="input-group">
+													<input type="checkbox" name="Depth[]" value="Normal (10-17 cm)"> Normal (10-17 cm)
+													<br><input type="checkbox" name="Depth[]" value="Sedang (17-30 cm)"> Sedang (17-30 cm)
+													<br><input type="checkbox" name="Depth[]" value="Dalam (30-40 cm)"> Dalam (30-40 cm)
+													<br><input type="checkbox" name="Depth[]" value="Sangat Dalam (lebih dari 40 cm)"> Sangat Dalam (lebih dari 40 cm)
+												</div>
+											</div>
+										</div>
+										<div class="col-md-2">
+											<div class="form-group">
+												<label>Weeds</label>
+												<div class="input-group">
+		      											<input type="checkbox" name="Weeds[]" value="Rumput liar">Rumput liar
+													<br><input type="checkbox" name="Weeds[]" value="Tanaman perdu"> Tanaman perdu
+													<br><input type="checkbox" name="Weeds[]" value="Alang - alang"> Alang - alang
+												</div>
+											</div>
+										</div>
+										<div class="col-md-2">
+											<div class="form-group">
+												<label>Topography</label>
+												<div class="input-group">
+													<input type="checkbox" name="Topography[]" value="Dataran rendah"> Dataran rendah
+													<br><input type="checkbox" name="Topography[]" value="Dataran tinggi"> Dataran tinggi
+													<br><input type="checkbox" name="Topography[]" value="Terasiring"> Terasiring
+												</div>
+											</div>
+										</div>
+									</div>
+									<div role="tabpanel" class="tab-pane" id="Details">
+										<div class="col-md-10 col-md-offset-1">
+											<div class="form-group">
+													<label>Details Chronology of Events</label>
+												<div class="input-group">
+													<div class="input-group-addon">
+														<i class="glyphicon glyphicon-user"></i>
+													</div>
+													<textarea name="Chronology" placeholder="Details Chronology of Events" class="form-control" rows="4" data-toggle="tooltip" data-placement="top" title="Masukkan detail kronologis kejadian" required></textarea>
 												</div>
 											</div>
 										</div>
 									</div>
 								</div>
+								
 							</div>
 						</div>
 						<div class="row">
@@ -179,7 +317,6 @@
 											</li>
 											<li><a href="#additional" data-toggle="tab">Additional Activity</a>
 											</li>
-											
 										</ul>
 
 										<div class="tab-content">
