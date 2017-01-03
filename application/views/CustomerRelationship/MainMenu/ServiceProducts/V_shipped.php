c3f6876559164d56f7a715aa9bc2470df8aa2e5f
F2032 <ratno_firmansyah@quick.com>
2017-01-03 08:26:20 +0700

    Merubah Tampilan Form shipped

--- application/views/CustomerRelationship/MainMenu/ServiceProducts/V_shipped.php
+++ application/views/CustomerRelationship/MainMenu/ServiceProducts/V_shipped.php
@@ -1,9 +1,4 @@
 <div class="form-group">
 	<label>Sent date</label>
-	<div class="input-group">
-		<div class="input-group-addon">
-			<i class="glyphicon glyphicon-user"></i>
-		</div>
-		<input type="text" class="form-control" id="sentDate" name="sentDate" placeholder="Sent date items" data-toggle="tooltip" data-placement="top" title="Masukkan waktu produk klaim dikirim ke CV. KHS Yogyakarta" required>
-	</div>
+	<input type="text" class="form-control" id="sentDate" name="sentDate" placeholder="Sent date items">
 </div>
