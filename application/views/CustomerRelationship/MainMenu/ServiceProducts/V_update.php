64b9eaacfde2ac668d2d18e31084bcd32c2ab5c4
F2032 <ratno_firmansyah@quick.com>
2017-01-06 13:38:01 +0700

    delete modal di file update

--- application/views/CustomerRelationship/MainMenu/ServiceProducts/V_update.php
+++ application/views/CustomerRelationship/MainMenu/ServiceProducts/V_update.php
@@ -804,20 +804,3 @@
 	</div>
 </section>
 </form>
-<div class="modal fade" id="approve" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Approval Confirmation</h4>
-      </div>
-      <div class="modal-body">
-        <h3>Ask for Approvall?</h3>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-        <a href="<?php echo base_url('CustomerRelationship/ServiceProducts/AskForApproval/'.$id); ?>" class="btn btn-danger">Yes</a>
-      </div>
-    </div>
-  </div>
-</div>
