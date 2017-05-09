ab62324d01639dd71bcbe96cd2011e3f7044f198
amri <khoerul_amri@quick.com>
2017-05-09 08:49:13 +0700

    Penambahan Fitur Reset Kondite

--- application/views/PayrollManagementNonStaff/Kondite/V_index.php
+++ application/views/PayrollManagementNonStaff/Kondite/V_index.php
@@ -23,6 +23,9 @@
                     <div class="col-lg-12">
                         <div class="box box-primary box-solid">
                             <div class="box-header with-border">
+                                <a href="<?php echo site_url('PayrollManagementNonStaff/ProsesGaji/Kondite/clear_data/') ?>" data-toggle="tooltip" data-placement="left" style="float:right;margin-right:1%;margin-top:-0.5%;" alt="Kosongkan Data" title="Kosongkan Data" >
+                                    <button type="button" class="btn btn-default btn-sm"><i class="fa fa-trash fa-2x"></i></button>
+                                </a>
                                 <a href="<?php echo site_url('PayrollManagementNonStaff/ProsesGaji/Kondite/Import') ?>" style="float:right;margin-right:1%;margin-top:-0.5%;" class="btn btn-default btn-sm" alt="Import Data" title="Import Data" data-toggle="tooltip" data-placement="left" >
                                     <i class="fa fa-upload fa-2x"></i>
                                 </a>
