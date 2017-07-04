7fda60a45efadf6a1e43254c2ae7ddc25dc98be6
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-07-04 13:45:32 +0700

    add new function for correction data

--- application/controllers/StorageLocation/MainMenu/C_Correction.php
+++ application/controllers/StorageLocation/MainMenu/C_Correction.php
@@ -6,6 +6,7 @@ class C_Correction extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('SystemAdministration/MainMenu/M_user');
+		$this->load->model('StorageLocation/MainMenu/M_correction');
 		$this->load->helper('form');
         $this->load->helper('url');
         $this->load->helper('html');
@@ -36,5 +37,61 @@ class C_Correction extends CI_Controller
 		$this->load->view('V_Sidemenu',$data);
 		$this->load->view('StorageLocation/MainMenu/V_koreksi');
 		$this->load->view('V_Footer',$data);
+	}
+
+	public function searchComponent()
+	{
+		$org_id	= $this->input->post('org');
+		$sub_inv= $this->input->post('sub_inv');
+		$item 	= $this->input->post('item');
+		$locator= $this->input->post('locator');
+		
+		$data['Component'] = $this->M_correction->getSearchComponent($org_id,$sub_inv,$item,$locator);
+		$this->load->view('StorageLocation/MainMenu/V_tablesearchcomponent',$data);
+	}
+
+	public function searchAssy()
+	{
+		$org_id		= $this->input->post('org');
+		$sub_inv	= $this->input->post('sub_inv');
+		$kode_assy	= $this->input->post('kode_assy');
+		$data['Assy'] = $this->M_correction->getSearchAssy($org_id,$sub_inv,$kode_assy);
+		$this->load->view('StorageLocation/MainMenu/V_tablesearchassy',$data);
+	}
+
+	public function saveAlamat()
+	{
+		$user 		= $this->session->userdata('user');
+		$alamat		= $this->input->post('alamat');
+		$item		= $this->input->post('item');
+		$kode_assy	= $this->input->post('kode_assy');
+		$type_assy	= $this->input->post('type_assy');
+		$sub_inv	= $this->input->post('sub_inv');
+
+		$this->M_correction->save_alamat($user, $alamat, $item, $kode_assy, $type_assy, $sub_inv);
+	}
+
+	public function saveLmk()
+	{
+		$user 		= $this->session->userdata('user');
+		$lmk		= $this->input->post('lmk');
+		$item		= $this->input->post('item');
+		$kode_assy	= $this->input->post('kode_assy');
+		$type_assy	= $this->input->post('type_assy');
+		$sub_inv	= $this->input->post('sub_inv');
+
+		$this->M_correction->save_lmk($user, $lmk, $item, $kode_assy, $type_assy, $sub_inv);
+	}
+
+	public function savePicklist()
+	{
+		$user 		= $this->session->userdata('user');
+		$picklist	= $this->input->post('picklist');
+		$item		= $this->input->post('item');
+		$kode_assy	= $this->input->post('kode_assy');
+		$type_assy	= $this->input->post('type_assy');
+		$sub_inv	= $this->input->post('sub_inv');
+
+		$this->M_correction->save_picklist($user, $picklist, $item, $kode_assy, $type_assy, $sub_inv);
 	}
 }
