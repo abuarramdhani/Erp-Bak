0aca78a8aa2bdb7dc9d59dff4072b7c072a8bac5
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-07-04 13:59:29 +0700

    alter html code dan menghilangkan load file js dan inline script

--- application/views/StorageLocation/MainMenu/V_tablesearchassy.php
+++ application/views/StorageLocation/MainMenu/V_tablesearchassy.php
@@ -1,54 +1,56 @@
-				<table style="width:100%; font-size: 12px" id="table_SA" class="table bg-blue text-left table-responsive table-striped table-hover table-bordered" >
-					<thead >
-						<tr>
-							<th width="3%;" style="vertical-align: middle; text-align:center">No</th>
-							<th style="vertical-align: middle; text-align:center">Kode Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Nama Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Type Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Item</th>
-							<th style="vertical-align: middle; text-align:center">Description</th>
-							<th style="vertical-align: middle; text-align:center">Subinventory</th>
-							<th style="vertical-align: middle; text-align:center">Alamat</th>
-							<th width="5%" style="vertical-align: middle; text-align:center">LPPB / MO / KIB</th>
-							<th width="5%" style="vertical-align: middle; text-align:center">Picklist</th>
-						</tr>
-					</thead>
-					<tbody style="background-color: white; color: black" >
-					<?php 
-					$num = 1;
-					foreach ($Assy as $SA) { 
-						if ($SA ['LMK'] == "1"){ $centang ="checked";}
-						else { $centang ="";}			
-						if ($SA ['PICKLIST'] == "1"){$centang2 ="checked";}
-						else {$centang2 ="";}
-					?>
-						<tr>
-							<td> <?php echo $num; ?> </td>
-							<td> <?php echo $SA['KODE_ASSEMBLY']; ?> </td>
-							<td> <?php echo $SA['NAMA_ASSEMBLY']; ?> </td>
-							<td> <?php echo $SA['TYPE_ASSEMBLY']; ?> </td>
-							<td> <?php echo $SA['ITEM']; ?> </td>
-							<td> <?php echo $SA['DESCRIPTION']; ?> </td>
-							<td> <?php echo $SA['SUB_INV']; ?> </td>
-							<td align="center">
-							<input type="text" name="" class="alamat form-control" onkeypress="entir(event, this)"  value=" <?php echo $SA['ALAMAT']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="item form-control" value="<?php echo $SA['ITEM']; ?>" > 
-							<input style="display: none" type="text" hiden name="" class="kode_assy form-control" value="<?php echo $SA['KODE_ASSEMBLY']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="type_assy form-control" value="<?php echo $SA['TYPE_ASSEMBLY']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="sub_inv form-control" value="<?php echo $SA['SUB_INV']; ?>"> 
-						</td>
-						<td><input type="checkbox" class="lmk"  <?php echo "$centang"; ?> onchange="enter(event,this)" />
-
-						</td>
-						<td><input type="checkbox" class="picklist" <?php echo "$centang2"; ?> onchange="enter2(event,this)"  /></td>
-						</tr>
-					<?php $num++; } ?>
-					</tbody>
-				</table>
-
-	<!-- Custom.js -->
-	<script src="<?php echo base_url('assets/js/custom.js');?>"></script>
-	<!-- PAGE LEVEL SCRIPTS FOR DATATABLES-->
-    <script src="<?php echo base_url('assets/plugins/dataTables/jquery.dataTables.js');?>"></script>
-    <script src="<?php echo base_url('assets/plugins/dataTables/dataTables.bootstrap.min.js');?>" type="text/javascript"></script>
-
+<table style="min-width:1600px;" id="table_SA" class="table table-responsive table-striped table-hover table-bordered">
+	<thead class="bg-primary">
+		<tr>
+			<th style="vertical-align: middle; text-align:center; width: 3%">No</th>
+			<th style="vertical-align: middle; text-align:center">Kode Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Nama Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Type Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Item</th>
+			<th style="vertical-align: middle; text-align:center">Description</th>
+			<th style="vertical-align: middle; text-align:center">Subinventory</th>
+			<th style="vertical-align: middle; text-align:center">Alamat</th>
+			<th style="vertical-align: middle; text-align:center; width: 5%;">LPPB / MO / KIB</th>
+			<th style="vertical-align: middle; text-align:center; width: 5%;">Picklist</th>
+		</tr>
+	</thead>
+	<tbody style="background-color: white; color: black" >
+		<?php 
+			$num = 1;
+			foreach ($Assy as $SA) {
+		?>
+		<tr>
+			<td> <?php echo $num; ?> </td>
+			<td> <?php echo $SA['KODE_ASSEMBLY']; ?> </td>
+			<td> <?php echo $SA['NAMA_ASSEMBLY']; ?> </td>
+			<td> <?php echo $SA['TYPE_ASSEMBLY']; ?> </td>
+			<td> <?php echo $SA['ITEM']; ?> </td>
+			<td> <?php echo $SA['DESCRIPTION']; ?> </td>
+			<td> <?php echo $SA['SUB_INV']; ?> </td>
+			<td align="center">
+				<input type="text" class="alamat form-control" onkeypress="entir(event, this)"  value="<?php echo $SA['ALAMAT'];?>"  data-toggle="tooltip" data-placement="top" title="Press Enter to save!"> 
+				<input type="hidden" class="item form-control" value="<?php echo $SA['ITEM']; ?>" > 
+				<input type="hidden" class="kode_assy form-control" value="<?php echo $SA['KODE_ASSEMBLY']; ?>"> 
+				<input type="hidden" class="type_assy form-control" value="<?php echo $SA['TYPE_ASSEMBLY']; ?>"> 
+				<input type="hidden" class="sub_inv form-control" value="<?php echo $SA['SUB_INV']; ?>"> 
+			</td>
+			<td>
+				<select class="lmk form-control select-2" name="txtLmk[]" onchange="enter(event,this)" style="width: auto;">
+					<option></option>
+					<option value="1" <?php if ($SA['LMK'] == "1") {echo "selected";} ?>>YES</option>
+					<option value="0" <?php if ($SA['LMK'] == "0") {echo "selected";} ?>>NO</option>
+				</select>
+			</td>
+			<td>
+				<select class="form-control select-2 picklist" name="txtPicklist[]" onchange="enter2(event,this)" style="width: auto;">
+					<option></option>
+					<option value="1" <?php if ($SA['PICKLIST'] == "1") {echo "selected";} ?>>YES</option>
+					<option value="0" <?php if ($SA['PICKLIST'] == "0") {echo "selected";} ?>>NO</option>
+				</select>
+			</td>
+		</tr>
+		<?php
+			$num++;
+			}
+		?>
+	</tbody>
+</table>
--- application/views/StorageLocation/MainMenu/V_tablesearchcomponent.php
+++ application/views/StorageLocation/MainMenu/V_tablesearchcomponent.php
@@ -1,61 +1,58 @@
-				<table style="width:100%; font-size: 12px" id="table_comp" class="table bg-blue text-left table-responsive table-striped table-hover table-bordered" >
-					<thead >
-						<tr>
-							<th width="3%;" style="vertical-align: middle; text-align:center">No</th>
-							<th style="vertical-align: middle; text-align:center">Item</th>
-							<th style="vertical-align: middle; text-align:center">Description</th>
-							<th style="vertical-align: middle; text-align:center">Kode Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Nama Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Type Assembly</th>
-							<th style="vertical-align: middle; text-align:center">Subinventory</th>
-							<th style="vertical-align: middle; text-align:center">Alamat</th>
-							<th width="5%" style="vertical-align: middle; text-align:center">LPPB / MO / KIB</th>
-							<th width="5%" style="vertical-align: middle; text-align:center">Picklist</th>
-						</tr>
-					</thead>
-					<tbody  id="loading" style="background-color: white; color: black" >
-
-				<?php 
-				$num = 1;
-				foreach ($Component as $comp) { 
-
-
-					if ($comp ['LMK'] == "1"){ $centang ="checked";}
-					else { $centang ="";}			
-					if ($comp ['PICKLIST'] == "1"){$centang2 ="checked";}
-					else {$centang2 ="";}
-				?>
-					<tr   >
-						<td> <?php echo $num; ?> </td>
-						<td> <?php echo $comp['ITEM']; ?> </td>
-						<td> <?php echo $comp['DESCRIPTION']; ?> </td>
-						<td> <?php echo $comp['KODE_ASSEMBLY']; ?> </td>
-						<td> <?php echo $comp['NAMA_ASSEMBLY']; ?> </td>
-						<td> <?php echo $comp['TYPE_ASSEMBLY']; ?> </td>
-						<td> <?php echo $comp['SUB_INV']; ?> </td>
-						<td align="center">
-							<input type="text" name="" class="alamat form-control" onkeypress="entir(event, this)"  value=" <?php echo $comp['ALAMAT']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="item form-control" value="<?php echo $comp['ITEM']; ?>" > 
-							<input style="display: none" type="text" hiden name="" class="kode_assy form-control" value="<?php echo $comp['KODE_ASSEMBLY']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="type_assy form-control" value="<?php echo $comp['TYPE_ASSEMBLY']; ?>"> 
-							<input style="display: none" type="text" hiden name="" class="sub_inv form-control" value="<?php echo $comp['SUB_INV']; ?>"> 
-						</td>
-						<td><input type="checkbox" class="lmk"  <?php echo "$centang"; ?> onchange="enter(event,this)" />
-
-						</td>
-						<td><input type="checkbox" class="picklist" <?php echo "$centang2"; ?> onchange="enter2(event,this)"  /></td>
-					</tr>
-					<?php $num++; } ?>
-					</tr>
-					</tbody>
-				</table>
-
-	<!-- Custom.js -->
-	<script src="<?php echo base_url('assets/js/custom.js');?>"></script>
-	<!-- PAGE LEVEL SCRIPTS FOR DATATABLES-->
-    <script src="<?php echo base_url('assets/plugins/dataTables/jquery.dataTables.js');?>"></script>
-    <script src="<?php echo base_url('assets/plugins/dataTables/dataTables.bootstrap.min.js');?>" type="text/javascript"></script>
-
-
-
-
+<table id="table_comp" class="table table-responsive table-striped table-hover table-bordered" style="min-width: 1600px;">
+	<thead class="bg-primary">
+		<tr>
+			<th style="vertical-align: middle; text-align:center; width: 3%;">No</th>
+			<th style="vertical-align: middle; text-align:center">Item</th>
+			<th style="vertical-align: middle; text-align:center">Description</th>
+			<th style="vertical-align: middle; text-align:center">Kode Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Nama Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Type Assembly</th>
+			<th style="vertical-align: middle; text-align:center">Subinventory</th>
+			<th style="vertical-align: middle; text-align:center">Alamat</th>
+			<th style="vertical-align: middle; text-align:center; width: 5%;">LPPB / MO / KIB</th>
+			<th style="vertical-align: middle; text-align:center; width: 5%;">Picklist</th>
+		</tr>
+	</thead>
+	<tbody id="loading">
+		<?php
+			$num = 1;
+			foreach ($Component as $comp) {
+		?>
+		<tr>
+			<td> <?php echo $num; ?> </td>
+			<td> <?php echo $comp['ITEM']; ?> </td>
+			<td> <?php echo $comp['DESCRIPTION']; ?> </td>
+			<td> <?php echo $comp['KODE_ASSEMBLY']; ?> </td>
+			<td> <?php echo $comp['NAMA_ASSEMBLY']; ?> </td>
+			<td> <?php echo $comp['TYPE_ASSEMBLY']; ?> </td>
+			<td> <?php echo $comp['SUB_INV']; ?> </td>
+			<td align="center">
+				<input type="text" class="alamat form-control" onkeypress="entir(event, this)"  value="<?php echo $comp['ALAMAT']; ?>"> 
+				<input style="display: none" type="text" hiden class="item form-control" value="<?php echo $comp['ITEM']; ?>" > 
+				<input style="display: none" type="text" hiden class="kode_assy form-control" value="<?php echo $comp['KODE_ASSEMBLY']; ?>">
+				<input style="display: none" type="text" hiden class="type_assy form-control" value="<?php echo $comp['TYPE_ASSEMBLY']; ?>">
+				<input style="display: none" type="text" hiden class="sub_inv form-control" value="<?php echo $comp['SUB_INV']; ?>"> 
+			</td>
+			<td>
+				<select class="lmk form-control select-2" name="txtLmk[]" onchange="enter(event,this)" style="width: auto;">
+					<option></option>
+					<option value="1" <?php if ($comp['LMK'] == "1") {echo "selected";} ?>>YES</option>
+					<option value="0" <?php if ($comp['LMK'] == "0") {echo "selected";} ?>>NO</option>
+				</select>
+				<!-- <input type="checkbox" class="lmk" onchange="enter(event,this)" <?php echo $centang;?>/> -->
+			</td>
+			<td>
+				<select class="form-control select-2 picklist" name="txtPicklist[]" onchange="enter2(event,this)" style="width: auto;">
+					<option></option>
+					<option value="1" <?php if ($comp['PICKLIST'] == "1") {echo "selected";} ?>>YES</option>
+					<option value="0" <?php if ($comp['PICKLIST'] == "0") {echo "selected";} ?>>NO</option>
+				</select>
+				<!-- <input type="checkbox" class="picklist" onchange="enter2(event,this)" <?php echo $centang2;?>/> -->
+			</td>
+		</tr>
+		<?php
+			$num++;
+			}
+		?>
+	</tbody>
+</table>
