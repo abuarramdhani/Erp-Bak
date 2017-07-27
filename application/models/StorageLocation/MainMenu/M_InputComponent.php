d6257fd0dcfa7847112e0db6b33c5ec5a290172d
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-07-27 15:14:27 +0700

    tambah query get last insert data

--- application/models/StorageLocation/MainMenu/M_InputComponent.php
+++ application/models/StorageLocation/MainMenu/M_InputComponent.php
@@ -53,7 +53,6 @@ class M_inputcomponent extends CI_Model {
     function insertData($org_id,$sub_inv,$kode_assy,$type_assy,$kode_item_save,$locator,$alamat_simpan_save,$lppbmokib_save,$picklist_save,$user_name){
         $sql="INSERT INTO KHS.KHSLOKASISIMPAN (ORGANIZATION_ID, SUB_INV, ASSEMBLY, ASSEMBLY_TYPE, COMPONENT, LOCATOR, ALAMAT_SIMPAN, LPPB_MO_KIB, PICKLIST, CREATED_BY, CREATION_DATE) VALUES ('$org_id', '$sub_inv', '$kode_assy', '$type_assy', '$kode_item_save', '$locator', '$alamat_simpan_save', '$lppbmokib_save', '$picklist_save', '$user_name', sysdate)";
         $query = $this->oracle->query($sql);
-        return;
     }
 
     function updateData($org_id,$sub_inv,$kode_assy,$type_assy,$kode_item_save,$locator,$alamat_simpan_save,$lppbmokib_save,$picklist_save,$user_name){
@@ -80,6 +79,25 @@ class M_inputcomponent extends CI_Model {
 
         $sql ="UPDATE khs.khslokasisimpan set ALAMAT_SIMPAN ='$alamat_simpan_save' , LPPB_MO_KIB ='$lppbmokib_save' ,  PICKLIST ='$picklist_save', LAST_UPDATED_BY ='$user_name' ,LAST_UPDATE_DATE = sysdate WHERE ORGANIZATION_ID ='$org_id' AND $a2 AND $b2 AND $c2 AND $d2 ";
         $query = $this->oracle->query($sql);
-        return;
+    }
+
+    public function getLastInserted($table, $id) {
+        $this->oracle->select_max($id);
+        $Q = $this->oracle->get($table);
+        $row = $Q->row_array();
+        return $row[$id];
+    }
+
+    function getLastData($id)
+    {
+        $sql = "SELECT DISTINCT kls.* , 
+                    msib.DESCRIPTION DESCRIPTION,
+                    msib2.DESCRIPTION NAMA_ASSEMBLY
+                FROM mtl_system_items_b msib,
+                    mtl_system_items_b msib2 RIGHT JOIN khs.khslokasisimpan kls ON msib2.segment1 = kls.assembly
+                WHERE kls.COMPONENT = msib.SEGMENT1
+                    AND kls.KHSLOKASISIMPAN_ID = '$id'";
+        $query = $this->oracle->query($sql);
+        return $query->result_array();
     }
 }
