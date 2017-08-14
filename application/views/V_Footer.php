27ec77d284095ebf82a8324e0a38f8e343b14b05
ayu_rakhmadani <ayu_rakhmadani@quick.com>
2017-08-14 10:06:19 +0700

    Limbah B3 lanjutan

--- application/views/V_Footer.php
+++ application/views/V_Footer.php
@@ -139,6 +139,50 @@
 		
 		
 	</script>
+
+	<script type="text/javascript">
+		$('#txtTanggalKirimHeader').datepicker({
+    		"autoclose": true,
+    		"todayHiglight": true,
+    		"format": 'dd M yyyy'
+      	});	
+
+      	$('#txtTanggalTransaksiHeader').datepicker({
+    		"autoclose": true,
+    		"todayHiglight": true,
+    		"format": 'dd M yyyy'
+      	});
+
+      	$('#txtMaksPenyimpananHeader').datepicker({
+    		"autoclose": true,
+    		"todayHiglight": true,
+    		"format": 'dd M yyyy'
+      	});
+
+      	$('#txtTanggalKeluarHeader').datepicker({
+    		"autoclose": true,
+    		"todayHiglight": true,
+    		"format": 'dd M yyyy'
+      	});	
+
+      	$("#periode").daterangepicker({  
+      		"autoclose": true,
+    		"todayHiglight": true,
+    		locale: {
+    			cancelLabel: 'Clear'
+    		},
+    		autoUpdateInput: false,
+      	});
+
+      	$('input[id="periode"]').on('apply.daterangepicker', function(ev, picker) {
+			$(this).val(picker.startDate.format('DD/MM/YYYY') + ' - ' + picker.endDate.format('DD/MM/YYYY'));
+		});
+
+		$('input[id="periode"]').on('cancel.daterangepicker', function(ev, picker) {
+			$(this).val('');
+		});
+	</script>
+
 	<?php
 	if (empty($alert)) {
 		$alert = '';
