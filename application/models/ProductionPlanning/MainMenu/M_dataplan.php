fc2cec59eecd208d54691828ae8931f27d1ca29e
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-08-26 14:17:28 +0700

    alter query + add query insert

--- application/models/ProductionPlanning/MainMenu/M_dataplan.php
+++ application/models/ProductionPlanning/MainMenu/M_dataplan.php
@@ -11,27 +11,45 @@ class M_dataplan extends CI_Model {
 		if ($id === FALSE){
       		$this->db->select('*');
       		$this->db->from('pp.pp_daily_plans');
-      		$this->db->order_by('created_date', 'ASC');
+      		$this->db->order_by('priority, created_date', 'ASC');
+          $query = $this->db->get();
+        }else{
+          // $query = $this->db->get_where('pp.pp_daily_plans', array('daily_plan_id' => $id));
+          $this->db->select('*');
+          $this->db->from('pp.pp_daily_plans');
+          $this->db->where('section_id', $id);
+          $this->db->order_by('priority, created_date', 'ASC');
        		$query = $this->db->get();
-       	}else{
-       		$query = $this->db->get_where('pp.pp_daily_plans', array('daily_plan_id' => $id));
        	}
        	return $query->result_array();
     }
 
-    public function getSection($id = FALSE)
+    public function getSection($id = FALSE, $section_id = FALSE)
     {
     	if ($id == FALSE) {
     		$this->db->select('*');
-      		$this->db->from('pp.pp_section');
-      		$this->db->order_by('section_id', 'ASC');
+      	$this->db->from('pp.pp_section');
+      	$this->db->order_by('section_id', 'ASC');
+    	}elseif ($section_id == FALSE) {
+    		$this->db->select('ps.*');
+      	$this->db->from('pp.pp_section ps, pp.pp_user_group pug, pp.pp_user pu');
+        $this->db->where('ps.section_id = pug.section_id AND pug.pp_user_id = pu.pp_user_id');
+      	$this->db->where('pu.user_id', $id);
+      	$this->db->order_by('ps.section_name', 'ASC');
     	}else{
-    		$this->db->select('*');
-      		$this->db->from('pp.pp_section');
-      		$this->db->where('section_id', $id);
-      		$this->db->order_by('section_id', 'ASC');
-    	}
+        $this->db->select('ps.*');
+        $this->db->from('pp.pp_section ps, pp.pp_user_group pug, pp.pp_user pu');
+        $this->db->where('ps.section_id = pug.section_id AND pug.pp_user_id = pu.pp_user_id');
+        $this->db->where('pu.user_id', $id);
+        $this->db->where('ps.section_id', $section_id);
+        $this->db->order_by('ps.section_name', 'ASC');
+      }
     	$query = $this->db->get();
-       	return $query->result_array();
+      return $query->result_array();
+    }
+
+    public function insertDataPlan($dataIns)
+    {
+      $this->db->insert('pp.pp_daily_plans', $dataIns);
     }
 }
