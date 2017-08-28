9af91e80f6ddd1c175d30d5afbdf8e231052bd6a
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-08-28 10:56:17 +0700

    alter dynamic hari kerja

--- assets/js/customPP.js
+++ assets/js/customPP.js
@@ -5,7 +5,15 @@ $(document).ready(function() {
 
 function chartFabricationMon() {
     var ctx = document.getElementById('month-fabrication').getContext('2d');
-    var hari_kerja = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+    var hari_kerja = [];
+    var currentTime = new Date();
+    var currentMonth = currentTime.getMonth() + 1;
+    var currentYear = currentTime.getFullYear();
+    var maxTime = new Date(currentYear, currentMonth, 0);
+    var maxday = maxTime.getDate();
+    for (var i = 1; i <= maxday; i++) {
+        hari_kerja.push(i);
+    }
     var data = [{
         label: '% PLAN',
         backgroundColor: 'rgb(123, 170, 247)',
@@ -37,7 +45,8 @@ function chartFabricationMon() {
                 xAxes: [{
                     display: true,
                     gridLines: {
-                        display: false
+                        display: false,
+                        color: "#c6c6c6"
                     }
                 }],
                 yAxes: [{
