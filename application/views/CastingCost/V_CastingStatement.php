060540dbaf60617c27561da3e4e52223d3c89a5d
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-09-06 09:46:54 +0700

    add number format (separator ribuan + 2 angka dibelakang koma)

--- application/views/CastingCost/V_CastingStatement.php
+++ application/views/CastingCost/V_CastingStatement.php
@@ -54,7 +54,7 @@
 							<td style="border:1px solid #333 ;padding: 5px">2</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Berat Casting</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php if($data_casting['casting_weight'] != ''){echo $data_casting['casting_weight'].' Kg';}  
+							<?php if($data_casting['casting_weight'] != ''){echo number_format($data_casting['casting_weight'], 2).' Kg';}  
 									else{ echo "-";}
 							?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
@@ -63,42 +63,42 @@
 							<td style="border:1px solid #333 ;padding: 5px">3</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Berat Remelt</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['remelt_weight'].' Kg'; ?></td>
+							<?php echo number_format($data_casting['remelt_weight'], 2).' Kg'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">4</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Berat Cairan	</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['liquid_weight'].' Kg'; ?></td>
+							<?php echo number_format($data_casting['liquid_weight'], 2).' Kg'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">5</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Yield Casting	</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['yield_casting'].' %'; ?></td>
+							<?php echo number_format($data_casting['yield_casting'], 2).' %'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">6</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Scrap	</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;">
-							 <?php echo $data_casting['scrap_reject'].' %'; ?></td>
+							 <?php echo number_format($data_casting['scrap_reject'], 2).' %'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">7</td>
 							<td style="border:1px solid #333 ;padding: 5px">Basic Tonage</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['basic_tonage'].' Kg/Shift '; ?></td>
+							<?php echo number_format($data_casting['basic_tonage'], 2).' Kg/Shift '; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">8</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Material Inti</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php if($data_casting['material_core'] != ''){echo $data_casting['material_core'];}  
+							<?php if($data_casting['material_core'] != ''){echo number_format($data_casting['material_core'], 2);}  
 									else{ echo "-";}
 							?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
@@ -107,7 +107,7 @@
 							<td style="border:1px solid #333 ;padding: 5px">9</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Berat Inti</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php if($data_casting['core_weight'] != ''){echo $data_casting['core_weight'].' Kg';}  
+							<?php if($data_casting['core_weight'] != ''){echo number_format($data_casting['core_weight'], 2).' Kg';}  
 									else{ echo "-";}
 							?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
@@ -116,7 +116,7 @@
 							<td style="border:1px solid #333 ;padding: 5px">10</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Target Pembuatan Inti</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;0"> 
-							<?php if($data_casting['target_core'] != ''){echo $data_casting['target_core'].' Pieces/Shift';}  
+							<?php if($data_casting['target_core'] != ''){echo number_format($data_casting['target_core'], 2).' Pieces/Shift';}  
 									else{ echo "-";}
 							?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
@@ -140,49 +140,49 @@
 							<td style="border:1px solid #333 ;padding: 5px">13</td>
 							<td style="border:1px solid #333 ;padding: 5px">Target Cetak/Shift</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['target_mold_pieces'].' Pieces/Shift'; ?></td>
+							<?php echo number_format($data_casting['target_mold_pieces'], 2).' Pieces/Shift'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">14</td>
 							<td style="border:1px solid #333 ;padding: 5px"> Cavity per Flask</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['cavity'].' cavity'; ?></td>
+							<?php echo number_format($data_casting['cavity'], 2).' cavity'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">15</td>
 							<td style="border:1px solid #333 ;padding: 5px">Target Cetak/Shift</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['target_mold_flask'].' Flask/Shift'; ?></td>
+							<?php echo number_format($data_casting['target_mold_flask'], 2).' Flask/Shift'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">16</td>
 							<td style="border:1px solid #333 ;padding: 5px">Berat Pasir Cetak</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['sand_mold_weight'].' Kg'; ?></td>
+							<?php echo number_format($data_casting['sand_mold_weight'], 2).' Kg'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">17</td>
 							<td style="border:1px solid #333 ;padding: 5px">Konsumsi Batu Gerinda / Pcs</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;">
-							 <?php echo $data_casting['grindstone'].' Pieces'; ?></td>
+							 <?php echo number_format($data_casting['grindstone'], 2).' Pieces'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">18</td>
 							<td style="border:1px solid #333 ;padding: 5px">Target Grinding / Shift</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php echo $data_casting['target_grinding'].' Pieces'; ?></td>
+							<?php echo number_format($data_casting['target_grinding'], 2).' Pieces'; ?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
 						</tr>
 						<tr style="border:1px solid #333 ;padding: 5px">
 							<td style="border:1px solid #333 ;padding: 5px">19</td>
 							<td style="border:1px solid #333 ;padding: 5px">Waktu Pembuatan Pola</td>
 							<td style="border:1px solid #333 ;padding: 5px; text-align: center;"> 
-							<?php if($data_casting['time_making'] != ''){echo $data_casting['time_making'].' Jam';}  
+							<?php if($data_casting['time_making'] != ''){echo number_format($data_casting['time_making'], 2).' Jam';}  
 									else{ echo "-";}
 							?></td>
 							<td style="border:1px solid #333 ;padding: 5px"></td>
