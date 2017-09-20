922676cffab4d8def647abeecc1edf392d11a4bb
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-09-20 15:19:16 +0700

    alter script line chart

--- assets/js/customPP.js
+++ assets/js/customPP.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $('#tbdataplan').DataTable();
     $('#tbdatagroupsection').DataTable();
-    $('.mon-fab-table').DataTable({
-        // "paging": false,
-        // "lengthChange": false,
-        "searching": false,
-        "ordering": false,
-        "info": false,
-        "autoWidth": false,
-        "pageLength": 5
-    });
+    // $('.mon-fab-table').DataTable({
+    //     // "paging": false,
+    //     "lengthChange": false,
+    //     "searching": false,
+    //     "ordering": false,
+    //     "info": false,
+    //     "autoWidth": false,
+    //     "pageLength": 5
+    // });
 });
 
 window.onload = function() {
@@ -19,41 +19,13 @@ window.onload = function() {
     });
 }
 
-function chartFabricationMon() {
+function chartFabricationMon(labels, value, color, color2, label) {
     var ctx = document.getElementById('month-fabrication').getContext('2d');
-    var hari_kerja = [];
-    var currentTime = new Date();
-    var currentMonth = currentTime.getMonth() + 1;
-    var currentYear = currentTime.getFullYear();
-    var maxTime = new Date(currentYear, currentMonth, 0);
-    var maxday = maxTime.getDate();
-    for (var i = 1; i <= maxday; i++) {
-        hari_kerja.push(i);
-    }
-    var data = [{
-        label: '% PLAN',
-        backgroundColor: 'rgb(123, 170, 247)',
-        borderColor: 'rgb(123, 170, 247)',
-        borderWidth: 1,
-        data: [
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
-        ],
-        fill: false
-    }, {
-        label: '% Achiev',
-        backgroundColor: 'rgb(123, 247, 204)',
-        borderColor: 'rgb(123, 247, 204)',
-        borderWidth: 1,
-        data: [
-            1, 2, 3, 10, 5, 6, 7, 8, 25, 10, 11, 12, 35, 14, 15, 16, 20, 18, 19, 15, 21, 22, 23, 35, 25, 26, 15, 28, 35, 30, 50
-        ],
-        fill: false
-    }];
-    var chart = new Chart(ctx, {
+    var chartMF = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: hari_kerja,
-            datasets: data
+            labels: labels,
+            datasets: []
         },
         options: {
             responsive: true,
@@ -76,4 +48,45 @@ function chartFabricationMon() {
         }
     });
     Chart.defaults.global.defaultFontColor = 'white';
+
+    var count = label.length;
+    for(var i = 0; i < count; i++) {
+        chartMF.data.datasets.push({borderWidth: 1, fill: false, label: label[i], backgroundColor: color[i], borderColor: color2[i], data: []});
+        for(var j = 0; j < value[i].length; j++) {
+            chartMF.data.datasets[i].data.push(value[i][j]);
+        }
+        chartMF.update();
+    }
+}
+
+function getDataLineChartPP(){
+    $.ajax({
+        url: baseurl+'ProductionPlanning/Monitoring/getSumPlanMonth',
+        data:{
+            section: 9
+        },
+        type: 'POST',
+    }).done(function(data){
+        var data = JSON.parse(data);
+
+        var array = $.map(data['plan'], function(value, index) {
+            return [value];
+        });
+
+        var labels = [];
+        var temp = [];
+        var temp1 = [];
+        var value = [];
+        for (var i = 0; i < 1; i++) {
+            for (var n = 0; n < array.length; n++) {
+                labels.push(Number(array[n]['label']));
+                temp.push(Number(array[n]['plan_qty']));
+                temp1.push(Number(array[n]['achieve_qty']));
+            }
+            value.push(temp);
+            value.push(temp1);
+        }
+        
+        chartFabricationMon(labels, value, ['rgba(163, 60, 82, 0.5)', 'rgba(94, 169, 218, 0.7)'], ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'], ['% PLAN', '% ACHIEV']);
+    });
 }
