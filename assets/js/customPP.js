b7f5e266780a321e7419eb05b4389f8f21bd5049
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-09-27 15:27:22 +0700

    alter script

--- assets/js/customPP.js
+++ assets/js/customPP.js
@@ -10,6 +10,47 @@ window.onload = function() {
     });
 }
 
+function getSectionMon(){
+    var count = $('input[name="sectionCount"]').val();
+    var id = [];
+    for (var i = 0; i < count; i++) {
+        id[i] = $('input[name="sectionId'+i+'"]').val();
+        getDataLineChartPP(id[i], 'month-fabrication'+id[i]);
+    }
+}
+
+function getDataLineChartPP(section,canvasid){
+    $.ajax({
+        url: baseurl+'ProductionPlanning/Monitoring/getSumPlanMonth',
+        data:{
+            section: section
+        },
+        type: 'POST',
+    }).done(function(data){
+        var data = JSON.parse(data);
+
+        var array = $.map(data['plan'], function(value, index) {
+            return [value];
+        });
+
+        var labels = [];
+        var temp = [];
+        var temp1 = [];
+        var value = [];
+        for (var i = 0; i < 1; i++) {
+            for (var n = 0; n < array.length; n++) {
+                labels.push(Number(array[n]['label']));
+                temp.push(Number(array[n]['plan_qty']));
+                temp1.push(Number(array[n]['achieve_qty']));
+            }
+            value.push(temp);
+            value.push(temp1);
+        }
+        
+        chartFabricationMon(canvasid, labels, value, ['rgba(163, 60, 82, 0.5)', 'rgba(94, 169, 218, 0.7)'], ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'], ['% PLAN', '% ACHIEV']);
+    });
+}
+
 function chartFabricationMon(canvasid, labels, value, color, color2, label) {
     var ctx = document.getElementById(canvasid);
     var chartMF = new Chart(ctx, {
@@ -51,17 +92,10 @@ function chartFabricationMon(canvasid, labels, value, color, color2, label) {
     $('canvas#'+canvasid).height("250px");
 }
 
-function getSectionMon(){
-    var count = $('input[name="sectionCount"]').val();
-    var id = [];
-    for (var i = 0; i < count; i++) {
-        id[i] = $('input[name="sectionId'+i+'"]').val();
-        getDataLineChartPP(id[i], 'month-fabrication'+id[i]);
-    }
-}
-
-function getDataLineChartPP(section,canvasid){
-    $.ajax({
+function showHideNormalPlanning(){
+    var ckBegin = $('input[name="checkpointBegin"]').val();
+    var ckEnd = $('input[name="checkpointEnd"]').val();
+     $.ajax({
         url: baseurl+'ProductionPlanning/Monitoring/getSumPlanMonth',
         data:{
             section: section
@@ -73,21 +107,8 @@ function getDataLineChartPP(section,canvasid){
         var array = $.map(data['plan'], function(value, index) {
             return [value];
         });
-
-        var labels = [];
-        var temp = [];
-        var temp1 = [];
-        var value = [];
-        for (var i = 0; i < 1; i++) {
-            for (var n = 0; n < array.length; n++) {
-                labels.push(Number(array[n]['label']));
-                temp.push(Number(array[n]['plan_qty']));
-                temp1.push(Number(array[n]['achieve_qty']));
-            }
-            value.push(temp);
-            value.push(temp1);
-        }
-        
-        chartFabricationMon(canvasid, labels, value, ['rgba(163, 60, 82, 0.5)', 'rgba(94, 169, 218, 0.7)'], ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'], ['% PLAN', '% ACHIEV']);
     });
+
+    console.log(ckBegin);
+    console.log(ckEnd);
 }
