2238149ff300e1b912281656a70e8e85fdb25beb
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-10-19 19:46:03 +0700

    add controller

---
+++ application/controllers/ProductionPlanning/MainMenu/C_StorageMonitoring.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_StorageMonitoring extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('url');
+        $this->load->helper('html');
+        $this->load->library('form_validation');
+		$this->load->library('session');
+		$this->load->model('M_Index');
+		$this->load->model('SystemAdministration/MainMenu/M_user');
+        $this->load->model('ProductionPlanning/MainMenu/M_storagemonitoring');
+    }
+	
+	public function checkSession()
+    {
+		if($this->session->is_logged){
+			
+		}else{
+			redirect('');
+		}
+	}
+	
+	public function index()
+	{
+		$this->checkSession();
+		$user_id = $this->session->userid;
+
+		$data['Menu'] = 'Dashboard';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+		
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+        $data['storagepp']      = $this->M_storagemonitoring->getStoragePP();
+
+        // echo "<pre>";
+        // print_r($data['storagepp']);
+        // echo "</pre>";
+        // exit();
+		
+		$this->load->view('V_Header',$data);
+		$this->load->view('V_Sidemenu',$data);
+        $this->load->view('ProductionPlanning/MainMenu/StorageMonitoring/V_Index',$data);
+        $this->load->view('V_Footer',$data);
+    }
+
+    public function Open()
+    {
+        $storage_id = $this->input->post('storagepp');
+		$this->load->view('ProductionPlanning/MainMenu/StorageMonitoring/V_StorageMonitoring',$data);
+    }
+}
