dc935629447ae1e6c2f8cecf8c0c86cdb2c0fb6b
bimo_akbar <bimo_akbar_f@quick.com>
2017-10-20 13:37:20 +0700

    Revert Report Tanpa Faktur

--- application/controllers/AccountPayables/Report/C_Report.php
+++ application/controllers/AccountPayables/Report/C_Report.php
@@ -130,7 +130,7 @@ class C_Report extends CI_Controller {
 				$baris++;
 			}
 		}else{
-			$objget->getStyle("A1:L1")->applyFromArray(
+			$objget->getStyle("A1:I1")->applyFromArray(
 				array(
 					'fill' => array(
 						'type' => PHPExcel_Style_Fill::FILL_SOLID,
@@ -143,10 +143,10 @@ class C_Report extends CI_Controller {
 				)
 			);
 
-			$cols = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L");
-			$val = array("Vendor Name", "Invoice Type", "Invoice Date", "Invoice Number", "Payment Date", "DPP", "PPN", "Total", "No. Faktur","No. LPPB", "No. PO", "Currency");
+			$cols = array("A", "B", "C", "D", "E", "F", "G", "H", "I");
+			$val = array("Vendor Name", "Invoice Type", "Invoice Date", "Invoice Number", "Payment Date", "DPP", "PPN", "Total", "No. Faktur");
 
-			for ($a=0;$a<12; $a++) {
+			for ($a=0;$a<9; $a++) {
 				$objset->setCellValue($cols[$a].'1', $val[$a]);
 				$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setWidth(45);
 				$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setWidth(13);
@@ -157,9 +157,6 @@ class C_Report extends CI_Controller {
 				$objPHPExcel->getActiveSheet()->getColumnDimension('G')->setWidth(13);
 				$objPHPExcel->getActiveSheet()->getColumnDimension('H')->setWidth(13);
 				$objPHPExcel->getActiveSheet()->getColumnDimension('I')->setWidth(20);
-				$objPHPExcel->getActiveSheet()->getColumnDimension('J')->setWidth(10);
-				$objPHPExcel->getActiveSheet()->getColumnDimension('K')->setWidth(10);
-				$objPHPExcel->getActiveSheet()->getColumnDimension('L')->setWidth(10);
 				$style = array(
 					'alignment' => array(
 						'horizontal' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
@@ -184,9 +181,6 @@ class C_Report extends CI_Controller {
 					$numfaktur = '-';
 				};
 				$objset->setCellValue("I".$baris, $numfaktur);
-				$objset->setCellValue("J".$baris, $frow['LPPB']);
-				$objset->setCellValue("K".$baris, $frow['PO_NUM']);
-				$objset->setCellValue("L".$baris, $frow['CURRENCY']);
 				$baris++;
 			}
 		};
