a7218024ca8af24a76ff96788e542e8fe1e38fe6
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-10-23 13:19:32 +0700

    add view

---
+++ application/views/ProductionPlanning/Settings/Section/V_update.php
@@ -0,0 +1,57 @@
+<section class="content">
+    <div class="inner" >
+        <div class="row">
+            <form method="post" action="<?php echo site_url('ProductionPlanning/Setting/Section/update/'.$id);?>" class="form-horizontal">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="col-lg-11">
+                                <div class="text-right"><h1><b><?= $Title ?></b></h1></div>
+                            </div>
+                            <div class="col-lg-1 ">
+                                <div class="text-right hidden-md hidden-sm hidden-xs">
+                                    <a class="btn btn-default btn-lg" href="<?php echo site_url('ProductionPlanning/Section/');?>">
+                                        <i class="icon-wrench icon-2x"></i>
+                                        <span ><br /></span>
+                                    </a>                             
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="box box-primary box-solid">
+                                <div class="box-header with-border">Update Section</div>
+                                <?php
+                                    foreach ($Section as $headerRow):
+                                ?>
+                                <div class="box-body">
+                                    <div class="panel-body">
+                                        <div class="row">
+											<div class="form-group">
+                                                <label for="txtSectionNameHeader" class="control-label col-lg-4">Section Name</label>
+                                                <div class="col-lg-4">
+                                                    <input type="text" placeholder="Section Name" name="txtSectionNameHeader" id="txtSectionNameHeader" class="form-control" value="<?php echo $headerRow['section_name']; ?>"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="panel-footer">
+                                        <div class="row text-right">
+                                            <a href="javascript:history.back(1)" class="btn btn-primary btn-lg btn-rect">Back</a>
+                                            &nbsp;&nbsp;
+                                            <button type="submit" class="btn btn-primary btn-lg btn-rect">Save Data</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
