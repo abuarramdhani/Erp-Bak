573f71bcb36d976d1cea6c44543db0598943c7a9
mentari_enggar_r <mentari_enggar_r@quick.com>
2017-10-25 11:26:30 +0700

    View Efektivitas Training

---
+++ application/views/ADMPelatihan/Report/Rekap/EfektivitasTraining/V_index2.php
@@ -0,0 +1,23 @@
+							<div class="table-responsive" style="overflow:hidden;">
+								<table class="table table-striped table-bordered table-hover text-left" id="tblrecord" style="font-size:14px;table-layout:fixed;width:1080px;">
+									<thead class="bg-primary">
+										<tr>
+											<th width="7%" style="text-align:center;">No</th>
+											<th >Jenis Training</th>
+											<th width="20%" style="text-align:center;">Presentase Kelulusan</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td style="text-align:center;">1</td>
+											<td>Pelatihan Orientasi</td>
+											<td></td>
+										</tr>
+										<tr>
+											<td style="text-align:center;">2</td>
+											<td>Pelatihan Non Orientasi</td>
+											<td></td>
+										</tr>
+									</tbody>															
+								</table>
+							</div>	
