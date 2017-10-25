eafe9db836c915363ec23c3d3270817991591338
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-10-25 18:55:20 +0700

    alter html code

--- application/views/ProductionPlanning/MainMenu/DataPlan/Daily/V_Create.php
+++ application/views/ProductionPlanning/MainMenu/DataPlan/Daily/V_Create.php
@@ -33,48 +33,51 @@
                             <div class="box-header with-border">
                                 Create New Plans
                             </div>
-                            <div class="panel-body">
+                            <div class="box-body">
                                 <?php echo $message; ?>
                                 <form enctype="multipart/form-data" class="form-horizontal" method="post" action="<?php echo base_url('ProductionPlanning/DataPlanDaily/CreateSubmit'); ?>">
+                                <div class="panel-body">
                                     <div class="row">
                                         <div class="col-md-12 text-center">
                                             <h2><b>UPLOAD FILE EXCEL</b></h2>
                                             <p>-- Klik button download to download sample format data plans --</p>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-md-12 text-center">
-                                            <a class="btn btn-default" href="<?php echo base_url('ProductionPlanning/DataPlanDaily/DownloadSample');?>">
-                                                <i aria-hidden="true" class="fa fa-download"></i> DOWNLOAD SAMPLE
-                                            </a>
-                                            <br><br>
+                                    <div class="row"> 
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3" for="dp2">Section</label>
+                                            <div class="col-md-7">
+                                                <select class="form-control select4" name="section" required="">
+                                                    <option></option>
+                                                    <?php foreach ($section as $s) { ?>
+                                                        <option value="<?php echo $s['section_id']; ?>"> <?php echo $s['section_name']; ?> </option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="control-label col-md-3" for="dp2">Data Plan</label>
+                                            <div class="col-lg-7">
+                                                <input type="file" name="dataPlan" class="form-control" required>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="row" style="padding-bottom: 50px;"> 
-                                            <div class="form-group">
-                                                <label class="control-label col-md-offset-2 col-md-2" for="dp2">Section</label>
-                                                <div class="col-lg-6">
-                                                    <select class="form-control select2" name="section">
-                                                        <?php foreach ($section as $s) { ?>
-                                                            <option value="<?php echo $s['section_id']; ?>"> <?php echo $s['section_name']; ?> </option>
-                                                        <?php } ?>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <br>
-                                            <br>
-                                            <div class="form-group">
-                                                <label class="control-label col-md-offset-2 col-md-2" for="dp2">Data Plan</label>
-                                                <div class="col-lg-6">
-                                                    <input type="file" name="dataPlan" class="form-control" required>
-                                                </div>
-                                            </div>
-                                    </div>
-                                        <div class="row text-center">
+                                </div>
+                                <div class="panel-footer">
+                                    <div class="row text-right">
+                                        <div class="col-md-12">
+                                            <a class="btn btn-default" href="<?php echo base_url('ProductionPlanning/DataPlanDaily');?>">
+                                                CANCEL
+                                            </a>
+                                            <a class="btn btn-warning" href="<?php echo base_url('ProductionPlanning/DataPlanDaily/DownloadSample');?>">
+                                                <i aria-hidden="true" class="fa fa-download"></i> DOWNLOAD SAMPLE
+                                            </a>
                                             <button type="submit" class="btn btn-primary">
                                                 <i aria-hidden="true" class="fa fa-upload"></i> UPLOAD PLANS
                                             </button>
                                         </div>
+                                    </div>
+                                </div>
                                 </form>
                             </div>
                         </div>
