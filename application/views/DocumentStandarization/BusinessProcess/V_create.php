71bdeba95429aae24ed3b289a85c42b216afc39f
Milton Sandyka <milton_sandyka@quick.com>
2017-10-30 13:12:33 +0700

    Perbaikan Fitur (30-10-2017)
    1. Menghapus atribut 'required' untuk input dan edit BP, CD, dan SOP.

--- application/views/DocumentStandarization/BusinessProcess/V_create.php
+++ application/views/DocumentStandarization/BusinessProcess/V_create.php
@@ -83,7 +83,7 @@
 											<div class="form-group">
                                                 <label for="txtDiperiksa1Header" class="control-label col-lg-4">Diperiksa 1</label>
                                                 <div class="col-lg-4">
-                                                     <select id="cmbPekerjaPemeriksa1" name="cmbPekerjaDiperiksa1" class="select2" data-placeholder="Pilih" style="width: 100%" required="">
+                                                     <select id="cmbPekerjaPemeriksa1" name="cmbPekerjaDiperiksa1" class="select2" data-placeholder="Pilih" style="width: 100%">
                                                         <option value=""></option>
                                                         <?php
                                                         /*
