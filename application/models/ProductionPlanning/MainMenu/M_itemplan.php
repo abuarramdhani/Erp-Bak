f15b62df303765761a02ea12d34697cd50211cf0
ratnofirmansyah <ratno_firmansyah@quick.com>
2017-11-02 14:31:10 +0700

    alter query get item data

--- application/models/ProductionPlanning/MainMenu/M_itemplan.php
+++ application/models/ProductionPlanning/MainMenu/M_itemplan.php
@@ -6,14 +6,32 @@ class M_itemplan extends CI_Model {
     $this->load->database();
   }
 
-  public function getItemData($section_id=FALSE,$item_code=FALSE)
+  public function getItemData($section_id=FALSE,$item_code=FALSE,$user_id=FALSE)
   {
-    if($section_id==FALSE && $item_code==FALSE){
+    if($section_id==FALSE && $item_code==FALSE && $user_id==FALSE){
       $this->db->select('*');
       $this->db->from('pp.pp_item_data pid, pp.pp_section ps');
       $this->db->where('pid.section_id = ps.section_id');
       $this->db->order_by('pid.item_code, ps.section_id', 'ASC');
       $query = $this->db->get();
+    }elseif (!$user_id==FALSE) {
+      $sql = "SELECT *
+              FROM
+                (
+                  SELECT
+                    ps.section_id,
+                    ps.section_name
+                  FROM
+                    pp.pp_section ps right join pp.pp_user_group pug on ps.section_id = pug.section_id,
+                    pp.pp_user pu
+                  WHERE
+                    pug.pp_user_id = pu.pp_user_id
+                    and pu.user_id = $user_id
+                ) a,
+                pp.pp_item_data pid
+              WHERE a.section_id = pid.section_id
+              order by pid.item_code asc";
+      $query = $this->db->query($sql);
     }else{
       $sql = "SELECT pid.item_code,
               pid.from_inventory,
