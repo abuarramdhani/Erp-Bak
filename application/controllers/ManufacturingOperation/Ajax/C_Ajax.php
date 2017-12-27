070946117ff8464fe88cc6be6d2f051b9f16a384
ratnofirmansyah <liviamarcelb@gmail.com>
2017-12-27 13:04:22 +0700

    add controller

---
+++ application/controllers/ManufacturingOperation/Ajax/C_Ajax.php
@@ -0,0 +1,79 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class C_Ajax extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('session');
+		$this->load->library('encrypt');
+
+		$this->load->model('ManufacturingOperation/Ajax/M_ajax');
+	}
+
+	public function getComponent()
+	{
+		$term = strtoupper($this->input->post('term'));
+		$data = $this->M_ajax->getComponent($term);
+
+		echo json_encode($data);
+	}
+
+	public function getPrintCode()
+	{
+		$monthCode = array(
+			array('month' => 1,'code' => 'A'),
+			array('month' => 2,'code' => 'B'),
+			array('month' => 3,'code' => 'C'),
+			array('month' => 4,'code' => 'D'),
+			array('month' => 5,'code' => 'E'),
+			array('month' => 6,'code' => 'F'),
+			array('month' => 7,'code' => 'G'),
+			array('month' => 8,'code' => 'H'),
+			array('month' => 9,'code' => 'J'),
+			array('month' => 10,'code' => 'K'),
+			array('month' => 11,'code' => 'L'),
+			array('month' => 12,'code' => 'M')
+		);
+		$yearCode = array(
+			array('year' => 0,'code' => 'N'),
+			array('year' => 1,'code' => 'P'),
+			array('year' => 2,'code' => 'Q'),
+			array('year' => 3,'code' => 'R'),
+			array('year' => 4,'code' => 'S'),
+			array('year' => 5,'code' => 'U'),
+			array('year' => 6,'code' => 'V'),
+			array('year' => 7,'code' => 'W'),
+			array('year' => 8,'code' => 'Y'),
+			array('year' => 9,'code' => 'Z'),
+		);
+
+		$tanggal = $this->input->post('tanggal');
+		$val = explode('/', $tanggal);
+
+		foreach ($monthCode as $value) {
+			if ($val[1] == $value['month']) {
+				$bulan = $value['code'];
+			}
+		}
+		foreach ($yearCode as $v) {
+			if (substr($val[0], 3) == $v['year']) {
+				$tahun = $v['code'];
+			}
+		}
+
+		echo '<div class="col-md-3">
+                <input type="radio" name="print_code" placeholder="Print Code" value="22M W" required> '.$val[2].$bulan.' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="M22 W" required> '.$bulan.$val[2].' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="T22M W" required> T'.$val[2].$bulan.' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="TM22 W" required> T'.$bulan.$val[2].' '.$tahun.'
+            </div>
+            <div class="col-md-3">
+                <input type="radio" name="print_code" placeholder="Print Code" value="X22M W" required> X'.$val[2].$bulan.' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="XM22 W" required> X'.$bulan.$val[2].' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="XT22M W" required> XT'.$val[2].$bulan.' '.$tahun.'
+                <br><input type="radio" name="print_code" placeholder="Print Code" value="XTM22 W" required> XT'.$bulan.$val[2].' '.$tahun.'
+            </div>';
+	}
+}
---
+++ application/controllers/ManufacturingOperation/MainMenu/C_Moulding.php
@@ -0,0 +1,206 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class C_Moulding extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->helper('html');
+
+		$this->load->library('form_validation');
+		$this->load->library('session');
+		$this->load->library('encrypt');
+
+		$this->load->model('SystemAdministration/MainMenu/M_user');
+		$this->load->model('ManufacturingOperation/MainMenu/M_moulding');
+
+		$this->checkSession();
+	}
+
+	/* CHECK SESSION */
+	public function checkSession()
+	{
+		if($this->session->is_logged){
+
+		} else {
+			redirect('index');
+		}
+	}
+
+	/* LIST DATA */
+	public function index()
+	{
+		$user = $this->session->username;
+
+		$user_id = $this->session->userid;
+
+		$data['Title'] = 'Moulding';
+		$data['Menu'] = 'Manufacturing Operation';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+
+		$data['Moulding'] = $this->M_moulding->getMoulding();
+
+		$this->load->view('V_Header',$data);
+		$this->load->view('V_Sidemenu',$data);
+		$this->load->view('ManufacturingOperation/Moulding/V_index', $data);
+		$this->load->view('V_Footer',$data);
+	}
+
+	/* NEW DATA */
+	public function create()
+	{
+		$user_id = $this->session->userid;
+
+		$data['Title'] = 'Moulding';
+		$data['Menu'] = 'Manufacturing Operation';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+
+		/* HEADER DROPDOWN DATA */
+
+		/* LINES DROPDOWN DATA */
+
+		$this->form_validation->set_rules('txtComponentCodeHeader', 'ComponentCode', 'required');
+		$this->form_validation->set_rules('txtProductionDateHeader', 'ProductionDate', 'required');
+		$this->form_validation->set_rules('txtMouldingQuantityHeader', 'MouldingQuantity', 'required');
+		$this->form_validation->set_rules('txtScrapQuantityHeader', 'ScrapQuantity', 'required');
+		$this->form_validation->set_rules('txtScrapTypeHeader', 'ScrapType', 'required');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('V_Header',$data);
+			$this->load->view('V_Sidemenu',$data);
+			$this->load->view('ManufacturingOperation/Moulding/V_create', $data);
+			$this->load->view('V_Footer',$data);	
+		} else {
+			$data = array(
+				'component_code' => $this->input->post('txtComponentCodeHeader'),
+				'component_description' => $this->input->post('txtComponentDescriptionHeader'),
+				'production_date' => $this->input->post('txtProductionDateHeader'),
+				'moulding_quantity' => $this->input->post('txtMouldingQuantityHeader'),
+				'job_id' => $this->input->post('txtJobIdHeader'),
+				'scrap_quantity' => $this->input->post('txtScrapQuantityHeader'),
+				'scrap_type' => $this->input->post('txtScrapTypeHeader'),
+				'created_by' => $this->session->userid,
+    		);
+			$this->M_moulding->setMoulding($data);
+			$header_id = $this->db->insert_id();
+
+			redirect(site_url('ManufacturingOperation/Moulding'));
+		}
+	}
+
+	/* UPDATE DATA */
+	public function update($id)
+	{
+		$user_id = $this->session->userid;
+
+		$data['Title'] = 'Moulding';
+		$data['Menu'] = 'Manufacturing Operation';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+
+		$plaintext_string = str_replace(array('-', '_', '~'), array('+', '/', '='), $id);
+		$plaintext_string = $this->encrypt->decode($plaintext_string);
+
+		$data['id'] = $id;
+
+		/* HEADER DATA */
+		$data['Moulding'] = $this->M_moulding->getMoulding($plaintext_string);
+
+		/* LINES DATA */
+
+		/* HEADER DROPDOWN DATA */
+
+		/* LINES DROPDOWN DATA */
+
+		$this->form_validation->set_rules('txtComponentCodeHeader', 'ComponentCode', 'required');
+		$this->form_validation->set_rules('txtProductionDateHeader', 'ProductionDate', 'required');
+		$this->form_validation->set_rules('txtMouldingQuantityHeader', 'MouldingQuantity', 'required');
+		$this->form_validation->set_rules('txtScrapQuantityHeader', 'ScrapQuantity', 'required');
+		$this->form_validation->set_rules('txtScrapTypeHeader', 'ScrapType', 'required');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('V_Header',$data);
+			$this->load->view('V_Sidemenu',$data);
+			$this->load->view('ManufacturingOperation/Moulding/V_update', $data);
+			$this->load->view('V_Footer',$data);	
+		} else {
+			$data = array(
+				'component_code' => $this->input->post('txtComponentCodeHeader',TRUE),
+				'component_description' => $this->input->post('txtComponentDescriptionHeader',TRUE),
+				'production_date' => $this->input->post('txtProductionDateHeader',TRUE),
+				'moulding_quantity' => $this->input->post('txtMouldingQuantityHeader',TRUE),
+				'job_id' => $this->input->post('txtJobIdHeader',TRUE),
+				'scrap_quantity' => $this->input->post('txtScrapQuantityHeader',TRUE),
+				'scrap_type' => $this->input->post('txtScrapTypeHeader',TRUE),
+				'last_updated_by' => $this->session->userid,
+    			);
+			$this->M_moulding->updateMoulding($data, $plaintext_string);
+
+			redirect(site_url('ManufacturingOperation/Moulding'));
+		}
+	}
+
+	/* READ DATA */
+	public function read($id)
+	{
+		$user_id = $this->session->userid;
+
+		$data['Title'] = 'Moulding';
+		$data['Menu'] = 'Manufacturing Operation';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+
+		$plaintext_string = str_replace(array('-', '_', '~'), array('+', '/', '='), $id);
+		$plaintext_string = $this->encrypt->decode($plaintext_string);
+
+		$data['id'] = $id;
+
+		/* HEADER DATA */
+		$data['Moulding'] = $this->M_moulding->getMoulding($plaintext_string);
+
+		/* LINES DATA */
+
+		$this->load->view('V_Header',$data);
+		$this->load->view('V_Sidemenu',$data);
+		$this->load->view('ManufacturingOperation/Moulding/V_read', $data);
+		$this->load->view('V_Footer',$data);
+	}
+
+    /* DELETE DATA */
+    public function delete($id)
+    {
+        $plaintext_string = str_replace(array('-', '_', '~'), array('+', '/', '='), $id);
+		$plaintext_string = $this->encrypt->decode($plaintext_string);
+
+		$this->M_moulding->deleteMoulding($plaintext_string);
+
+		redirect(site_url('ManufacturingOperation/Moulding'));
+    }
+
+
+
+}
+
+/* End of file C_Moulding.php */
+/* Location: ./application/controllers/ManufacturingOperation/MainMenu/C_Moulding.php */
+/* Generated automatically on 2017-12-20 14:49:32 */
