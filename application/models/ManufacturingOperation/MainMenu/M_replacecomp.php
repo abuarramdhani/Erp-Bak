58873c121dea311d617976cc8337ac6f66059276
ratnofirmansyah <liviamarcelb@gmail.com>
2018-01-26 14:28:11 +0700

    add model aplikasi reject komponen

---
+++ application/models/ManufacturingOperation/MainMenu/M_replacecomp.php
@@ -0,0 +1,89 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_replacecomp extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();    
+        $this->oracle = $this->load->database('oracle',true);    
+        $this->personalia = $this->load->database('personalia',true);    
+    }
+
+    public function getJobHeader($id=FALSE)
+    {
+      $sql = "SELECT we.WIP_ENTITY_NAME ,
+                     TO_CHAR(wdj.DATE_RELEASED,'DD/MM/YYYY hh:mi:ss') RELEASE ,
+                     msib.SEGMENT1 ,
+                     msib.DESCRIPTION
+              FROM mtl_system_items_b msib ,
+                   wip_entities we ,
+                   wip_discrete_jobs wdj
+              WHERE msib.ORGANIZATION_ID = 102
+                AND we.WIP_ENTITY_ID = wdj.WIP_ENTITY_ID
+                AND wdj.PRIMARY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                AND wdj.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND we.WIP_ENTITY_NAME = '$id'
+              ORDER BY wdj.DATE_RELEASED";
+      $query = $this->oracle->query($sql);
+      return $query->result_array();
+    }
+
+    public function getJobLine($id=FALSE)
+    {
+      $sql = "SELECT we.WIP_ENTITY_NAME ,
+                     msib.segment1 ASSY ,
+                     msib.DESCRIPTION ,
+                     mil.SEGMENT1 Seksi ,
+                     bcb.ITEM_NUM ,
+                     msib2.SEGMENT1,
+                     msib2.DESCRIPTION ,
+                     bcb.COMPONENT_QUANTITY,
+                     msib2.PRIMARY_UOM_CODE ,
+                     decode(bcb.WIP_SUPPLY_TYPE, 1,'Push',3,'Pull') supply_type
+              FROM bom_bill_of_materials bbom ,
+                   bom_components_b bcb ,
+                   mtl_system_items_b msib ,
+                   mtl_system_items_b msib2 ,
+                   wip_entities we ,
+                   wip_discrete_jobs wdj ,
+                   bom_operational_routings bor ,
+                   mtl_item_locations mil
+              WHERE bbom.BILL_SEQUENCE_ID = bcb.BILL_SEQUENCE_ID
+                AND bbom.ASSEMBLY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                AND bbom.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND msib2.INVENTORY_ITEM_ID = bcb.COMPONENT_ITEM_ID
+                AND msib2.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND bbom.ORGANIZATION_ID = 102
+                AND bbom.ALTERNATE_BOM_DESIGNATOR IS NULL
+                AND msib.INVENTORY_ITEM_STATUS_CODE = 'Active'
+                AND bcb.DISABLE_DATE IS NULL
+                AND we.WIP_ENTITY_ID = wdj.WIP_ENTITY_ID
+                AND wdj.PRIMARY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                AND wdj.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND we.WIP_ENTITY_NAME = '$id'
+                AND bor.ASSEMBLY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                AND bor.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND bor.COMPLETION_LOCATOR_ID = mil.INVENTORY_LOCATION_ID
+                AND mil.ORGANIZATION_ID = msib.ORGANIZATION_ID
+              GROUP BY we.WIP_ENTITY_NAME ,
+                       msib.segment1 ,
+                       msib.DESCRIPTION ,
+                       mil.SEGMENT1 ,
+                       bcb.ITEM_NUM ,
+                       msib2.SEGMENT1,
+                       msib2.DESCRIPTION ,
+                       bcb.COMPONENT_QUANTITY,
+                       msib2.PRIMARY_UOM_CODE ,
+                       bcb.WIP_SUPPLY_TYPE
+              ORDER BY bcb.ITEM_NUM ASC";
+      $query = $this->oracle->query($sql);
+      return $query->result_array();
+    }
+
+    public function deleteRejectComp($id)
+    {
+      $this->db->where('job_number', $id);
+      $this->db->delete('mo.mo_replacement_component');
+    }
+}
