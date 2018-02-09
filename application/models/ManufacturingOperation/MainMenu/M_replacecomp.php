f2042afbd9ea32073a63d6c89ae7362b6280fdfb
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-02-09 11:19:43 +0700

    alter query

--- application/models/ManufacturingOperation/MainMenu/M_replacecomp.php
+++ application/models/ManufacturingOperation/MainMenu/M_replacecomp.php
@@ -15,15 +15,27 @@ class M_replacecomp extends CI_Model
       $sql = "SELECT we.WIP_ENTITY_NAME ,
                      TO_CHAR(wdj.DATE_RELEASED,'DD/MM/YYYY hh:mi:ss') RELEASE ,
                      msib.SEGMENT1 ,
-                     msib.DESCRIPTION
+                     msib.DESCRIPTION,
+                     mil.segment1 seksi
               FROM mtl_system_items_b msib ,
                    wip_entities we ,
-                   wip_discrete_jobs wdj
+                   wip_discrete_jobs wdj,
+                   bom_operational_routings bor,
+                   mtl_item_locations mil
               WHERE msib.ORGANIZATION_ID = 102
                 AND we.WIP_ENTITY_ID = wdj.WIP_ENTITY_ID
                 AND wdj.PRIMARY_ITEM_ID = msib.INVENTORY_ITEM_ID
                 AND wdj.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND bor.ASSEMBLY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                AND bor.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                AND wdj.COMPLETION_LOCATOR_ID = mil.INVENTORY_LOCATION_ID
                 AND we.WIP_ENTITY_NAME = '$id'
+              GROUP BY we.WIP_ENTITY_NAME,
+                       wdj.DATE_RELEASED,
+                       msib.SEGMENT1,
+                       msib.DESCRIPTION,
+                       mil.segment1,
+                       wdj.COMPLETION_SUBINVENTORY
               ORDER BY wdj.DATE_RELEASED";
       $query = $this->oracle->query($sql);
       return $query->result_array();
@@ -92,11 +104,50 @@ class M_replacecomp extends CI_Model
       $this->db->delete('mo.mo_replacement_component');
     }
 
-    public function getJobLineReject($id)
+    public function getJobLineReject($id, $subinv=FALSE)
+    {
+      if ($subinv === FALSE) {
+        $this->db->select('*');
+        $this->db->from('mo.mo_replacement_component');
+        $this->db->where('job_number', $id);
+      }else{
+        $this->db->select('*');
+        $this->db->from('mo.mo_replacement_component');
+        $this->db->where('job_number', $id);
+        $this->db->where('subinventory_code', $subinv);
+      }
+      $query = $this->db->get();
+      return $query->result_array();
+    }
+
+    public function getJobReplacementNumber($id)
     {
       $this->db->select('*');
-      $this->db->from('mo.mo_replacement_component');
+      $this->db->from('mo.mo_replacement_job');
       $this->db->where('job_number', $id);
+
+      $query = $this->db->get();
+      return $query->result_array();
+    }
+
+    public function getLatestJobReplacementNumber($codeNumber)
+    {
+      $sql  = " SELECT max(replacement_number)
+                FROM mo.mo_replacement_job
+                WHERE replacement_number LIKE '$codeNumber%'";
+      $query= $this->db->query($sql);
+      return $query->result_array();
+    }
+
+    public function setJobReplacementNumber($data)
+    {
+      $this->db->insert('mo.mo_replacement_job', $data);
+
+      $id = $this->db->insert_id();
+      $this->db->select('*');
+      $this->db->from('mo.mo_replacement_job');
+      $this->db->where('replacement_job_id', $id);
+
       $query = $this->db->get();
       return $query->result_array();
     }
