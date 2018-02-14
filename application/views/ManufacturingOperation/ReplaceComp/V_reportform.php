d2b8cc1f75b95f0f517ebd1d6202f87a6562f322
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-02-14 15:19:52 +0700

    alter view

--- application/views/ManufacturingOperation/ReplaceComp/V_reportform.php
+++ application/views/ManufacturingOperation/ReplaceComp/V_reportform.php
@@ -21,23 +21,21 @@
 </head>
 <body>
 	<?php
-	foreach ($jobHeader as $head) {
+	foreach ($rejectData as $head) {
 	?>
-		<table class="table table-bordered">
+		<table class="table table-bordered" style="border-bottom: 0px;">
 			<tr>
-				<td colspan="2" rowspan="3" style="padding: 3px; text-align: center; vertical-align: middle;">
-					<img src="<?php echo base_url('assets/img/logo.png') ?>" class="img" style="max-width: 60px">
+				<td rowspan="3" style="padding: 1px; text-align: center; vertical-align: middle;">
+					<img src="<?php echo base_url('assets/img/logo.png') ?>" class="img" style="max-width: 50px">
 				</td>
-				<td rowspan="3" colspan="2" style="padding: 3px;">
-					<strong><h5>CV. KARYA HIDUP SENTOSA</h5></strong>
-					<small>Jl. Magelang no. 144, Yogyakarta</small>
-					<strong>
-						<h5>PERMINTAAN PENGGANTIAN</h5>
-						<h5>KOMPONEN & MATERIAL PICKLIST JOB/BATCH</h5>
-						<h5>(REJECT MATERIAL)</h5>
-					</strong>
+				<td rowspan="3" style="padding: 3px; border-right: 0px;">
+					<h3>CV. KARYA HIDUP SENTOSA</h3>
+					Jl. Magelang No. 144, Yogyakarta
+					<h2>PERMINTAAN PENGGANTIAN</h2>
+					<h2>KOMPONEN & MATERIAL PICKLIST JOB/BATCH</h2>
+					<h2>(REJECT MATERIAL)</h2>
 				</td>
-				<td rowspan="3" colspan="2" style="padding: 3px; text-align: center; vertical-align: middle;">
+				<td rowspan="3" style="padding: 3px; text-align: center; vertical-align: middle; border-left: 0px;">
 					<img src="<?php echo base_url('assets/upload/ManufacturingOperation/temp/qrcode/'.$replacement_number.'.png') ?>" class="img" style="max-width: 65px">
 				</td>
 				<td style="padding: 3px;">Tanggal</td>
@@ -52,7 +50,7 @@
 				<td style="padding: 3px;">: <strong><?php echo $head['WIP_ENTITY_NAME']; ?></strong></td>
 			</tr>
 			<tr>
-				<td rowspan="2" colspan="4" style="padding: 3px;">
+				<td width="250px" rowspan="2" colspan="4" style="padding: 3px; border-bottom: 0px;">
 					<p>Produk / Assy Description :</p>
 					<strong>
 						<p><?php echo $head['SEGMENT1']; ?></p>
@@ -61,68 +59,96 @@
 				</td>
 			</tr>
 			<tr>
-				<td colspan="2" style="padding: 3px;">Gudang</td>
-				<td colspan="4" style="padding: 3px;">: <strong><?php echo $subinv; ?></strong></td>
+				<td style="padding: 3px; border-bottom: 0px; border-right: 0px;">Gudang</td>
+				<td colspan="2" style="padding: 3px; border-bottom: 0px; border-left: 0px;">: <strong><?php echo $subinv; ?></strong></td>
 			</tr>
-			<tr>
-				<td class="text-center" rowspan="2" style="padding: 3px;"><strong>No.</strong></td>
-				<td class="text-center" rowspan="2" colspan="2" style="padding: 3px;"><strong>Kode</strong></td>
-				<td class="text-center" rowspan="2" colspan="2" style="padding: 3px;"><strong>Nama Barang</strong></td>
-				<td class="text-center" rowspan="2" style="padding: 3px;"><strong>Satuan</strong></td>
-				<td class="text-center" colspan="3" style="padding: 3px;"><strong>Qty</strong></td>
-				<td class="text-center" rowspan="2" style="padding: 3px;"><strong>Keterangan reject</strong></td>
-			</tr>
-			<tr>
-				<td class="text-center" style="padding: 3px;"><strong>Picklist</strong></td>
-				<td class="text-center" style="padding: 3px;"><strong>Reject</strong></td>
-				<td class="text-center" style="padding: 3px;"><strong>Ganti</strong></td>
-			</tr>
-			<?php $no=1; foreach ($jobLineReject as $ln) { ?>
-				<tr>
-					<td class="text-center" style="padding: 3px;"><?php echo $no++; ?></td>
-					<td style="padding: 3px;" colspan="2"><?php echo $ln['component_code']; ?></td>
-					<td style="padding: 3px;" colspan="2"><?php echo $ln['component_description']; ?></td>
-					<td class="text-center" style="padding: 3px;"><?php echo $ln['uom']; ?></td>
-					<td class="text-center" style="padding: 3px;"><?php echo $ln['picklist_quantity']; ?></td>
-					<td class="text-center" style="padding: 3px;"><?php echo $ln['return_quantity']; ?></td>
-					<td style="padding: 3px;"></td>
-					<td style="padding: 3px;"><?php echo $ln['return_information']; ?></td>
-				</tr>
-			<?php } ?>
 		</table>
 		<table class="table table-bordered">
 			<tr>
-				<td style="padding: 3px;" class="text-center" colspan="2">
-					<small>Penerima</small>
-					<small>(Seksi)</small>
+				<td width="25px" class="text-center" rowspan="2" style="padding: 3px;">
+					<strong>No.</strong>
 				</td>
-				<td style="padding: 3px;" class="text-center">
-					<small>Diterima :</small>
-					<small>Gudang</small>
+				<td width="110px" class="text-center" rowspan="2" style="padding: 3px;">
+					<strong>Kode</strong>
 				</td>
-				<td style="padding: 3px;" class="text-center" colspan="2">
-					<small>Mengetahui :</small>
-					<small>PPIC</small>
+				<td width="228px" class="text-center" rowspan="2" style="padding: 3px;">
+					<strong>Nama Barang</strong>
 				</td>
-				<td style="padding: 3px;" class="text-center" colspan="2">
-					<small>Mengetahui :</small>
-					<small>QC</small>
+				<td width="30px" class="text-center" rowspan="2" style="padding: 3px;">
+					<strong>Uom</strong>
 				</td>
-				<td style="padding: 3px;" class="text-center">
-					<small>Mengetahui :</small>
-					<small>Kepala Seksi/Spv.</small>
+				<td class="text-center" colspan="3" style="padding: 3px;">
+					<strong>Qty</strong>
 				</td>
-				<td style="padding: 3px;" class="text-center" colspan="2">
-					<small>Diminta :</small>
+				<td class="text-center" rowspan="2" style="padding: 3px;">
+					<strong>Keterangan reject</strong>
 				</td>
 			</tr>
 			<tr>
-				<td style="height: 50px;" colspan="2"></td>
-				<td style="height: 50px;"></td>
-				<td style="height: 50px;" colspan="2"></td>
-				<td style="height: 50px;" colspan="2"></td>
-				<td style="height: 50px;"></td>
-				<td style="height: 50px;" colspan="2"></td>
+				<td width="35px" class="text-center" style="padding: 3px;"><strong>Picklist</strong></td>
+				<td width="35px" class="text-center" style="padding: 3px;"><strong>Reject</strong></td>
+				<td width="35px" class="text-center" style="padding: 3px;"><strong>Ganti</strong></td>
+			</tr>
+			<?php $no=1; foreach ($head['DATA_BODY'] as $ln) { ?>
+				<tr>
+					<td class="text-center" style="padding: 3px; vertical-align: top;">
+						<?php echo $no++; ?>
+					</td>
+					<td style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['component_code']; ?>
+					</td>
+					<td style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['component_description']; ?>
+					</td>
+					<td class="text-center" style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['uom']; ?>
+					</td>
+					<td class="text-center" style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['picklist_quantity']; ?>
+					</td>
+					<td class="text-center" style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['return_quantity']; ?>
+					</td>
+					<td style="padding: 3px;"></td>
+					<td style="padding: 3px; vertical-align: top;">
+						<?php echo $ln['return_information']; ?>
+					</td>
+				</tr>
+			<?php } ?>
+		</table>
+		<table class="table table-bordered" style="border-top: 0px;">
+			<tr>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px;" class="text-center">
+					Penerima<br>
+					(Seksi)
+				</td>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px;" class="text-center">
+					Diterima :<br>
+					Gudang
+				</td>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px;" class="text-center">
+					Mengetahui :<br>
+					PPIC
+				</td>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px;" class="text-center">
+					Mengetahui :<br>
+					QC
+				</td>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px;" class="text-center">
+					Mengetahui :<br>
+					Kepala Seksi/Spv.
+				</td>
+				<td style="padding: 3px; border-top: 0px; border-bottom: 0px; vertical-align: top;" class="text-center">
+					Diminta :
+				</td>
+			</tr>
+			<tr>
+				<td style="height: 60px;"></td>
+				<td style="height: 60px;"></td>
+				<td style="height: 60px;"></td>
+				<td style="height: 60px;"></td>
+				<td style="height: 60px;"></td>
+				<td style="height: 60px;"></td>
 			</tr>
 		</table>
 	<?php } ?>
