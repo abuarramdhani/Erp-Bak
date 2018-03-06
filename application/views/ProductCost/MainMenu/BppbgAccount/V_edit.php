84d8156e78b912c753fdfc0ff29c37ee9665cff7
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-03-06 10:24:41 +0700

    alter view edit

--- application/views/ProductCost/MainMenu/BppbgAccount/V_edit.php
+++ application/views/ProductCost/MainMenu/BppbgAccount/V_edit.php
@@ -30,54 +30,56 @@
                                 Create New Bppbg Account
                             </div>
                             <div class="box-body">
-                                <form class="form-horizontal" action="<?php echo base_url('ProductCost/BppbgAccount/edit/'.$id) ?>" method="post">
-                                    <div class="row">
-                                        <div class="col-md-8 col-md-offset-2">
-                                            <div class="form-group">
-                                                <label>Using Category Code</label>
-                                                <div class="input-group">
-                                                    <input type="hidden" name="using_category_code_checkout" value="0">
-                                                    <input type="text" name="using_category_code" class="form-control toupper checking-database-account" placeholder="Using Category Code" required="">
-                                                    <span id="using_category_code" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                <?php foreach ($account as $value) { ?>
+                                    <form class="form-horizontal" action="<?php echo base_url('ProductCost/BppbgAccount/edit/'.$id) ?>" method="post">
+                                        <div class="row">
+                                            <div class="col-md-8 col-md-offset-2">
+                                                <div class="form-group">
+                                                    <label>Using Category Code</label>
+                                                    <div class="input-group">
+                                                        <input type="hidden" name="using_category_code_checkout" value="0">
+                                                        <input type="text" name="using_category_code" class="form-control toupper checking-database-account" placeholder="Using Category Code" required="" value="<?php echo $value['USING_CATEGORY_CODE'] ?>">
+                                                        <span id="using_category_code" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Using Category</label>
-                                                <input class="form-control toupper" type="text" name="using_category" placeholder="Using Category" required="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Cost Center</label>
-                                                <div class="input-group">
-                                                    <input type="hidden" name="cost_center_checkout" value="0">
-                                                    <input type="text" name="cost_center" class="form-control toupper checking-database-account" placeholder="Cost Center" required="">
-                                                    <span id="cost_center" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                                <div class="form-group">
+                                                    <label>Using Category</label>
+                                                    <input class="form-control toupper" type="text" name="using_category" placeholder="Using Category" required="" value="<?php echo $value['USING_CATEGORY'] ?>">
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Cost Center Description</label>
-                                                <input class="form-control toupper" type="text" name="cost_center_description" placeholder="Cost Center Description" required="">
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Account Number</label>
-                                                <div class="input-group">
-                                                    <input type="hidden" name="account_number_checkout" value="0">
-                                                    <input type="text" name="account_number" class="form-control toupper checking-database-account" placeholder="Account Number" required="">
-                                                    <span id="account_number" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                                <div class="form-group">
+                                                    <label>Cost Center</label>
+                                                    <div class="input-group">
+                                                        <input type="hidden" name="cost_center_checkout" value="0">
+                                                        <input type="text" name="cost_center" class="form-control toupper checking-database-account" placeholder="Cost Center" required="" value="<?php echo $value['COST_CENTER'] ?>">
+                                                        <span id="cost_center" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label>Account Attribute</label>
-                                                <input class="form-control toupper" type="text" name="account_attribute" placeholder="Account Attribute">
+                                                <div class="form-group">
+                                                    <label>Cost Center Description</label>
+                                                    <input class="form-control toupper" type="text" name="cost_center_description" placeholder="Cost Center Description" required="" value="<?php echo $value['COST_CENTER_DESCRIPTION'] ?>">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Account Number</label>
+                                                    <div class="input-group">
+                                                        <input type="hidden" name="account_number_checkout" value="0">
+                                                        <input type="text" name="account_number" class="form-control toupper checking-database-account" placeholder="Account Number" required="" value="<?php echo $value['ACCOUNT_NUMBER'] ?>">
+                                                        <span id="account_number" class="input-group-addon" data-toggle="tooltip" data-placement="top" title="Checking data in database"><i class="fa fa-info"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Account Attribute</label>
+                                                    <input class="form-control toupper" type="text" name="account_attribute" placeholder="Account Attribute" value="<?php echo $value['ACCOUNT_ATTRIBUTE'] ?>">
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <button id="bAccountSubmitBtn" type="submit" class="btn btn-primary pull-right" disabled>SUBMIT</button>
-                                            <a class="btn btn-default pull-right" href="<?php echo base_url('ProductCost/BppbgAccount') ?>">BACK</a>
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <button id="bAccountSubmitBtn" type="submit" class="btn btn-primary pull-right">SUBMIT</button>
+                                                <a class="btn btn-default pull-right" href="<?php echo base_url('ProductCost/BppbgAccount') ?>">BACK</a>
+                                            </div>
                                         </div>
-                                    </div>
-                                </form>
+                                    </form>
+                                <?php } ?>
                             </div>
                         </div>
                     </div>
