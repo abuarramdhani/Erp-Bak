40c1d8c469b9a7dbfd8d3d309c83483cd37ee9cb
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-03-06 10:25:13 +0700

    alter controller update function

--- application/controllers/ProductCost/MainMenu/C_BppbgAccount.php
+++ application/controllers/ProductCost/MainMenu/C_BppbgAccount.php
@@ -81,7 +81,7 @@ class C_BppbgAccount extends CI_Controller {
 			$f = $this->input->post('account_number');
 			$g = $this->input->post('account_attribute');
 
-			$aa = $this->M_bppbgaccount->setAccount($a,$b,$c,$d,$e,$f,$g);
+			$this->M_bppbgaccount->setAccount($a,$b,$c,$d,$e,$f,$g);
 
 			redirect(site_url('ProductCost/BppbgAccount/'));
 		}
@@ -110,23 +110,15 @@ class C_BppbgAccount extends CI_Controller {
 			$this->load->view('ProductCost/MainMenu/BppbgAccount/V_edit', $data);
 			$this->load->view('V_Footer',$data);	
 		} else {
-			$accountAttr = $this->input->post('account_attribute');
-			if (empty($accountAttr)) {
-				$accountAttr = NULL;
-			}
 
-			$dataInsert = array(
-				'ACCOUNT_ID'				=> KHS_BPPBG_ACCOUNT_SEQ_REV.NEXTVAL,
-				'USING_CATEGORY_CODE'		=> $this->input->post('using_category_code'),
-				'USING_CATEGORY'			=> $this->input->post('using_category'),
-				'COST_CENTER'				=> $this->input->post('cost_center'),
-				'COST_CENTER_DESCRIPTION'	=> $this->input->post('cost_center_description'),
-				'ACCOUNT_NUMBER'			=> $this->input->post('account_number'),
-				'ACCOUNT_ATTRIBUTE'			=> $accountAttr,
-				'CREATION_DATE'				=> SYSDATE
-    		);
+			$b = $this->input->post('using_category_code');
+			$c = $this->input->post('using_category');
+			$d = $this->input->post('cost_center');
+			$e = $this->input->post('cost_center_description');
+			$f = $this->input->post('account_number');
+			$g = $this->input->post('account_attribute');
 
-			$this->M_bppbgaccount->setAccount($dataInsert);
+			$this->M_bppbgaccount->updateAccount($id,$b,$c,$d,$e,$f,$g);
 
 			redirect(site_url('ProductCost/BppbgAccount/'));
 		}
