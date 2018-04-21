9802390e51fc054782010aa84054c01d150b5bc7
Milton Sandyka <milton_sandyka@quick.com>
2018-04-21 08:03:49 +0700

    Perbaikan Fitur (2018-04-21)
    1. Perbaikan Javascript customPRS.js

--- assets/js/customPRS.js
+++ assets/js/customPRS.js
@@ -87,10 +87,60 @@ $(document).ready(function(){
 
 });
 
+// 	-------Presensi Catering--------------------------------------------start
+	$(function()
+	{
+		// 	Daterangepicker
+		// 	{
+				$('.PresensiCatering-daterangepickersingledate').daterangepicker({
+				    "singleDatePicker": true,
+				    "showDropdowns": true,
+				    "autoApply": true,
+				    "mask": true,
+				    "locale": {
+				        "format": "YYYY-MM-DD",
+				        "separator": " - ",
+				        "applyLabel": "OK",
+				        "cancelLabel": "Batal",
+				        "fromLabel": "Dari",
+				        "toLabel": "Hingga",
+				        "customRangeLabel": "Custom",
+				        "weekLabel": "W",
+				        "daysOfWeek": [
+				            "Mg",
+				            "Sn",
+				            "Sl",
+				            "Rb",
+				            "Km",
+				            "Jm",
+				            "Sa"
+				        ],
+				        "monthNames": [
+				            "Januari",
+				            "Februari",
+				            "Maret",
+				            "April",
+				            "Mei",
+				            "Juni",
+				            "Juli",
+				            "Agustus ",
+				            "September",
+				            "Oktober",
+				            "November",
+				            "Desember"
+				        ],
+				        "firstDay": 1
+				    }
+				}, function(start, end, label) {
+				  console.log("New date range selected: ' + start.format('DD-MM-YYYY H:i:s') + ' to ' + end.format('DD-MM-YYYY H:i:s') + ' (predefined range: ' + label + ')");
+				});
+		//	}
+
 		// 	Datatables
 		//	{
 				$('#PresensiCatering-tabelRekapTransaksiCatering').DataTable({
 					scrollX: false,
+					scrollY: 250,
 					lengthChange: false,
 					paging: false,
 				});
