e9dc194225b1d6bec03c5c159423ed86658747ab
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-08-13 12:51:03 +0700

    add script custom

--- assets/js/customWH.js
+++ assets/js/customWH.js
@@ -1,7 +1,7 @@
 function getDataSPB() {
     event.preventDefault();
     $.ajax({
-        url: baseurl+"Warehouse/Ajax/Spb",
+        url: baseurl + "Warehouse/Ajax/getSPB",
         type: 'POST',
         data: $('#formSPB').serialize(),
         beforeSend: function() {
@@ -9,12 +9,82 @@ function getDataSPB() {
             $('#tableSPBArea').empty();
         },
         success: function(result) {
-        	console.log(result);
+            console.log(result);
             $('#tableSPBArea').html(result);
             $('#loadingArea').hide();
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
-            $.toaster(textStatus+' | '+errorThrown, name, 'danger');
+            $.toaster(textStatus + ' | ' + errorThrown, name, 'danger');
         }
     });
+}
+
+function getDataPackingList() {
+    event.preventDefault();
+    $.ajax({
+        url: baseurl + "Warehouse/Ajax/PackingList",
+        type: 'POST',
+        data: $('#formPackingList').serialize(),
+        beforeSend: function() {
+            $('#loadingArea').show();
+            $('#tablePackingListArea').empty();
+        },
+        success: function(result) {
+            console.log(result);
+            $('input[name="nomerSPB"]').val('');
+            $('#tablePackingListArea').html(result);
+            $('#loadingArea').hide();
+            $('.select2-custom').select2({
+                placeholder: "Choose Option",
+                allowClear: true,
+                width: 'element',
+                tags: true,
+                id: function(object) {
+                    return object.text;
+                },
+                createSearchChoice: function(term, data) {
+                    if ($(data).filter(function() {
+                            return this.text.localeCompare(term) === 0;
+                        }).length === 0) {
+                        return {
+                            id: term,
+                            text: term
+                        };
+                    }
+                }
+            });
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            $.toaster(textStatus + ' | ' + errorThrown, name, 'danger');
+        }
+    });
+}
+
+function updatePackingQty(e, th) {
+    if (e.keyCode === 13) {
+        var value = $(th).val();
+        var qty = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty"]').val());
+        var maxPack = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="maxPack"]').val());
+        var qtyNow = qty+1;
+
+        if (qtyNow>maxPack) {
+            $.toaster('ERROR', 'JUMLAH ITEM TIDAK BOLEH MELEBIHI PERMINTAAN', 'danger');
+        }else{
+            $('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty"]').val(qtyNow);
+        }
+        $(th).val('')
+    }
+}
+
+function mdlPackingQtyCustom(th) {
+    var qty = $(th).closest('tr').find('input[name="packingqty"]').val();
+    $('#packingqtyMdl input[name="qty"]').val(qty);
+    $('#packingqtyMdl').modal('show');
+}
+
+function getSum(th) {
+    var qty = Number($(th).closest('tr').find('input[name="qty"]').val());
+    var items = Number($(th).val());
+    var sum = qty*items;
+    $(th).closest('tr').find('input[name="sum"]').val(sum);
 }
