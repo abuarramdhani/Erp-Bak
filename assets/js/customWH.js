e14b34dfb3e401ad5ce5eb1fdf6c0120ed214b1a
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-08-16 08:34:48 +0700

    alter script

--- assets/js/customWH.js
+++ assets/js/customWH.js
@@ -61,9 +61,9 @@ function getDataPackingList() {
 function updatePackingQty(e, th) {
     if (e.keyCode === 13) {
         var value = $(th).val();
-        var qty = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty"]').val());
-        var maxPack = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="maxPack"]').val());
-        var maxOnhand = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="maxOnhand"]').val());
+        var qty = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty[]"]').val());
+        var maxPack = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="maxPack[]"]').val());
+        var maxOnhand = Number($('#tblSPB tbody tr[data-row="'+value+'"] input[name="maxOnhand[]"]').val());
         var qtyNow = qty+1;
 
         if (qtyNow>maxPack) {
@@ -71,17 +71,21 @@ function updatePackingQty(e, th) {
         }else if (qtyNow>maxOnhand) {
             $.toaster('ERROR', 'JUMLAH ITEM TIDAK BISA MELEBIHI ONHAND', 'danger');
         }else{
-            $('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty"]').val(qtyNow);
+            $('#tblSPB tbody tr[data-row="'+value+'"] input[name="packingqty[]"]').val(qtyNow);
         }
 
-        $(th).val('')
+        $(th).val('');
+        $('#btnSubmitPacking').attr('disabled', false);
+        if (true) {
+            $('#cetakPackingList').attr('disabled', false);
+        }
     }
 }
 
 function mdlPackingQtyCustom(th, itemcode) {
-    var qty = $(th).closest('tr').find('input[name="packingqty"]').val();
-    var onhand = $(th).closest('tr').find('input[name="maxOnhand"]').val();
-    var required = $(th).closest('tr').find('input[name="maxPack"]').val();
+    var qty = $(th).closest('tr').find('input[name="packingqty[]"]').val();
+    var onhand = $(th).closest('tr').find('input[name="maxOnhand[]"]').val();
+    var required = $(th).closest('tr').find('input[name="maxPack[]"]').val();
     $('#packingqtyMdl input[name="qty"]').val(qty);
     $('#packingqtyMdl input[name="onhand"]').val(onhand);
     $('#packingqtyMdl input[name="required"]').val(required);
@@ -107,15 +111,46 @@ function getSum(th) {
     }
 }
 
-function packing() {
-    var kemasan = $('select[name="kemasan"]').val();
-    var ekspedisi = $('select[name="ekspedisi"]').val();
-}
-
 function packingqtyCustom(th) {
     event.preventDefault();
     var sum = $(th).closest('form').find('input[name="sum"]').val();
     var itemcode = $(th).closest('form').find('input[name="itemcode"]').val();
-    $('#tblSPB tbody tr[data-row="'+itemcode+'"] input[name="packingqty"]').val(sum);
+    $('#tblSPB tbody tr[data-row="'+itemcode+'"] input[name="packingqty[]"]').val(sum);
     $('#packingqtyMdl').modal('hide');
+}
+
+function setPacking() {
+    event.preventDefault();
+    $.ajax({
+        url: baseurl + "Warehouse/Ajax/setPacking",
+        type: 'POST',
+        data: $('#formSetPacking').serialize(),
+        beforeSend: function() {
+            $('#loadingMdl').modal('show');
+            $('#submitPacking').modal('hide');
+        },
+        success: function(result) {
+            var data = JSON.parse(result);
+            var array = $.map(data, function(value, index) {
+                return [value];
+            });
+
+            for (var n = 0; n < array.length; n++) {
+                var qtyBefore = $('#tblSPB tbody tr[data-id="'+array[n]['INVENTORY_ITEM_ID']+'"] input[name="maxPack[]"]').val();
+                var qtyAfter = qtyBefore-(Number(array[n]['PACKING_QTY']));
+                $('#tblSPB tbody tr[data-id="'+array[n]['INVENTORY_ITEM_ID']+'"] td.quantityArea').html(qtyAfter);
+                $('#tblSPB tbody tr[data-id="'+array[n]['INVENTORY_ITEM_ID']+'"] input[name="maxPack[]"]').val(qtyAfter);
+                $('#tblSPB tbody tr[data-id="'+array[n]['INVENTORY_ITEM_ID']+'"] input[name="packingqty[]"]').val('');
+            }
+
+            var a = Number($('#formSetPacking input[name="packingNumber"]').val());
+            var b = a+1;
+            $('#formSetPacking input[name="packingNumber"]').val(b);
+            $('#loadingMdl').modal('hide');
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            $('#loadingMdl').modal('hide');
+            $.toaster(textStatus + ' | ' + errorThrown, name, 'danger');
+        }
+    });
 }
