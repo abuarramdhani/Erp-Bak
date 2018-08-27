1b13f1be44d56ca749f7567730d90c0e28d1c5f2
ratnofirmansyah <ratno_firmansyah@quick.com>
2018-08-27 13:53:34 +0700

    add report view

---
+++ application/views/Warehouse/MainMenu/TransactionPackingList/Report/V_Footer.php
@@ -0,0 +1,45 @@
+<table class="table table-bordered">
+    <tr>
+        <td rowspan="2" width="13%" class="center-hor center-ver">
+            <img src="<?php echo base_url('assets/upload/Warehouse/temp/qrcode/'.$spbNumber.'.png') ?>" style="height: 80px;"/>
+        </td>
+        <td rowspan="2" class="top-ver">
+            Catatan
+        </td>
+        <td colspan="2" class="center-hor">
+            Gudang
+        </td>
+    </tr>
+    <tr class="center-hor" style="font-size: 9px">
+        <td style="width:15%;">
+            tgl: .......................
+            <br>
+            Pengawas :
+            <br>
+            <br>
+            <br>
+            <br>
+            (  Latifa H  )
+        </td>
+        <td style="width:15%;">
+            tgl: .......................
+            <br>
+            Petugas Packing :
+            <br>
+            <br>
+            <br>
+            <br>
+            ( ....................... )
+        </td>
+    </tr>
+</table>
+<table class="table" style="font-size: 9px">
+	<tr>
+		<td>
+			FRM-WHS-02-SP-01, REV.02
+		</td>
+		<td class="right-hor">
+			Page {PAGENO} of {nb}
+		</td>
+	</tr>
+</table>
