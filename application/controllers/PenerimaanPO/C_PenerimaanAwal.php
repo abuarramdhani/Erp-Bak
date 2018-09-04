b3e49f60b638a29fe8bc90c8ca27f34c9a761ff2
alifianadexe <alifianadexe@gmail.com>
2018-09-04 09:34:18 +0700

    modifikasi penambahan fitur

--- application/controllers/PenerimaanPO/C_PenerimaanAwal.php
+++ application/controllers/PenerimaanPO/C_PenerimaanAwal.php
@@ -3,11 +3,11 @@
 class C_penerimaanAwal extends CI_Controller {
 
 	function __construct(){
-	   parent::__construct();
+		parent::__construct();
 		$this->load->helper('form');
-        $this->load->helper('url');
-        $this->load->helper('html');
-        $this->load->library('form_validation');
+		$this->load->helper('url');
+		$this->load->helper('html');
+		$this->load->library('form_validation');
           //load the login model
 		$this->load->library('session');
 		  //$this->load->library('Database');
@@ -64,6 +64,14 @@ class C_penerimaanAwal extends CI_Controller {
 		echo json_encode($data);
 	}
 
+	public function loadSubinv($PO){
+		$data = $this->M_penerimaanawal->Subinv($PO);
+		if(!$data){
+			$data[0]['SUB_INVENTORY']='-';
+		}
+		echo json_encode($data);
+	}
+
 	public function loadPoLine($PO){
 		$data = $this->M_penerimaanawal->loadPoLine($PO);
 		echo json_encode($data);
@@ -85,13 +93,53 @@ class C_penerimaanAwal extends CI_Controller {
 		$spDate	   = $this->input->post('spDate');	
 		$qtyActual = $this->input->post('qtyActual');	
 		$qtyPO	   = $this->input->post('qtyPO');
+		$qtyReceipt = $this->input->post('qtyReceipt');
+		$keterangan = $this->input->post('keterangan');
+		$status = $this->input->post('status');
+		$data = $this->M_penerimaanawal->insertDataAwal($po,$sj,$vendor,$item,$desc,$qtySJ,$rcptDate,$spDate,$qtyActual,$qtyPO,$qtyReceipt,$keterangan,$status);
 
-		$data = $this->M_penerimaanawal->insertDataAwal($po,$sj,$vendor,$item,$desc,$qtySJ,$rcptDate,$spDate,$qtyActual,$qtyPO);
 		echo json_encode($data);
+
+	}
+
+	public function cetakPDF(){
+		
+		$where = $this->input->get('id');
+		$p_barang = $this->input->get('p-barang');
+		$whs = $this->input->get('whs');
+		$sj = $this->input->get('sj');
+
+		$data = $this->M_penerimaanawal->selectGenerate($where,$sj);
+			if($whs == '' || $whs == '-'){
+				$whs = '<b style="color:white;">ADEXE</b>';
+			}
+			// echo "<pre>";
+			// print_r($datalist);
+			// exit();
+
+			$this->load->library('pdf');
+			$pdf = $this->pdf->load();
+			$pdf = new mPDF('utf-8',array(80,110));
+		
+			$filename = 'Form-Pendaftaran-Resource.pdf';
+			
+			foreach ($data as $datalist) {
+			
+			$datalist['sad'] = '';
+			$pdf->SetHTMLHeader('<table style="margin:-40px;width:100%;border: 1px solid black; padding: 0px">
+				<tr>
+					<td style="width:20%;border-right: 1px solid black;"><center><img style="width: 30px;" src='.base_url('assets/img/logo.png').'></center></td>
+					<td style="width:40%;border-right: 1px solid black;"><center><p style="font-size:10px;"><b>WHS</b>: '.$whs.'</p></center></td>
+					<td style="width:40%"><center><p style="font-size:10px;"><b>TGL </b>:'.$datalist['RECEIPT_DATE'].'</h1></center></td>
+				</tr>
+			</table>');
+				$html = $this->load->view('PenerimaanPO/V_CreateForm', $datalist, true);
+				$pdf->WriteHTML($html, 2);
+			}
+			$pdf->Output($filename, 'I');
 	}
 
 }
-	
-	
-	
-	
+
+
+
--- assets/js/customPPO.js
+++ assets/js/customPPO.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    //  $(document).ajaxStart(function () {
+    //     $("#loading").show();
+    // }).ajaxStop(function () {
+    //     $("#loading").hide();
+    // });
 
     var listItem = [];
     var SJ;
@@ -13,7 +18,7 @@ $(document).ready(function() {
        todayHighlight: true,
        format: 'dd/mm/yyyy'
     });
-
+ 
     $(".selectVendor").select2({
         allowClear: true,
         placeholder: "Vendor",
@@ -70,16 +75,19 @@ $(document).ready(function() {
 
     $("#b_procces").click(function() {
         var PO = $('#in_po').val();
+        $("#loading").show();
         $.ajax({
             url: baseurl + "PenerimaanPO/awal/loadVendor/" + PO,
             success: function(data) {
                 var arrayOne = $.parseJSON(data);
                 var vendor = arrayOne[0].VENDOR_NAME;
+
                 $('#sel_vendor').html('<option value="'+vendor+'">' + vendor + '</option>');
                 $('#sel_vendor').val(vendor).trigger('change');
 
                 $('#in_sj').val('');
                 $('#in_dateShip').val('');
+                $("#loading").hide();
             }
         })
         $.ajax({
@@ -92,6 +100,17 @@ $(document).ready(function() {
                 });
                 showChange();
                 $.toaster('Data loaded!', 'Load', 'success');
+                $("#loading").hide();
+            }
+        })
+         $.ajax({
+            url: baseurl + "PenerimaanPO/awal/loadSubinv/" + PO,
+            success: function(results) {
+                var arrayOne = $.parseJSON(results);
+                var subinv = arrayOne[0]['SUB_INVENTORY'];
+                $('#subinv').html('<label value="'+subinv+'">' + subinv + '</label>');
+                $('#subinv').val(subinv).trigger('change');
+                $("#loading").hide();
             }
         })
     });
@@ -128,38 +147,66 @@ $(document).ready(function() {
         var SJ     = $('#in_sj').val();
         var Vendor = $('#sel_vendor').val();
         var tglSP  = $('#in_dateShip').val();
+        var PO = $('#in_po').val()
+        var whs = $('#subinv').val();
+        console.log(listItem);
 
         if(SJ.length==0||Vendor.length==0||tglSP.length==0){
             $.toaster('Lengkapi data header!', 'Error', 'error');
         }else{
             for(i=0;i<listItem.length;i++){
-            var objItem = listItem[i];
+                var objItem = listItem[i];
+                console.log(objItem);
+                var keterangan = $('#ket_qty'+i).val();
+                console.log(keterangan);
+                
+            if($('#adexe_qty'+i).is(":checked")){
+                 var keterangan = $('#ket_qty'+i).val();
 
-            if(objItem.ISI==0){
-                ++actCount;
-            }else{
-                $.ajax({
-                url: baseurl + "PenerimaanPO/C_PenerimaanAwal/insertDataAwal",
-                type:"POST",
-                data: jQuery.param({
-                    po        : $('#in_po').val(),
-                    sj        : SJ,
-                    vendor    : Vendor,
-                    item      : objItem.SEGMENT1,
-                    desc      : objItem.ITEM_DESCRIPTION,
-                    qtySJ     : objItem.ISI,
-                    rcptDate  : "sysdate",
-                    spDate    : tglSP,
-                    qtyActual : 0,
-                    qtyPO     : objItem.QUANTITY  
-                }),
-                success:function(data){
-                    if(++actCount==listItem.length){
-                        $.toaster('Data Inserted!', 'Success', 'success');
-                        location.reload();
+                if(objItem.ISI==0){
+                    ++actCount;
+                }else{
+                    
+                    var qc = 'NON-QC';
+                    if(objItem.ROUTING == "Inspection Required"){
+                        qc = 'QC';
                     }
+
+                    $.ajax({
+                        url: baseurl + "PenerimaanPO/C_PenerimaanAwal/insertDataAwal",  
+                        dataType:'json',
+                        type:"POST",
+                        data:{
+                            po        : PO,
+                            sj        : SJ,
+                            vendor    : Vendor,
+                            item      : objItem.SEGMENT1,
+                            desc      : objItem.ITEM_DESCRIPTION,
+                            qtySJ     : objItem.ISI,
+                            rcptDate  : "sysdate",
+                            spDate    : tglSP,
+                            qtyActual : 0,
+                            qtyPO     : objItem.QUANTITY,
+                            qtyReceipt: objItem.QTY_RECEIPT,
+                            keterangan: keterangan,
+                            status:qc
+                        },
+                        success:function(data){
+                            console.log(data);
+                            if(++actCount==listItem.length){
+                                $.toaster('Data Inserted!', 'Success', 'success');
+                                window.open(baseurl+"PenerimaanPO/awal/cetakPDF?id="+PO+"&whs="+whs+"&sj="+SJ,'_blank');
+                            }
+                        },
+                        error:function(error,status){
+                            console.log(error);
+                            console.log(status);
+                        }
+                    });
                 }
-            })
+            }else{
+                console.log('cuk');
+                ++actCount;
             }
         }
         }
@@ -169,7 +216,7 @@ $(document).ready(function() {
         var row = '';
         for (i = 0; i < listItem.length; i++) {
             var objItem = listItem[i];
-            row = row + ' <div class="col-md-12" style="padding: 5px"><div class="col-md-8"><div>' + objItem.SEGMENT1 + '</div><div>' + objItem.ITEM_DESCRIPTION + '</div></div><div class="col-md-2"><div style="margin-top: 10px">' + objItem.QUANTITY + '</div></div><div class="col-md-2"><div><input class="form-control changeQty" id="in_qty' + i + '" name="in_qty' + i + '" type="number" value="' + objItem.ISI + '"/></div></div></div>';
+            row = row + ' <div class="col-md-12" style="padding: 2px;border-bottom:1px solid black;"><div class="col-md-3"><div>' + objItem.SEGMENT1 + '</div><div>' + objItem.ITEM_DESCRIPTION + '</div></div><div class="col-md-1"><div style="margin-top: 10px"><center>' + objItem.QUANTITY + '</center></div></div><div class="col-md-1"><div style="margin-top: 10px"><center>' + objItem.QTY_RECEIPT + '</center></div></div><div class="col-md-2"><div style="margin-top: 10px"><center>' + objItem.BELUM_DELIVER + '</center></div></div><div class="col-md-2"><input type="text" id="ket_qty' + i + '" name="ket_qty' + i + '" > </div><div class="col-md-1"><input type="checkbox" value="fuck" class="adexe_qty'+i+'" id="adexe_qty'+i+'" name="adexe_qty'+i+'"> </div><div class="col-md-2"><input class="form-control changeQty" id="in_qty' + i + '" name="in_qty' + i + '" type="number"/></div></div>';
         }
         $("#div_root").html(row);
         $('.changeQty').change(function(e) {
@@ -184,7 +231,7 @@ $(document).ready(function() {
         });
     }
 
-    ////////////////////////PENGECEKAN
+    //////////////////////// PENGECEKAN
     $('#b_processSJ').click(function(){
          SJ = $('#in_sj').val();
          $.ajax({
@@ -204,28 +251,87 @@ $(document).ready(function() {
     });
 
     $('#b_update').click(function() {
-        var updateCount = 0;
+        console.log('januck');
+        var updateCount = 0
         for(i=0;i<listItem.length;i++){
             var objItem = listItem[i];
-
-            $.ajax({
-                url: baseurl + "PenerimaanPO/C_Pengecekan/updateData",
-                type:"POST",
-                data: jQuery.param({
-                    qtyActual : objItem.QTY_ACTUAL,
-                    SJ        : SJ,
-                    itemName  : objItem.ITEM_NAME,
-                    itemDesc  : objItem.ITEM_DESCRIPTION
-                }),
-                success:function(data){
-                    if(++updateCount==listItem.length){
-                        $.toaster('Data Updated!', 'Update', 'success');
-                        location.reload();
+                $.ajax({
+                    url: baseurl + "PenerimaanPO/C_Pengecekan/updateData",
+                    type:"POST",
+                    data: jQuery.param({
+                        qtyActual : objItem.QTY_ACTUAL,
+                        SJ        : SJ,
+                        itemName  : objItem.ITEM_NAME,
+                        itemDesc  : objItem.ITEM_DESCRIPTION
+                    }),
+                    success:function(res){
+                        console.log(res);   
+                      if(++updateCount==listItem.length){
+                            $.toaster('Data Updated!', 'Update', 'success');
+                            runAPIone(SJ);
+                        }
                     }
-                }
-            })
+                });
+          
         }
     });
+
+
+    function runAPIone(){
+        $.ajax({
+                url: baseurl + "PenerimaanPO/C_Pengecekan/runAPIone",
+                type:"POST",
+                data: jQuery.param({
+                    SJ : SJ
+                }),
+                success:function(res){
+                    console.log(res);
+                    runAPItwo(res);
+                }
+        });
+    }
+
+    function runAPItwo(id){
+        $.ajax({
+                url: baseurl + "PenerimaanPO/C_Pengecekan/runAPItwo",
+                type:"POST",
+                data: jQuery.param({
+                    GROUP_ID : id,
+                    SJ : SJ
+                }),
+                success:function(res){
+                    console.log(res);
+                    deleteAll(res);
+                }
+        });
+    }
+
+    function deleteAll(res){
+            $.ajax({
+                url: baseurl + "PenerimaanPO/C_Pengecekan/deleteAll",
+                type:"POST",
+                data: jQuery.param({
+                   IP:res 
+                }),
+                success:function(adexe){
+                    console.log(res);   
+                    res = res.replace('"','');
+                    res = res.replace('"','');
+                    window.open("http://192.168.168.102/print_pdf_prod/print_letter_ai.php?id="+res,'_blank');
+                    console.log(adexe);
+                }
+        });
+    }
+
+    function fuckedCheck(i){
+        if($('#chk_qty'+i).checked){
+            return true;
+        }
+        return false;
+    }
+
+
+
 //PO    VENDOR  TO_CHAR(RECEIPT_DATE,'DDMONYYYY')   TO_CHAR(SP_DATE,'DDMONYYYY')    ITEM_NAME   ITEM_DESCRIPTION    QTY_SJ  QTY_ACTUAL
     function showChangePengecekan() {
         var row = '';
@@ -245,4 +351,4 @@ $(document).ready(function() {
             listItem.splice(index,1,objItem);        
         });
     }
-});
+}); 
