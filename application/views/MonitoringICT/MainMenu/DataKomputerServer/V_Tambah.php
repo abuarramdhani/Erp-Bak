4871dff9fc3b865a9a3f8dd7a5a62017bd803e68
enggal aldiansyah <enggal_aldiansyah@quick.com>
2018-10-09 14:50:02 +0700

    Monitoring ICT 09/10/2018
    update :
    - ditambahkannya data validation pada input data komputer server

--- application/views/MonitoringICT/MainMenu/DataKomputerServer/V_Tambah.php
+++ application/views/MonitoringICT/MainMenu/DataKomputerServer/V_Tambah.php
@@ -25,57 +25,49 @@
 						</div>
 					</div>
 					<br />
-					<center>
-						<di12>
-							<div style="position: center; margin: auto;" align="center" class="col-lg-12">
-								<div class="box box-primary box-solid">
-									<div class="box-header with-border">
-										Tambah Data Server
-									</div>
-									<div class="box-body">
-										<div class="col-lg-12">
-											<center>
-												<form method="post" action="<?php echo site_url('MonitoringICT/DataServer/add'); ?>">
-													<div style="margin-top: 50px" class="col-lg-10 col-lg-offset-3">
-														<div class="form-group">
-															<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-2 control-label">Hostname</label>
-															<div class="col-lg-4">
-																<input required type="text" name="txthostname" class="form-control" id="monitoringict-hostname" placeholder="hostname.com">
-															</div>
-														</div> 
-													</div>
-													<div style="margin-top: 20px" class="col-lg-10 col-lg-offset-3">
-														<div class="form-group">
-															<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-2 control-label">IP Address &nbsp<i title="Contoh IP Address : 192.168.168.128" style="align-items: left" class="icon-question-sign"></i></label>
-															<div class="col-lg-4">
-																<input required name="txtipaddress" class="form-control" id="monitoringict-ipaddress" placeholder="xxx.xxx.xxx.xxx">
-																<span id="result" class="result"></span> 
-															</div>
-														</div> 
-													</div>
-													<div style="margin-top: 20px" class="col-lg-10 col-lg-offset-3">
-														<div class="form-group">
-															<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-2 control-label">Lokasi</label>
-															<div class="col-lg-4">
-																<select required name="txtlokasi" class="form-control ict-lokasi select2" id="monitoringict-lokasi" style="width: 100%">
-                                                                	<option value=""></option>
-                                                           	 </select>
-															</div>
-														</div> 
-													</div>
-													<div class="row col-lg-3 col-lg-offset-6">
-														<button  style="margin-top: 50px" type="submit" class="btn btn-primary btn-md btn-rect">
-														Simpan
-														</button>
-													</div>
-												</form>
-											</center>
-										</div>
+					<div>
+						<div align="center" class="col-lg-12">
+							<div class="box box-primary box-solid">
+								<div class="box-header with-border">
+									Tambah Data Server
+								</div>
+								<div class="box-body">
+									<div class="col-lg-12">
+										<form class="form-horizontal" method="post" action="<?php echo site_url('MonitoringICT/DataServer/add'); ?>">
+											<div class="form-group">
+												<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-4 control-label">Hostname</label>
+												<div data-toggle="tooltip" data-placement="top" id="ict-host" class="col-lg-4 has-feedback">
+													<input required type="text" name="txthostname" class="form-control" id="monitoringict-hostname" placeholder="hostname.com" pattern="^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$">
+													<span id="ict-host-span" class="glyphicon  form-control-feedback"></span>
+												</div>
+											</div> 
+											<div class="form-group">
+												<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-4 control-label">IP Address &nbsp<i title="Contoh IP Address : 192.168.168.128" style="align-items: left" class="icon-question-sign" data-toggle="tooltip" data-placement="left"></i></label>
+												<div data-toggle="tooltip" data-placement="top" id="ict-stat1" class="col-lg-4 has-feedback">
+													<input required name="txtipaddress" pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$" class="form-control" id="monitoringict-ipaddress" placeholder="xxx.xxx.xxx.xxx">
+													<span id="ict-stat2" class="glyphicon  form-control-feedback"></span>
+												</div>
+											</div>
+											<div class="form-group">
+												<label style="margin-top: 5px" for="txtKodesieLama" class="col-lg-4 control-label">Lokasi</label>
+												<div class="col-lg-4">
+													<select required name="txtlokasi" class="form-control ict-lokasi select2" id="monitoringict-lokasi" style="width: 100%">
+														<option value=""></option>
+													</select>
+												</div>
+											</div> 
+											<div class="row col-lg-3 col-lg-offset-6">
+											<!-- <input type="button" class="btn btn-primary" onclick="window.history.back(-1);">back</input> -->
+												<button  style="margin-top: 50px" type="submit" class="btn btn-primary btn-md btn-rect">
+													Simpan
+												</button>
+											</div>
+										</form>
 									</div>
 								</div>
 							</div>
 						</div>
-					</center>
+					</div>
 				</div>
 			</div>
 		</div>
