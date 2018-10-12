d0f4d03ffbd959ee46346c957c491085185da509
bagus <bagus_pria@quick.com>
2018-10-12 08:36:23 +0700

    edit routes

--- application/views/V_Footer.php
+++ application/views/V_Footer.php
@@ -120,9 +120,10 @@
 	<script src="<?php echo base_url('assets/js/customLKKK.js');?>" type="text/javascript"></script>
 	<script src="<?php echo base_url('assets/js/customWH.js');?>" type="text/javascript"></script>
 	<script src="<?php echo base_url('assets/js/customWMS.js');?>" type="text/javascript"></script>
-	<script src="<?php echo base_url('assets/js/customMI.js');?>" type="text/javascript"></script>
+	<script src="<?php echo base_url('assets/js/customHLCM.js');?>" type="text/javascript"></script>
+    	<script src="<?php echo base_url('assets/js/customMI.js');?>" type="text/javascript"></script>
 	<script src="<?php echo base_url('assets/js/customSI.js');?>" type="text/javascript"></script>
-    <script type="text/javascript">
+<script type="text/javascript">
 		if(counter_row <= 0){
 			var counter_row = 0;
 		}
