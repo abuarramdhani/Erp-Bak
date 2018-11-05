a68c78f080c36c6d0d3bee5760baea6667742f72
ndaru <kuswandaru@quick.com>
2018-11-05 16:30:27 +0700

    update (05/11/2018)

--- application/models/SystemIntegration/M_report.php
+++ application/models/SystemIntegration/M_report.php
@@ -38,30 +38,37 @@ class M_report extends CI_Model
 
 	function getMember($id)
 		{
+			$id = substr($id, 0, 7);
+			$date =  date('m');
 			$sql = "SELECT visu.user_name no_induk, visu.section_name SEKSI , visu.employee_name ,
 						(SELECT COUNT(*) 
 							FROM si.si_kaizen kai 
 						 WHERE kai.noinduk = visu.user_name
+						 AND EXTRACT(MONTH FROM created_date) = $date 
 						 AND kai.status = 3) kaizen_approve_ide,
 						(SELECT COUNT(*) 
 							FROM si.si_kaizen kai 
 						 WHERE kai.noinduk = visu.user_name
+						 AND EXTRACT(MONTH FROM created_date) = $date 
 						 AND kai.status = 9) kaizen_reported,
 						(SELECT COUNT(*) 
 							FROM si.si_kaizen kai 
 						 WHERE kai.noinduk = visu.user_name
+						 AND EXTRACT(MONTH FROM created_date) = $date 
 						 AND (kai.status = 4 OR kai.status = 5)) kaizen_reject_revisi,
 						(SELECT COUNT(*) 
 							FROM si.si_kaizen kai 
 						 WHERE kai.noinduk = visu.user_name
+						 AND EXTRACT(MONTH FROM created_date) = $date 
 						 AND kai.status = 2) kaizen_unchecked,
 						 (SELECT COUNT(*) 
 							FROM si.si_kaizen kai 
 						 WHERE kai.noinduk = visu.user_name
+						 AND EXTRACT(MONTH FROM created_date) = $date 
 						 AND (kai.status = 0 OR kai.status = 1)) kaizen_created
 				FROM sys.vi_sys_user_data visu 
 				INNER JOIN er.er_employee_all emp ON emp.employee_code = visu.user_name
-				WHERE emp.section_code = '$id' AND emp.resign = 0";
+				WHERE emp.section_code like '$id%' AND emp.resign = 0";
 			$query = $this->db->query($sql);
 			return $query->result_array();
 		}
