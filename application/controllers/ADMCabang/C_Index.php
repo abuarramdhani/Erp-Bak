eb5864740c236f909c733485135ff6aaaf052ebd
aji <aji_kurniawan@quick.com>
2018-11-08 07:29:36 +0700

    upload ADM Cabang

---
+++ application/controllers/ADMCabang/C_Index.php
@@ -0,0 +1,53 @@
+<?php
+Defined('BASEPATH') or exit('No Direct Sekrip Akses Allowed');
+/**
+ * 
+ */
+class C_Index extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->helper('html');
+		$this->load->helper('file');
+
+		$this->load->library('form_validation');
+		$this->load->library('session');
+		$this->load->library('encrypt');
+
+		$this->load->model('SystemAdministration/MainMenu/M_user');
+
+		$this->checkSession();
+	}
+
+	public function checkSession()
+	{
+		if($this->session->is_logged){
+
+		} else {
+			redirect('index');
+		}
+	}
+
+	public function index(){
+		$user_id = $this->session->userid;
+
+		$data['Title'] = 'ADM Cabang';
+		$data['Menu'] = '';
+		$data['SubMenuOne'] = '';
+		$data['SubMenuTwo'] = '';
+
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+
+		$this->load->view('V_Header',$data);
+		$this->load->view('V_Sidemenu',$data);
+		$this->load->view('ADMCabang/V_index',$data);
+		$this->load->view('V_Footer',$data);
+	}
+}
+?>
---
+++ application/models/ADMCabang/M_presensiharian.php
@@ -0,0 +1,87 @@
+<?php
+Defined('BASEPATH') or exit('No Direct Script Access Allowed');
+/**
+ * 
+ */
+class M_presensiharian extends Ci_Model
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->personalia = $this->load->database('personalia',true);
+	}
+
+	public function getSeksiByKodesie($kd){
+		$sql = "select kodesie,seksi from hrd_khs.tseksi where kodesie = '$kd'";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+
+	public function getPekerjaByKodesie($kd){
+		$sql = "select noind,nama 
+				from hrd_khs.tpribadi 
+				where left(kodesie,7) = left('$kd',7) 
+				and keluar = false
+				order by noind;";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+
+	public function getPresensiByNoind($noind,$tgl){
+		$sql = "select waktu
+				from \"FrontPresensi\".tpresensi tp
+				where tp.noind = '$noind' 
+				and tp.tanggal = '$tgl'
+				order by tp.waktu";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+
+	public function getTIMByNoind($noind,$tgl){
+		$sql = "select sum(point) point
+				from \"Presensi\".tdatatim 
+				where noind = '$noind' 
+				and tanggal = '$tgl'";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+
+	public function getKeteranganByNoind($noind,$tgl){
+		$sql = "select tk.keterangan
+				from (
+					select kd_ket
+					from \"Presensi\".tdatapresensi 
+					where noind = '$noind' 
+					and tanggal = '$tgl'
+					and kd_ket != 'PKJ'
+					union
+					select kd_ket
+					from \"Presensi\".tdatatim 
+					where noind = '$noind' 
+					and tanggal = '$tgl'
+				) as tp 
+				inner join \"Presensi\".tketerangan tk on tp.kd_ket = tk.kd_ket";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+
+	public function getShiftByNoind($noind,$tgl){
+		$tanggal = explode(" - ", $tgl);
+		$tgl1 = $tanggal[0];
+		$tgl2 = $tanggal[1];
+		$sql = "select tsp.tanggal,
+				to_char(tsp.tanggal,'dd/mm/yyyy') tgl,
+				ts.shift 
+				from \"Presensi\".tshiftpekerja tsp
+				inner join \"Presensi\".tshift ts 
+					on ts.kd_shift = tsp.kd_shift
+				where tsp.noind = '$noind' 
+				and tsp.tanggal between '$tgl1' and '$tgl2'
+				order by tsp.tanggal";
+		$result = $this->personalia->query($sql);
+		return $result->result_array();
+	}
+}
+?>
