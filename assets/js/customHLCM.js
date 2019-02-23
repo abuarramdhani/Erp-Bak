096069999b54b0cff45e8bfb36e6e8bc7a9817be
muhammad_nurwidya_ardiansyah <muhammad_nurwidya_ardiansyah@quick.com>
2019-02-23 11:01:59 +0700

    23 februari 2019 --> HLCM .js

--- assets/js/customHLCM.js
+++ assets/js/customHLCM.js
@@ -272,48 +272,48 @@ $(function () {
 	  console.log("New date range selected: ' + start.format('DD-MM-YYYY H:i:s') + ' to ' + end.format('DD-MM-YYYY H:i:s') + ' (predefined range: ' + label + ')");
 	});
 
-	// $('.KecelakaanKerja-daterangepickersingledate').daterangepicker({
-	// 	"singleDatePicker": true,
-	// 	"showDropdowns": true,
-	// 	"autoApply": true,
-	// 	"mask": true,
-	// 	"locale": {
-	// 	    "format": "YYYY-MM-DD",
-	// 	    "separator": " - ",
-	// 	    "applyLabel": "OK",
-	// 	    "cancelLabel": "Batal",
-	// 	    "fromLabel": "Dari",
-	// 	    "toLabel": "Hingga",
-	// 	    "customRangeLabel": "Custom",
-	// 	    "weekLabel": "W",
-	// 	    "daysOfWeek": [
-	// 	        "Mg",
-	// 	        "Sn",
-	// 	        "Sl",
-	// 	        "Rb",
-	// 	        "Km",
-	// 	        "Jm",
-	// 	        "Sa"
-	// 	    ],
-	// 	    "monthNames": [
-	// 	        "Januari",
-	// 	        "Februari",
-	// 	        "Maret",
-	// 	        "April",
-	// 	        "Mei",
-	// 	        "Juni",
-	// 	        "Juli",
-	// 	        "Agustus ",
-	// 	        "September",
-	// 	        "Oktober",
-	// 	        "November",
-	// 	        "Desember"
-	// 	    ],
-	// 	    "firstDay": 1
-	// 	}
-	// }, function(start, end, label) {
-	//   console.log("New date range selected: ' + start.format('DD-MM-YYYY H:i:s') + ' to ' + end.format('DD-MM-YYYY H:i:s') + ' (predefined range: ' + label + ')");
-	// });
+	$('.prosesgaji-daterangepickersingledate').daterangepicker({
+		"singleDatePicker": true,
+		"showDropdowns": true,
+		"autoApply": true,
+		"mask": true,
+		"locale": {
+		    "format": "YYYY-MM-DD",
+		    "separator": " - ",
+		    "applyLabel": "OK",
+		    "cancelLabel": "Batal",
+		    "fromLabel": "Dari",
+		    "toLabel": "Hingga",
+		    "customRangeLabel": "Custom",
+		    "weekLabel": "W",
+		    "daysOfWeek": [
+		        "Mg",
+		        "Sn",
+		        "Sl",
+		        "Rb",
+		        "Km",
+		        "Jm",
+		        "Sa"
+		    ],
+		    "monthNames": [
+		        "Januari",
+		        "Februari",
+		        "Maret",
+		        "April",
+		        "Mei",
+		        "Juni",
+		        "Juli",
+		        "Agustus ",
+		        "September",
+		        "Oktober",
+		        "November",
+		        "Desember"
+		    ],
+		    "firstDay": 1
+		}
+	}, function(start, end, label) {
+	  console.log("New date range selected: ' + start.format('DD-MM-YYYY H:i:s') + ' to ' + end.format('DD-MM-YYYY H:i:s') + ' (predefined range: ' + label + ')");
+	});
 
 	// $('.KecelakaanKerja-daterangepickersingledatewithtime').daterangepicker({
 	//     "timePicker": true,
