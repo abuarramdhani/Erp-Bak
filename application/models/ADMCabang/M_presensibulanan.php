7d6e2abafa67a89877566e67a6dbd5e7daf7f82e
Khoerul Amri <khoerul_amri@quick.com>
2019-03-01 07:37:46 +0700

    Update M_presensibulanan.php

--- application/models/ADMCabang/M_presensibulanan.php
+++ application/models/ADMCabang/M_presensibulanan.php
@@ -22,7 +22,7 @@ class M_presensibulanan extends Ci_Model
 	public function getPekerjaByKodesie($kd){
 		$sql = "select noind,nama 
 				from hrd_khs.tpribadi 
-				where left(kodesie,6) = left('$kd',6) 
+				where left(kodesie,7) = left('$kd',7) 
 				and keluar = false
 				order by noind;";
 		$result = $this->personalia->query($sql);
