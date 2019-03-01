ce38f0e8a9ef7ab70280bed9a23b57818ad01b78
Khoerul Amri <khoerul_amri@quick.com>
2019-03-01 15:03:54 +0700

    Update M_presensiharian.php

--- application/models/ADMCabang/M_presensiharian.php
+++ application/models/ADMCabang/M_presensiharian.php
@@ -26,11 +26,22 @@ class M_presensiharian extends Ci_Model
 	}
 
 	public function getPekerjaByKodesie($kd){
+	    if('306030'==left('$kd',6))
+	    {
+	    $sql = "select noind,nama 
+				from hrd_khs.tpribadi 
+				where left(kodesie,6) = left('$kd',6) 
+				and keluar = false
+				order by noind;";    
+	    }
+	    else
+	    {
 		$sql = "select noind,nama 
 				from hrd_khs.tpribadi 
 				where left(kodesie,7) = left('$kd',7) 
 				and keluar = false
 				order by noind;";
+	    }
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
 	}
