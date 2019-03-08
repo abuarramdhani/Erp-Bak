896ec8e1b12adfbefe330ae211fb8b0bc8e6d2cd
muhammad_nurwidya_ardiansyah <muhammad_nurwidya_ardiansyah@quick.com>
2019-03-08 08:14:12 +0700

    8 Maret 2019 --> Presensi Harian pemasaran

--- application/models/ADMCabang/M_presensibulanan.php
+++ application/models/ADMCabang/M_presensibulanan.php
@@ -27,13 +27,13 @@ class M_presensibulanan extends Ci_Model
 	    		where (left(kodesie,6) = left('$kd',6) or noind in ('J1171','J7004'))
 	    		and keluar = false
 	    		order by kodesie,noind;";    
-	    }elseif($noind == 'B0370'){
-			 $sql = "select noind,nama 
-				from hrd_khs.tpribadi 
-				where (left(kodesie,6) = left('$kd',6) or noind in ('D1535','P0426'))
-				and keluar = false
-				order by kodesie,noind;";
-		}else{
+	    }elseif ($noind == 'B0370') {
+	    	 $sql = "select noind,nama 
+	    		from hrd_khs.tpribadi 
+	    		where (left(kodesie,6) = left('$kd',6) or noind in ('D1535','P0426'))
+	    		and keluar = false
+	    		order by kodesie,noind;";    
+	    } else{
 
 		    if('306030'==substr($kd,6))
 		    {
