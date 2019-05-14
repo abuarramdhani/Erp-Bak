2e5063506744accfac2a453cb2be73a7a9f5de73
ezra_lazuardy_w <ezra_lazuardy_w@quick.com>
2019-05-14 11:34:22 +0700

    Merge branch 'ezra' of http://git.quick.com/erp/khs-erp.git

--- application/controllers/SystemIntegration/MainMenu/ApprovalKaizen/C_ApprovalKaizen.php
+++ application/controllers/SystemIntegration/MainMenu/ApprovalKaizen/C_ApprovalKaizen.php
@@ -264,7 +264,7 @@ class C_ApprovalKaizen extends CI_Controller
 			if ($status) {
 				if ($level == 1 && (array_key_exists(2, $NoindApprover) === true)) {
 					$this->EmailAlert($NoindApprover[2], $kaizen_id, $status);
-					$this->sendPidgin($NoindApprover[2], $kaizen_id), $status;
+					$this->sendPidgin($NoindApprover[2], $kaizen_id, $status);
 					$updateReady = $this->M_approvalkaizen->updateReady(2, $kaizen_id, 1);
 				} else if ($level == 2 && (array_key_exists(3, $NoindApprover) === true)) {
 					$this->EmailAlert($NoindApprover[3], $kaizen_id, $status);
