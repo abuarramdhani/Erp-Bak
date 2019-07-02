da0b0a75859032b2e92d58dd3c25335c6b890369
Rosyidatun Nur Rohmah <rosyidatun_nur_r @quick.com>
2019-07-02 13:06:25 +0700

    02 Juli 2019
    Order #954281
    + Penambahan Hak Akses B0717 pada erp.quick.com (Presensi Harian dan Bulanan) untuk seksi Gudang Produksi dan Ekspedisi

--- application/models/ADMCabang/M_presensiharian.php
+++ application/models/ADMCabang/M_presensiharian.php
@@ -1,11 +1,11 @@
 <?php
 Defined('BASEPATH') or exit('No Direct Script Access Allowed');
 /**
- * 
+ *
  */
 class M_presensiharian extends Ci_Model
 {
-	
+
 	function __construct()
 	{
 		parent::__construct();
@@ -28,54 +28,61 @@ class M_presensiharian extends Ci_Model
 	public function getPekerjaByKodesie($kd){
 		$noind = $this->session->user;
 		if ($noind == 'B0380') { // ada di ticket
-			 $sql = "select a.noind,a.nama, b.seksi 
+			 $sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
 				where (left(a.kodesie,7) = left('$kd',7) or a.noind in ('J1171','J7004','L8001'))
 				and a.keluar = false
-				order by a.kodesie,a.noind;";    
+				order by a.kodesie,a.noind;";
 		}elseif ($noind == 'B0370') { //ada di ticket
-			 $sql = "select a.noind,a.nama, b.seksi 
+			 $sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
 				where (left(a.kodesie,7) = left('$kd',7) or a.noind in ('D1535','P0426'))
 				and a.keluar = false
-				order by a.kodesie,a.noind;";    
+				order by a.kodesie,a.noind;";
 		}elseif ($noind == 'H7726') { //Order #972784 (PENAMBAHAN AKSES BUKA PRESENSI DI PROGRAM ERP)
-	    	 $sql = "select a.noind,a.nama, b.seksi 
+	    	 $sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
 	    		where left(a.kodesie,5) = left('$kd',5)
 	    		and a.keluar = false
-				order by a.kodesie,a.noind;";    
+				order by a.kodesie,a.noind;";
+		}elseif ($noind == 'B0717') { //Order ##954281 (PERMOHONAN HAK AKSES DI PROGRAM ERP)
+	    	 $sql = "select a.noind,a.nama, b.seksi
+				from hrd_khs.tpribadi a
+				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
+	    		where left(a.kodesie,7) in ('3070103','3070104')
+	    		and a.keluar = false
+				order by a.kodesie,a.noind;";
 	    }elseif ($noind == 'J1378') { //Order #112817 (Pembuatan Login ERP)
-	    	 $sql = "select a.noind,a.nama, b.seksi 
+	    	 $sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
 	    		where left(a.kodesie,5) in ('10101','10102')
 	    		and a.keluar = false
-				order by a.kodesie,a.noind;";    
+				order by a.kodesie,a.noind;";
 	    }  else{
 			    if('306030'==substr($kd,0,6)) //ada diticket
 			    {
-			    $sql = "select a.noind,a.nama, b.seksi 
+			    $sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
-						where left(a.kodesie,6) = left('$kd',6) 
+						where left(a.kodesie,6) = left('$kd',6)
 						and a.keluar = false
-						order by a.kodesie,a.noind;";    
+						order by a.kodesie,a.noind;";
 			    }
 			    else
 			    {
-				$sql = "select a.noind,a.nama, b.seksi 
+				$sql = "select a.noind,a.nama, b.seksi
 				from hrd_khs.tpribadi a
 				left join hrd_khs.tseksi b on a.kodesie=b.kodesie
-						where left(a.kodesie,7) = left('$kd',7) 
+						where left(a.kodesie,7) = left('$kd',7)
 						and a.keluar = false
 						order by a.kodesie,a.noind;";
 			    }
 		}
-	   
+
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
 	}
@@ -85,7 +92,7 @@ class M_presensiharian extends Ci_Model
 		{
 			$sql = "select waktu
 				from \"Presensi\".tprs_shift2 tp
-				where tp.noind = '$noind' 
+				where tp.noind = '$noind'
 				and tp.tanggal = '$tgl'
 				order by tp.waktu";
 		}
@@ -93,10 +100,10 @@ class M_presensiharian extends Ci_Model
 		{
 			$sql = "select waktu
 				from \"FrontPresensi\".tpresensi tp
-				where tp.noind = '$noind' 
+				where tp.noind = '$noind'
 				and tp.tanggal = '$tgl'
 				order by tp.waktu";
-		}		
+		}
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
 	}
@@ -107,7 +114,7 @@ class M_presensiharian extends Ci_Model
 		$tgl2 = $tanggal[1];
 		$sql = "select tp.noind,tp.tanggal,tp.waktu
 				from \"FrontPresensi\".tpresensi tp
-				where tp.noind in ($noind) 
+				where tp.noind in ($noind)
 				and tp.tanggal between '$tgl1' and '$tgl2'
 				order by tp.noind,tp.tanggal,tp.waktu";
 		$result = $this->personalia->query($sql);
@@ -116,8 +123,8 @@ class M_presensiharian extends Ci_Model
 
 	public function getTIMByNoind($noind,$tgl){
 		$sql = "select sum(point) point
-				from \"Presensi\".tdatatim 
-				where noind = '$noind' 
+				from \"Presensi\".tdatatim
+				where noind = '$noind'
 				and tanggal = '$tgl'";
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
@@ -128,8 +135,8 @@ class M_presensiharian extends Ci_Model
 		$tgl1 = $tanggal[0];
 		$tgl2 = $tanggal[1];
 		$sql = "select noind,tanggal,point
-				from \"Presensi\".tdatatim 
-				where noind in ($noind) 
+				from \"Presensi\".tdatatim
+				where noind in ($noind)
 				and tanggal between '$tgl1' and '$tgl2'
 				order by noind,tanggal";
 		$result = $this->personalia->query($sql);
@@ -140,16 +147,16 @@ class M_presensiharian extends Ci_Model
 		$sql = "select tk.keterangan
 				from (
 					select kd_ket
-					from \"Presensi\".tdatapresensi 
-					where noind = '$noind' 
+					from \"Presensi\".tdatapresensi
+					where noind = '$noind'
 					and tanggal = '$tgl'
 					and kd_ket != 'PKJ'
 					union
 					select kd_ket
-					from \"Presensi\".tdatatim 
-					where noind = '$noind' 
+					from \"Presensi\".tdatatim
+					where noind = '$noind'
 					and tanggal = '$tgl'
-				) as tp 
+				) as tp
 				inner join \"Presensi\".tketerangan tk on tp.kd_ket = tk.kd_ket";
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
@@ -162,16 +169,16 @@ class M_presensiharian extends Ci_Model
 		$sql = "select tp.noind,tp.tanggal,tk.keterangan
 				from (
 					select noind,tanggal,kd_ket
-					from \"Presensi\".tdatapresensi 
-					where noind in ($noind) 
+					from \"Presensi\".tdatapresensi
+					where noind in ($noind)
 					and tanggal between '$tgl1' and '$tgl2'
 					and kd_ket != 'PKJ'
 					union
 					select noind,tanggal,kd_ket
-					from \"Presensi\".tdatatim 
+					from \"Presensi\".tdatatim
 					where noind in ($noind)
 					and tanggal between '$tgl1' and '$tgl2'
-				) as tp 
+				) as tp
 				inner join \"Presensi\".tketerangan tk on tp.kd_ket = tk.kd_ket";
 		$result = $this->personalia->query($sql);
 		return $result->result_array();
@@ -185,11 +192,11 @@ class M_presensiharian extends Ci_Model
 				(
 					select tsp.tanggal,
 					to_char(tsp.tanggal,'dd/mm/yyyy') tgl,
-					ts.shift 
+					ts.shift
 					from \"Presensi\".tshiftpekerja tsp
-					inner join \"Presensi\".tshift ts 
+					inner join \"Presensi\".tshift ts
 						on ts.kd_shift = tsp.kd_shift
-					where tsp.noind = '$noind' 
+					where tsp.noind = '$noind'
 					and tsp.tanggal between '$tgl1' and '$tgl2'
 					union
 					select cast(dates as date) , to_char(dates,'dd/mm/yyyy') tgl, null
@@ -197,7 +204,7 @@ class M_presensiharian extends Ci_Model
 					where dates not in(
 									select tsp.tanggal
 									from \"Presensi\".tshiftpekerja tsp
-									where tsp.noind = '$noind' 
+									where tsp.noind = '$noind'
 									and tsp.tanggal between '$tgl1' and '$tgl2'
 										)
 				) as shift
@@ -214,11 +221,11 @@ class M_presensiharian extends Ci_Model
 				(
 					select tsp.noind,tsp.tanggal,
 					to_char(tsp.tanggal,'dd/mm/yyyy') tgl,
-					ts.shift 
+					ts.shift
 					from \"Presensi\".tshiftpekerja tsp
-					inner join \"Presensi\".tshift ts 
+					inner join \"Presensi\".tshift ts
 						on ts.kd_shift = tsp.kd_shift
-					where tsp.noind in ($noind) 
+					where tsp.noind in ($noind)
 					and tsp.tanggal between '$tgl1' and '$tgl2'
 				) as shift
 				order by shift.noind,shift.tanggal";
@@ -226,6 +233,6 @@ class M_presensiharian extends Ci_Model
 		return $result->result_array();
 	}
 
-	
+
 }
 ?>
