4620b3d4e27ba97df2a027d2a7394706cd80d1e8
enggal aldiansyah <enggal_aldiansyah@quick.com>
2019-08-10 09:59:00 +0700

    ADM Seleksi 10/08/2019
    update :
    - fix bug customAS.js

--- assets/js/customAS.js
+++ assets/js/customAS.js
@@ -1,23 +1,3 @@
-function setInputFilter(textbox, inputFilter) {
-  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
-    textbox.addEventListener(event, function() {
-      if (inputFilter(this.value)) {
-        this.oldValue = this.value;
-        this.oldSelectionStart = this.selectionStart;
-        this.oldSelectionEnd = this.selectionEnd;
-      } else if (this.hasOwnProperty("oldValue")) {
-        this.value = this.oldValue;
-        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-      }
-    });
-  });
-}
-//plugin dari https://jsfiddle.net/emkey08/zgvtjc51
-
-// Install input filters.
-setInputFilter(document.getElementById("input_KTP"), function(value) {
-  return /^\d*$/.test(value); });
-
 $(document).ready(function(){
   $('.bio_tempat_lahir').select2({
     placeholder: "Tempat Lahir",
