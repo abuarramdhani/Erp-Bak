8eaaea791ea3470cad83be93847feff00688b871
Ezra Lazuardy <ezra_lazuardy_w@quick.com>
2019-08-13 15:18:57 +0700

    Fix bug customGRR

--- assets/js/customGRR.js
+++ assets/js/customGRR.js
@@ -1,19 +1,19 @@
 const loading = {
-	showInButton: async (buttonIcon, icon) => {
-		buttonIcon.parentNode.disabled = true;
-		buttonIcon.classList.remove(icon);
-		buttonIcon.classList.add('fa-spin', 'fa-spinner');
-	},
-	hideInButton: async (buttonIcon, icon) => {
-		buttonIcon.classList.remove('fa-spinner', 'fa-spin');
-		buttonIcon.classList.add(icon);
-		buttonIcon.parentNode.disabled = false;
-	}
+    showInButton: function(buttonIcon, icon) {
+        buttonIcon.parentNode.disabled = true;
+        buttonIcon.classList.remove(icon);
+        buttonIcon.classList.add('fa-spin', 'fa-spinner');
+    },
+    hideInButton: function(buttonIcon, icon) {
+        buttonIcon.classList.remove('fa-spinner', 'fa-spin');
+        buttonIcon.classList.add(icon);
+        buttonIcon.parentNode.disabled = false;
+    }
 };
 
-$(document).ready(function(){
-	$('#sdm_select_tahun').select2({
-		allowClear: false,
-		placeholder: "Pilih Tahun",
-	});
+$(document).ready(function() {
+    $('#sdm_select_tahun').select2({
+        allowClear: false,
+        placeholder: "Pilih Tahun",
+    });
 });
