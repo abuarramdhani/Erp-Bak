f34e3baa59e51c18dc8656037f62363bbf6e79e9
Ezra Lazuardy Wijaya <ezra_lazuardy_w@quick.com>
2019-08-30 09:57:02 +0700

    Update V_Footer.php

--- application/views/V_Footer.php
+++ application/views/V_Footer.php
@@ -166,7 +166,6 @@
 		<script src="<?= base_url('assets/js/customMPR.js');?>"></script>
 		<script src="<?= base_url('assets/js/customMBI.js');?>"></script>
 		<script src="<?= base_url('assets/js/customPLO.js');?>"></script>
-		<script src="<?= base_url('assets/js/customSPS.js');?>"></script>
 		<script src="<?= base_url('assets/js/customSI.js') ?>"></script>
 		<!-- <script src="<?= base_url('assets/js/customMO.js') ?>"></script> -->
 		<!-- <script src="<?= base_url('assets/js/customPR.js') ?>"></script> -->
