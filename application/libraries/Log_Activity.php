655f095f5bff637569e2583773524ea6591a1286
Rosyidatun Nur Rohmah <rosyidatun_nur_r @quick.com>
2019-10-02 08:08:50 +0700

    Rabu, 02 Oktober 2019 -> Create Log Activity

---
+++ application/libraries/Log_Activity.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Log_Activity
+{
+
+  function __construct()
+  {
+     $this->CI = &get_instance();
+     date_default_timezone_set('Asia/Jakarta');
+     $this->CI->load->model('M_log_all_activity');
+  }
+
+  function activity_log($aksi, $detail){
+    $param['log_time']   = date('Y-m-d H:i:s');
+    $param['log_user']   = $this->CI->session->user;
+    $param['log_aksi']   = $aksi;
+    $param['log_detail'] = $detail;
+
+    //save to database
+    $this->CI->M_log_all_activity->save_log($param);
+  }
+}
+?>
---
+++ application/models/M_log_all_activity.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ *
+ */
+class M_log_all_activity extends CI_Model
+{
+
+  function __construct()
+  {
+    $this->load->database();
+  }
+
+  public function save_log($param)
+  {
+    return $this->db->insert('sys.sys_log_activity', $param);
+
+  }
+}
+
+ ?>
