7496a43a9a8d57add09f96614d98d3aa2fdf79cf
aji kurniawan <aji.kurniawan.29.01.2000@gmail.com>
2019-10-22 12:18:50 +0700

    update dbf
    menghilangkan separator

--- application/controllers/MasterPresensi/ReffGaji/C_TransferReffGaji.php
+++ application/controllers/MasterPresensi/ReffGaji/C_TransferReffGaji.php
@@ -663,8 +663,8 @@ class C_TransferReffGaji extends CI_Controller
 				$record->HR_UM =  $um_ ;
 				$record->HR_CATER = 0;
 				$record->P_BONSB = 0;
-				$record->P_I_KOP =  round($ds['pikop']);
-				// $record->P_I_KOP =  number_format(trim($ds['pikop']),2,'.',',');
+				// $record->P_I_KOP =  round($ds['pikop']);
+				$record->P_I_KOP =  number_format(trim($ds['pikop']),2,'.','');
 				$record->P_UT_KOP =  round($ds['putkop']) ;
 				$record->P_LAIN = round($ds['plain']);
 				$record->P_DUKA =  round($ds['pduka']) ;
