0b72451822c431fdec66862a94cb3a53ee9d506a
dicka_ismaji <dicka_ismaji@quick.com>
2019-12-07 11:24:39 +0700

    deploy

---
+++ application/controllers/PengirimanDokumen/Menu/C_Personalia.php
@@ -0,0 +1,280 @@
+<?php
+
+class C_Personalia extends CI_Controller
+{
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('M_Index');
+        $this->load->model('SystemAdministration/MainMenu/M_user');
+            
+        if ($this->session->userdata('logged_in')!=true) {
+            $this->load->helper('url');
+            $this->session->set_userdata('last_page', current_url());
+            $this->session->set_userdata('Responsbility', 'some_value');
+        }
+        $this->checkSession();
+
+        $user_id = $this->session->userid;
+        $this->load->model('PengirimanDokumen/M_inputdata');
+
+        $this->data['SubMenuOne'] = '';
+        $this->data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
+        $this->data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
+        $this->data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+        $this->data['Menu'] = 'Approve Personalia';
+    }
+
+    function checkSession()
+    {
+        if (!$this->session->is_logged) {
+            redirect();
+        }
+    }
+
+    function queryApproval($status, $level){ //ambyaar
+        $kodesie = substr($this->session->kodesie, 0, 7);
+
+        if($status == 'pending'){
+            if($level == 1){
+                $stat = '0';
+                $where = "ap.kodesie='$kodesie' and ap.tingkat='$level' and td.status='$stat'";
+            }else{
+                $stat = '1';
+                $where = "ap.kodesie='$kodesie' and ap.tingkat='$level' and tr.status='$stat' and td.status='$stat'";
+            }
+
+            $selecta = "SELECT td.id_data, td.noind, emp.employee_name nama, tm.keterangan, td.tanggal::date,tr.status, max(tr.tgl_update)::date tgl_update, ap.kodesie, td.alasan
+                        FROM ps.tdata td inner join ps.tappr ap on td.id_master = ap.id 
+                                    inner join ps.tmaster tm on tm.id = td.id_master
+                                    inner join ps.triwayat tr on tr.id_data = td.id_data
+                                    inner join er.er_employee_all emp on emp.employee_code = td.noind 
+                        WHERE $where
+                        GROUP BY td.id_data,td.noind, emp.employee_name, tm.keterangan, td.tanggal,tr.status, ap.kodesie, td.alasan
+            ;";
+        }else{
+            if ($status == 'approved') {
+                if ($level == 1) {
+                    $stat = '1';
+                } else {
+                    $stat = '3';
+                }
+            } else {
+                if ($level == 1) {
+                    $stat = '2';
+                } else {
+                    $stat = '4';
+                }
+            }
+
+            $selecta = "SELECT td.id_data, td.noind, emp.employee_name nama, tm.keterangan, td.tanggal::date,tr.status, max(tr.tgl_update)::date tgl_update, tr.seksi,td.alasan
+                        FROM ps.tdata td
+                        inner join ps.tmaster tm on tm.id = td.id_master 
+                        inner join ps.triwayat tr on tr.id_data = td.id_data 
+                        inner join er.er_employee_all emp on emp.employee_code = td.noind 
+                        WHERE tr.seksi='$kodesie' and tr.status = '$stat' and tr.level='$level' 
+                        GROUP BY td.id_data,td.noind, emp.employee_name, tm.keterangan, td.tanggal,tr.status, tr.seksi, td.alasan ;
+                        ";
+        }
+        // echo $selecta;die;
+        return $this->db->query($selecta);
+    }
+
+    function PersonaliaOne(){
+        //query pending 1,0
+        $this->data['pending'] = $this->queryApproval('pending',1)->num_rows();
+
+        //query approved 1,1
+        $this->data['approved'] = $this->queryApproval('approved',1)->num_rows();
+
+        //query rejected 1,2
+        $this->data['rejected'] = $this->queryApproval('rejected',1)->num_rows();
+
+        $this->data['lv'] = 'One';
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_PersonaliaHome', $this->data);
+        $this->load->view('V_Footer', $this->data);
+        
+    }
+
+    function PersonaliaTwo(){
+        //query pending 2,1
+        $this->data['pending'] = $this->queryApproval('pending',2)->num_rows();
+        
+        //query approved 2,3
+        $this->data['approved'] = $this->queryApproval('approved',2)->num_rows();
+        
+        //query rejected 2,4
+        $this->data['rejected'] = $this->queryApproval('rejected',2)->num_rows();
+
+        $this->data['lv'] = 'Two';
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_PersonaliaHome', $this->data);
+        $this->load->view('V_Footer', $this->data);
+    }
+
+    function NewData($level){
+        if($level == 1){
+            $data = $this->queryApproval('pending',1)->result_array();
+        }else{
+            $data = $this->queryApproval('pending',2)->result_array();
+        }
+
+        $this->data['table'] = $data;
+        $this->data['lv']    = $level;
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_NewData', $this->data);
+        $this->load->view('V_Footer', $this->data);
+    }
+
+    function ajaxNewdata(){
+        $level = $_GET['level'];
+        if($level == 1){
+            $data = $this->queryApproval('pending',1)->result_array();
+        }else{
+            $data = $this->queryApproval('pending',2)->result_array();
+        }
+
+        echo json_encode($data);
+    }
+
+    function Approved($level){
+        if($level == 1){
+            $data = $this->queryApproval('approved',1)->result_array();
+        }else{
+            $data = $this->queryApproval('approved',2)->result_array();
+        }
+
+        $this->data['table'] = $data;
+        $this->data['lv']    = $level;
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_Approved', $this->data);
+        $this->load->view('V_Footer', $this->data);
+    }
+
+    function Rejected($level){
+        if($level == 1){
+            $data = $this->queryApproval('rejected',1)->result_array();
+        }else{
+            $data = $this->queryApproval('rejected',2)->result_array();
+        }
+
+        $this->data['table'] = $data;
+        $this->data['lv']    = $level;
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_Rejected', $this->data);
+        $this->load->view('V_Footer', $this->data);
+    }
+
+    function ApprovalData(){
+        $idall  = $_POST['id_data'];
+        $user   = $this->session->user;
+        $level  = $_POST['level'];
+        $stat   = $_POST['stat'];
+
+        foreach($idall as $id):
+            if($stat == 'approve'){
+                $alasan = null;
+                if($level == 1){
+                    $status = 1;
+                }else{
+                    $status = 3;
+                }
+            }else{
+                $alasan = $_POST['alasan'];
+                if($level == 1){
+                    $status = 2;
+                }else{
+                    $status = 4;
+                }
+            }
+
+            //update tdata
+            $sql = "UPDATE ps.tdata set status='$status', alasan='$alasan' where id_data='$id'";
+            $this->db->query($sql);
+
+            //insert ke triwayat
+            $triwayat = array(
+                'id_data' => $id,
+                'status'  => $status,
+                'user'    => $user,
+                'level'   => $level,
+                'seksi'   => substr($this->session->kodesie,0,7)
+            );
+
+            $this->db->insert('ps.triwayat', $triwayat);
+        endforeach;
+    }
+
+    function rekapAll($periode=false, $kodedokumen=false, $kodeseksi=false){
+        $this->load->model('PengirimanDokumen/M_inputdata');
+        $this->load->model('PengirimanDokumen/M_masterdata');
+
+        $start = $end = date('Y-m-d');
+        $info_periode = date('Y/m/d');
+        if(isset($_GET['periode'])){
+            $range = explode(' - ', $_GET['periode']);
+            $start = date('Y/m/d', strtotime($range['0']));
+            $end   = date('Y/m/d', strtotime($range['1']));
+            $info_periode = $start." - ".$end;
+        }
+
+        $dokumen = '';
+        if(isset($_GET['doc']) && $_GET['doc'] != 'all'){
+            $kodedokumen = $_GET['doc'];
+            $dokumen = "and tm.id='$kodedokumen'";
+        }
+
+        $seksi = '';
+        if(isset($_GET['section']) && $_GET['section'] != 'all'){
+            $kodeseksi = $_GET['section'];
+            $seksi = "and substring(emp.section_code,0,8) = '$kodeseksi' ";
+        }
+
+        $selectRekap = 
+        "SELECT td.id_data, 
+                td.noind, 
+                emp.employee_name nama, 
+                substring(emp.section_code,0,8) kodesie, 
+                tm.id, 
+                tm.keterangan, 
+                td.status, 
+                td.tanggal::date, 
+                tr.seksi as approver
+        FROM ps.tdata td 
+            inner join er.er_employee_all emp on td.noind = emp.employee_code 
+            inner join ps.tmaster tm on td.id_master = tm.id 
+            inner join ps.triwayat tr on tr.id_data = td.id_data
+        WHERE tanggal between '$start' and '$end' 
+            and td.status not in ('0','2','4') 
+            and tr.level = (select max(level) from ps.triwayat where id_data = td.id_data) 
+        ORDER BY tanggal asc"; 
+        
+        $table = $this->db->query($selectRekap)->result_array();
+
+        for($i=0; $i < count($table); $i++){
+            $table[$i]['kodesie'] = $this->M_inputdata->getNameSeksi($table[$i]['kodesie']);
+            $table[$i]['status'] = 'Approved by '.$this->M_inputdata->getNameSeksi($table[$i]['approver']);;
+        }
+
+        $this->data['listDocument'] = $this->M_inputdata->ajaxListMaster();
+        $this->data['listSeksi']    = $this->M_masterdata->ajaxSeksi();
+        $this->data['table']        = $table;
+        $this->data['info_periode'] = $info_periode;
+
+        $this->load->view('V_Header', $this->data);
+		$this->load->view('V_Sidemenu',$this->data);
+        $this->load->view('PengirimanDokumen/Menu/Personalia/V_All', $this->data);
+        $this->load->view('V_Footer', $this->data);
+    }
+
+}
---
+++ application/views/PengirimanDokumen/Menu/Personalia/V_PersonaliaHome.php
@@ -0,0 +1,115 @@
+<style media="screen">
+  h8{
+    font-size: 2em;
+    font-weight: bold;
+  }
+
+  .small-box .icon{
+    display:inherit;
+  }
+
+  .badge{
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    /* padding: 6px 10px; */
+    border-radius: 50%;
+    transform: scale(1.5);
+    color: white;
+    transition: 0.4s;
+  }
+
+  .badge-green{
+    background-color: #00A65A;
+  }
+
+  .badge-red{
+    background-color: #DD4B39;
+  }
+
+  .badge-red-strong{
+    background-color: red;
+  }
+
+  .badge-orange{
+    background-color: #F39C12;
+  }
+
+  .badge:hover{
+    transform: scale(2);
+  }
+
+  .blinkanim{
+    animation: blink_anim 1.5s linear infinite;
+  }
+
+  @keyframes blink_anim {
+    0% {
+      opacity: 1;
+    }
+    10% {
+      opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    }
+
+  }
+  .width-33{
+      width: 33% !important;
+  }
+
+  @media screen and (max-width: 767px) {
+    .personal{
+      display:none;
+    }
+  }
+</style>
+<section class="content">
+    <div class="panel-body">
+        <div class="row">
+            <div class="box box-primary box-solid">
+                <div class="box-header with-border">
+                    <h2><b>Personalia <?= $lv?></b></h2>
+                </div>
+                <div class="box-body ">
+                    <div class="col-lg-3 col-xs-6 width-33">
+                        <div class="small-box bg-yellow">
+                        <div class="inner">
+                            <p>Data baru</p>
+                            <h8 class="<?php echo ($pending > 0)? 'blinkanim':'' ?>"><?=$pending?> Data</h8>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-clock-o"></i>
+                        </div>
+                        <a href="<?= site_url('PengirimanDokumen/Personalia'.$lv.'/new') ?>" class="small-box-footer">Info Lengkap <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6 width-33">
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <p>Disetujui</p>
+                            <h8><?=$approved?> Data</h8>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-check"></i>
+                        </div>
+                        <a href="<?= site_url('PengirimanDokumen/Personalia'.$lv.'/approved') ?>" class="small-box-footer">Info Lengkap <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6 width-33">
+                    <div class="small-box bg-red">
+                        <div class="inner">
+                            <p>Ditolak</p>
+                            <h8><?=$rejected?> Data</h8>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-close"></i>
+                        </div>
+                        <a href="<?= site_url('PengirimanDokumen/Personalia'.$lv.'/rejected') ?>" class="small-box-footer">Info Lengkap <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
