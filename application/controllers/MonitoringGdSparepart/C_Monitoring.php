5c47f4cbe0f53d570a70d7fea27bfdd082a6c6bf
Meyta <meyta_rizki_nugraheni@quick.com>
2019-12-09 12:39:08 +0700

    MGS gudang asal

--- application/controllers/MonitoringGdSparepart/C_Monitoring.php
+++ application/controllers/MonitoringGdSparepart/C_Monitoring.php
@@ -75,10 +75,23 @@ class C_Monitoring extends CI_Controller
 					$getBody = $this->M_monitoring->tampilbody($no_document[$a]);
 					if ($value['JENIS_DOKUMEN'] == 'IO') {
 						$getKet = $this->M_monitoring->getKet($no_document[$a]);
+						$gudang = $this->M_monitoring->gdAsalIO($no_document[$a]);
+						if (empty($gudang)) {
+							$gdAsal = '';
+						}else {
+							$gdAsal = $gudang[0]['GUDANG_ASAL'];
+						}
 					}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 						$getKet = $this->M_monitoring->getKetLPPB($no_document[$a]);
+						$gdAsal = '';
 					}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 						$getKet = $this->M_monitoring->getKetKIB($no_document[$a]);
+						$gudang = $this->M_monitoring->gdAsalKIB($no_document[$a]);
+						if (empty($gudang)) {
+							$gdAsal = '';
+						}else {
+							$gdAsal = $gudang[0]['SUBINVENTORY_CODE'];
+						}
 					}
 					$hitung_bd = count($getBody);
 					$hitung_ket = count($getKet);
@@ -93,10 +106,23 @@ class C_Monitoring extends CI_Controller
 					$getBody = $this->M_monitoring->tampilbody($no_document[$a]);
 					if ($value['JENIS_DOKUMEN'] == 'IO') {
 						$getKet = $this->M_monitoring->getKet($no_document[$a]);
+						$gudang = $this->M_monitoring->gdAsalIO($no_document[$a]);
+						if (empty($gudang)) {
+							$gdAsal = '';
+						}else {
+							$gdAsal = $gudang[0]['GUDANG_ASAL'];
+						}
 					}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 						$getKet = $this->M_monitoring->getKetLPPB($no_document[$a]);
+						$gdAsal = '';
 					}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 						$getKet = $this->M_monitoring->getKetKIB($no_document[$a]);
+						$gudang = $this->M_monitoring->gdAsalKIB($no_document[$a]);
+						if (empty($gudang)) {
+							$gdAsal = '';
+						}else {
+							$gdAsal = $gudang[0]['SUBINVENTORY_CODE'];
+						}
 					}
 					$hitung_bd = count($getBody);
 					$hitung_ket = count($getKet);
@@ -109,6 +135,7 @@ class C_Monitoring extends CI_Controller
 				}
 				$array_terkelompok[$value['NO_DOCUMENT']]['header'] = $value; 
 				$array_terkelompok[$value['NO_DOCUMENT']]['header']['statusket'] = $status; 
+				$array_terkelompok[$value['NO_DOCUMENT']]['header']['gd_asal'] = $gdAsal; 
 				$array_terkelompok[$value['NO_DOCUMENT']]['body'] = $getBody; 
 				}
 			}	
@@ -155,10 +182,23 @@ class C_Monitoring extends CI_Controller
 						$getBody = $this->M_monitoring->tampilbody($no_document[$a]);
 						if ($value['JENIS_DOKUMEN'] == 'IO') {
 							$getKet = $this->M_monitoring->getKet($no_document[$a]);
+							$gudang = $this->M_monitoring->gdAsalIO($no_document[$a]);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['GUDANG_ASAL'];
+							}
 						}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 							$getKet = $this->M_monitoring->getKetLPPB($no_document[$a]);
+							$gdAsal = '';
 						}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 							$getKet = $this->M_monitoring->getKetKIB($no_document[$a]);
+							$gudang = $this->M_monitoring->gdAsalKIB($no_document[$a]);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['SUBINVENTORY_CODE'];
+							}
 						}
 						$hitung_bd = count($getBody);
 						$hitung_ket = count($getKet);
@@ -172,10 +212,23 @@ class C_Monitoring extends CI_Controller
 						$getBody = $this->M_monitoring->tampilbody($no_document[$a]);
 						if ($value['JENIS_DOKUMEN'] == 'IO') {
 							$getKet = $this->M_monitoring->getKet($no_document[$a]);
+							$gudang = $this->M_monitoring->gdAsalIO($no_document[$a]);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['GUDANG_ASAL'];
+							}
 						}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 							$getKet = $this->M_monitoring->getKetLPPB($no_document[$a]);
+							$gdAsal = '';
 						}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 							$getKet = $this->M_monitoring->getKetKIB($no_document[$a]);
+							$gudang = $this->M_monitoring->gdAsalKIB($no_document[$a]);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['SUBINVENTORY_CODE'];
+							}
 						}
 						$hitung_bd = count($getBody);
 						$hitung_ket = count($getKet);
@@ -188,6 +241,7 @@ class C_Monitoring extends CI_Controller
 					}
 					$array_terkelompok[$value['NO_DOCUMENT']]['header'] = $value; 
 					$array_terkelompok[$value['NO_DOCUMENT']]['header']['statusket'] = $status; 
+					$array_terkelompok[$value['NO_DOCUMENT']]['header']['gd_asal'] = $gdAsal; 
 					$array_terkelompok[$value['NO_DOCUMENT']]['body'] = $getBody; 
 					}
 				}	
@@ -209,10 +263,23 @@ class C_Monitoring extends CI_Controller
 						$getBody = $dataGET;
 						if ($value['JENIS_DOKUMEN'] == 'IO') {
 							$getKet = $this->M_monitoring->getKet($no_document);
+							$gudang = $this->M_monitoring->gdAsalIO($no_document);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['GUDANG_ASAL'];
+							}
 						}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 							$getKet = $this->M_monitoring->getKetLPPB($no_document);
+							$gdAsal = '';
 						}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 							$getKet = $this->M_monitoring->getKetKIB($no_document);
+							$gudang = $this->M_monitoring->gdAsalKIB($no_document);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang[0]['SUBINVENTORY_CODE'];
+							}
 						}
 						$hitung_bd = count($getBody);
 						$hitung_ket = count($getKet);
@@ -225,10 +292,24 @@ class C_Monitoring extends CI_Controller
 						$getBody = $this->M_monitoring->getSearch($value['NO_DOCUMENT'], $jenis_dokumen,  $tglAwal, $tglAkhir, $pic, $item);
 						if ($value['JENIS_DOKUMEN'] == 'IO') {
 							$getKet = $this->M_monitoring->getKet($value['NO_DOCUMENT']);
+							$gudang = $this->M_monitoring->gdAsalIO($value['NO_DOCUMENT']);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang['0']['GUDANG_ASAL'];
+							}
 						}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
 							$getKet = $this->M_monitoring->getKetLPPB($value['NO_DOCUMENT']);
+							$gdAsal = '';
 						}else if($value['JENIS_DOKUMEN'] == 'KIB'){
 							$getKet = $this->M_monitoring->getKetKIB($value['NO_DOCUMENT']);
+							$gudang = $this->M_monitoring->gdAsalKIB($value['NO_DOCUMENT']);
+							if (empty($gudang)) {
+								$gdAsal = '';
+							}else {
+								$gdAsal = $gudang['0']['SUBINVENTORY_CODE'];
+							}
+							
 						}
 						$hitung_bd = count($getBody);
 						$hitung_ket = count($getKet);
@@ -240,63 +321,9 @@ class C_Monitoring extends CI_Controller
 					}
 					$array_terkelompok[$value['NO_DOCUMENT']]['header'] = $value; 
 					$array_terkelompok[$value['NO_DOCUMENT']]['header']['statusket'] = $status; 
+					$array_terkelompok[$value['NO_DOCUMENT']]['header']['gd_asal'] = $gdAsal; 
 					$array_terkelompok[$value['NO_DOCUMENT']]['body'] = $getBody; 
 				}
-				// else{
-				// 	array_push($array_sudah, $value['NO_DOCUMENT']);	
-				// 	if ($search == 'tanggal') {
-				// 		$getBody = $this->M_monitoring->getBodyTanggal($value['NO_DOCUMENT'], $tglAwal, $tglAkhir);
-				// 		if ($value['JENIS_DOKUMEN'] == 'IO') {
-				// 			$getKet = $this->M_monitoring->getKet($value['NO_DOCUMENT']);
-				// 		}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
-				// 			$getKet = $this->M_monitoring->getKetLPPB($value['NO_DOCUMENT']);
-				// 		}else if($value['JENIS_DOKUMEN'] == 'KIB'){
-				// 			$getKet = $this->M_monitoring->getKetKIB($value['NO_DOCUMENT']);
-				// 		}
-				// 		$hitung_bd = count($getBody);
-				// 		$hitung_ket = count($getKet);
-				// 		if ($hitung_bd == $hitung_ket) {
-				// 			$status= 'Sudah terlayani';
-				// 		} else {
-				// 			$status = 'Belum terlayani';
-				// 		}
-				// 	}else if ($search == 'pic') {
-				// 		$getBody = $this->M_monitoring->getBodyPIC($value['NO_DOCUMENT'], $pic);
-				// 		if ($value['JENIS_DOKUMEN'] == 'IO') {
-				// 			$getKet = $this->M_monitoring->getKet($value['NO_DOCUMENT']);
-				// 		}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
-				// 			$getKet = $this->M_monitoring->getKetLPPB($value['NO_DOCUMENT']);
-				// 		}else if($value['JENIS_DOKUMEN'] == 'KIB'){
-				// 			$getKet = $this->M_monitoring->getKetKIB($value['NO_DOCUMENT']);
-				// 		}
-				// 		$hitung_bd = count($getBody);
-				// 		$hitung_ket = count($getKet);
-				// 		if ($hitung_bd == $hitung_ket) {
-				// 			$status= 'Sudah terlayani';
-				// 		} else {
-				// 			$status = 'Belum terlayani';
-				// 		}
-				// 	}else{
-				// 		$getBody = $this->M_monitoring->getSearch($value['NO_DOCUMENT'], $no_document, $jenis_dokumen, $tglAwal, $tglAkhir, $pic, $item);
-				// 		if ($value['JENIS_DOKUMEN'] == 'IO') {
-				// 			$getKet = $this->M_monitoring->getKet($value['NO_DOCUMENT']);
-				// 		}else if ($value['JENIS_DOKUMEN'] == 'LPPB') {
-				// 			$getKet = $this->M_monitoring->getKetLPPB($value['NO_DOCUMENT']);
-				// 		}else if($value['JENIS_DOKUMEN'] == 'KIB'){
-				// 			$getKet = $this->M_monitoring->getKetKIB($value['NO_DOCUMENT']);
-				// 		}
-				// 		$hitung_bd = count($getBody);
-				// 		$hitung_ket = count($getKet);
-				// 		if ($hitung_bd == $hitung_ket) {
-				// 			$status= 'Sudah terlayani';
-				// 		} else {
-				// 			$status = 'Belum terlayani';
-				// 		}
-				// 	}
-					// $array_terkelompok[$value['NO_DOCUMENT']]['header'] = $value; 
-					// $array_terkelompok[$value['NO_DOCUMENT']]['header']['statusket'] = $status; 
-					// $array_terkelompok[$value['NO_DOCUMENT']]['body'] = $getBody; 
-					// }
 				}
 			$data['value'] = $array_terkelompok;
 		}
@@ -338,6 +365,7 @@ class C_Monitoring extends CI_Controller
 			$satuan 	= $this->input->post('uom[]');
 			$jok 		= $this->input->post('qty_ok[]');
 			$not 		= $this->input->post('qty_not[]');
+			$asal 		= $this->input->post('gd_asal[]');
 			$ket 		= $this->input->post('ktrgn[]');
 
 			$dataMGS = array();
@@ -352,6 +380,7 @@ class C_Monitoring extends CI_Controller
 					'satuan' 		=> $satuan[$i],
 					'status_ok' 	=> $jok[$i],
 					'not_ok' 		=> $not[$i],
+					'asal' 			=> $asal[$i],
 					'ket' 			=> $ket[$i],
 				);
 				array_push($dataMGS, $array);
@@ -418,7 +447,7 @@ class C_Monitoring extends CI_Controller
 			);
 
 			$excel->setActiveSheetIndex(0)->setCellValue('A1', "Monitoring Gudang Sparepart"); 
-			$excel->getActiveSheet()->mergeCells('A1:K1'); 
+			$excel->getActiveSheet()->mergeCells('A1:L1'); 
 			$excel->getActiveSheet()->getStyle('A1')->applyFromArray($style_title);
 
 			$excel->setActiveSheetIndex(0)->setCellValue('A3', "NO.");
@@ -432,7 +461,8 @@ class C_Monitoring extends CI_Controller
 			$excel->setActiveSheetIndex(0)->setCellValue('I3', "STATUS");
 			$excel->setActiveSheetIndex(0)->setCellValue('I4', "OK");
 			$excel->setActiveSheetIndex(0)->setCellValue('J4', "NOT OK");
-			$excel->setActiveSheetIndex(0)->setCellValue('K3', "KETERANGAN");
+			$excel->setActiveSheetIndex(0)->setCellValue('K3', "ASAL");
+			$excel->setActiveSheetIndex(0)->setCellValue('L3', "KETERANGAN");
 			$excel->getActiveSheet()->mergeCells('A3:A4'); 
 			$excel->getActiveSheet()->mergeCells('B3:B4'); 
 			$excel->getActiveSheet()->mergeCells('C3:C4'); 
@@ -443,6 +473,7 @@ class C_Monitoring extends CI_Controller
 			$excel->getActiveSheet()->mergeCells('H3:H4'); 
 			$excel->getActiveSheet()->mergeCells('I3:J3'); 
 			$excel->getActiveSheet()->mergeCells('K3:K4'); 
+			$excel->getActiveSheet()->mergeCells('L3:L4'); 
 
 			$excel->getActiveSheet()->getStyle('A3')->applyFromArray($style_col);
 			$excel->getActiveSheet()->getStyle('A4')->applyFromArray($style_col);
@@ -466,6 +497,8 @@ class C_Monitoring extends CI_Controller
 			$excel->getActiveSheet()->getStyle('J4')->applyFromArray($style_col);
 			$excel->getActiveSheet()->getStyle('K3')->applyFromArray($style_col);
 			$excel->getActiveSheet()->getStyle('K4')->applyFromArray($style_col);
+			$excel->getActiveSheet()->getStyle('L3')->applyFromArray($style_col);
+			$excel->getActiveSheet()->getStyle('L4')->applyFromArray($style_col);
 
 			if (count($dataMGS) == 0){
 				$excel->setActiveSheetIndex(0)->setCellValue('A3');
@@ -478,6 +511,7 @@ class C_Monitoring extends CI_Controller
 				$excel->setActiveSheetIndex(0)->setCellValue('I3');
 				$excel->setActiveSheetIndex(0)->setCellValue('J3');
 				$excel->setActiveSheetIndex(0)->setCellValue('K3');
+				$excel->setActiveSheetIndex(0)->setCellValue('L3');
 
 				$excel->getActiveSheet()->getStyle('A3')->applyFromArray($style_col);
 				$excel->getActiveSheet()->getStyle('B3')->applyFromArray($style_col);
@@ -490,6 +524,7 @@ class C_Monitoring extends CI_Controller
 				$excel->getActiveSheet()->getStyle('I3')->applyFromArray($style_col);
 				$excel->getActiveSheet()->getStyle('J3')->applyFromArray($style_col);
 				$excel->getActiveSheet()->getStyle('K3')->applyFromArray($style_col);
+				$excel->getActiveSheet()->getStyle('L3')->applyFromArray($style_col);
 			}else {
 				$no=1;
 				$numrow = 5;
@@ -504,7 +539,8 @@ class C_Monitoring extends CI_Controller
 						$excel->setActiveSheetIndex(0)->setCellValue('H'.$numrow, $dM['satuan']);
 						$excel->setActiveSheetIndex(0)->setCellValue('I'.$numrow, $dM['status_ok']);
 						$excel->setActiveSheetIndex(0)->setCellValue('J'.$numrow, $dM['not_ok']);
-						$excel->setActiveSheetIndex(0)->setCellValue('K'.$numrow, $dM['ket']);
+						$excel->setActiveSheetIndex(0)->setCellValue('K'.$numrow, $dM['asal']);
+						$excel->setActiveSheetIndex(0)->setCellValue('L'.$numrow, $dM['ket']);
 
 						$excel->getActiveSheet()->getStyle('A'.$numrow)->applyFromArray($style2);
 						$excel->getActiveSheet()->getStyle('B'.$numrow)->applyFromArray($style2);
@@ -517,6 +553,7 @@ class C_Monitoring extends CI_Controller
 						$excel->getActiveSheet()->getStyle('I'.$numrow)->applyFromArray($style2);
 						$excel->getActiveSheet()->getStyle('J'.$numrow)->applyFromArray($style2);
 						$excel->getActiveSheet()->getStyle('K'.$numrow)->applyFromArray($style2);
+						$excel->getActiveSheet()->getStyle('L'.$numrow)->applyFromArray($style2);
 					$numrow++;
 					$no++; 
 					}
@@ -533,6 +570,7 @@ class C_Monitoring extends CI_Controller
 			$excel->getActiveSheet()->getColumnDimension('I')->setWidth(10);
 			$excel->getActiveSheet()->getColumnDimension('J')->setWidth(10);
 			$excel->getActiveSheet()->getColumnDimension('K')->setWidth(20);
+			$excel->getActiveSheet()->getColumnDimension('L')->setWidth(20);
 
 			// Set height semua kolom menjadi auto (mengikuti height isi dari kolommnya, jadi otomatis)
 			$excel->getActiveSheet()->getDefaultRowDimension()->setRowHeight(-1);
@@ -578,6 +616,15 @@ class C_Monitoring extends CI_Controller
 		$doc 	= $this->input->post('doc');
 		
 		$query = "set KETERANGAN = '$ket'";
+		$this->M_monitoring->dataUpdate($item, $query, $doc);
+	}
+
+	public function saveAction(){
+		$action = $this->input->post('action');
+		$item = $this->input->post('item');
+		$doc 	= $this->input->post('doc');
+		
+		$query = "set ACTION = '$action'";
 		$this->M_monitoring->dataUpdate($item, $query, $doc);
 	}
 	
--- application/models/MonitoringGdSparepart/M_monitoring.php
+++ application/models/MonitoringGdSparepart/M_monitoring.php
@@ -11,7 +11,7 @@ class M_monitoring extends CI_Model {
         $sql ="
                 SELECT NO_DOCUMENT, JENIS_DOKUMEN, ITEM, DESCRIPTION, 
                 UOM, QTY, CREATION_DATE, to_char(CREATION_DATE, 'hh24:mi:ss') as JAM_INPUT, 
-                STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN 
+                STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN, ACTION 
                 from KHS_MONITORING_GD_SP 
                 where creation_date BETWEEN TO_DATE( '$date', 'DD/MM/YYYY' ) AND TO_DATE( '$date2', 'DD/MM/YYYY' ) 
                 order by CREATION_DATE DESC";
@@ -24,7 +24,7 @@ class M_monitoring extends CI_Model {
     public function getSearch($no_document, $jenis_dokumen, $tglAwal, $tglAkhir, $pic, $item) {
         $oracle = $this->load->database('oracle', true);
         $no_document == null? $nodoku = '' : $nodoku = "and no_document = '$no_document'";
-        $pic == null ? $pic2 = '' : $pic2 = "and pic = '$pic'";
+        $pic == null ? $pic2 = '' : $pic2 = "and pic like '%$pic%'";
         $item == null ? $item2 = '' : $item2 = "and ITEM = '$item'";
         $jenis_dokumen == null ? $dokudoku = '' : $dokudoku= "and jenis_dokumen = '$jenis_dokumen'";
         if ($tglAkhir != null && $tglAwal != null) {
@@ -35,7 +35,7 @@ class M_monitoring extends CI_Model {
         $sql ="
                 SELECT NO_DOCUMENT, JENIS_DOKUMEN, ITEM, DESCRIPTION,
                         UOM, QTY, CREATION_DATE, to_char(CREATION_DATE, 'hh24:mi:ss') as JAM_INPUT,
-                        STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN 
+                        STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN, ACTION
                 from KHS_MONITORING_GD_SP
                 WHERE CREATION_DATE IS NOT NULL
                 $dokudoku $nodoku $tanggal $pic2 $item2
@@ -58,42 +58,6 @@ class M_monitoring extends CI_Model {
         return $query->result_array();
         // echo $sql;
     }
-
-    // public function getBodyTanggal($no_document, $tglAwal, $tglAkhir) {
-    //     $oracle = $this->load->database('oracle', true);
-    //     $sql ="SELECT NO_DOCUMENT, JENIS_DOKUMEN, ITEM, DESCRIPTION,
-    //             UOM, QTY, CREATION_DATE, to_char(CREATION_DATE, 'hh24:mi:ss') as JAM_INPUT,
-    //             STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN 
-    //     from KHS_MONITORING_GD_SP
-    //     WHERE no_document = '$no_document'
-    //     AND creation_date BETWEEN TO_DATE( '$tglAwal', 'DD/MM/YYYY' ) AND TO_DATE( '$tglAkhir', 'DD/MM/YYYY' )
-    //     order by CREATION_DATE DESC";
-    //     $query = $oracle->query($sql);
-    //     return $query->result_array();
-    //     // echo $sql;
-    // }
-
-    // public function getBodyPIC($no_document, $pic) {
-    //     $oracle = $this->load->database('oracle', true);
-    //     $sql ="SELECT NO_DOCUMENT, JENIS_DOKUMEN, ITEM, DESCRIPTION,
-    //             UOM, QTY, CREATION_DATE, to_char(CREATION_DATE, 'hh24:mi:ss') as JAM_INPUT,
-    //             STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN 
-    //     from KHS_MONITORING_GD_SP
-    //     WHERE no_document = '$no_document'
-    //     AND pic = '$pic'
-    //     order by CREATION_DATE DESC";
-    //     $query = $oracle->query($sql);
-    //     return $query->result_array();
-    //     // echo $sql;
-    // }
-
-    // public function dataUpdate($item, $jml_ok, $jml_not_ok, $keterangan)
-    // {
-    //     $oracle = $this->load->database('oracle', true);
-    //     $sql="UPDATE KHS_MONITORING_GD_SP set JML_OK = '$jml_ok', JML_NOT_OK = '$jml_not_ok', KETERANGAN ='$keterangan' where ITEM ='$item'";
-    //     $query = $oracle->query($sql);
-	// 		// echo $sql;
-    //     }  
 
         public function dataUpdate($item, $query, $doc)
     {
@@ -147,7 +111,8 @@ class M_monitoring extends CI_Model {
         khs_kib kk,
         wip_discrete_jobs wdj         
         where kk.ORDER_ID = wdj.WIP_ENTITY_ID  
-        and kk.PRIMARY_ITEM_ID = wdj.PRIMARY_ITEM_ID      
+        and kk.PRIMARY_ITEM_ID = wdj.PRIMARY_ITEM_ID  
+        and kk.INVENTORY_TRANS_FLAG = 'Y'     
         and kk.KIBCODE = '$no_document'";
           $query = $oracle->query($sql);
           return $query->result_array();
@@ -159,7 +124,7 @@ class M_monitoring extends CI_Model {
         $sql ="
                 SELECT NO_DOCUMENT, JENIS_DOKUMEN, ITEM, DESCRIPTION,
                         UOM, QTY, CREATION_DATE, to_char(CREATION_DATE, 'hh24:mi:ss') as JAM_INPUT,
-                        STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN 
+                        STATUS, JML_OK, JML_NOT_OK, PIC, KETERANGAN, ACTION 
                 from KHS_MONITORING_GD_SP
                 where NO_DOCUMENT = '$no_document'
                 order by CREATION_DATE DESC";
@@ -167,7 +132,58 @@ class M_monitoring extends CI_Model {
         return $query->result_array();
         // echo $sql;
     }
-    
+
+    public function gdAsalIO($no_document) {
+        $oracle = $this->load->database('oracle', true);
+        $sql ="select mmt.SHIPMENT_NUMBER  no_interorg
+                        ,msib.SEGMENT1       
+                        ,mmt.SUBINVENTORY_CODE gudang_asal
+                from mtl_material_transactions mmt
+                    ,mtl_system_items_b msib
+                    ,(select mmt2.TRANSACTION_QUANTITY qty_receipt
+                            ,mmt2.ORGANIZATION_ID org
+                            ,mmt2.SHIPMENT_NUMBER shipment_num
+                            ,mmt2.INVENTORY_ITEM_ID item_id
+                            ,mmt2.TRANSACTION_TYPE_ID type_id
+                        from mtl_material_transactions mmt2
+                    where mmt2.TRANSACTION_TYPE_ID = 12 -- Intransit Receipt
+                        ) mmt2
+                where msib.INVENTORY_ITEM_ID = mmt.INVENTORY_ITEM_ID
+                and msib.ORGANIZATION_ID = mmt.ORGANIZATION_ID
+                and mmt.SHIPMENT_NUMBER = '$no_document'
+                and mmt.TRANSACTION_TYPE_ID = 21 -- Intransit Shipment
+                --
+                and mmt2.ORG(+) = mmt.TRANSFER_ORGANIZATION_ID
+                and mmt2.SHIPMENT_NUM(+) = mmt.SHIPMENT_NUMBER
+                and mmt2.ITEM_ID(+) = mmt.INVENTORY_ITEM_ID
+                and mmt.ORGANIZATION_ID in (102,225) -- YSP";
+        $query = $oracle->query($sql);
+        return $query->result_array();
+        // echo $sql;
+    }
+
+    public function gdAsalKIB($no_document) {
+        $oracle = $this->load->database('oracle', true);
+        $sql ="select kk.KIBCODE no_interorg
+                        ,msib.SEGMENT1 item
+                        ,mmt.SUBINVENTORY_CODE
+                from khs_kib kk
+                    ,mtl_system_items_b msib 
+                    ,wip_discrete_jobs wdj
+                    --
+                    ,mtl_material_transactions mmt   
+                where kk.PRIMARY_ITEM_ID = msib.INVENTORY_ITEM_ID
+                    and kk.ORGANIZATION_ID = msib.ORGANIZATION_ID
+                    and kk.ORDER_ID = wdj.WIP_ENTITY_ID 
+                    and kk.PRIMARY_ITEM_ID = wdj.PRIMARY_ITEM_ID 
+                    --
+                    and wdj.WIP_ENTITY_ID = mmt.TRANSACTION_SOURCE_ID
+                    and wdj.PRIMARY_ITEM_ID = mmt.INVENTORY_ITEM_ID
+                    and kk.KIBCODE = '$no_document'";
+        $query = $oracle->query($sql);
+        return $query->result_array();
+        // echo $sql;
+    }
 
 }
 
