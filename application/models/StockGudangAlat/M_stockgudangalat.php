3fe3982fe79f33317ead7910ea928407f13807ae
ALDI PRADANA <aldi_pradana@quick.com>
2019-12-10 15:19:02 +0700

    Update M_stockgudangalat.php

--- application/models/StockGudangAlat/M_stockgudangalat.php
+++ application/models/StockGudangAlat/M_stockgudangalat.php
@@ -2,7 +2,7 @@
 
 class M_stockgudangalat extends CI_Model
 {
-	var $oracle;
+	// var $oracle;
 	function __construct()
 	{
 		parent::__construct();
@@ -11,10 +11,10 @@ class M_stockgudangalat extends CI_Model
       $this->dev = $this->load->database('dpostgre', true);
    }
 
-   function insertData($no_po,$nama,$merk,$pilihan,$qty,$tag,$subinv) 
+   function insertData($no_po,$nama,$merk,$pilihan,$qty,$tag,$subinv)
    {
       $db = $this->load->database();
-      $sql ="INSERT into msg.msg_stok_gdg_alat_tst (no_po, tag, nama, merk, qty, jenis, subinv) 
+      $sql ="INSERT into msg.msg_stok_gdg_alat_tst (no_po, tag, nama, merk, qty, jenis, subinv)
 		VALUES ('$no_po','$tag', '$nama', '$merk', '$qty', '$pilihan', '$subinv')";
       $query = $this->dev->query($sql);
       // echo $sql.'<br>';
@@ -22,12 +22,12 @@ class M_stockgudangalat extends CI_Model
       // return $sql;
    }
 
-   function insertTable() 
+   function insertTable()
    {
       $db = $this->load->database();
       $sql = "SELECT distinct msgab.no_po, msgab.tag, msgab.nama, msgab.merk, msgab.subinv,
-      msgab.qty, 
-      count(nama) OVER (partition by nama) jml, msgab.jenis, msgab.id FROM 
+      msgab.qty,
+      count(nama) OVER (partition by nama) jml, msgab.jenis, msgab.id FROM
       msg.msg_stok_gdg_alat_tst msgab order by nama asc";
       $query = $this->dev->query($sql);
       return $query->result_array();
@@ -50,12 +50,20 @@ class M_stockgudangalat extends CI_Model
    function deleteData($id)
    {
       $db = $this->load->database();
-      $sql ="DELETE FROM msg.msg_stok_gdg_alat_tst 
+      $sql ="DELETE FROM msg.msg_stok_gdg_alat_tst
       WHERE id ='$id'";
-      
+
       $this->dev->query($sql);
    }
-   
+
+	 // tambahan
+	 public function getDataComp($id)
+	 {
+		$this->db->where('id', $id);
+		$query = $this->db->get('msg.msg_stok_gdg_alat_tst');
+		return $query->result_array();
+	 }
+
 }
 
 ?>
