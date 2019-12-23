302261a2d5ee9d899663403ead128773bce9dff1
aji kurniawan <aji.kurniawan.29.01.2000@gmail.com>
2019-12-23 08:39:50 +0700

    update lelayu
    perbaikan cutoff awal di button simpan

--- application/controllers/MasterPresensi/Lelayu/C_Lelayu.php
+++ application/controllers/MasterPresensi/Lelayu/C_Lelayu.php
@@ -103,7 +103,7 @@ class C_Lelayu extends CI_Controller
     );
     $this->M_lelayu->insertAll($array);
 
-    $bulancutoff = date('Y-m-21');
+    $bulancutoff = $this->M_lelayu->getCutoffBulanIni();
     $tanggalcutoff = date('d');
     $bulanlalu = date('Y-m-d', strtotime($bulancutoff. ' -1 month'));
 
