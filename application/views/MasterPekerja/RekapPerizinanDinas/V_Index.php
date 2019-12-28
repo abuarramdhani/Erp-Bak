a7b22d136fd0f14d01d7ee82a89815f9e2768386
rosyidatun_nur_r <rosyidatun_nur_r@quick.com>
2019-12-28 11:39:37 +0700

    Update Rekap Perizinan Dinas di Master Pekerja terkait Order #900106

--- application/views/MasterPekerja/RekapPerizinanDinas/V_Index.php
+++ application/views/MasterPekerja/RekapPerizinanDinas/V_Index.php
@@ -19,59 +19,38 @@
                 </div>
                 <br/>
                 <div class="row">
-                <form id="" method="post" action="<?php echo site_url('MasterPekerja/RekapPerizinanDinas/rekapbulanan');?>" class="form-horizontal" >
-                <div class="col-lg-12">
-                    <div class="box box-primary box-solid">
-                        <div class="box-header with-border"></div>
+                    <div class="col-lg-12">
+                        <div class="box box-primary box-solid">
+                            <div class="box-header with-border"></div>
                             <div class="box-body">
                                 <div class="panel-body">
                                     <div class="row">
-                                        <div class="col-lg-8">
-                                            <div class="form-group">
-                                                <label for="txtTanggalCetak" class="col-lg-3 control-label text-left">Periode Rekap</label>
-                                                <div class="col-lg-7">
-                                                   <input class="form-control periodeRekap"  autocomplete="off" type="text" name="periodeRekap" id="periodeRekap" placeholder="Masukkan Periode" value=""/>
-                                                    <!-- <button type="submit" class="btn btn-primary">Cari</button> -->
-                                                   <p>*kosongkan kolom periode , untuk menampilkan semua data</p>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="col-lg-2">
-                                                        <button type="submit" class="btn btn-primary">Cari</button>
-                                                    </div>
-                                                </div>
+                                        <div class="col-lg-12">
+                                            <label for="txtTanggalCetak" class="col-lg-2 control-label text-left">Periode Rekap:</label>
+                                            <div class="col-lg-5">
+                                               <input class="form-control periodeRekap"  autocomplete="off" type="text" name="periodeRekap" id="periodeRekap" placeholder="Masukkan Periode" value=""/>
+                                               <p style="color: red;">*kosongkan kolom periode , untuk menampilkan semua data</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <label class="col-lg-2">Jenis Rekap :</label>
+                                            <div class="form-group col-lg-5">
+                                                <input type="radio" name="PerSurat" class="RD_radioDinas" value="1" required>Rekap Per Surat<br>
+                                                <input type="radio" name="PerSurat" class="RD_radioDinas" value="2" required>Rekap Per Pekerja
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="col-lg-5 text-center">
+                                                <button class="btn btn-primary" id="PD_Cari">Cari</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                              </div>
                         </div>
-                     </div>
-                     </form>
-                    <div class="col-lg-12">
-                        <div class="box box-primary box-solid">
-                            <div class="box-header with-border">
-                            </div>
-                            <div class="box-body">
-                              <div id="izin-ok">
-                              <table class="datatable table table-striped table-bordered table-hover tabel_izin" style="width: 100%">
-                                <thead>
-                                  <tr>
-                                    <th class="text-center">No</th>
-                                    <th class="text-center">ID Izin</th>
-                                    <th class="text-center">Tanggal Pengajuan</th>
-                                    <th class="text-center">Nama Pekerja</th>
-                                    <th class="text-center">Jenis Izin</th>
-                                    <th class="text-center">Atasan Approved</th>
-                                    <th class="text-center">Tempat Makan</th>
-                                    <th class="text-center">Keterangan</th>
-                                    <th class="text-center">Status</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                              </table>
-                              </div>
-                        </div>
+                    </div>
+                    <div class="col-lg-12" id="areaRekapIzin">
+
                     </div>
                 </div>
             </div>
