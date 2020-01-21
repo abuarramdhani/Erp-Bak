a46b6e1ede506651464f666d53bd57e831a46557
aji Kurniawan <aji_kurniawan@quick.com>
2020-01-21 20:18:17 +0700

    Update M_pekerjacutoff.php
    tambah function cek_ijin_keluar

--- application/models/MasterPresensi/ReffGaji/M_pekerjacutoff.php
+++ application/models/MasterPresensi/ReffGaji/M_pekerjacutoff.php
@@ -133,6 +133,93 @@ class M_pekerjacutoff extends CI_Model
 				and b.noind in ($noind)";
 		return $this->personalia->query($sql)->result_array();
 	}
+	
+	public function cek_ijin_keluar($keluar,$masuk,$break_mulai,$break_selesai,$ist_mulai,$ist_selesai){
+		$jam_ijin = 0;
+		// echo $jam_ijin;
+		// echo $keluar;exit();
+		if ($keluar >= $ist_mulai && $masuk >= $ist_mulai) {
+			if ($ist_selesai <= $keluar && $ist_selesai <= $masuk) {
+				$lama_ijin = $masuk - $keluar;
+				// $lama_ist = $ist_selesai - $ist_mulai;
+				// $jam_ijin = $lama_ijin - $lama_ist;
+				$jam_ijin = $lama_ijin;
+				// 	echo "hai";
+				// echo $jam_ijin;
+				// exit();
+			}else if($keluar >= $ist_selesai && $masuk >= $ist_selesai){
+				$jam_ijin = $masuk - $keluar;
+			}else if($masuk >= $ist_selesai){
+				$jam_ijin = $masuk - $ist_selesai;
+			}
+		}else if($keluar <= $ist_mulai && $masuk >= $ist_mulai){
+			if ($ist_selesai >= $keluar && $ist_selesai >= $masuk) {
+				if ($keluar <= $break_mulai && $keluar <= $break_selesai) {
+					$setelah_break = $masuk - $break_selesai;
+					$sebelum_break = $break_mulai - $keluar;
+					$jam_ijin = $setelah_break + $sebelum_break;
+				}else if($keluar > $break_mulai && $masuk){
+					$jam_ijin = $ist_mulai - $break_selesai;
+				}else{
+					$jam_ijin = $ist_mulai - $keluar;
+					if ($jam_ijin <= 30) {
+						$jam_ijin = 0;
+					}
+				}
+			}else if($keluar <= $ist_selesai && $masuk > $ist_selesai){
+				if ($keluar <= $break_mulai) {
+					$sebelum_break = $break_mulai - $keluar;
+					$setelah_break = $ist_mulai - $break_selesai;
+					$setelah_ist = $masuk - $ist_selesai;
+					if ($sebelum_break < 0) {
+						$sebelum_break = 0;
+					}
+					if ($setelah_break < 0) {
+						$setelah_break = 0;
+					}
+					if ($setelah_ist < 0) {
+						$setelah_ist = 0;
+					}
+					$jam_ijin = $sebelum_break + $setelah_break + $setelah_ist;
+				}else if( $keluar >= $break_selesai){
+					$sebelum_ist = $ist_mulai - $keluar;
+					$setelah_ist = $masuk - $ist_selesai;
+					$jam_ijin = $sebelum_ist + $setelah_ist;
+				}else if($keluar >= $break_mulai && $keluar <= $break_selesai){
+					$sebelum_ist = $ist_mulai - $break_selesai;
+					$setelah_ist = $masuk - $ist_selesai;
+					$jam_ijin = $sebelum_ist + $setelah_ist;
+				}
+			}
+		}else{
+			if ($keluar >= $break_mulai && $masuk >= $break_mulai) {
+				if ($break_selesai >= $keluar && $break_selesai >= $masuk) {
+					$jam_ijin = 0;
+				}else if($keluar >= $break_selesai && $masuk >= $break_selesai){
+					$jam_ijin = $masuk - $keluar;
+				}else{
+					$jam_ijin = $masuk - $break_selesai;
+				}
+			}else if($keluar <= $break_mulai && $masuk >= $break_mulai){
+				if ($break_selesai >= $keluar && $break_selesai >= $masuk) {
+					$jam_ijin = $break_mulai - $keluar;
+				}else{
+					$sebelum_break = $break_mulai - $keluar;
+					$setelah_break = $masuk - $break_selesai;
+					$jam_ijin = $setelah_break + $sebelum_break;
+				}
+			}else{
+				$jam_ijin = $masuk - $keluar;
+			}
+		}
+		if ($jam_ijin > 0 && $jam_ijin < 60) {
+			$jam_ijin = $jam_ijin/60;
+		}else if($jam_ijin >= 60){
+			$jam_ijin = $jam_ijin/60;
+		}
+		// echo $jam_ijin."<br>";
+		return $jam_ijin;
+	}
 
 	public function insertMemo($data){
 		$this->personalia->insert('"Presensi".tcutoff_custom_memo',$data);
