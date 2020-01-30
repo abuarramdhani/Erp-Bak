e0d975bbd785c5205d4598e1c3cba0f3a1347e94
alfansyah_nori_p <alfansyah_nori_p@quick.com>
2020-01-30 14:01:38 +0700

    ERP Mob Version Control

--- application/views/SystemAdministration/MainMenu/Android/List/V_Index.php
+++ application/views/SystemAdministration/MainMenu/Android/List/V_Index.php
@@ -1,4 +1,16 @@
 	<section class="content">
+	<style type="text/css">
+		#cover-spin {
+	    position:fixed;
+	    width:100%;
+	    left:0;right:0;top:0;bottom:0;
+	    z-index:9999;
+	    display: none;
+	    background: url(<?php echo base_url('assets/img/gif/loadingquick.gif'); ?>) 
+	              50% 50% no-repeat rgba(0,0,0,0.7);
+	}
+	</style>
+	<div id="cover-spin"></div>
 
 	<?php if(!empty($this->session->flashdata('msg'))){ ?>
 	<script type="text/javascript">
@@ -15,6 +27,58 @@
 				<button style="margin-top: 10px;padding: 10px;" data-toggle="modal" data-target="#tambahData" class="btn btn-primary pull-right"><i class="fa fa-plus"></i> Tambah Data</button>
 			</div>
 	</div>
+		<div class="panel box-body">
+		<table id="tblLatestVersion" class="table table-striped table-bordered">
+			<thead>
+				<tr style="background-color:#367FA9; color:white ">
+				<th class="text-center">Versi Aplikasi Terbaru</th>
+				<th class="text-center">Mandatory Update</th>
+				<th class="text-center">Last Update By</th>
+				<th class="text-center">Last Update Date</th>
+				<th class="text-center">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($versi as $key => $value) :
+					 ?>
+				<tr>
+
+					<td class="text-center">
+						<div class="input-group">
+	                     <span class="input-group-addon">ERP Mobile Versi</span>
+					<input id="versiTerbaru" disabled class="form-control text-center" type="number" value="<?= $value['versi_terbaru'] ?>"></input></td>
+						</div>
+					<td>
+					  <select disabled class="form-control" id="mandUpdate" name="frcupdate">
+					    <option value="1" <?php if($value['mandatory_update'] == 't') echo "selected"; ?> >True</option>
+					    <option value="0" <?php if($value['mandatory_update'] == 'f') echo "selected"; ?> >False</option>
+					  </select>
+					</td>
+					<td class="text-center">
+						<input id="lu_by" class="form-control" disabled value="<?= $value['last_update_by'] ?>" ></input>
+					</td>
+					<td class="text-center">
+						<input id="lu_date" class="form-control" disabled value="<?= $value['last_update_date'] ?>" ></input>
+					</td>
+					<td class="text-center" style="width: 25%;">
+						<button class="btn btn-success btn-update">
+							Update
+						</button>
+						<button class="btn btn-primary btn-simpan" style="display: none;">
+							Simpan
+						</button>
+						<button class="btn btn-danger btn-batal" style="display: none;">
+							Batal
+						</button>
+
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+
+		</div>
+
 		<div class="panel box-body" >
 
 		<div class="table-responsive">
@@ -27,6 +91,7 @@
 						<th class="text-center ">IMEI</th>
 						<th class="text-center ">Hardware Serial</th>
 						<th class="text-center ">GSF</th>
+						<th class="text-center ">Versi Aplikasi</th>
 						<th class="text-center ">Employee</th>
 						<th class="text-center ">Validation</th>						
 					</tr>
@@ -69,6 +134,7 @@
 					<td><?php echo $andro['imei']?></td>
 					<td><?php echo $andro['hardware_serial']?></td>
 					<td><?php echo $andro['gsf']?></td>
+					<td><?php echo $andro['versi_aplikasi']?></td>
 					<td><?php echo $andro['info_1']?></td>
 					<td class="text-center" id="tdStatus"><span class="<?=$classLabel ?>"><?php echo $status;?></span></td>
 											
@@ -137,16 +203,6 @@
 	$(document).ready(function(){
 		$("#tableAndroidApproval").DataTable();
 
-		jQuery.each($('tbody tr td span'), function () {
-        if (this.textContent == "Approved") {
-            $(this).closest('span').addClass("label label-success");
-        }else if(this.textContent == "Rejected"){
-        	$(this).closest('span').addClass("label label-danger");
-        }else{
-        	$(this).closest('span').addClass("label label-default");
-        }
-    });
-
 		$('.btn-hapus').click(function(e){
 			e.preventDefault();
 			var getLink = $(this).attr('href');
@@ -166,6 +222,78 @@
 			  }
 			})
 		})
+
+		$('.btn-update').on('click',function(e){
+			e.preventDefault();
+			$("#versiTerbaru").prop('disabled',false)
+			$("#mandUpdate").prop('disabled',false)
+			$(".btn-simpan").show()
+			$(".btn-batal").show()
+			$(this).hide();
+		})
+
+		$('.btn-batal').on('click',function(){
+			$("#versiTerbaru").prop('disabled',true)
+			$("#mandUpdate").prop('disabled',true)
+			$(".btn-simpan").hide()
+			$(".btn-update").show()
+			$(this).hide();
+		})
+
+		$('.btn-simpan').on('click',function(){
+			let versi_terbaru = $("#versiTerbaru").val();
+			let mandUpdate = $("#mandUpdate").val();
+			$("#cover-spin").fadeIn()
+			//POST JSON
+			postData('<?php echo base_urL(''); ?>SystemAdministration/Android/updateVersi', { versiTerbaru: versi_terbaru,mandUpdate:mandUpdate })
+			  .then((data) => {
+			  	$(".btn-simpan").hide()
+				$(".btn-update").show()
+				$(".btn-batal").hide();
+				$("#versiTerbaru").prop('disabled',true)
+				$("#mandUpdate").prop('disabled',true)
+				$("#cover-spin").fadeOut()
+				$("#lu_by").val(data.last_update_by);
+				$("#lu_date").val(data.last_update_date);
+			    console.log(data); // JSON data parsed by `response.json()` call
+			  })
+			  .catch((err)=>{
+			  	console.log(err)
+			  	$("#cover-spin").fadeOut()
+			  	alert('Gagal Mengupdate Data !')
+			  });
+
+			 //POST www-form
+
+			// fetch('<?php echo base_urL(''); ?>SystemAdministration/Android/updateVersi',{
+			// 		    method: 'POST',
+			// 		    headers: {'Content-Type':'application/x-www-form-urlencoded'}, // this line is important, if this content-type is not set it wont work
+			// 		    body: 'versiTerbaru='+versi_terbaru+'&mandUpdate='+mandUpdate+''
+			// 		})
+			// .then((res) =>{
+			// 	return res.json()
+			// })
+			// .then((res) => {
+			// 	console.log(res)
+			// })
+		})
+
+		async function postData(url = '', data = {}) {
+			  const response = await fetch(url, {
+			    method: 'POST', 
+			    mode: 'cors',
+			    cache: 'no-cache',
+			    credentials: 'same-origin',
+			    headers: {
+			      'Content-Type': 'application/json'
+			    },
+			    redirect: 'follow',
+			    referrerPolicy: 'no-referrer', 
+			    body: JSON.stringify(data) 
+			  });
+			  return await response.json(); 
+			}
+
 
 	});
 </script>
