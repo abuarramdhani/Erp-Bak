a1530979b955144602466c3e71498bbf4501a1f3
dicka_ismaji <dicka_ismaji@quick.com>
2020-01-31 14:33:39 +0700

    dynamic page title

--- application/controllers/C_Index.php
+++ application/controllers/C_Index.php
@@ -44,6 +44,7 @@ class C_Index extends CI_Controller {
 	public function index()
 	{
 		if($this->session->is_logged){
+			$this->session->set_userdata('module_name', 'Dashboard');
 
 			//$usr = "D1178";
 			$user_id = $this->session->userid;
@@ -61,6 +62,7 @@ class C_Index extends CI_Controller {
 			$this->load->view('V_Index',$data);
 			$this->load->view('V_Footer',$data);
 		}else{
+			$this->session->set_userdata('module_name', 'Login');
 			if($this->session->gagal){
 				$data['error'] = "Login error, please enter correct username and password";
 				$aksi = 'Login';
@@ -95,6 +97,7 @@ class C_Index extends CI_Controller {
 					// $UserResponsibility_item['user_group_menu_id'] = 0;
 				// }
 				$this->session->set_userdata('responsibility_id', $UserResponsibility_item['user_group_menu_id']);
+				$this->session->set_userdata('module_name', $UserResponsibility_item['module_name']);
 				$this->session->set_userdata('module_link', $UserResponsibility_item['module_link']);
 				$this->session->set_userdata('org_id', $UserResponsibility_item['org_id']);
 			}
@@ -112,6 +115,7 @@ class C_Index extends CI_Controller {
 				$data['error'] = "";
 			}
 
+			$this->session->set_userdata('module_name', 'Login');
 			$this->load->view('V_Login',$data);
 			$this->session->unset_userdata('gagal');
 		}
