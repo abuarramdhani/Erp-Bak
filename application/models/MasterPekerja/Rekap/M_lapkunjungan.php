ded74108372ba75e3f152437b5db0c19e8f7b0b5
rosyidatun_nur_r <rosyidatun_nur_r@quick.com>
2020-02-22 13:25:10 +0700

    Modifikasi pengambilan alamat dan data pekerja dari tpribadi terkait Order #945273

--- application/models/MasterPekerja/Rekap/M_lapkunjungan.php
+++ application/models/MasterPekerja/Rekap/M_lapkunjungan.php
@@ -2,8 +2,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_lapkunjungan extends CI_Model {
-	function __construct() 
-	{ 
+	function __construct()
+	{
 		parent:: __construct();
 		$this->personalia 	= 	$this->load->database('personalia', TRUE);
 		$this->erp 			=	$this->load->database('erp_db', TRUE);
@@ -11,20 +11,24 @@ class M_lapkunjungan extends CI_Model {
 	}
 
     public function getDataLapkun(){
-        $this->personalia->order_by('no_surat',asc);
-        return $this->personalia->get('Surat.t_lapkun')->result_array();
+		$sql = "SELECT tl.id_laporan, tl.tanggal_laporan, tl.no_surat, tl.noinduk_pekerja,
+				(SELECT trim(nama) FROM hrd_khs.tpribadi where noind = tl.noinduk_pekerja) as nama_pekerja,
+				(SELECT trim(ts.seksi) FROM hrd_khs.tpribadi tp join hrd_khs.tseksi ts on ts.kodesie = tp.kodesie where noind = tl.noinduk_pekerja) as seksi_pekerja,
+				(SELECT concat(tl.noinduk_petugas, ' - ', nama) from hrd_khs.tpribadi where noind = tl.noinduk_petugas) as petugas
+				FROM \"Surat\".t_lapkun tl order by tl.tanggal_laporan desc";
+        return $this->personalia->query($sql)->result_array();
     }
 
 	public function informasiPekerja($keyword){
         $sql = "select * from sys.vi_sys_user where user_name like '%$keyword%' or upper(employee_name) like '%$keyword%' order by user_id asc";
         $query = $this->db->query($sql);
         return $query->result_array();
-    }  
+    }
 
     public function getInfoPekerja($keyword){
     	$sql ="SELECT tp.noind,rtrim(tp.nama) as nama,rtrim(alamat) as alamat, ts.seksi as seksi ,ts.unit as unit, tr.jabatan as jabatan FROM hrd_khs.tpribadi tp LEFT JOIN hrd_khs.tseksi ts ON tp.kodesie = ts.kodesie INNER JOIN hrd_khs.trefjabatan tr ON tp.noind = tr.noind WHERE tp.noind LIKE '%$keyword%' or UPPER(tp.nama) LIKE '%$keyword%' AND tp.keluar = false;";
     	return $this->personalia->query($sql)->result_array();
-    } 
+    }
 
     public function getMaxNoSurat($bulanLaporan){
         $sql = "SELECT MAX(no_surat) as max_no_surat FROM \"Surat\".t_lapkun WHERE substr(TO_CHAR(tanggal_laporan,'YYYY-MM-DD'), 1, 7) = '$bulanLaporan'";
@@ -45,9 +49,13 @@ class M_lapkunjungan extends CI_Model {
         $this->personalia->where('id_laporan',$id_laporan);
         return $this->personalia->get('Surat.t_lapkun')->result_array();
     }
-	
+
     public function getLapkunPeriode($tanggal_awal,$tanggal_akhir){
-        $sql = "SELECT * from \"Surat\".t_lapkun WHERE tanggal_laporan BETWEEN '$tanggal_awal' and '$tanggal_akhir' order by no_surat asc";
+        $sql = "SELECT tl.id_laporan, tl.tanggal_laporan, tl.no_surat, tl.noinduk_pekerja,
+				(SELECT trim(nama) FROM hrd_khs.tpribadi where noind = tl.noinduk_pekerja) as nama_pekerja,
+				(SELECT trim(ts.seksi) FROM hrd_khs.tpribadi tp join hrd_khs.tseksi ts on ts.kodesie = tp.kodesie where noind = tl.noinduk_pekerja) as seksi_pekerja,
+				(SELECT concat(tl.noinduk_petugas, ' - ', nama) from hrd_khs.tpribadi where noind = tl.noinduk_petugas) as petugas
+				from \"Surat\".t_lapkun tl WHERE tl.tanggal_laporan BETWEEN '$tanggal_awal' and '$tanggal_akhir' order by tl.no_surat asc";
         $query = $this->personalia->query($sql);
         return $query->result_array();
 
@@ -62,5 +70,14 @@ class M_lapkunjungan extends CI_Model {
         $this->personalia->where('id_laporan',$id_laporan);
         $this->personalia->delete('Surat.t_lapkun');
     }
-	
+
+	public function getDataPribadi($noind)
+	{
+		$sql = "SELECT tp.noind, trim(tp.nama) as nama, tp.alamat, (SELECT jabatan from hrd_khs.trefjabatan where kodesie = tp.kodesie and noind = tp.noind and kd_jabatan = tp.kd_jabatan) as jabatan,
+				(SELECT seksi from hrd_khs.tseksi where kodesie = tp.kodesie) as seksi
+				FROM hrd_khs.tpribadi tp
+				WHERE noind = '$noind'";
+		return $this->personalia->query($sql)->result_array();
+	}
+
 };
