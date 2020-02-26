b295015ddc5b1fb3f097163c6bddcbf4c4c2b23d
rosyidatun_nur_r <rosyidatun_nur_r@quick.com>
2020-02-26 08:44:54 +0700

    Perbaikan tanggal keluar dan phk di perhitungan pesangon terkait order #799068

--- application/views/MasterPekerja/PerhitunganPesangon/V_Cetak.php
+++ application/views/MasterPekerja/PerhitunganPesangon/V_Cetak.php
@@ -116,15 +116,15 @@
 			<td style="border-right: 1px solid black;border-left: 1px solid black;border-bottom: 1px solid black;width: 10%;padding-left: 20px"></td>
 		</tr>
 		<tr>
-		    <td style="border-left: 1px solid black;border-bottom: 1px solid black;width: 29%;padding-left: 20px">TANGGAL AKHIR KERJA</td>
+		    <td style="border-left: 1px solid black;border-bottom: 1px solid black;width: 29%;padding-left: 20px">TANGGAL KELUAR</td>
 			<td style="border-bottom: 1px solid black;width: 1%;padding-left: 20px">:</td>
-			<td style="border-bottom: 1px solid black;width: 60%;padding-left: 20px"><?php echo $data['0']['akhir']?></td>
+			<td style="border-bottom: 1px solid black;width: 60%;padding-left: 20px"><?php echo $data['0']['metu']?></td>
 			<td style="border-right: 1px solid black;border-left: 1px solid black;border-bottom: 1px solid black;width: 10%;padding-left: 20px"></td>
 		</tr>
 		<tr>
 		    <td style="border-left: 1px solid black;border-bottom: 1px solid black;width: 29%;padding-left: 20px">PROSES PHK</td>
 			<td style="border-bottom: 1px solid black;width: 1%;padding-left: 20px">:</td>
-			<td style="border-bottom: 1px solid black;width: 60%;padding-left: 20px"><?php echo $data['0']['metu']?></td>
+			<td style="border-bottom: 1px solid black;width: 60%;padding-left: 20px"><?php echo (!empty($data['0']['tgl_phk']))? $data['0']['tgl_phk']:''?></td>
 			<td style="border-right: 1px solid black;border-left: 1px solid black;border-bottom: 1px solid black;width: 10%;padding-left: 20px"></td>
 		</tr>
 		<tr>
