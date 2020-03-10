ad62cdd47807f72d00e99ca19804f3b743db78c7
Meyta <meyta_rizki_nugraheni@quick.com>
2020-03-10 13:38:16 +0700

    push lihat stock

--- application/models/StockGdSparepart/M_minmax.php
+++ application/models/StockGdSparepart/M_minmax.php
@@ -9,7 +9,7 @@ class M_minmax extends CI_Model
     }
 
     public function insertData($item, $min, $max, $uom){
-        $oracle = $this->load->database('oracle_dev', true);
+        $oracle = $this->load->database('oracle', true);
         $sql = "insert into khs_sp_minmax (item, min, max, uom, last_update, last_update_by)
                 values ('$item', '$min', '$max', '$uom', sysdate, 225)";
         $query = $oracle->query($sql);
@@ -17,14 +17,14 @@ class M_minmax extends CI_Model
     }
 
     public function getdata(){
-        $oracle = $this->load->database('oracle_dev', true);
+        $oracle = $this->load->database('oracle', true);
         $sql = "select * from khs_sp_minmax";
         $query = $oracle->query($sql);
         return $query->result_array();
     }
 
     public function cekimport($item){
-        $oracle = $this->load->database('oracle_dev', true);
+        $oracle = $this->load->database('oracle', true);
         $sql = "select * from khs_sp_minmax where item = '$item'";
         $query = $oracle->query($sql);
         return $query->result_array();
@@ -32,7 +32,7 @@ class M_minmax extends CI_Model
 
     
     public function updateData($item, $min, $max){
-        $oracle = $this->load->database('oracle_dev', true);
+        $oracle = $this->load->database('oracle', true);
         $sql = "update khs_sp_minmax set min = '$min', max = '$max' where item = '$item'";
         $query = $oracle->query($sql);
         $query = $oracle->query('commit');
