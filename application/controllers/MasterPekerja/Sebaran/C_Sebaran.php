2d543a68dfa7cd49265587112ed8f54bca31e7b2
aji kurniawan <aji_kurniawan@quick.com>
2020-03-30 09:39:28 +0700

    data dari database

--- application/controllers/MasterPekerja/Sebaran/C_Sebaran.php
+++ application/controllers/MasterPekerja/Sebaran/C_Sebaran.php
@@ -52,121 +52,57 @@ class C_Sebaran extends CI_Controller
 		$data['sebaran_pusat'] = array();
 		$data['sebaran_tuksono'] = array();
 
-		// $sebaran = $this->M_sebaran->getSebaran();
+		$sebaran = $this->M_sebaran->getSebaran();
 
-		// if (isset($sebaran) && !empty($sebaran)) {
-		// 	foreach ($sebaran as $sbr) {
-		// 		if ($sbr['lokasi_kerja'] == "JOGJA") {
-		// 			$data['sebaran_tuksono'][] = array(
-		// 				'posX' 		=> ,
-		// 				'posY' 		=> ,
-		// 				'biru'		=> $sbr['bapil'],
-		// 				'merah' 	=> $sbr['bapilsak'],
-		// 				'abu'		=> $sbr['matira'],
-		// 				'hitam'		=>,
-		// 				'title'		=> 'tes',
-		// 				'color'		=> 'red',
-		// 				'jumlah'	=> $sbr['sudah_isi']
-		// 			);
-		// 		}else if ($sbr['lokasi_kerja'] == "TUKSONO") {
-		// 			$data['sebaran_tuksono'][] = array(
-		// 				'posX' 		=> rand(280,679),
-		// 				'posY' 		=> rand(107,551),
-		// 				'biru'		=> 0,
-		// 				'merah' 	=> 2,
-		// 				'abu'		=> 12,
-		// 				'hitam'		=>,
-		// 				'title'		=> 'tes',
-		// 				'color'		=> 'red',
-		// 				'jumlah'	=> 14
-		// 			);
-		// 		}
-		// 	}
-		// }
-		$data['sebaran_tuksono'] = array(
-			0 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 0,
-					'merah' 	=> 2,
-					'abu'		=> 12,
-					'title'		=> 'tes',
-					'color'		=> 'red',
-					'jumlah'	=> 14
-				),
-			1 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 0,
-					'merah' 	=> 6,
-					'abu'		=> 30,
-					'title'		=> 'tes2',
-					'color'		=> 'grey',
-					'jumlah'	=> 36
-				),
-			2 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 5,
-					'merah' 	=> 4,
-					'abu'		=> 33,
-					'title'		=> 'tes3',
-					'color'		=> 'red',
-					'jumlah'	=> 42
-				),
-			3 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 5,
-					'merah' 	=> 8,
-					'abu'		=> 16,
-					'title'		=> 'tes4',
-					'color'		=> 'red',
-					'jumlah'	=> 29
-				),
-			4 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 0,
-					'merah' 	=> 0,
-					'abu'		=> 24,
-					'title'		=> 'tes5',
-					'color'		=> 'grey',
-					'jumlah'	=> 24
-				),
-			5 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 1,
-					'merah' 	=> 2,
-					'abu'		=> 12,
-					'title'		=> 'tes6',
-					'color'		=> 'red',
-					'jumlah'	=> 15
-				),
-			6 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 4,
-					'merah' 	=> 0,
-					'abu'		=> 23,
-					'title'		=> 'tes7',
-					'color'		=> 'blue',
-					'jumlah'	=> 27
-				),
-			7 => array(
-					'posX' 		=> rand(280,679),
-					'posY' 		=> rand(107,551),
-					'biru'		=> 7,
-					'merah' 	=> 0,
-					'abu'		=> 13,
-					'title'		=> 'tes8',
-					'color'		=> 'blue',
-					'jumlah'	=> 20
-				)
-		);
+		if (isset($sebaran) && !empty($sebaran)) {
+			foreach ($sebaran as $sbr) {
+				$warna = "#2d3436";
+				$warna2 = "#636e72";
+				if ($sbr['bapilsak'] > 0) {
+					$warna = "#d63031";
+					$warna2 = "#ff7675";
+				}else{
+					if ($sbr['bapil'] > 0) {
+						$warna = "#0984e3";
+						$warna2 = "#74b9ff";
+					}else{
+						if ($sbr['matira'] > 0) {
+							$warna = "#b2bec3";
+							$warna2 = "#dfe6e9";
+						}else{
+							$warna = "#2d3436";
+							$warna2 = "#636e72";
+						}
+					}
+				}
+				if ($sbr['lokasi_kerja'] == "JOGJA") {
+					$data['sebaran_pusat'][] = array(
+						'posX' 		=> $sbr['posisix'],
+						'posY' 		=> $sbr['posisiy'],
+						'biru'		=> $sbr['bapil'],
+						'merah' 	=> $sbr['bapilsak'],
+						'abu'		=> $sbr['matira'],
+						'title'		=> $sbr['unit'],
+						'color'		=> $warna,
+						'border'	=> $warna2,
+						'jumlah'	=> $sbr['sudah_isi']
+					);
+				}else if ($sbr['lokasi_kerja'] == "TUKSONO") {
+					$data['sebaran_tuksono'][] = array(
+						'posX' 		=> $sbr['posisix'],
+						'posY' 		=> $sbr['posisiy'],
+						'biru'		=> $sbr['bapil'],
+						'merah' 	=> $sbr['bapilsak'],
+						'abu'		=> $sbr['matira'],
+						'title'		=> $sbr['unit'],
+						'color'		=> $warna,
+						'border'	=> $warna2,
+						'jumlah'	=> $sbr['sudah_isi']
+					);
+				}
+			}
+		}
 
-		// echo "<pre>";print_r($data['sebaran']);exit();
 		$this->load->view('V_Header',$data);
 		$this->load->view('V_Sidemenu',$data);
 		$this->load->view('MasterPekerja/Sebaran/V_index',$data);
