57f02e7cd8651b4c4c6aed682b930cb30ef8bef5
Simon Hertoyo <simon_hertoyo@quick.com>
2020-04-06 15:00:35 +0700

    update query assign

--- application/models/MonitoringDO/M_monitoringdo.php
+++ application/models/MonitoringDO/M_monitoringdo.php
@@ -290,8 +290,8 @@ class M_monitoringdo extends CI_Model
 
     public function runAPIDO($rm)
     {
-        $conn = oci_connect('APPS', 'APPS', '192.168.7.3:1522/DEV');
-        // $conn = oci_connect('APPS', 'APPS', '192.168.7.1:1521/PROD');
+        // $conn = oci_connect('APPS', 'APPS', '192.168.7.3:1522/DEV');
+        $conn = oci_connect('APPS', 'APPS', '192.168.7.1:1521/PROD');
         if (!$conn) {
             $e = oci_error();
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
