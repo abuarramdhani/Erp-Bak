06fe88f4ceecf5b0e323c0384dd039ec586d202e
aji kurniawan <aji_kurniawan@quick.com>
2020-04-22 12:55:59 +0700

    memunculkan pekerja keluar yang masuk lagi

--- application/models/MasterPresensi/ReffGaji/M_transferreffgaji.php
+++ application/models/MasterPresensi/ReffGaji/M_transferreffgaji.php
@@ -73,7 +73,7 @@ class M_transferreffgaji extends CI_Model
 				 			or left(noind,1) = 'Q'
 				 		) 
 				 and to_char(tanggal_keluar,'mmyy') ='$periode'
-				 and (select count(*) from hrd_khs.tpribadi pri2 where (select nik from hrd_khs.tpribadi pri where refkel.noind = pri.noind) = pri2.nik and pri2.keluar = '0') = 0
+				 /*and (select count(*) from hrd_khs.tpribadi pri2 where (select nik from hrd_khs.tpribadi pri where refkel.noind = pri.noind) = pri2.nik and pri2.keluar = '0') = 0*/
 				) as tbl 
 				left join hrd_khs.tpribadi tpri 
 					on tpri.noind = tbl.noind
