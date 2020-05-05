dde0a3e5eabce80313fa57f621ed77d8f6bf1b8b
Simon Hertoyo <simon_hertoyo@quick.com>
2020-05-05 11:21:54 +0700

    Update V_Ajax_Allocate.php

--- application/views/MonitoringDO/V_Ajax_Allocate.php
+++ application/views/MonitoringDO/V_Ajax_Allocate.php
@@ -2,14 +2,14 @@
   <table class="table table-striped table-bordered table-hover text-left " id="tblMonitoringDOAllocate" style="font-size:12px;width:100%">
     <thead>
       <tr class="bg-primary">
-        <th><center>NO</center></th>
-        <th><center>NO.DOK</center></th>
-        <th><center>NOMOR SO</center></th>
+        <th style="width:5px"><center>NO</center></th>
+        <th style="width:70px"><center>NO.DOK</center></th>
+        <th style="width:70px"><center>NOMOR SO</center></th>
         <th><center>TUJUAN</center></th>
-        <th><center>KOTA</center></th>
-        <th><center>PLAT NOMOR</center></th>
-        <th><center>ASSIGN</center></th>
-        <th><center>DETAIL</center></th>
+        <th style="width:100px"><center>KOTA</center></th>
+        <th style="width:70px"><center>PLAT NOMOR</center></th>
+        <th style="width:70px"><center>ASSIGN</center></th>
+        <th style="width:15px"><center>DETAIL</center></th>
       </tr>
     </thead>
     <tbody>
