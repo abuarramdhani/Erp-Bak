8305e03fb33e1a6085ae212b3c51a1b101d060dc
aji kurniawan <aji_kurniawan@quick.com>
2020-05-11 16:41:12 +0700

    update rekap thr hlcm

--- application/views/UpahHlCm/THR/V_cetakperhitungan.php
+++ application/views/UpahHlCm/THR/V_cetakperhitungan.php
@@ -9,6 +9,9 @@
 		<th style="text-align: center;vertical-align: middle;">LOKASI KERJA</th>
 		<th style="text-align: center;vertical-align: middle;">MASUK KERJA</th>
 		<th style="text-align: center;vertical-align: middle;">MASA KERJA</th>
+		<th style="text-align: center;vertical-align: middle;">NO REKENING</th>
+		<th style="text-align: center;vertical-align: middle;">PEMILIK REKENING</th>
+		<th style="text-align: center;vertical-align: middle;">BANK</th>
 		<th style="text-align: center;vertical-align: middle;">NOMINAL THR</th>
 	</tr>
 </thead>
@@ -25,6 +28,9 @@
 				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo $dt['location_name'] ?></td>
 				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo date('d M Y',strtotime($dt['tgl_masuk'])) ?></td>
 				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo $dt['masa_kerja'] ?></td>
+				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo $dt['no_rekening'] ?></td>
+				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo $dt['atas_nama'] ?></td>
+				<td style="text-align: left;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo $dt['nama_bank'] ?></td>
 				<td style="text-align: right;vertical-align: middle;padding-left: 5px;padding-right: 5px;"><?php echo number_format($dt['nominal_thr'],2,',','.')  ?></td>
 			</tr>
 			<?php 
@@ -36,13 +42,15 @@
 </table>
 <br>
 <br>
-<table>
+<table style="width: 100%">
 	<tr>
+		<td></td>
 		<td></td>
 		<td style="text-align: right;">Yogyakarta, <?php echo date('d F Y',strtotime($waktu_dibuat)) ?></td>
 	</tr>
 	<tr>
 		<td style="text-align: center">Mengetahui,</td>
+		<td style="text-align: center">Menyetujui,</td>
 		<td style="text-align: center">Dibuat,</td>
 	</tr>
 	<tr>
@@ -54,11 +62,13 @@
 		<td>&nbsp;</td>
 	</tr>
 	<tr>
-		<td style="text-align: center"><?php echo $mengetahui->nama ?></td>
-		<td style="text-align: center"><?php echo $dibuat->nama ?></td>
+		<td style="text-align: center">Novita Sari</td>
+		<td style="text-align: center">Yoga Andriawan</td>
+		<td style="text-align: center">Subardi</td>
 	</tr>
 	<tr>
-		<td style="text-align: center"><?php echo $mengetahui->jabatan ?></td>
-		<td style="text-align: center"><?php echo $dibuat->jabatan ?></td>
+		<td style="text-align: center">Asisten Kepala Unit Akuntansi</td>
+		<td style="text-align: center">Kepala Seksi Madya</td>
+		<td style="text-align: center">Pekerja Staff Keuangan</td>
 	</tr>
 </table>
