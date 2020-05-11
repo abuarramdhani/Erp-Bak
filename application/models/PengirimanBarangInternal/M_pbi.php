d7b0603f17f11408127fbb7baf8244bf280e5696
aldi_pradana <aldi_pradana@quick.com>
2020-05-11 09:59:17 +0700

    add creation date

--- application/models/PengirimanBarangInternal/M_pbi.php
+++ application/models/PengirimanBarangInternal/M_pbi.php
@@ -67,7 +67,7 @@ class M_pbi extends CI_Model
                                    ->get('hrd_khs.tpribadi')
                                    ->row();
 
-        $sql = "SELECT distinct kki.doc_number, kki.user_tujuan, kki.seksi_tujuan, kki.tujuan, kki.seksi_kirim, kki.status,
+        $sql = "SELECT distinct kki.doc_number, kki.user_tujuan, kki.seksi_tujuan, kki.tujuan, kki.seksi_kirim, kki.status, kki.creation_date,
                  CASE
                     WHEN kki.status = 1
                        THEN 'Dipersiapkan Seksi Pengirim'
