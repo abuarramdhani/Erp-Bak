f8ca299251af85aed04ebc9bc03f41a54c1006b0
dicka_ismaji <dicka_ismaji@quick.com>
2020-05-26 13:09:59 +0700

    Perbaikan lembur

--- application/models/SPLSeksi/M_splseksi.php
+++ application/models/SPLSeksi/M_splseksi.php
@@ -312,7 +312,7 @@ class M_splseksi extends CI_Model{
 				where tdt.noind = '$noind'
 				and tdt.tanggal = '$tanggal'
 				and trim(tdt.kd_ket) = 'TM'";
-		return $this->prs->query($sql)->result_array();
+		return $this->prs->query($sql)->row();
 	}
 
 	public function getPresensi($noind,$tanggal){
