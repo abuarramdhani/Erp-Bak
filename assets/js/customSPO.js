34c589211ebb462ca2b2a426c4d64fb43da3a7d2
yusuf_taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2020-05-30 13:13:03 +0700

    Update BCC Email Address

--- assets/js/customSPO.js
+++ assets/js/customSPO.js
@@ -207,7 +207,7 @@ $(document).ready(function(){
         }
     });
 
-    $('#txtPMSPOBCCEmailAddr').val('purchasingsec12.quick@gmail.com').on({
+    $('#txtPMSPOBCCEmailAddr').val('purchasingsec12.quick1@gmail.com').on({
         'click': function() {
             $('.divPMSPOBCCEmailAddrWarn').fadeIn();
         },
