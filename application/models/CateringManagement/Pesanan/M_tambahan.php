6f90928827542997b458b8db42a943e9575f2a90
aji kurniawan <aji_kurniawan@quick.com>
2020-06-02 08:52:37 +0700

    menmbahkan menu pengurangan dan tambahan katering versi web

---
+++ application/models/CateringManagement/Pesanan/M_tambahan.php
@@ -0,0 +1,210 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_tambahan extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->personalia = $this->load->database('personalia',TRUE);
+    }
+
+    public function getActiveEmployeebyKey($key){
+    	$sql= "select noind,trim(nama) as nama
+				from hrd_khs.tpribadi
+				where keluar = '0'
+				and (
+					upper(noind) like concat(upper(?),'%')
+					or 
+					upper(nama) like concat('%',upper(?),'%')
+					)";
+    	return $this->personalia->query($sql,array($key,$key))->result_array();
+    }
+
+    public function getListTambahanByTanggal($tanggal){
+        $sql = "select  
+                    count(tamb.fs_tempat_makan), 
+                    tamb.fs_tempat_makan,
+                    shf.shift, 
+                    tamb.fn_jumlah_pesanan,
+                    case when tamb.fb_kategori = '1' then 
+                        'Lembur'
+                    when tamb.fb_kategori = '2' then 
+                        'Shift Tanggung'
+                    when tamb.fb_kategori = '3' then 
+                        'Tugas Ke Pusat'
+                    when tamb.fb_kategori = '4' then 
+                        'Tugas Ke Tuksono'
+                    when tamb.fb_kategori = '5' then 
+                        'Tugas Ke Mlati'
+                    when tamb.fb_kategori = '6' then 
+                        'Tamu'
+                    when tamb.fb_kategori = '7' then 
+                        'Cadangan'
+                    end fb_kategori, 
+                    tamb.id_tambahan, 
+                    tamb.fs_pemohon, 
+                    tamb.fs_keterangan
+                 from \"Catering\".tpesanantambahan tamb 
+                 left join \"Presensi\".tshift shf
+                 on tamb.fs_kd_shift = shf.kd_shift
+                 Where tamb.fd_tanggal= ?
+                 group by tamb.fs_tempat_makan, shf.shift, tamb.fn_jumlah_pesanan, tamb.fb_kategori,
+                     tamb.id_tambahan , tamb.fs_pemohon, tamb.fs_keterangan
+                 order by shf.shift,tamb.fb_kategori,tamb.fs_tempat_makan";
+        return $this->personalia->query($sql,array($tanggal))->result_array();
+    }
+
+    public function getKatering(){
+        $sql = "select 
+                    fs_tempat_makan,
+                    case when fs_lokasi = '1' then 
+                        'Yogyakarta'
+                    when fs_lokasi = '2' then 
+                        'Tuksono'
+                    when fs_lokasi = '3' then 
+                        'Mlati'
+                    else
+                        'Tidak Diketahui'
+                    end as lokasi
+                from \"Catering\".ttempat_makan
+                order by fs_lokasi,fs_tempat_makan";
+        return $this->personalia->query($sql)->result_array();
+    }
+
+    public function getTambahanByIdTambahan($id_tambahan){
+        $sql = "select *,
+                    (
+                        select trim(nama)
+                        from hrd_khs.tpribadi tp
+                        where tp.noind = tamb.fs_pemohon
+                    ) as nama_pemohon
+                From \"Catering\".tpesanantambahan tamb
+                left join \"Presensi\".tshift ts 
+                on ts.kd_shift = tamb.fs_kd_shift
+                Where tamb.id_tambahan = ? ";
+        return $this->personalia->query($sql,array($id_tambahan))->row();
+    }
+
+    public function getTambahanDetailByIdTambahan($id_tambahan){
+        $sql = "select *
+                from \"Catering\".tpesanantambahan_detail
+                where id_tambahan = ? ";
+        return $this->personalia->query($sql,array($id_tambahan))->result_array();
+    }
+
+    public function getUrutanByTanggalTempatMakanShift($tanggal,$tempat_makan,$shift){
+        $sql = "select fs_tanda 
+                from \"Catering\".tpesanan 
+                where fd_tanggal= ?
+                and fs_kd_shift = ?
+                and fs_tempat_makan = ? ";
+        return $this->personalia->query($sql,array($tanggal,$shift,$tempat_makan))->row();
+    }
+
+    public function getTambahanDetailByIdTambahanNoind($id_tambahan, $noind){
+        $sql = "select *
+                from \"Catering\".tpesanantambahan_detail
+                where id_tambahan = ?
+                and fs_noind = ? ";
+        return $this->personalia->query($sql,array($id_tambahan,$noind))->result_array();
+    }
+
+    public function insertTambahanDetail($id_tambahan,$noind){
+        $sql = "insert into \"Catering\".tpesanantambahan_detail 
+                (id_tambahan, fs_noind, fs_nama, fs_ket)
+                select ?,noind,trim(nama),trim(jabatan)
+                from hrd_khs.tpribadi
+                where noind = ? ";
+        return $this->personalia->query($sql,array($id_tambahan,$noind));
+    }
+
+    public function updateTambahanJumlahByIdTambahan($id_tambahan){
+        $sql = "update \"Catering\".tpesanantambahan
+                set fn_jumlah_pesanan = (
+                    select count(distinct fs_noind)
+                    from \"Catering\".tpesanantambahan_detail
+                    where id_tambahan = ?
+                )
+                where id_tambahan = ?";
+        return $this->personalia->query($sql,array($id_tambahan,$id_tambahan));
+    }
+
+    public function updateTambahanByIdTambahan($id_tambahan,$data){
+        $this->personalia->where('id_tambahan',$id_tambahan);
+        $this->personalia->update('"Catering".tpesanantambahan',$data);
+    }
+
+    public function getLokasiTempatMakanByTempatMakan($tempat_makan){
+        $sql = "select *
+                from \"Catering\".ttempat_makan
+                where fs_tempat_makan = ? ";
+        return $this->personalia->query($sql, array($tempat_makan))->row();
+    }
+
+    public function insertPesanan($data){
+        $this->personalia->insert('"Catering".tpesanan',$data);
+    }
+
+    public function getPesananByTempatMakanTanggalShift($tempat_makan,$tanggal,$shift){
+        $sql = "select *
+                from \"Catering\".tpesanan
+                where fd_tanggal = ?
+                and fs_tempat_makan = ?
+                and fs_kd_shift = ?";
+        return $this->personalia->query($sql, array($tanggal, $tempat_makan, $shift))->row();
+    }
+
+    public function getTotalTambahanByTempatMakanTanggalShift($tempat_makan,$tanggal,$shift){
+        $sql = "select coalesce(
+                    (
+                        select sum(fn_jumlah_pesanan)
+                        from \"Catering\".tpesanantambahan
+                        where fd_tanggal = ?
+                        and fs_tempat_makan = ?
+                        and fs_kd_shift = ?
+                    ),
+                    0
+                ) as jumlah";
+        return $this->personalia->query($sql, array($tanggal, $tempat_makan, $shift))->row();
+    }
+
+    public function getTotalPenguranganByTempatMakanTanggalShift($tempat_makan,$tanggal,$shift){
+        $sql = "select coalesce(
+                    (
+                        select sum(fn_jml_tdkpesan)
+                        from \"Catering\".tpenguranganpesanan
+                        where fd_tanggal = ?
+                        and fs_tempat_makan = ?
+                        and fs_kd_shift = ?
+                    ),
+                    0
+                ) as jumlah";
+        return $this->personalia->query($sql, array($tanggal, $tempat_makan, $shift))->row();
+    }
+
+    public function updatePesananByTempatMakanTanggalShift($data,$tempat_makan,$tanggal,$shift){
+        $this->personalia->where('fs_tempat_makan',$tempat_makan);
+        $this->personalia->where('fd_tanggal',$tanggal);
+        $this->personalia->where('fs_kd_shift',$shift);
+        $this->personalia->update('"Catering".tpesanan',$data);
+    }
+
+    public function insertTambahan($data){
+        $this->personalia->insert('"Catering".tpesanantambahan',$data);
+        return $this->personalia->insert_id();
+    }
+
+    public function deleteTambahanDetailByIdTambahan($id_tambahan){
+        $this->personalia->where('id_tambahan',$id_tambahan);
+        $this->personalia->delete('"Catering".tpesanantambahan_detail');
+    }
+
+    public function deleteTambahanByIdTambahan($id_tambahan){
+        $this->personalia->where('id_tambahan',$id_tambahan);
+        $this->personalia->delete('"Catering".tpesanantambahan');
+    }
+
+}
+
+?>
---
+++ application/views/CateringManagement/Pesanan/Tambahan/V_index.php
@@ -0,0 +1,273 @@
+<section class="content">
+	<div class="inner">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="row">
+					<div class="col-lg-12">
+						<h1><b><?= $Title ?></b></h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="box box-primary box-solid">
+							<div class="box-header with-border">
+								
+							</div>
+							<div class="box-body">
+								<div class="row">
+									<div class="col-lg-12">
+										<form class="form-horizontal" id="frm-CM-Tambahan-Form">
+											<div class="col-lg-6">
+												<div class="form-group">
+													<label class="control-label col-lg-4">Tanggal</label>
+													<div class="col-lg-8">
+														<input type="text" name="txt-CM-Tambahan-Tanggal" id="txt-CM-Tambahan-Tanggal" class="form-control" placeholder="Tanggal" autocomplete="off" required value="<?php echo date('Y-m-d') ?>">
+														<input type="hidden" name="txt-CM-Tambahan-Tanggal-Baru" id="txt-CM-Tambahan-Tanggal-Baru">
+														<input type="hidden" name="txt-CM-Tambahan-IdTambahan" id="txt-CM-Tambahan-IdTambahan">
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-lg-4">Tempat makan</label>
+													<div class="col-lg-8">
+														<select class="select2" style="width: 100%" data-placeholder="Tempat Makan" name="slc-CM-Tambahan-TempatMakan" id="slc-CM-Tambahan-TempatMakan" autocomplete="off" disabled required>
+															<option></option>
+															<?php 
+															if (isset($katering) && !empty($katering)) {
+																foreach ($katering as $kt) {
+																	?>
+																	<option value="<?php echo $kt['fs_tempat_makan'] ?>"><?php echo $kt['fs_tempat_makan'].' - '.$kt['lokasi'] ?></option>
+																	<?php
+																}
+															}
+															?>
+														</select>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-lg-4">Shift</label>
+													<div class="col-lg-8">
+														<select class="select2" style="width: 100%" data-placeholder="Shift" name="slc-CM-Tambahan-Shift" id="slc-CM-Tambahan-Shift" autocomplete="off" disabled required>
+															<option></option>
+															<option value="1">Shift 1 & Umum</option>
+															<option value="2">Shift 2</option>
+															<option value="3">Shift 3</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-lg-4">Kategori</label>
+													<div class="col-lg-8">
+														<select class="select2" style="width: 100%" data-placeholder="Kategori" name="slc-CM-Tambahan-Kategori" id="slc-CM-Tambahan-Kategori" disabled required>
+															<option></option autocomplete="off">
+															<option value="1">Lembur</option>
+															<option value="2">Shift Tanggung</option>
+															<option value="3">Tugas Ke Pusat</option>
+															<option value="4">Tugas Ke Tuksono</option>
+															<option value="5">Tugas Ke Mlati</option>
+															<option value="6">Tamu</option>
+															<option value="7">Cadangan</option>
+														</select>
+													</div>
+												</div>
+												<div class="form-group">
+													<label class="control-label col-lg-4">urutan</label>
+													<div class="col-lg-8">
+														<select class="select2" style="width: 100%" data-placeholder="Urutan" name="slc-CM-Tambahan-Urutan" id="slc-CM-Tambahan-Urutan" autocomplete="off" disabled required></select>
+													</div>
+												</div>
+												<div class="form-group">
+													<div class="col-lg-4 text-center">
+														<button type="button" class="btn btn-primary" id="btn-CM-Tambahan-Tambah">Tambah</button>
+													</div>
+													<div class="col-lg-4 text-center">
+														<button type="button" class="btn btn-warning" id="btn-CM-Tambahan-Edit" disabled>Edit</button>
+													</div>
+													<div class="col-lg-4 text-center">
+														<button type="button" class="btn btn-danger" id="btn-CM-Tambahan-Hapus" disabled>Hapus</button>
+													</div>
+												</div>
+											</div>
+											<div class="col-lg-6">
+												<div class="form-group" id="opt-CM-Tambahan-JumlahPesan" style="display: none;">
+													<label class="control-label col-lg-4">Jumlah Pesan</label>
+													<div class="col-lg-8">
+														<input type="text" name="txt-CM-Tambahan-JumlahPesan" id="txt-CM-Tambahan-JumlahPesan" class="form-control" placeholder="Jumlah Pesan" autocomplete="off" disabled required>
+													</div>
+												</div>
+												<div class="form-group" id="opt-CM-Tambahan-Pemohon" style="display: none;">
+													<label class="control-label col-lg-4">Pemohon</label>
+													<div class="col-lg-8">
+														<select class="slc-CM-Tambahan-Pekerja" name="slc-CM-Tambahan-Pemohon" id="slc-CM-Tambahan-Pemohon" data-placeholder="Pemohon" autocomplete="off" style="width: 100%" disabled required></select>
+													</div>
+												</div>
+												<div class="form-group" id="opt-CM-Tambahan-Keterangan" style="display: none;">
+													<label class="control-label col-lg-4">Keterangan</label>
+													<div class="col-lg-8">
+														<textarea name="txt-CM-Tambahan-Keterangan" id="txt-CM-Tambahan-Keterangan" class="form-control" placeholder="Keterangan" autocomplete="off" disabled required></textarea>
+													</div>
+												</div>
+												<div class="form-group" id="opt-CM-Tambahan-Penerima" style="display: none;">
+													<label class="control-label col-lg-4">Penerima</label>
+													<div class="col-lg-8">
+														<select class="slc-CM-Tambahan-Pekerja" name="slc-CM-Tambahan-Penerima" id="slc-CM-Tambahan-Penerima" autocomplete="off" data-placeholder="Penerima" style="width: 100%" disabled></select>
+													</div>
+												</div>
+												<div class="form-group" id="opt-CM-Tambahan-Penerima-Table" style="display: none;">
+													<div class="col-lg-12">
+														<style type="text/css">
+															#tbl-CM-Tambahan-Penerima-Table {
+																width: 100% !important;
+															}
+															
+															#tbl-CM-Tambahan-Penerima-Table td:nth-child(odd) {
+																text-align: center;
+																vertical-align: middle;
+															}
+
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollHeadInner {
+																width: 100% !important;
+															}
+															
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollHeadInner table {
+																width: 100% !important;
+															}
+
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollHead th:nth-child(1),
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollBody td:nth-child(1) {
+																width: 92px !important;
+																box-sizing: border-box !important;
+															}
+
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollHead th:nth-child(2),
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollBody td:nth-child(2) {
+																width: 325px !important;
+																box-sizing: border-box !important;
+															}
+
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollHead th:nth-child(3),
+															#opt-CM-Tambahan-Penerima-Table .dataTables_scrollBody td:nth-child(3) {
+																width: 70px !important;
+																box-sizing: border-box !important;
+															}
+
+															#tbl-CM-Tambahan-Penerima-Table > thead > tr > th,
+															#tbl-CM-Tambahan-Penerima-Table > tbody > tr > th,
+															#tbl-CM-Tambahan-Penerima-Table > tfoot > tr > th, 
+															#tbl-CM-Tambahan-Penerima-Table > thead > tr > td, 
+															#tbl-CM-Tambahan-Penerima-Table > tbody > tr > td, 
+															#tbl-CM-Tambahan-Penerima-Table > tfoot > tr > td {
+																padding: 1px !important;
+															}
+														</style>
+														<table class="table table-hover table-bordered table-striped" id="tbl-CM-Tambahan-Penerima-Table">
+															<thead class="bg-info">
+																<th style="text-align: center;vertical-align: middle;">No. Induk</th>
+																<th style="text-align: center;vertical-align: middle;">Nama</th>
+																<th style="text-align: center;vertical-align: middle;">Action</th>
+															</thead>
+															<tbody>
+															</tbody>
+														</table>
+													</div>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-lg-12">
+										<style type="text/css">
+											.dt-buttons {
+												float: left;
+											}
+											.dataTables_filter {
+												float: right;
+											}
+
+											.dataTables_info {
+												float: left
+											}
+										</style>
+										<table class="table table-striped table-bordered table-hover" id="tbl-CM-Tambahan-Table" style="width: 100%">
+											<thead>
+												<tr>
+													<th style="text-align: center;vertical-align: middle;">No.</th>
+													<th style="text-align: center;vertical-align: middle;">Tempat Makan</th>
+													<th style="text-align: center;vertical-align: middle;">Shift</th>
+													<th style="text-align: center;vertical-align: middle;">Status</th>
+													<th style="text-align: center;vertical-align: middle;">Jumlah</th>
+													<th style="text-align: center;vertical-align: middle;">Pemohon</th>
+													<th style="text-align: center;vertical-align: middle;">Keterangan</th>
+												</tr>
+											</thead>
+											<tbody>
+												<?php 
+												if (isset($tambahan) && !empty($tambahan)) {
+													$nomor = 1;
+													foreach ($tambahan as $dt) {
+														?>
+														<tr>
+															<td style="text-align: center;"><?php echo $nomor ?></td>
+															<td><?php echo $dt['fs_tempat_makan']."<input type='hidden' value='".$dt['id_tambahan']."'>" ?></td>
+															<td><?php echo $dt['shift'] ?></td>
+															<td><?php echo $dt['fb_kategori'] ?></td>
+															<td style="text-align: center;"><?php echo $dt['fn_jumlah_pesanan'] ?></td>
+															<td><?php echo $dt['fs_pemohon'] ?></td>
+															<td><?php echo $dt['fs_keterangan'] ?></td>
+														</tr>
+														<?php 
+														$nomor++;
+													}
+												}
+												?>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<style type="text/css">
+	.loading {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0,0,0,.5);
+}
+.loading-wheel {
+    width: 40px;
+    height: 40px;
+    margin-top: -80px;
+    margin-left: -40px;
+    
+    position: absolute;
+    top: 50%;
+    left: 50%;
+}
+.loading-wheel-2 {
+    width: 100%;
+    height: 20px;
+    margin-top: -50px;
+    
+    position: absolute;
+    top: 70%;
+    font-weight: bold;
+    font-size: 30pt;
+    color: white;
+    text-align: center;
+}
+
+</style>
+<div class="loading" id="CateringTambahanLoading" style="display: none;">
+	<div class="loading-wheel"><img height="100px" width="100px" src="<?php echo site_url('assets/img/gif/loadingquick.gif') ?>"></div>
+	<div class="loading-wheel-2">Permintaan Anda Sedang Di Proses ..</div>
+</div>
