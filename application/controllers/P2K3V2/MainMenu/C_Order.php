2654ce244a218635bf182d776ebfddef3b6a3698
enggal aldiansyah <enggal_aldiansyah@quick.com>
2020-06-12 13:17:57 +0700

    P2K3 Seksi V2
    update:
    > Jika ada yang Belum di transcart maka tidak bias input bon

--- application/controllers/P2K3V2/MainMenu/C_Order.php
+++ application/controllers/P2K3V2/MainMenu/C_Order.php
@@ -1589,7 +1589,7 @@ class C_Order extends CI_Controller
 					$listtobon[$i]['bonTrans'] += isset($arrc[$listtobon[$i]['kode_item']]) ? $arrc[$listtobon[$i]['kode_item']] : 0;
 				}
 			}
-			$data['canSubmit'] = (strpos(end($endDataTrans), 'Y') === false) ? false : true;
+			$data['canSubmit'] = (strpos(end($endDataTrans), 'N')) ? false : true;
 			$data['notrans'] = $endDataTrans['NO_BON'];
 		}
 		// echo "<pre>";
