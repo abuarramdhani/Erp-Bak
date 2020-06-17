d1519f99b8976985d8361626db8ac63d90e67aa2
aji kurniawan <aji_kurniawan@quick.com>
2020-06-17 11:28:57 +0700

    penambahan filter tanggal di cetak pesanan makan untuk pekerja makan khusus

--- application/models/CateringManagement/HitungPesanan/M_hitungpesanan.php
+++ application/models/CateringManagement/HitungPesanan/M_hitungpesanan.php
@@ -1551,6 +1551,7 @@ class M_hitungpesanan extends Ci_Model
 				from \"Catering\".t_pesanan_detail tpd
 				inner join \"Catering\".t_pekerja_menu_khusus tpmk
 				on tpd.noind = tpmk.noind
+				and tpd.fd_tanggal between tpmk.tanggal_mulai and tpmk.tanggal_selesai
 				left join hrd_khs.tpribadi tp 
 				on tpd.noind = tp.noind
 				where tpd.tanggal = ?
