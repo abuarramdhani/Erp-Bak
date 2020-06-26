d6442d77240950ffc2f2025284ea2ce3ed5fd020
amri <khoerul_amri@quick.com>
2020-06-26 12:57:52 +0700

    Update Covid

--- assets/covid19/index.php
+++ assets/covid19/index.php
@@ -526,6 +526,6 @@
 </head>
 <body>
   <div id="map"></div>
-  <div id="legend"><h3>Positif Dirawat</h3><h5>Data Update Per 2020-06-19</h5></div>
+  <div id="legend"><h3>Positif Dirawat</h3><h5>Data Update Per 2020-06-19 <br> Jateng : 2020-06-26</h5></div>
 </body>
 </html>
