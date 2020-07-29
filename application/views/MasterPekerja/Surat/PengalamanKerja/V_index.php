304eef5e06e8b009de74809c256963d92bba294a
meida <meida_rahma@quick.com>
2020-07-29 11:09:42 +0700

    surat_pengalaman

--- application/views/MasterPekerja/Surat/PengalamanKerja/V_index.php
+++ application/views/MasterPekerja/Surat/PengalamanKerja/V_index.php
@@ -36,15 +36,19 @@
 													if (isset($data) && !empty($data)) {
 														$nomor = 1;
 														foreach ($data as $dt) {
-
-
+															// 2020-07_065
+															$link = trim($dt['kd_surat'])."_".trim($dt['no_surat']);
+															$link = str_replace("/", "-", $link);
 															?>
 															<tr>
 																<td><?php echo $nomor ?></td>
 																<td style="text-align: center">
-																	<a href="<?php echo site_url('MasterPekerja/Surat/SuratIsolasiMandiri/Ubah') ?>" class="btn btn-primary">Edit</a>
-																	<a href="<?php echo site_url('MasterPekerja/Surat/SuratIsolasiMandiri/PDF') ?>" target="_blank" class="btn btn-warning">PDF</a>
-																	<a href="<?php echo site_url('MasterPekerja/Surat/SuratIsolasiMandiri/Hapus') ?>" class="btn btn-danger" onclick="return confirm('apakah anda yakin ingin menghapus data ini ?')">Delete</a>
+																	<a href="<?php echo site_url('MasterPekerja/Surat/PengalamanKerja/Ubah/'.$link) ?>" class="btn btn-primary">Edit</a>
+																	
+																	<!--<a href="<?php echo site_url('MasterPekerja/Surat/PengalamanKerja/PDF/'.$link) ?>" class="btn btn-warning">PDF</a> -->
+																	<a  data-value='<?php echo $link ?>'  data-noind='<?php echo $dt['noind'] ?>' data-sampai='<?php echo $dt['tgl_kena'] ?>' class="btn btn-warning Modal_pdf_pengalaman" >PDF</a> 
+																	<!-- <a data-toggle="modal" data-target="#Modal_pdf_pengalaman" value='$link' class="btn btn-warning" class="Modal_pdf_pengalaman" >PDF</a>  -->
+																	<a href="<?php echo site_url('MasterPekerja/Surat/PengalamanKerja/Hapus/'.$link) ?>" class="btn btn-danger" onclick="return confirm('apakah anda yakin ingin menghapus data ini ?')">Delete</a>
 																</td>
 																<td><?php echo $dt['no_surat'] ?></td>
 																<td><?php echo $dt['tgl_surat'] ?></td>
@@ -68,3 +72,50 @@
 		</div>
 	</div>
 </section>
+
+<!--a href="<?php echo site_url('MasterPekerja/Surat/PengalamanKerja/PDF/'.$link) ?>" -->
+
+<div class="modal fade" id="Modal_pdf_pengalaman" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document" style="width: 600px">
+      <div class="modal-content">
+        <form method="POST" action="<?php echo base_url("MasterPekerja/Surat/PengalamanKerja/PDF"); ?>" target="_blank">
+        <div class="modal-header text-center">
+          <button type="button" class="close hover" data-dismiss="modal">&times;</button>
+          <h3>Input Data</h3>
+        </div>
+        <div class="modal-body" style="width: 100%; text-align: center;">
+           <div class="row">
+             <div class="col-lg-12">
+               <label class="col-lg-12 text-center">Jabatan</label><br>
+               <input class="form-control col-lg-8 text-center" style="margin-left: 23%; width: 55%; text-align: center;" name="jabatan_pengalaman" id="jabatan_pengalaman" autocomplete="off" value="">
+               <input type="hidden" name="link_pengalaman" id="link_pengalaman">
+               <input type="hidden" name="noind_pengalaman" id="noind_pengalaman">
+               <input type="hidden" name="sampai_pengalaman" id="sampai_pengalaman">
+             </div>
+           </div>
+           <br>
+           <div class="row">
+             <div class="col-lg-12">
+               <label class="col-lg-12 text-center">NIK</label> <br>
+               <input class="form-control col-lg-8 text-center" style="margin-left: 23%; width: 55%; text-align: center;" name="nik_pengalaman" id="nik_pengalaman" autocomplete="off" value="">
+               
+           </select>
+           <input type="checkbox" id="cekNIK" name="cekNIK" value="nik">
+             </div>
+           </div>
+           <br>
+           <div class="row">
+             <div class="col-lg-12">
+               <label class="col-lg-12 text-center">Tanggal Cetak</label><br>
+               <input class="form-control col-lg-8 text-center" style="margin-left: 23%; width: 55%; text-align: center;" name="pengalaman_tglCetak" id="pengalaman_tglCetak" autocomplete="off" value="">
+             </div>
+           </div>
+         </div>
+        <div class="modal-footer" style="text-align: center;">
+          <button type="submit" class="btn btn-success" id="prev_Pengalaman">Cetak</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<!-- required nya tak ilangi dulu bu -->
--- application/views/MasterPekerja/Surat/PengalamanKerja/V_tambah.php
+++ application/views/MasterPekerja/Surat/PengalamanKerja/V_tambah.php
@@ -1,3 +1,8 @@
+<style type="text/css">
+	.select2 {
+		width: 100% !important;
+	}
+</style>
 <section class="content">
 	<div class="inner">
 		<div class="row">
@@ -28,6 +33,10 @@
 													<input type="text" name="txtMPSuratPengalamanKerjaSeksi" id="txtMPSuratPengalamanKerjaSeksi" class="form-control " placeholder="Seksi"  readonly required>
 												</div>
 											</div>
+											<input type="hidden"  value="<?php echo $nomor['0']['tahun_bulan'] ?>"  name="txtMPSuratPengalamanKerjaKodeSurat" id="txtMPSuratPengalamanKerjaKodeSurat">
+											<input type="hidden"  value="<?php echo $nomor['0']['no_selanjutnya'] ?>"  name="txtMPSuratPengalamanKerjaNoSurat" id="txtMPSuratPengalamanKerjaNoSurat">
+											<input type="hidden"  value="<?php echo $nomor['0']['tanggal_surat'] ?>"  name="txtMPSuratPengalamanKerjaTanggalSurat" id="txtMPSuratPengalamanKerjaTanggalSurat">
+											<input type="hidden" name="txtMPSuratPengalamanKerjaKodesie" id="txtMPSuratPengalamanKerjaKodesie">
 											<div class="form-group">
 												<label class="control-label col-lg-4">Bidang</label>
 												<div class="col-lg-4">
@@ -46,6 +55,16 @@
 													<input type="text" name="txtMPSuratPengalamanKerjaDept" id="txtMPSuratPengalamanKerjaDept" class="form-control " placeholder="Departemen"  readonly required>
 												</div>
 											</div>
+											<!--<div class="form-group">
+												<label class="control-label col-lg-4">Jabatan</label>
+												<div class="col-lg-4">
+													<select name="txtMPSuratPengalamanKerjaJabatan" id="txtMPSuratPengalamanKerjaJabatan" class="form-control ">
+    															
+    															<option value=""></option>
+    												</select>
+												</div>
+											</div>
+											-->
 											<div class="form-group">
 												<label class="control-label col-lg-4">Masuk</label>
 												<div class="col-lg-4">
@@ -55,7 +74,10 @@
 											<div class="form-group">
 												<label class="control-label col-lg-4">Sampai</label>
 												<div class="col-lg-4">
-													<input type="text" name="txtMPSuratPengalamanKerjaSampai" id="txtMPSuratPengalamanKerjaSampai" class="form-control " placeholder="Sampai"  readonly required>
+													<select name="txtMPSuratPengalamanKerjaSampai" id="txtMPSuratPengalamanKerjaSampai" class="form-control ">
+    															<option value=""></option>
+    															
+    												</select>
 												</div>
 											</div>
 											<div class="form-group">
@@ -91,18 +113,33 @@
 											<div class="form-group">
 												<label class="control-label col-lg-4">Template Isi</label>
 												<div class="col-lg-4">
-													<input type="text" name="txtMPSuratPengalamanKerjaTemplate" id="" class="form-control " placeholder="" required>
+													<select class="form-control select4" id="pengalaman" name="TxtIsiSuratPengalaman" data-placeholder="Pilih isi surat" required>
+											             <option></option>
+										               <?php foreach ($isisuratpengalaman as $isi) {?>
+											            <option value="<?php echo $isi['kd_isi']?>"><?php echo $isi['kd_isi']?></option>
+										             <?php }?>
+									                     </select>
 												</div>
+												<div class="col-lg-2">
+                                                  <button type="button" data-toggle="modal" data-target="#modalTemplateIsi" class="btn btn-default btn-sm"><i class="fa fa-plus"></i></button>
+                                                </div>
+											</div>
+											
+											<div class="form-group">
+											  <label class="control-label col-lg-4"> Isi</label>
+												<div class="col-lg-6">
+                                                            <textarea required class="redactor MasterPekerja-Surat-txaPreview" name="txaPreview" id=""></textarea>
+                                                 </div>
 											</div>
 											<div class="form-group">
-												<label class="control-label col-lg-4">Tanda Tangan</label>
+												<label class="control-label col-lg-4">Pengembalian ADP</label>
 												<div class="col-lg-4">
-													<select class="slcMPSuratPengalamanKerjaPekerja" data-placeholder="Tanda Tangan" name="slcMPSuratPengalamanKerjaPekerja" id="slcMPSuratPengalamanKerjaPekerja" style="width: 100%" required></select>
+													<input type="text" name="txtMPSuratPengalamanKerjaAPD" id="txtMPSuratPengalamanKerjaAPD" class="form-control " placeholder="Pengembalian APD"  readonly  required>
 												</div>
 											</div>
 											<div class="form-group">
 												<div class="col-lg-6 text-right">
-													<button class="btn btn-primary" id="btnMPSuratPengalamanKerjaSubmit" type="submit" disabled><span class="fa fa-save"></span>&nbsp;Simpan</button>
+													<button class="btn btn-primary" id="btnMPSuratPengalamanKerjaSubmit" type="submit" ><span class="fa fa-save"></span>&nbsp;Simpan</button>
 												</div>
 											</div>
 										</form>
@@ -116,3 +153,218 @@
 		</div>
 	</div>
 </section>
+
+
+<!-- Modal Loading -->
+<div class="modal fade" id="modalTemplateIsi" role="dialog" aria-labelledby="loadMeLabel">
+  <div class="modal-dialog modal-md">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Tambah Template Isi</h4>
+      </div>
+      <div class="modal-body">
+        <div class="loader"></div>
+        <div clas="loader-txt">
+          <div class="form-group">
+	          <div class="col-lg-12">
+	            <label class="control-label col-lg-3">Template Isi</label>
+	            <div class="col-lg-9">
+	                <select class="form-control" id="template" name="TxtTemplateSuratPengalaman" data-placeholder="Pilih isi surat" required>
+						<option></option>
+						<?php foreach ($isisuratpengalaman as $isi) {?>
+						<option value="<?php echo $isi['kd_isi']?>"><?php echo $isi['kd_isi']?></option>
+						<?php }?>
+					</select>
+					<input class="form-control hidden" id="template_name" placeholder="Masukkan nama template"></input>
+	            </div>
+	          </div>
+
+	          <div class="col-md-12">
+	         	<label class="control-label col-lg-3"> Isi</label>
+				<div class="col-lg-9">
+	                <textarea required readonly class="redactor form-control MasterPekerja-SuratPengalaman-txaPreview" name="txaPreviewne" id="template_content"></textarea>
+	            </div>
+	          </div>
+	        </div>
+          <div class="text-center">
+            <button class="btn btn-primary" type="button" onclick="addTemplateSatuan()" id="btn-addSatuan" name="button">
+            	 Tambah
+            </button>
+            <button class="btn btn-primary hidden" type="button" data-action="save" id="btn-saveSatuan" name="button">
+            	 Simpan
+            </button>
+            <button class="btn btn-primary" type="button" onclick="editTemplateSatuan()" id="btn-editSatuan" name="button">
+            	 Edit
+            </button>
+            <button class="btn btn-primary hidden" type="button" onclick="batalTemplateSatuan()" id="btn-batalSatuan" name="button">
+            	 Batal
+            </button>
+            <button class="btn btn-primary" type="button" onclick="deleteTemplateSatuan()" id="btn-deleteSatuan" name="button">
+            	Delete
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
+<!-- End Modal -->
+
+<script type="text/javascript">
+	// plugin init
+	$(window).load(() => {
+		$('#template').select2()
+		//$('#template_content').redactor('core.editor').attr('contenteditable', 'false');
+	})
+
+	function request_edit(kd_isi, isi_surat) {
+		$.ajax({
+			url: baseurl + 'MasterPekerja/Surat/PengalamanKerja/edit_template',
+			method: 'POST',
+			data: {
+				kd_isi,
+				isi_surat
+			},
+			success() {
+				$('#btn-saveSatuan').addClass('hidden')
+				$('#btn-batalSatuan').addClass('hidden')
+				$('#btn-addSatuan').removeClass('hidden')
+				$('#btn-editSatuan').removeClass('hidden')
+
+				$('#template').next('.select2-container').removeClass('hidden')
+				$('#template_name').addClass('hidden')
+
+				$('#template_content').prop('disabled', true)
+			}
+		})
+	}
+
+	function request_save(kd_isi, isi_surat) {
+		$.ajax({
+			url: baseurl + 'MasterPekerja/Surat/PengalamanKerja/add_template',
+			method: 'POST',
+			data: {
+				kd_isi,
+				isi_surat
+			},
+			success() {
+				$('#btn-saveSatuan').addClass('hidden')
+				$('#btn-batalSatuan').addClass('hidden')
+				$('#btn-addSatuan').removeClass('hidden')
+				$('#btn-editSatuan').removeClass('hidden')
+
+				$('#template').next('.select2-container').removeClass('hidden')
+				$('#template_name').addClass('hidden')
+
+				$('#template_content').prop('disabled', true)
+				$('#template').append(`<option value="${kd_isi}">${isi_surat}</option>`)
+				$('#template_content').redactor('set', '')
+
+			}
+		})
+	}
+
+	// toogle list and add view
+	function toggle() {
+		$('#btn-batalSatuan').toggleClass('hidden')
+		$('#btn-addSatuan').toggleClass('hidden')
+		$('#btn-editSatuan').toggleClass('hidden')
+		$('#btn-saveSatuan').toggleClass('hidden')
+		$('#template').next('.select2-container').toggleClass('hidden')
+	}
+
+	// button tambah
+	function addTemplateSatuan() {
+		// show simpan, show batal
+		$('#template').addClass('hidden') //select2
+		$('#template_name').removeClass('hidden') // inputbox
+		$('#btn-batalSatuan').removeClass('hidden')
+		$('#btn-saveSatuan').removeClass('hidden')
+		$('#btn-addSatuan').addClass('hidden')
+		$('#btn-editSatuan').addClass('hidden')
+		$('#template').next('.select2-container').addClass('hidden')
+
+		$('#template_content').val('')
+		$('#template_name').val('')
+
+		$('#template_content').prop('disabled', false)
+		$('btn-saveSatuan').data('action', 'save')
+		$('#template_content').redactor('set', '')
+
+	}
+
+	// button edit
+	function editTemplateSatuan() {
+		// show simpan, show batal
+		$('#template').removeClass('hidden')
+		$('#btn-addSatuan').addClass('hidden')
+		$('#btn-editSatuan').addClass('hidden')
+		$('#btn-batalSatuan').removeClass('hidden')
+		$('#btn-saveSatuan').removeClass('hidden')
+
+		$('#btn-saveSatuan').data('action', 'edit')
+	}
+
+	// button simpan, -> tambah / edit
+	$('#btn-saveSatuan').on('click', function() {
+		const action = $(this).data('action')
+
+		const template = $('#template').val()
+		const template_name = $('#template_name').val()
+		const template_content = $('#template_content').val()
+
+		console.log(template, template_name, template_content)
+		if(action === 'save') {
+			if(!template_name || !template_content) return
+			request_save(template_name, template_content)
+		} else if(action === 'edit') {
+			if(!template || !template_content) return
+			request_edit(template, template_content)
+		}
+	})
+
+	// button delete
+	function deleteTemplateSatuan() {
+		const template_name = $('#template').val()
+
+		if(!template_name ) return
+
+		swal.fire({
+			title: 'Apakah anda yakin untuk menghapus template ini ?',
+			text: '',
+			showCancelButton: true,
+			type: 'question'
+		}).then(({  value }) => {
+			if(!value) return 
+
+			$.ajax({
+				url: baseurl + 'MasterPekerja/Surat/PengalamanKerja/delete_template',
+				method: 'POST',
+				data: {
+					kd_isi: template_name
+				},
+				success() {
+					$('#template_content').redactor('set', '')
+					$(`#template option[value="${template_name}"]`).remove()
+					$('#template').val('').trigger('change')
+				}
+			})
+		})
+	}
+
+	// button batal
+	function batalTemplateSatuan() {
+		// hide simpan, batal
+		$('#btn-batalSatuan').addClass('hidden')
+		$('#btn-saveSatuan').addClass('hidden')
+		$('#btn-addSatuan').removeClass('hidden')
+		$('#btn-editSatuan').removeClass('hidden')
+		$('#template').next('.select2-container').removeClass('hidden')
+		$('#template_name').addClass('hidden')
+	}
+</script>
