343b7723b3bcbfa38f56ef1025539d7bed2d0edc
aldi_pradana <aldi_pradana@quick.com>
2020-08-13 07:36:42 +0700

    jtip

--- application/models/JTIPembelian/M_jtipembelian.php
+++ application/models/JTIPembelian/M_jtipembelian.php
@@ -5,7 +5,7 @@ class M_jtipembelian extends CI_Model
     {
         parent::__construct();
         $this->load->database();
-        $this->oracle = $this->load->database('oracle_dev', true);
+        $this->oracle = $this->load->database('oracle', true);
     }
 
     public function updateNamaDriver($data, $param)
