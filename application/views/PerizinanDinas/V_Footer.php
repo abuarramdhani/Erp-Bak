f69a8ebdab55ae958d05dd1ba9648e27a32bb23b
rosyidatun_nur_r <rosyidatun_nur_r@quick.com>
2020-08-25 11:39:58 +0700

    Delete some Plugins on Perizinan Footer

--- application/views/PerizinanDinas/V_Footer.php
+++ application/views/PerizinanDinas/V_Footer.php
@@ -1,42 +1,32 @@
 </div>
 </div>
 <footer class="main-footer" style="margin:0;">
-<div class="pull-right hidden-xs">
-    Page rendered in <strong>{elapsed_time}</strong> seconds.
-    <strong>Copyright &copy; Quick 2015<?php if(date('Y')>2015) { echo '-'.date('Y'); } ?>.</strong> All rights reserved.
-</div>
-<b>Version</b> 1.0.0
+    <div class="pull-right hidden-xs">
+        Page rendered in <strong>{elapsed_time}</strong> seconds.
+        <strong>Copyright &copy; Quick 2015<?php if (date('Y') > 2015) {
+                                                echo '-' . date('Y');
+                                            } ?>.</strong> All rights reserved.
+    </div>
+    <b>Version</b> 1.0.0
 </footer>
-<script>const baseurl = "<?= base_url() ?>";</script>
+<script>
+    const baseurl = "<?= base_url() ?>";
+</script>
 <script src="<?= base_url('assets/plugins/slimScroll/jquery.slimscroll.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/fastclick/fastclick.min.js') ?>"></script>
 <script src="<?= base_url('assets/theme/js/app.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/datatables-latest/datatables.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table2excel/jquery.table2excel.min.js');?>"></script>
-<script src="<?= base_url('assets/plugins/sweetalert2.all.min.js');?>"></script>
-<script src="<?= base_url('assets/plugins/sweetalert2.all.js');?>"></script>
-<script src="<?= base_url('assets/plugins/canvasjs/canvasjs.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/chartjs/Chart.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/moment.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/table2excel/jquery.table2excel.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/sweetalert2.all.min.js'); ?>"></script>
+<script src="<?= base_url('assets/plugins/sweetalert2.all.js'); ?>"></script>
 <script src="<?= base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/input-mask/3.x') ?>/dist/jquery.inputmask.bundle.js"></script>
-<script src="<?= base_url('assets/plugins/input-mask/3.x') ?>/dist/inputmask/phone-codes/phone.js"></script>
-<script src="<?= base_url('assets/plugins/input-mask/3.x') ?>/dist/inputmask/phone-codes/phone-be.js"></script>
-<script src="<?= base_url('assets/plugins/input-mask/3.x') ?>/dist/inputmask/phone-codes/phone-ru.js"></script>
-<script src="<?= base_url('assets/plugins/input-mask/3.x') ?>/dist/inputmask/bindings/inputmask.binding.js"></script>
 <script src="<?= base_url('assets/plugins/intro.js-2.9.3/intro.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/multiselect/js/bootstrap-multiselect.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/touchspin/jquery.bootstrap-touchspin.min.js')?>"></script>
 <script src="<?= base_url('assets/plugins/fine-uploader/jquery.fine-uploader.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/fine-uploader/fine-uploader.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/chartjs/Chart.bundle.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/redactor/js/redactor.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/mdtimepicker/mdtimepicker.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/html2canvas/html2canvas.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/highchart/highcharts.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/highchart/exporting.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/highchart/offline-exporting.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/jquery.number.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/uniform/jquery.uniform.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/inputlimiter/jquery.inputlimiter.1.3.1.min.js') ?>"></script>
@@ -45,7 +35,6 @@
 <script src="<?= base_url('assets/plugins/colorpicker/js/bootstrap-colorpicker.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/tagsinput/jquery.tagsinput.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/validVal/js/jquery.validVal.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/dropzone/dropzone.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/daterangepicker-master/moment.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/daterangepicker-master/moment-precise-range.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/datepicker/js/bootstrap-datepicker.js') ?>"></script>
@@ -57,7 +46,6 @@
 <script src="<?= base_url('assets/plugins/switch/static/js/bootstrap-switch.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/jquery.dualListbox-1.3/jquery.dualListBox-1.3.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/autosize/jquery.autosize.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/jasny/js/bootstrap-inputmask.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/jquery-validation-1.11.1/dist/jquery.validate.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/validator/bootstrapValidator.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/validator/bootstrapValidator.js') ?>"></script>
@@ -65,20 +53,11 @@
 <script src="<?= base_url('assets/plugins/iCheck/icheck.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/jquery.toaster/jquery.toaster.js') ?>"></script>
 <script src="<?= base_url('assets/js/formsInit.js') ?>"></script>
-<script src="<?= base_url('assets/js/ajaxSearch.js')?>"></script>
-<script src="<?= base_url('assets/js/HtmlFunction.js')?>"></script>
+<script src="<?= base_url('assets/js/ajaxSearch.js') ?>"></script>
+<script src="<?= base_url('assets/js/HtmlFunction.js') ?>"></script>
 <script src="<?= base_url('assets/js/ChainArea.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/jQuery/jquery.toaster.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/qtip/jquery.qtip.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/jasny-bootstrap.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/inputmask/inputmask.bundle.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/sweetAlert/sweetalert.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/table2csv.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/tableHTMLExport.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/jspdf.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/jspdf.plugin.autotable.min.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/tableExport.js') ?>"></script>
-<script src="<?= base_url('assets/plugins/table-to-CSV/FileSaver.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/monthPicker/monthpicker.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/print/print.min.js') ?>"></script>
 <script src="<?= base_url('assets/js/formValidation.js') ?>"></script>
@@ -95,35 +74,37 @@
 
 
 <script>
-var id_gd;
-if(counter_row <= 0) { var counter_row = 0; }
-$(function() {
-    $('#dataTables-example').dataTable({
-        "bSort" : false
-    });
-    $('#dataTables-customer').dataTable({
-        "bSort" : false,
-        "searching": false,
-        "bLengthChange": false,
-        "bDestroy": true
-    });
-    $(".textarea").wysihtml5();
-    $('.pp-date').datepicker({
-        "autoclose": true,
-        "todayHiglight": true,
-        "allowClear" : true,
-        "format": 'dd M yyyy'
-    });
-    $('[data-toggle="tooltip"]').tooltip();
-    formInit();
-    <?php
-        if($this->session->flashdata('delete-menu-respond')) {
-            switch($this->session->flashdata('delete-menu-respond')) {
+    var id_gd;
+    if (counter_row <= 0) {
+        var counter_row = 0;
+    }
+    $(function() {
+        $('#dataTables-example').dataTable({
+            "bSort": false
+        });
+        $('#dataTables-customer').dataTable({
+            "bSort": false,
+            "searching": false,
+            "bLengthChange": false,
+            "bDestroy": true
+        });
+        $(".textarea").wysihtml5();
+        $('.pp-date').datepicker({
+            "autoclose": true,
+            "todayHiglight": true,
+            "allowClear": true,
+            "format": 'dd M yyyy'
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+        formInit();
+        <?php
+        if ($this->session->flashdata('delete-menu-respond')) {
+            switch ($this->session->flashdata('delete-menu-respond')) {
                 case 1:
-                    if($this->session->flashdata('delete-menu-name')) {
+                    if ($this->session->flashdata('delete-menu-name')) {
                         echo "
                             Swal.fire({
-                                text: 'Terjadi kesalahan saat menghapus menu ' + '".$this->session->flashdata('delete-menu-name')."',
+                                text: 'Terjadi kesalahan saat menghapus menu ' + '" . $this->session->flashdata('delete-menu-name') . "',
                                 confirmButtonText: 'Tutup',
                                 type: 'error'
                             });
@@ -131,10 +112,10 @@ $(function() {
                     }
                     break;
                 case 2:
-                    if($this->session->flashdata('delete-menu-name')) {
+                    if ($this->session->flashdata('delete-menu-name')) {
                         echo "
                             Swal.fire({
-                                text: 'Menu ' + '".$this->session->flashdata('delete-menu-name')."' + ' berhasil dihapus',
+                                text: 'Menu ' + '" . $this->session->flashdata('delete-menu-name') . "' + ' berhasil dihapus',
                                 confirmButtonText: 'Tutup',
                                 type: 'success'
                             });
@@ -143,13 +124,13 @@ $(function() {
                     break;
             }
         }
-        if($this->session->flashdata('delete-menu-list-respond')) {
-            switch($this->session->flashdata('delete-menu-list-respond')) {
+        if ($this->session->flashdata('delete-menu-list-respond')) {
+            switch ($this->session->flashdata('delete-menu-list-respond')) {
                 case 1:
-                    if($this->session->flashdata('delete-menu-list-name')) {
+                    if ($this->session->flashdata('delete-menu-list-name')) {
                         echo "
                             Swal.fire({
-                                text: 'Terjadi kesalahan saat menghapus menu list ' + '".$this->session->flashdata('delete-menu-list-name')."',
+                                text: 'Terjadi kesalahan saat menghapus menu list ' + '" . $this->session->flashdata('delete-menu-list-name') . "',
                                 confirmButtonText: 'Tutup',
                                 type: 'error'
                             });
@@ -157,10 +138,10 @@ $(function() {
                     }
                     break;
                 case 2:
-                    if($this->session->flashdata('delete-menu-list-name')) {
+                    if ($this->session->flashdata('delete-menu-list-name')) {
                         echo "
                             Swal.fire({
-                                text: 'Menu list ' + '".$this->session->flashdata('delete-menu-list-name')."' + ' berhasil dihapus',
+                                text: 'Menu list ' + '" . $this->session->flashdata('delete-menu-list-name') . "' + ' berhasil dihapus',
                                 confirmButtonText: 'Tutup',
                                 type: 'success'
                             });
@@ -169,8 +150,8 @@ $(function() {
                     break;
             }
         }
-        if($this->session->flashdata('delete-sub-menu-respond')) {
-            switch($this->session->flashdata('delete-sub-menu-respond')) {
+        if ($this->session->flashdata('delete-sub-menu-respond')) {
+            switch ($this->session->flashdata('delete-sub-menu-respond')) {
                 case 1:
                     echo "
                         Swal.fire({
@@ -191,37 +172,41 @@ $(function() {
                     break;
             }
         }
-    ?>
-});
+        ?>
+    });
 
-function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode;
-    if(charCode > 31 && (charCode < 48 || charCode > 57)) return false;
-    return true;
-}
+    function isNumberKey(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) return false;
+        return true;
+    }
 
-function noInput(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode
-    if(charCode != 13) return false;
-    return true;
-}
+    function noInput(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode
+        if (charCode != 13) return false;
+        return true;
+    }
 
-function callModal(link) {
-    $('#myModal').modal({ show: true, remote: link });
-}
-$(document).ready(function () {
-    $('.logmenu1[href!=#], .logmenu2[href!=#], logmenu3[href!=#]').click(function () {
-        let menu = $(this).text().trim()
+    function callModal(link) {
+        $('#myModal').modal({
+            show: true,
+            remote: link
+        });
+    }
+    $(document).ready(function() {
+        $('.logmenu1[href!=#], .logmenu2[href!=#], logmenu3[href!=#]').click(function() {
+            let menu = $(this).text().trim()
 
-                $.ajax({
-                    data: {
-                        menu1: menu
-                    },
-                    type: 'POST',
-                    url: baseurl + 'getLog'
-                });
+            $.ajax({
+                data: {
+                    menu1: menu
+                },
+                type: 'POST',
+                url: baseurl + 'getLog'
+            });
+        })
     })
-})
 </script>
 </body>
+
 </html>
