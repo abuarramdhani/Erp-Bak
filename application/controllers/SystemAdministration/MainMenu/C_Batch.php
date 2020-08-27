ca9fcdb325862fedb43bef2d3b8e256b4ffbb722
rosyidatun_nur_r <rosyidatun_nur_r@quick.com>
2020-08-27 13:37:33 +0700

    Kaizen - fitur delete responsibility batch

--- application/controllers/SystemAdministration/MainMenu/C_Batch.php
+++ application/controllers/SystemAdministration/MainMenu/C_Batch.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 defined('BASEPATH') or exit('tidak ada sistem yang aman');
 
 class C_Batch extends CI_Controller
@@ -10,54 +10,68 @@ class C_Batch extends CI_Controller
     }
 
     // @return render view
-    function index() {
+    function index()
+    {
         $this->load->model('SystemAdministration/MainMenu/M_user');
         $this->load->model('SystemAdministration/MainMenu/M_responsibility');
         $this->load->model('SystemAdministration/MainMenu/M_menu');
 
         $user_id = $this->session->userid;
 
-		$data['Menu'] = 'User';
-		$data['SubMenuOne'] = '';
+        $data['Menu'] = 'User';
+        $data['SubMenuOne'] = '';
 
-		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
-		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
-        $data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
-        
-        $this->load->view('V_Header',$data);
-		$this->load->view('V_Sidemenu',$data);
-		$this->load->view('SystemAdministration/MainMenu/Batch/V_Index',$data);
-		$this->load->view('V_Footer',$data);
+        $data['UserMenu'] = $this->M_user->getUserMenu($user_id, $this->session->responsibility_id);
+        $data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id, $this->session->responsibility_id);
+        $data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id, $this->session->responsibility_id);
+
+        $this->load->view('V_Header', $data);
+        $this->load->view('V_Sidemenu', $data);
+        $this->load->view('SystemAdministration/MainMenu/Batch/V_Index', $data);
+        $this->load->view('V_Footer', $data);
     }
 
     // @params POST
     // @return echo boolean
-    function addResponsibility() {
+    function addResponsibility()
+    {
         $params = [
-            'noind' => $this->input->post('noind'),// array
+            'noind' => $this->input->post('noind'), // array
             'inet'  => $this->input->post('inet'), // 1 / 0
             'local' => $this->input->post('local'),
-            'res_id'=> $this->input->post('res_id') // example: 1289
+            'res_id' => $this->input->post('res_id'), // example: 1289
         ];
-
-        echo $this->M_batch->addResponsbility($params) ? json_encode(array(
-            'success' => true,
-            'message' => "Sukses menambahkan responsbility"
-        )) : json_encode(array(
-            'success' => false,
-            'message' => "Terjadi kesalahan"
-        ));
+        $btn_val = $this->input->post('btn_val');
+        if ($btn_val == 'addRespo') {
+            echo $this->M_batch->addResponsbility($params) ? json_encode(array(
+                'success' => true,
+                'message' => "Sukses menambahkan responsbility"
+            )) : json_encode(array(
+                'success' => false,
+                'message' => "Terjadi kesalahan"
+            ));
+        } else {
+            $noind = str_replace(" ", "', '", $params['noind']);
+            echo $this->M_batch->deleteResponsbility($noind, $params['res_id']) ? json_encode(array(
+                'success' => true,
+                'message' => "Sukses menghapus responsbility"
+            )) : json_encode(array(
+                'success' => false,
+                'message' => "Terjadi kesalahan"
+            ));
+        }
     }
 
     // @params GET
     // @return json
-    function preview_person() {
-        if(!isset($_GET['noind'])) {
-        echo json_encode(array(
-            'success' => false,
-            'data' => null
-        ));
-        return false;
+    function preview_person()
+    {
+        if (!isset($_GET['noind'])) {
+            echo json_encode(array(
+                'success' => false,
+                'data' => null
+            ));
+            return false;
         }
 
         $noind = $_GET['noind'];
@@ -77,10 +91,11 @@ class C_Batch extends CI_Controller
     }
 
     // @return json
-    function getResponsbility() {
+    function getResponsbility()
+    {
         $this->load->model('SystemAdministration/MainMenu/M_responsibility');
         $data = $this->M_responsibility->getResponsibility();
-        
+
         echo json_encode($data);
     }
 }
