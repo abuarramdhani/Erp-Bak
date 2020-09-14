a62affef35cb5a1d1acd738f29b05c5e3be7d359
dicka_ismaji <dicka_ismaji@quick.com>
2020-09-14 08:59:44 +0700

    Fix long number is not display correctly

--- application/controllers/MasterPekerja/Pekerja/PencarianPekerja/C_Index.php
+++ application/controllers/MasterPekerja/Pekerja/PencarianPekerja/C_Index.php
@@ -139,10 +139,9 @@ class C_Index extends CI_Controller
       /**
        * For Debugging
        */
-      $number_cell_type = @$_GET['type'] ?: '0';
+      $number_cell_type = @$_GET['type'] ?: null;
       $file_format = @$_GET['format'] ?: ".xlsx";
       $excel_type = @$_GET['excel'] ?: 'Excel5';
-      $is_explicit = isset($_GET['explicit']);
       if (isset($_GET['debug'])) debug($data);
       if (isset($_GET['phpinfo'])) return phpinfo();
 
@@ -228,11 +227,8 @@ class C_Index extends CI_Controller
             $objPHPExcel->getActiveSheet()->getStyle($cell)->getNumberFormat()->setFormatCode($number_cell_type ?: PHPExcel_Style_NumberFormat::FORMAT_TEXT);
           }
 
-          if ($is_explicit) {
-            $objPHPExcel->getActiveSheet()->setCellValueExplicit($cell, $value, 's')->getStyle($cell)->applyFromArray($EXCEL_STYLE['bordered']);
-          } else {
-            $objPHPExcel->getActiveSheet()->setCellValue($cell, $value)->getStyle($cell)->applyFromArray($EXCEL_STYLE['bordered']);
-          }
+          $objPHPExcel->getActiveSheet()->setCellValueExplicit($cell, $value, PHPExcel_Cell_DataType::TYPE_STRING)->getStyle($cell)->applyFromArray($EXCEL_STYLE['bordered']);
+          // $objPHPExcel->getActiveSheet()->setCellValue($cell, $value)->getStyle($cell)->applyFromArray($EXCEL_STYLE['bordered']);
 
           // $objPHPExcel->getActiveSheet()->setCellValue($cell, $value)->getStyle('E' . $x)->applyFromArray($style_col1)->getNumberFormat()->setFormatCode('#,#0.##;[Red]-#,#0.##');
           // set cell format to number
