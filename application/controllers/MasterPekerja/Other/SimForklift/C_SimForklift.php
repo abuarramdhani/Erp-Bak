3918054837a9e6e94cade30f87bf6aaee235bb5c
aji kurniawan <aji_kurniawan@quick.com>
2020-09-25 14:26:48 +0700

    update sim forklift

--- application/controllers/MasterPekerja/Other/SimForklift/C_SimForklift.php
+++ application/controllers/MasterPekerja/Other/SimForklift/C_SimForklift.php
@@ -129,8 +129,260 @@ class C_SimForklift extends CI_Controller
 	}
 
 	public function cetakimg(){
-		echo "<pre>";
-		print_r($_GET);
+		$id = $this->input->get('data');
+		$pekerja = array();
+		foreach ($id as $key) {
+			$pekerja[] = $this->M_simforklift->getSimForkliftById($key);
+		}
+		// Create the size of image or blank image 
+		$jumlah_pekerja = count($pekerja);
+		$jumlah_samping = 5;
+		if ($jumlah_pekerja > $jumlah_samping) {
+			$lebar = 204 * $jumlah_samping;
+			$tinggi = ceil($jumlah_pekerja/$jumlah_samping)*648;
+		}else{
+			$lebar = 204 * $jumlah_pekerja;
+			$tinggi = 648;
+		}
+
+		$image = imagecreatetruecolor($lebar, $tinggi); 
+		
+		$bulan = array(
+			1 => 'Januari',
+			'Februari',
+			'Maret',
+			'April',
+			'Mei',
+			'Juni',
+			'Juli',
+			'Agustus',
+			'September',
+			'Oktober',
+			'November',
+			'Desember'
+		);
+
+		// Set the background color of image 
+		$background_color = imagecolorallocate($image, 0, 153, 0); 
+		  
+		// Set the text color of image 
+		$text_front_utama = imagecolorallocate($image, 255, 255, 255); 
+		$text_front_cadangan = imagecolorallocate($image, 255, 255, 0); 
+		$text_back_black = imagecolorallocate($image, 0, 0, 0); 
+		$text_back_white = imagecolorallocate($image, 255, 255, 255); 
+		  
+		// Function to create image which contains string. 
+		for ($i=0; $i < ceil($jumlah_pekerja/$jumlah_samping); $i++) { 
+			$coor_y = $i * 648;
+			for ($j=($jumlah_samping*$i); $j < ($jumlah_samping*$i) + $jumlah_samping; $j++) { 
+				if (isset($pekerja[$j])) {
+					$data = $pekerja[$j];
+					if ($data->jenis == "Utama") {
+						$front = imagecreatefrompng(base_url('assets/img/SimForklift/front_utama.png'));
+						$text_color = $text_front_utama;
+					}elseif ($data->jenis == "Cadangan") {
+						$front = imagecreatefrompng(base_url('assets/img/SimForklift/front_cadangan.png'));
+						$text_color = $text_front_cadangan;
+					}
+					$photo = imagecreatefromjpeg($data->photo);
+					imagecopyresized(
+						$image, 
+						$front, 
+						(($j-($jumlah_samping*$i)) * 204), 
+						$coor_y, 
+						0, 
+						0, 
+						204, 
+						324,
+						imagesx($front), 
+						imagesy($front)					
+					);
+					imagecopyresized(
+						$image, 
+						$photo,
+						34 + (($j-($jumlah_samping*$i)) * 204), 
+						30 + $coor_y, 
+						0, 
+						0, 
+						135,
+						180, 
+						imagesx($photo), 
+						imagesy($photo)
+					);
+					imagestring(
+						$image, 
+						5, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						210 + $coor_y,  
+						$data->nama, 
+						$text_color
+					); 
+					imagestring(
+						$image, 
+						5, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						230 + $coor_y,  
+						$data->noind, 
+						$text_color
+					); 
+					imagestring(
+						$image, 
+						5, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						250 + $coor_y,  
+						$data->seksi, 
+						$text_color
+					); 
+					
+				}
+				
+			}
+			for ($j=($jumlah_samping*$i); $j < ($jumlah_samping*$i) + $jumlah_samping; $j++) { 
+				if (isset($pekerja[$j])) {
+					$data = $pekerja[$j];
+					$text_white = $text_back_white;
+					$text_black = $text_back_black;
+					$back = imagecreatefrompng(base_url('assets/img/SimForklift/back.png'));
+					$barcode = imagecreatefrompng(base_url('assets/plugins/barcode.php?size=60&text='.$data->noind));
+					$photo = imagecreatefromjpeg($data->photo);
+					imagecopyresized(
+						$image, 
+						$back, 
+						(($j-($jumlah_samping*$i)) * 204), 
+						$coor_y + 324, 
+						0, 
+						0, 
+						204, 
+						324,
+						imagesx($front), 
+						imagesy($front)					
+					);
+					imagecopyresized(
+						$image, 
+						$barcode,
+						28 + (($j-($jumlah_samping*$i)) * 204), 
+						260 + $coor_y + 324, 
+						0, 
+						0, 
+						150,
+						40, 
+						imagesx($barcode), 
+						imagesy($barcode)
+					);
+					imagestring(
+						$image, 
+						5, 
+						50 + (($j-($jumlah_samping*$i)) * 204), 
+						60 + $coor_y + 324,  
+						"SIM FORKLIFT", 
+						$text_white
+					); 
+					imagestring(
+						$image, 
+						2, 
+						15 + (($j-($jumlah_samping*$i)) * 204), 
+						78 + $coor_y + 324,  
+						"SURAT IJIN MENGEMUDI FORKLIFT", 
+						$text_white
+					); 
+					imagestring(
+						$image, 
+						5, 
+						80 + (($j-($jumlah_samping*$i)) * 204), 
+						300 + $coor_y + 324,  
+						$data->noind, 
+						$text_black
+					);  
+					imagestring(
+						$image, 
+						3, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						100 + $coor_y + 324,  
+						"KETENTUAN:", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						115 + $coor_y + 324,  
+						"1. SIM harus dibawa dan", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						30 + (($j-($jumlah_samping*$i)) * 204), 
+						130 + $coor_y + 324,  
+						"dikenakan di depan saku", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						30 + (($j-($jumlah_samping*$i)) * 204), 
+						145 + $coor_y + 324,  
+						"pada saat mengemudikan", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						30 + (($j-($jumlah_samping*$i)) * 204), 
+						160 + $coor_y + 324,  
+						"forklift.", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						175 + $coor_y + 324,  
+						"2. SIM tidak boleh dipinjamkan /", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						30 + (($j-($jumlah_samping*$i)) * 204), 
+						190 + $coor_y + 324,  
+						"digunakan orang lain.", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						32 + (($j-($jumlah_samping*$i)) * 204), 
+						215 + $coor_y + 324,  
+						"Yogyakarta, ".$bulan[intval(date('m',strtotime($data->mulai_berlaku)))].' '.date('Y',strtotime($data->mulai_berlaku)), 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						140 + (($j-($jumlah_samping*$i)) * 204), 
+						230 + $coor_y + 324,  
+						"TIM UP2L", 
+						$text_black
+					); 
+					imagestring(
+						$image, 
+						2, 
+						10 + (($j-($jumlah_samping*$i)) * 204), 
+						245 + $coor_y + 324,  
+						"Berlaku: s/d ".$bulan[intval(date('m',strtotime($data->selesai_berlaku)))].' '.date('Y',strtotime($data->selesai_berlaku)), 
+						$text_black
+					); 
+					
+				}
+				
+			}
+		}
+		  
+		header("Content-Type: image/png"); 
+		  
+		imagepng($image); 
+		imagedestroy($image); 
 	}
 
 	public function cetakcrl(){
--- application/views/MasterPekerja/Other/SimForklift/V_Pdf.php
+++ application/views/MasterPekerja/Other/SimForklift/V_Pdf.php
@@ -3,7 +3,7 @@
 <head>
 </head>
 <body style="font-family: monospace;">
-	<!-- <?php 
+	<?php 
 	$bulan = array(
 		1 => 'Januari',
 		'Februari',
@@ -18,84 +18,6 @@
 		'November',
 		'Desember'
 	);
-	if (isset($pekerja) && !empty($pekerja)) {
-		$jumlah = 0;
-		foreach ($pekerja as $value) {
-			if ($value->jenis == "Utama") {
-				?>
-					<div style="height: 85.60mm;width: 53.98mm;border: 1px solid black;color: black;float: left;background-image: url(<?php echo base_url('assets/img/SimForklift/front_utama.png') ?>);background-repeat: no-repeat;background-attachment: fixed;background-size: 100% 100%;margin-right: 5mm;margin-bottom: 5mm;margin-left: 5mm;">
-						<div style="width: 100%;text-align: center;margin-top: 8mm;">
-							<img src="<?php echo $value->photo ?>" style="height: 47mm;width-left: 35mm;border: 2px solid white;">
-						</div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->nama,0,20) ?></div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->noind,0,20) ?></div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->seksi,0,20) ?></div>
-					</div>
-					<div style="height: 85.60mm;width: 53.98mm;border: 1px solid black;color: black;float: left;background-image: url(<?php echo base_url('assets/img/SimForklift/back.png') ?>);background-repeat: no-repeat;background-attachment: fixed;background-size: 100% 100%;margin-right: 5mm;margin-bottom: 5mm;margin-left: 5mm;font-family: calibri;">
-						<div style="width: 100%;font-size: 9pt;color: white;font-weight: bold;margin-top: 16mm;text-align: center;">SIM FORKLIFT</div>
-						<div style="width: 100%;font-size: 8pt;color: white;font-weight: bold;text-align: center;">SURAT IJIN MENGEMUDI FORKLIFT</div>
-						<div style="width: 100%;font-size: 9pt;padding-left: 5mm;font-weight: bold;margin-top: 3mm;">KETENTUAN:</div>
-						<div style="width: 100%;font-size: 9pt;">
-							<ol style="margin-top: 0;margin-bottom: 0;">
-								<li>SIM harus dibawa dan dikenakan di depan saku pada saat mengemudikan forklift.</li>
-								<li>SIM tidak boleh dipinjamkan / digunakan orang lain.</li>
-							</ol>
-						</div>
-						<div style="width: 100%;font-size: 9pt;text-align: right;padding-right: 5mm;">Yogyakarta, <?php echo $bulan[intval(date('m',strtotime($value->mulai_berlaku)))].' '.date('Y',strtotime($value->mulai_berlaku)) ?></div>
-						<div style="width: 100%;font-size: 9pt;text-align: right;padding-right: 5mm;">TIM UP2L</div>
-						<div style="width: 100%;font-size: 8pt;padding-left: 5mm;">Berlaku: s/d <?php echo $bulan[intval(date('m',strtotime($value->selesai_berlaku)))].' '.date('Y',strtotime($value->selesai_berlaku)) ?></div>
-						<div style="width: 100%;text-align: center;">
-							<img src="<?php echo base_url('assets/plugins/barcode.php?size=60&text='.$value->noind);?>" width="180px" height="34px">
-						</div>
-						<div style="width: 100%;text-align: center;letter-spacing: 15px;"><?php echo $value->noind ?></div>
-					</div>
-				<?php 
-				$jumlah++;
-				if ($jumlah%4 == 0  && $jumlah > 0) {
-					?>
-						<div style="page-break-after: always;"></div>
-					<?php 
-				}
-			}elseif ($value->jenis == "Cadangan") {
-				?>
-					<div style="height: 85.60mm;width: 53.98mm;border: 1px solid black;color: yellow;float: left;background-image: url(<?php echo base_url('assets/img/SimForklift/front_cadangan.png') ?>);background-repeat: no-repeat;background-attachment: fixed;background-size: 100% 100%;margin-right: 5mm;margin-bottom: 5mm;margin-left: 5mm;">
-						<div style="width: 100%;text-align: center;margin-top: 8mm;">
-							<img src="<?php echo $value->photo ?>" style="height: 47mm;width: 35mm;border: 2px solid white;">
-						</div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->nama,0,20) ?></div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->noind,0,20) ?></div>
-						<div style="width: 100%;text-align: center;font-weight: bold;"><?php echo substr($value->seksi,0,20) ?></div>
-					</div>
-					<div style="height: 85.60mm;width: 53.98mm;border: 1px solid black;color: black;float: left;background-image: url(<?php echo base_url('assets/img/SimForklift/back.png') ?>);background-repeat: no-repeat;background-attachment: fixed;background-size: 100% 100%;margin-right: 5mm;margin-bottom: 5mm;margin-left: 5mm;font-family: calibri;">
-						<div style="width: 100%;font-size: 9pt;color: white;font-weight: bold;margin-top: 16mm;text-align: center;">SIM FORKLIFT</div>
-						<div style="width: 100%;font-size: 8pt;color: white;font-weight: bold;text-align: center;">SURAT IJIN MENGEMUDI FORKLIFT</div>
-						<div style="width: 100%;font-size: 9pt;padding-left: 5mm;font-weight: bold;margin-top: 3mm;">KETENTUAN:</div>
-						<div style="width: 100%;font-size: 9pt;">
-							<ol style="margin-top: 0;margin-bottom: 0;">
-								<li>SIM harus dibawa dan dikenakan di depan saku pada saat mengemudikan forklift.</li>
-								<li>SIM tidak boleh dipinjamkan / digunakan orang lain.</li>
-							</ol>
-						</div>
-						<div style="width: 100%;font-size: 9pt;text-align: right;padding-right: 5mm;">Yogyakarta, <?php echo $bulan[intval(date('m',strtotime($value->mulai_berlaku)))].' '.date('Y',strtotime($value->mulai_berlaku)) ?></div>
-						<div style="width: 100%;font-size: 9pt;text-align: right;padding-right: 5mm;">TIM UP2L</div>
-						<div style="width: 100%;font-size: 8pt;padding-left: 5mm;">Berlaku: s/d <?php echo $bulan[intval(date('m',strtotime($value->selesai_berlaku)))].' '.date('Y',strtotime($value->selesai_berlaku)) ?></div>
-						<div style="width: 100%;text-align: center;">
-							<img src="<?php echo base_url('assets/plugins/barcode.php?size=60&text='.$value->noind);?>" width="180px" height="34px">
-						</div>
-						<div style="width: 100%;text-align: center;letter-spacing: 15px;"><?php echo $value->noind ?></div>
-					</div>
-				<?php 
-				$jumlah++;
-				if ($jumlah%4 == 0 && $jumlah > 0) {
-					?>
-						<div style="page-break-after: always;"></div>
-					<?php 
-				}
-			}
-		}
-	}
-	?> -->
-	<?php 
 	if (isset($pekerja) && !empty($pekerja)) {
 		$jumlah = count($pekerja);
 		for ($i=0; $i < ceil($jumlah/5); $i++) { 
