580e081bc6df99fde6c73f0a53b34cd96e6d4ce1
aldi_pradana <aldi_pradana@quick.com>
2020-10-06 13:32:43 +0700

    his

--- application/controllers/JembatanTimbang/C_Index.php
+++ application/controllers/JembatanTimbang/C_Index.php
@@ -51,7 +51,7 @@ class C_Index extends CI_Controller
         $this->load->view('JembatanTimbang/V_Index', $data);
         $this->load->view('V_Footer', $data);
     }
-    
+
     public function History()
     {
         $this->checkSession();
@@ -69,7 +69,37 @@ class C_Index extends CI_Controller
         $data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id, $this->session->responsibility_id);
         // $berat = explode(",",$this->input->post('berat1'));
 
-        $data['get'] = $this->db->where('weight !=', '')->where('weight_2 !=', '')->get('jti.jt_weight_histories')->result_array();
+        // $data['get'] = $this->db->where('weight !=', '')
+        //                         ->where('weight_2 !=', '')
+        //                         ->get('jti.jt_weight_histories')->result_array();
+
+        $data['get'] = $this->db->select('
+                              jti.jt_documents.id as document_id,
+                              jti.jt_documents.document_number,
+                              jti.jt_documents.estimation,
+                              jti.jt_documents.type,
+                              jti.jt_document_type.name as document_type,
+                              jti.jt_tickets_1.ticket_number as ticket_number,
+                              jti.jt_tickets_1.vehicle_number,
+                              jti.jt_tickets_1.created_by,
+                              jti.jt_tickets_1.created_at,
+                              jti.jt_tickets_1.driver_id as driver_id,
+                              jti.jt_tickets_2.ticket_number as ticket_number_2,
+                              jti.jt_drivers.name,
+                              jti.jt_drivers.id as driver_id,
+                              jti.jt_weight_histories.weight,
+                              jti.jt_weight_histories.weight_2,
+                              jti.jt_weight_histories.operator_name,
+                            ')
+                            ->join('jti.jt_tickets_1', 'jti.jt_tickets_1.document_id = jti.jt_documents.id', 'left')
+                            ->join('jti.jt_tickets_2', 'jti.jt_tickets_2.ticket_number = jti.jt_tickets_1.ticket_number', 'left')
+                            ->join('jti.jt_drivers', 'jti.jt_drivers.document_id = jti.jt_documents.id', 'left')
+                            ->join('jti.jt_document_type', 'jti.jt_document_type.id = jti.jt_documents.document_type', 'left')
+                            ->join('jti.jt_weight_histories', 'jti.jt_weight_histories.ticket_number = jti.jt_tickets_1.ticket_number', 'left')
+                            ->where('jti.jt_weight_histories.weight !=', null)
+                            ->order_by('jti.jt_documents.id', 'DESC')
+                            ->get('jti.jt_documents')
+                            ->result_array();
 
         $this->load->view('V_Header', $data);
         $this->load->view('V_Sidemenu', $data);
--- application/views/JembatanTimbang/V_History.php
+++ application/views/JembatanTimbang/V_History.php
@@ -14,26 +14,56 @@
                     <thead>
                       <tr class="bg-primary">
                         <th class="text-center">NO</th>
+                        <th class="text-center">NO DOKUMEN</th>
                         <th class="text-center">TIKET NUMBER</th>
                         <th class="text-center">BERAT TIMBANG - 1</th>
                         <th class="text-center">BERAT TIMBANG - 2</th>
+                        <th class="text-center">SELISIH</th>
                         <th class="text-center">OPERATOR</th>
                         <th class="text-center">TANGGAL CETAK</th>
                         <th class="text-center">BUKTI TIMBANG</th>
                       </tr>
                     </thead>
                     <tbody >
-                      <?php foreach ($get as $key => $value): ?>
+                      <?php foreach ($get as $key => $g): ?>
                         <tr>
                           <td style="text-align:center"><?php echo $key+1 ?></td>
-                          <td style="text-align:center"><?php echo $value['ticket_number'] ?></td>
-                          <td style="text-align:center"><?php echo $value['weight'] ?></td>
-                          <td style="text-align:center"><?php echo $value['weight_2'] ?></td>
-                          <td style="text-align:center"><?php echo $value['operator_name'] ?></td>
-                          <td style="text-align:center"><?php echo $value['created_at'] ?></td>
+                          <td style="text-align:center"><?php echo $g['document_number'] ?></td>
+                          <td style="text-align:center"><?php echo $g['ticket_number'] ?></td>
                           <td style="text-align:center">
-                             <a href="http://produksi.quick.com/api-jti/out/report/pdf/<?php echo $value['ticket_number'] ?>" target="_blank" type="button" class="btn btn-danger" name="button"> <b class="fa fa-file-pdf-o"></b> </a> 
-                            <!--<a href="http://192.168.168.196/api-jti-master/out/report/pdf/<?php echo $value['ticket_number'] ?>" target="_blank" type="button" class="btn btn-danger" name="button"> <b class="fa fa-file-pdf-o"></b> </a>-->
+                            <?php if (!empty($g['weight']) && empty($g['weight_2'])) { ?>
+                              <?php echo empty($g['weight']) ? '-' : $g['weight'].' Kg' ?>
+                            <?php }else { ?>
+                              <?php echo empty($g['weight_2']) ? '-' : $g['weight_2'].' Kg' ?>
+                            <?php } ?>
+                          </td>
+                          <td style="text-align:center">
+                            <?php if (!empty($g['weight']) && empty($g['weight_2'])) { ?>
+                              <?php echo empty($g['weight_2']) ? '-' : $g['weight_2'].' Kg' ?>
+                            <?php }else { ?>
+                              <?php echo empty($g['weight']) ? '-' : $g['weight'].' Kg' ?>
+                            <?php } ?>
+                          </td>
+                          <td style="text-align:center">
+                            <?php
+                            if (!empty($g['ticket_number'])) {
+                              $cek = explode('-', $g['ticket_number']);
+                              if ($cek[1] == 'O') {
+                                $selisih = $g['weight'] - $g['weight_2'];
+                              }else {
+                                $selisih = $g['weight_2'] - $g['weight'];
+                              }
+                            }else {
+                              $selisih = '-';
+                            }
+                            ?>
+                            <?php echo $selisih == '-' ? '-' : $selisih.' Kg' ?>
+                          </td>
+                          <td style="text-align:center"><?php echo $g['operator_name'] ?></td>
+                          <td style="text-align:center"><?php echo $g['created_at'] ?></td>
+                          <td style="text-align:center">
+                             <a href="http://produksi.quick.com/api-jti/out/report/pdf/<?php echo $g['ticket_number'] ?>" target="_blank" type="button" class="btn btn-danger" name="button"> <b class="fa fa-file-pdf-o"></b> </a>
+                            <!--<a href="http://192.168.168.196/api-jti-master/out/report/pdf/<?php echo $g['ticket_number'] ?>" target="_blank" type="button" class="btn btn-danger" name="button"> <b class="fa fa-file-pdf-o"></b> </a>-->
                           </td>
                         </tr>
                       <?php endforeach; ?>
