1cb8bcdb27b1b6520c216cc7408eaca3cafef18b
rayhan_aswiansyah <rayhan_aswiansyah@quick.com>
2020-10-12 13:34:42 +0700

    Penambahan fitur cetak data pekerja keluar, diperbantukan, dimutasi

--- application/models/MasterPekerja/Cetak/DataPekerjaAkanKeluar/M_DataPekerjaAkanKeluar.php
+++ application/models/MasterPekerja/Cetak/DataPekerjaAkanKeluar/M_DataPekerjaAkanKeluar.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class M_DataPekerjaAkanKeluar extends CI_Model
+class M_datapekerjaakankeluar extends CI_Model
 {
     public function __construct()
     {
