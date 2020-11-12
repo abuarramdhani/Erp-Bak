a6f2cb4f53e33be963430ca1df25592275647ce4
Martinus Juan Prasetyo <juanprasetyo040502@gmail.com>
2020-11-12 09:23:17 +0700

    Tambah rule visualisasi warna

--- application/views/PurchaseManagementSendPO/MainMenu/V_PoLog.php
+++ application/views/PurchaseManagementSendPO/MainMenu/V_PoLog.php
@@ -102,9 +102,9 @@
                                         <tbody>
                                             <?php foreach ($PoLog as $key => $value) : ?>
                                             <tr class="
-                                            <?php if($value['SELISIH_WAKTU_1'] > 24 && $value['VENDOR_CONFIRM_DATE'] === NULL && $value['SEND_DATE_2'] === NULL){
+                                            <?php if($value['DISTRIBUTION_METHOD'] !== "none" && $value['SELISIH_WAKTU_1'] > 24 && $value['VENDOR_CONFIRM_DATE'] === NULL && $value['SEND_DATE_2'] === NULL){
                                                 echo 'warning';
-                                            } elseif (($value['SELISIH_WAKTU_2'] > 24 && $value['VENDOR_CONFIRM_DATE'] === NULL)) {
+                                            } elseif ($value['DISTRIBUTION_METHOD'] !== "none" && $value['SELISIH_WAKTU_2'] > 24 && $value['VENDOR_CONFIRM_DATE'] === NULL) {
                                                 echo 'danger';
                                             } else {
                                                 echo '';
