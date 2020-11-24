5a5ca329167b46d1dce1b3da825b06a8c03a8123
rayhan_aswiansyah <rayhan_aswiansyah@quick.com>
2020-11-24 08:21:04 +0700

    fitur exp pdf dan penambahan tampilan detail monitoring limbah

--- application/models/WasteManagement/MainMenu/M_limbahkelola.php
+++ application/models/WasteManagement/MainMenu/M_limbahkelola.php
@@ -7,53 +7,98 @@ Defined('BASEPATH') or exit('No direct script access allowed');
 class M_limbahkelola extends CI_Model
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->database();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 
-	public function getLimbahKirim($lokasi = ''){
+    // public function getJenisLimbah()
+    // {
+    //     $sql = "SELECT trim(jenis_limbah),id_jenis_limbah
+    //     FROM ga.ga_limbah_jenis
+    //     order by jenis_limbah";
+    //     $result = $this->db->query($sql);
+    //     return $result->result_array();
+    // }
+
+    // public function getSeksiPengirim()
+    // {
+    //     $sql = "SELECT distinct section_name
+    //     FROM er.er_section sect,ga.ga_limbah_kirim limkir
+    //     where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00'
+    //     order by sect.section_name";
+    //     $result = $this->db->query($sql);
+    //     return $result->result_array();
+    // }
+
+    public function getLimbahKirim($lokasi = '')
+    {
         $approveByWasteManagement = 1;
         $approveByKasie = 4;
+        // print_r("<pre>");
+        // print_r($periode2);
+        // die;
 
-        if($lokasi == 'yogyakarta') {
+        if ($lokasi == 'yogyakarta') {
             $filterLokasi = "and limkir.lokasi_kerja = '01' ";
-        } else if($lokasi == 'tuksono') {
+        } else if ($lokasi == 'tuksono') {
             $filterLokasi = "and limkir.lokasi_kerja = '02' ";
-        } else if($lokasi == 'all' || $lokasi == '' || $lokasi == null) {
+        } else if ($lokasi == 'all' || $lokasi == '' || $lokasi == null) {
             $filterLokasi = "";
         }
 
-		$query = 	"select limkir.id_kirim,
-                        cast(limkir.tanggal_kirim as date) tanggal,
-                        cast(limkir.tanggal_kirim as time) waktu,
-                        limjen.jenis_limbah,
-                        (select sect.section_name from er.er_section sect where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00') seksi,
-                        concat(limkir.jumlah_kirim, ' ',(select limbah_satuan
-                        from ga.ga_limbah_satuan limsat
-                        where limsat.id_jenis_limbah = limjen.id_jenis_limbah)) jumlah,
-												(select sect.section_name from er.er_section sect where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00') seksi,
-												concat(limkir.jumlah_kirim, ' ',(select limbah_satuan_all
-												from ga.ga_limbah_satuan_all limsatall
-												where limsatall.id_satuan_all = limkir.id_satuan)) jumlahall,
-                        limkir.lokasi_kerja,
-                        limkir.berat_kirim,
-                        limkir.status_kirim,
-						limkir.id_satuan,
-                        (select concat(employee_code,' - ',employee_name) from er.er_employee_all where employee_code = limkir.noind_pengirim and resign = '0')
-                        pekerja,
-                        (select concat(location_code,' - ',location_name) from er.er_location where location_code = limkir.lokasi_kerja) noind_location
-                    from ga.ga_limbah_kirim limkir
-                    inner join ga.ga_limbah_jenis limjen on limjen.id_jenis_limbah = limkir.id_jenis_limbah
-                    where limkir.status_kirim in('1','2','4', '3') $filterLokasi 
+        // if ($limbah != 'all') {
+        //     $filterLimbah = "and limjen.jenis_limbah like '$limbah'";
+        // } else {
+        //     $filterLimbah = "";
+        // }
+
+        // if ($pengirim != 'all') {
+        //     $filterPengirim = "and ers.section_name like '$pengirim'";
+        // } else {
+        //     $filterPengirim = "";
+        // }
+
+        // if ($periode1 != '' && $periode2 != '') {
+        //     $periode1 = date('Y-m-d', strtotime($periode1));
+        //     $periode2 = date('Y-m-d', strtotime($periode2));
+        //     $per = "and limkir.tanggal_kirim::date between '$periode1' and '$periode2'";
+        // } else {
+        //     $per = '';
+        // }
+
+        $query = "SELECT limkir.id_kirim, 
+                    limkir.kodesie_kirim, ers.section_name as seksi,
+                                to_char(limkir.tanggal_kirim, 'YYYY-MM-DD') as tanggal,
+                                cast(limkir.tanggal_kirim as time) waktu,
+                                limjen.jenis_limbah,concat(limkir.jumlah_kirim, ' ',(select limbah_satuan
+                                from ga.ga_limbah_satuan limsat
+                                where limsat.id_jenis_limbah = limjen.id_jenis_limbah)) jumlah,
+                    concat(limkir.jumlah_kirim, ' ',(select limbah_satuan_all
+                    from ga.ga_limbah_satuan_all limsatall
+                    where limsatall.id_satuan_all = limkir.id_satuan)) jumlahall,
+                                limkir.lokasi_kerja,
+                                limkir.berat_kirim,
+                                limkir.status_kirim,
+                    limkir.id_satuan,
+                                (select concat(employee_code,' - ',employee_name) from er.er_employee_all where employee_code = limkir.noind_pengirim)
+                                pekerja,
+                                (select concat(location_code,' - ',location_name) from er.er_location where location_code = limkir.lokasi_kerja) noind_location
+                            FROM 
+                                ga.ga_limbah_kirim limkir
+                                inner join ga.ga_limbah_jenis limjen on limjen.id_jenis_limbah = limkir.id_jenis_limbah
+                                inner join er.er_employee_all ema on ema.employee_code = limkir.noind_pengirim
+                                left join er.er_section ers on ers.section_code = ema.section_code
+                            WHERE limkir.status_kirim in('1','2','4', '3') $filterLokasi 
                     order by limkir.tanggal_kirim desc";
         $result = $this->db->query($query);
         return $result->result_array();
-	}
+    }
 
-	public function getLimbahKirimById($id){
-		$query = 	"select limkir.id_kirim,
+    public function getLimbahKirimById($id)
+    {
+        $query =     "select limkir.id_kirim,
                         cast(limkir.tanggal_kirim as date) tanggal,
                         cast(limkir.tanggal_kirim as time) waktu,
                         limjen.jenis_limbah,
@@ -80,13 +125,15 @@ class M_limbahkelola extends CI_Model
                     order by limkir.tanggal_kirim desc";
         $result = $this->db->query($query);
         return $result->result_array();
-	}
+    }
 
-	public function DeleteLimbahKirim($id){
-		$query = "delete from ga.ga_limbah_kirim where id_kirim = '$id'";
-		$this->db->query($query);
-	}
-    public function getLokasi(){
+    public function DeleteLimbahKirim($id)
+    {
+        $query = "delete from ga.ga_limbah_kirim where id_kirim = '$id'";
+        $this->db->query($query);
+    }
+    public function getLokasi()
+    {
         $query2 = "select location_code,location_name
                 from er.er_location
                 order by location_code";
@@ -95,17 +142,20 @@ class M_limbahkelola extends CI_Model
         return $result->result_array();
     }
 
-	public function updateLimbahStatus($status,$id){
-		$query = "update ga.ga_limbah_kirim set status_kirim = '$status' where id_kirim = '$id'";
-		$this->db->query($query);
-	}
+    public function updateLimbahStatus($status, $id)
+    {
+        $query = "update ga.ga_limbah_kirim set status_kirim = '$status' where id_kirim = '$id'";
+        $this->db->query($query);
+    }
 
-	public function updateLimbahberat($berat,$id){
-		$query = "update ga.ga_limbah_kirim set berat_kirim = '$berat' where id_kirim = '$id'";
-		$this->db->query($query);
-	}
+    public function updateLimbahberat($berat, $id)
+    {
+        $query = "update ga.ga_limbah_kirim set berat_kirim = '$berat' where id_kirim = '$id'";
+        $this->db->query($query);
+    }
 
-    public function getSeksiEmail($id){
+    public function getSeksiEmail($id)
+    {
         $query = "select created_by from ga.ga_limbah_kirim where id_kirim = '$id'";
         $hasil = $this->db->query($query);
         $userseksi = $hasil->result_array();
@@ -117,7 +167,8 @@ class M_limbahkelola extends CI_Model
         return $result->result_array();
     }
 
-    public function getLimKirimMin($id){
+    public function getLimKirimMin($id)
+    {
         $query = "select limjen.jenis_limbah,
                     cast(limkir.tanggal_kirim as date) tanggal,
                     (select sect.section_name from er.er_section sect where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00') seksi,
@@ -133,22 +184,23 @@ class M_limbahkelola extends CI_Model
         return $result->result_array();
     }
 
-    function getDataLimbah($start, $end, $limbah,$lokasi, $detailed) {
+    function getDataLimbah($start, $end, $limbah, $lokasi, $detailed)
+    {
         $start = date('Y-m-d', strtotime($start));
         $end = date('Y-m-d', strtotime($end));
 
         $filterLimbah = '';
-        if(count($limbah)) {
-            $limbah = array_map(function($item){
-            return "'$item'";
+        if (count($limbah)) {
+            $limbah = array_map(function ($item) {
+                return "'$item'";
             }, $limbah);
             $limbah = implode(',', $limbah);
             $filterLimbah = "and limkir.id_jenis_limbah in ($limbah)";
         }
         $filterLokasi = '';
-        if(count($lokasi)) {
-            $lokasi = array_map(function($item){
-            return "'$item'";
+        if (count($lokasi)) {
+            $lokasi = array_map(function ($item) {
+                return "'$item'";
             }, $lokasi);
             $lokasi = implode(',', $lokasi);
             $filterlokasi = "and limkir.lokasi_kerja in ($lokasi)";
@@ -156,29 +208,29 @@ class M_limbahkelola extends CI_Model
 
         // not detailed
         //if($detailed == 'false') {
-           // $sql = "SELECT 
-            //       distinct limkir.id_jenis_limbah,
-            //      limjen.jenis_limbah,
-            //      (select trunc(sum(berat_kirim)::numeric, 3) from ga.ga_limbah_kirim where id_jenis_limbah = limkir.id_jenis_limbah and tanggal_kirim::date between '$start' and '$end') as berat_kirim
-            //    FROM ga.ga_limbah_kirim limkir inner join ga.ga_limbah_jenis limjen 
-            //        on limkir.id_jenis_limbah = limjen.id_jenis_limbah
-             //   WHERE limkir.status_kirim = '1' and limkir.tanggal_kirim::date between '$start' and '$end' $filterLimbah $filterlokasi
-             //   ORDER BY jenis_limbah";
-       // } else {
-            // detailed
-            //$sql = "SELECT
-                  //      limjen.jenis_limbah,
-                   //     to_char(limkir.tanggal_kirim, 'YYYY-MM-DD') as tanggal_kirim,
-                    //    (select sect.section_name from er.er_section sect where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00') section_name,
-                     //   trunc(limkir.berat_kirim::numeric, 3) as berat_kirim
-                    //FROM ga.ga_limbah_kirim limkir inner join ga.ga_limbah_jenis limjen 
-                     //       on limkir.id_jenis_limbah = limjen.id_jenis_limbah
-                   // WHERE limkir.status_kirim = '1' and limkir.tanggal_kirim::date between '$start' and '$end' $filterLimbah $filterlokasi
-                    //ORDER BY limkir.tanggal_kirim"; // 1 adalah yg sudah diapprove oleh waste management
+        // $sql = "SELECT 
+        //       distinct limkir.id_jenis_limbah,
+        //      limjen.jenis_limbah,
+        //      (select trunc(sum(berat_kirim)::numeric, 3) from ga.ga_limbah_kirim where id_jenis_limbah = limkir.id_jenis_limbah and tanggal_kirim::date between '$start' and '$end') as berat_kirim
+        //    FROM ga.ga_limbah_kirim limkir inner join ga.ga_limbah_jenis limjen 
+        //        on limkir.id_jenis_limbah = limjen.id_jenis_limbah
+        //   WHERE limkir.status_kirim = '1' and limkir.tanggal_kirim::date between '$start' and '$end' $filterLimbah $filterlokasi
+        //   ORDER BY jenis_limbah";
+        // } else {
+        // detailed
+        //$sql = "SELECT
+        //      limjen.jenis_limbah,
+        //     to_char(limkir.tanggal_kirim, 'YYYY-MM-DD') as tanggal_kirim,
+        //    (select sect.section_name from er.er_section sect where left(sect.section_code,7) = limkir.kodesie_kirim and sect.section_code like '%00') section_name,
+        //   trunc(limkir.berat_kirim::numeric, 3) as berat_kirim
+        //FROM ga.ga_limbah_kirim limkir inner join ga.ga_limbah_jenis limjen 
+        //       on limkir.id_jenis_limbah = limjen.id_jenis_limbah
+        // WHERE limkir.status_kirim = '1' and limkir.tanggal_kirim::date between '$start' and '$end' $filterLimbah $filterlokasi
+        //ORDER BY limkir.tanggal_kirim"; // 1 adalah yg sudah diapprove oleh waste management
         //}
 
         // not detailed
-        if($detailed == 'false') {
+        if ($detailed == 'false') {
             $sql = "SELECT 
                     limkir.id_jenis_limbah,
                     limjen.jenis_limbah,
@@ -192,23 +244,25 @@ class M_limbahkelola extends CI_Model
         } else {
             // detailed
             $sql = "SELECT 
-                     distinct
-                     limkir.id_jenis_limbah,
-                     limjen.jenis_limbah,
-                     to_char(limkir.tanggal_kirim, 'YYYY-MM-DD') as tanggal_kirim,
-                     trim(sect.section_name) section_name,
-                      trunc(limkir.berat_kirim::numeric, 3) as berat_kirim
-                     FROM ga.ga_limbah_kirim limkir 
-                     inner join ga.ga_limbah_jenis limjen  on limkir.id_jenis_limbah = limjen.id_jenis_limbah
-                    left join er.er_section sect on left(sect.section_code,7) = limkir.kodesie_kirim 
-                    WHERE limkir.status_kirim = '1' and limkir.tanggal_kirim::date between '$start' and '$end'
-                    $filterLimbah $filterlokasi 
-                    group by limkir.id_jenis_limbah, limjen.jenis_limbah,limkir.tanggal_kirim,sect.section_name,limkir.berat_kirim ORDER BY jenis_limbah";
-
-       }
+                        distinct limkir.id_jenis_limbah,
+                        (select concat(employee_code,' - ',employee_name) from er.er_employee_all where employee_code = limkir.noind_pengirim and resign = '0') pekerja,
+                        limjen.jenis_limbah,
+                        to_char(limkir.tanggal_kirim, 'YYYY-MM-DD') as tanggal_kirim,
+                        cast(limkir.tanggal_kirim as time) waktu,
+                        trim(sect.section_name) section_name,
+                        concat(limkir.jumlah_kirim, ' ',(select limbah_satuan_all
+                          from ga.ga_limbah_satuan_all limsatall
+                          where limsatall.id_satuan_all = limkir.id_satuan)) jumlahall,
+                        trunc(limkir.berat_kirim::numeric, 3) as berat_kirim
+                    FROM ga.ga_limbah_kirim limkir 
+                        inner join ga.ga_limbah_jenis limjen  on limkir.id_jenis_limbah = limjen.id_jenis_limbah
+                        left join er.er_section sect on left(sect.section_code,7) = limkir.kodesie_kirim 
+                    WHERE limkir.status_kirim = '1'  and limkir.tanggal_kirim::date between '$start' and '$end' $filterLimbah $filterlokasi 
+                        group by limkir.id_jenis_limbah, limjen.jenis_limbah,limkir.tanggal_kirim,sect.section_name,pekerja,limkir.berat_kirim,waktu,jumlahall
+                    ORDER BY jenis_limbah
+                    ";
+        }
         // echo $sql;exit();
         return $this->db->query($sql)->result_array();
     }
 }
-
-?>
---
+++ application/views/WasteManagement/LimbahMonitoring/V_exp_pdf_detail.php
@@ -0,0 +1,60 @@
+<style>
+    h4,
+    h5 {
+        text-align: center;
+    }
+
+    table {
+        font-size: 12px;
+        width: 100%;
+        border-collapse: collapse;
+        margin: auto;
+    }
+
+    th {
+        border: 0.5px solid black;
+        text-align: center;
+    }
+
+    td {
+        border: 0.5px solid black;
+    }
+</style>
+
+<h4>Monitoring Limbah</h4>
+<h5>Periode : <?= $start . " s/d " . $end ?> </h5>
+<table id="tableLimbah" class="table table-bordered table-striped">
+    <thead class="bg-primary ">
+        <tr>
+            <th>No</th>
+            <th>Jenis Limbah</th>
+            <th>Pekerja Pengirim</th>
+            <th>Tanggal Masuk</th>
+            <th>Waktu</th>
+            <th>Seksi Pengirim</th>
+            <th>Jumlah Limbah</th>
+            <th>Berat(Kg)</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $i = 1;
+        foreach ($data as $data) : ?>
+            <tr>
+                <td style="text-align: center;"><?= $i++ ?></td>
+                <td><?= $data['jenis_limbah'] ?></td>
+                <td><?= $data['pekerja'] ?></td>
+                <td style="text-align: center;"><?= $data['tanggal_kirim'] ?></td>
+                <td style="text-align: center;"><?= $data['waktu'] ?></td>
+                <td><?= $data['section_name'] ?></td>
+                <td><?= $data['jumlahall'] ?></td>
+                <td style="text-align: right;"><?= floatval($data['berat_kirim']) ?></td>
+            </tr>
+        <?php $totalBerat = $totalBerat + floatval($data['berat_kirim']);
+        endforeach ?>
+        <tr>
+            <td colspan="7"> <strong>Total Berat Limbah</strong></td>
+            <td style="text-align: right;"> <strong><?= $totalBerat; ?></strong></td>
+        </tr>
+    </tbody>
+
+</table>
