d91258b88b7e45ab6b006a879dce31adf3b0a092
dicka_ismaji <dicka_ismaji@quick.com>
2020-11-28 13:40:42 +0700

    Refactoring and add method for 404 Page

--- application/controllers/C_Index.php
+++ application/controllers/C_Index.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class C_Index extends CI_Controller {
+class C_Index extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -19,31 +20,31 @@ class C_Index extends CI_Controller {
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function __construct()
-  {
-      parent::__construct();
+	{
+		parent::__construct();
 
-      $this->load->helper('form');
-      $this->load->helper('url');
-      $this->load->helper('html');
-			$this->load->library('Log_Activity');
-      $this->load->library('form_validation');
-          //load the login model
-		  $this->load->library('session');
-		  //$this->load->library('Database');
-		  $this->load->model('M_Index');
-		  $this->load->model('SystemAdministration/MainMenu/M_user');
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->helper('html');
+		$this->load->library('Log_Activity');
+		$this->load->library('form_validation');
+		//load the login model
+		$this->load->library('session');
+		//$this->load->library('Database');
+		$this->load->model('M_Index');
+		$this->load->model('SystemAdministration/MainMenu/M_user');
 
-		  if($this->session->userdata('logged_in')!=TRUE) {
-		  $this->load->helper('url');
-		  $this->session->set_userdata('last_page', current_url());
-		  //redirect('');
-    }
-		  //$this->load->model('CustomerRelationship/M_Index');
-  }
+		if ($this->session->userdata('logged_in') != TRUE) {
+			$this->load->helper('url');
+			$this->session->set_userdata('last_page', current_url());
+			//redirect('');
+		}
+		//$this->load->model('CustomerRelationship/M_Index');
+	}
 
 	public function index()
 	{
-		if($this->session->is_logged){
+		if ($this->session->is_logged) {
 			$this->session->set_userdata('responsibility', 'Dashboard');
 
 			//$usr = "D1178";
@@ -51,47 +52,46 @@ class C_Index extends CI_Controller {
 			//$data['user'] = $usr;
 			$data['Menu'] = 'dashboard';
 
-			if(base_url('')=='http://182.23.18.195/'){
+			if (base_url('') == 'http://182.23.18.195/') {
 				$data['UserResponsibility'] = $this->M_user->getUserResponsibilityInternet($user_id);
-			}else{
+			} else {
 				$data['UserResponsibility'] = $this->M_user->getUserResponsibility($user_id);
 			}
 
-			$tema=$this->M_Index->getTheme();
+			$tema = $this->M_Index->getTheme();
 
-			$this->session->set_userdata('tema',$tema[0]->theme);
+			$this->session->set_userdata('tema', $tema[0]->theme);
 
-			$this->load->view('V_Header',$data);
-			$this->load->view('V_Sidemenu',$data);
-			$this->load->view('V_Index',$data);
-			$this->load->view('V_Footer',$data);
-		}else{
+			$this->load->view('V_Header', $data);
+			$this->load->view('V_Sidemenu', $data);
+			$this->load->view('V_Index', $data);
+			$this->load->view('V_Footer', $data);
+		} else {
 			$this->session->set_userdata('responsibility', 'Login');
-			if($this->session->gagal){
+			if ($this->session->gagal) {
 				$data['error'] = "Login error, please enter correct username and password";
 				$aksi = 'Login';
 				$detail = 'Gagal Login';
 				$this->log_activity->activity_log($aksi, $detail);
-
-			}else{
+			} else {
 				$data['error'] = "";
 			}
-			$this->load->view('V_Login',$data);
+			$this->load->view('V_Login', $data);
 			$this->session->unset_userdata('gagal');
 		}
 	}
 
 	public function Responsibility($responsibility_id)
 	{
-		if($this->session->is_logged){
+		if ($this->session->is_logged) {
 			//$usr = "D1178";
 			$user_id = $this->session->userid;
 			//$data['user'] = $usr;
 			//$data['Menu'] = 'dashboard';
 
-			$UserResponsibility = $this->M_user->getUserResponsibility($user_id,$responsibility_id);
-			// print_r($UserResponsibility);exit();
-			foreach($UserResponsibility as $UserResponsibility_item){
+			$UserResponsibility = $this->M_user->getUserResponsibility($user_id, $responsibility_id);
+
+			foreach ($UserResponsibility as $UserResponsibility_item) {
 				$aksi = 'Akses Responsibility';
 				$detail = $UserResponsibility_item['user_group_menu_name'];
 
@@ -99,7 +99,7 @@ class C_Index extends CI_Controller {
 
 				$this->session->set_userdata('responsibility', $UserResponsibility_item['user_group_menu_name']);
 				// if(empty($UserResponsibility_item['user_group_menu_id'])){
-					// $UserResponsibility_item['user_group_menu_id'] = 0;
+				// $UserResponsibility_item['user_group_menu_id'] = 0;
 				// }
 				$this->session->set_userdata('responsibility_id', $UserResponsibility_item['user_group_menu_id']);
 				$this->session->set_userdata('module_link', $UserResponsibility_item['module_link']);
@@ -113,15 +113,15 @@ class C_Index extends CI_Controller {
 			// $this->load->view('V_Sidemenu',$data);
 			// $this->load->view('V_Index',$data);
 			// $this->load->view('V_Footer',$data);
-		}else{
-			if($this->session->gagal){
+		} else {
+			if ($this->session->gagal) {
 				$data['error'] = "Login error, please enter correct username and password";
-			}else{
+			} else {
 				$data['error'] = "";
 			}
 
 			$this->session->set_userdata('responsibility', 'Login');
-			$this->load->view('V_Login',$data);
+			$this->load->view('V_Login', $data);
 			$this->session->unset_userdata('gagal');
 		}
 	}
@@ -139,37 +139,38 @@ class C_Index extends CI_Controller {
 		//$data['user'] = $usr;
 		$data['Menu'] = 'dashboard';
 
-		$data['UserMenu'] = $this->M_user->getUserMenu($user_id,$this->session->responsibility_id);
-		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id,$this->session->responsibility_id);
-		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id,$this->session->responsibility_id);
+		$data['UserMenu'] = $this->M_user->getUserMenu($user_id, $this->session->responsibility_id);
+		$data['UserSubMenuOne'] = $this->M_user->getMenuLv2($user_id, $this->session->responsibility_id);
+		$data['UserSubMenuTwo'] = $this->M_user->getMenuLv3($user_id, $this->session->responsibility_id);
 
-		$this->load->view('V_Header',$data);
-		$this->load->view('V_Sidemenu',$data);
-		$this->load->view('CustomerRelationship/V_Index',$data);
-		$this->load->view('V_Footer',$data);
+		$this->load->view('V_Header', $data);
+		$this->load->view('V_Sidemenu', $data);
+		$this->load->view('CustomerRelationship/V_Index', $data);
+		$this->load->view('V_Footer', $data);
 	}
 
-	public function checkSession(){
-		if($this->session->is_logged){
-
-		}else{
+	public function checkSession()
+	{
+		if ($this->session->is_logged) {
+		} else {
 			redirect('');
 		}
 	}
 
-	public function login(){
+	public function login()
+	{
 
 		//$this->load->model('M_index');
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
 		$password_md5 = md5($password);
-		$log = $this->M_Index->login($username,$password_md5);
+		$log = $this->M_Index->login($username, $password_md5);
 
-		if($log){
+		if ($log) {
 			$user = $this->M_Index->getDetail($username);
 			$path = $this->M_Index->path_photo($username);
 
-			foreach($user as $user_item){
+			foreach ($user as $user_item) {
 				$iduser 			= $user_item->user_id;
 				$password_default 	= $user_item->password_default;
 				$kodesie			= $user_item->section_code;
@@ -178,14 +179,14 @@ class C_Index extends CI_Controller {
 			}
 			$path_photo 		= trim($path);
 			$ses = array(
-							'is_logged' 		=> 1,
-							'userid' 			=> $iduser,
-							'user' 				=> strtoupper($username),
-							'employee'  		=> $employee_name,
-							'kodesie' 			=> $kodesie,
-							'kode_lokasi_kerja'	=> $kode_lokasi_kerja,
-							'path_photo'		=> $path_photo,
-						);
+				'is_logged' 		=> 1,
+				'userid' 			=> $iduser,
+				'user' 				=> strtoupper($username),
+				'employee'  		=> $employee_name,
+				'kodesie' 			=> $kodesie,
+				'kode_lokasi_kerja'	=> $kode_lokasi_kerja,
+				'path_photo'		=> $path_photo,
+			);
 
 			$isDefaultPass = $this->M_Index->getPassword($username);
 			$ses['pass_is_default'] = $isDefaultPass;
@@ -197,23 +198,22 @@ class C_Index extends CI_Controller {
 			$this->log_activity->activity_log($aksi, $detail);
 
 			redirect($this->session->userdata('last_page'));
-		}else{
+		} else {
 			$ses = array(
-							'gagal' => 1,
-						);
+				'gagal' => 1,
+			);
 			$this->session->set_userdata($ses);
-			if(isset($this->session->last_page)){
+			if (isset($this->session->last_page)) {
 				redirect($this->session->last_page);
-			}else{
+			} else {
 				redirect('');
 			}
-
 		}
-
 	}
 
 	public function ChangePassword()
-	{	$this->checkSession();
+	{
+		$this->checkSession();
 		//$usr = "D1178";
 		$user_id = $this->session->userid;
 		$noind = $this->session->user;
@@ -221,59 +221,54 @@ class C_Index extends CI_Controller {
 		$data['UserData'] = $this->M_user->getUser($user_id);
 		$data['id'] = $user_id;
 
-		foreach($data['UserData'] as $user){
+		foreach ($data['UserData'] as $user) {
 			$password = $user['user_password'];
 		}
-		
+
 		$data['password'] = $password;
 
 		$this->form_validation->set_rules('txtPasswordNow', 'username', 'required');
 
-		if ($this->form_validation->run() === FALSE)
-		{
-				$data['error'] = '';
-				$this->load->view('V_ChangePassword',$data);
-		}
-		else
-		{
-			if($password == $this->input->post('txtPasswordNow')){
-				if($this->input->post('txtPassword')!='' and $this->input->post('txtPasswordCheck')!=''){
+		if ($this->form_validation->run() === FALSE) {
+			$data['error'] = '';
+			$this->load->view('V_ChangePassword', $data);
+		} else {
+			if ($password == $this->input->post('txtPasswordNow')) {
+				if ($this->input->post('txtPassword') != '' and $this->input->post('txtPasswordCheck') != '') {
 					if (md5($this->input->post('txtPassword')) == $password) {
 						$data['error'] = "Password Masih Sama";
 						$this->load->view('V_ChangePassword', $data);
-					}else{
+					} else {
 						$aksi = 'Change Password';
 						$detail = 'Change Password';
 						$this->log_activity->activity_log($aksi, $detail);
-	
+
 						$data = array(
 							'user_password'	=> md5($this->input->post('txtPassword')),
-							'creation_date'=>  $this->input->post('hdnDate'),
-							'created_by'=>  $this->input->post('hdnUser')
-							);
-						$this->M_user->updateUser($data,$user_id);
+							'creation_date' =>  $this->input->post('hdnDate'),
+							'created_by' =>  $this->input->post('hdnUser')
+						);
+						$this->M_user->updateUser($data, $user_id);
 						$this->EmailAlert($noind, date('Y-m-d H:i:s'));
 						redirect(base_url('logout'));
 					}
-				}
-				else{
+				} else {
 					$data['error'] = 'New Password Empty';
-					$this->load->view('V_ChangePassword',$data);
+					$this->load->view('V_ChangePassword', $data);
 				}
-			}else{
+			} else {
 				$aksi = 'Error Change Password';
 				$detail = 'Error Change Password';
 				$this->log_activity->activity_log($aksi, $detail);
 
 				$data['error'] = 'Password Wrong';
-				$this->load->view('V_ChangePassword',$data);
+				$this->load->view('V_ChangePassword', $data);
 			}
-
 		}
-
 	}
 
-	public function logout(){
+	public function logout()
+	{
 		$aksi = 'Log Out';
 		$detail = 'Log Out ERP';
 
@@ -281,7 +276,7 @@ class C_Index extends CI_Controller {
 
 
 		$this->session->unset_userdata('is_logged');
-		if($this->session->gagal){
+		if ($this->session->gagal) {
 			$this->session->unset_userdata('gagal');
 		}
 
@@ -294,7 +289,12 @@ class C_Index extends CI_Controller {
 		}
 		//variabel session spl (surat perintah lembur) HR akhir
 
-		redirect('');
+		redirect('/');
+	}
+
+	public function page_404()
+	{
+		return $this->load->view('V_404');
 	}
 
 	public function getLog()
@@ -302,66 +302,66 @@ class C_Index extends CI_Controller {
 		$menu = $_POST['menu1'];
 
 		$aksi = 'Akses Menu';
-		$detail = 'Mengakses Menu '.$menu;
+		$detail = 'Mengakses Menu ' . $menu;
 
 		$this->log_activity->activity_log($aksi, $detail);
-
 	}
 
-	public function EmailAlert($noind, $waktu) {
+	public function EmailAlert($noind, $waktu)
+	{
 		//email
 		$getnama = $this->M_user->getDataUpdatePassword($noind);
 		$emailUser = 'kasie_ict_hrd@quick.com';
-	
-			$subject = "Reset Password ERP";
-			$body = "Pemberitahuan,
+
+		$subject = "Reset Password ERP";
+		$body = "Pemberitahuan,
 					<br>Pekerja dengan data sebagai berikut telah mengganti password erp:
 					<br>
 					<br><b>Noind&emsp;&emsp;:</b> $noind
 					<br>
-					<br><b>Nama&emsp;&emsp;:</b> ".$getnama[0]['nama']."
+					<br><b>Nama&emsp;&emsp;:</b> " . $getnama[0]['nama'] . "
 					<br>
-					<br><b>Seksi &emsp;&emsp;:</b> ".$getnama[0]['seksi']."
+					<br><b>Seksi &emsp;&emsp;:</b> " . $getnama[0]['seksi'] . "
 					<br>
-					<br><b>Tanggal &nbsp;&nbsp;:</b> ".date('d-m-Y H:i:s', strtotime($waktu))."
+					<br><b>Tanggal &nbsp;&nbsp;:</b> " . date('d-m-Y H:i:s', strtotime($waktu)) . "
 					<br><br>
 					<hr>
 					<br>Demikian yang dapat kami informasikan. Terimakasih";
-	
-			//send Email
-			$this->load->library('PHPMailerAutoload');
-			$mail = new PHPMailer();
-			$mail->SMTPDebug = 0;
-			$mail->Debugoutput = 'html';
-	
-			// set smtp
-			$mail->isSMTP();
-			$mail->Host = 'm.quick.com';
-			$mail->Port = 465;
-			$mail->SMTPAuth = true;
-			$mail->SMTPSecure = 'ssl';
-			$mail->SMTPOptions = array(
-					'ssl' => array(
-					'verify_peer' => false,
-					'verify_peer_name' => false,
-					'allow_self_signed' => true)
-				);
-			$mail->Username = 'no-reply';
-			$mail->Password = '123456';
-			$mail->WordWrap = 50;
-	
-			// set email content
-			$mail->setFrom('no-reply@quick.com', 'Email Sistem');
-			$mail->addAddress($getnama[0]['email']);
-			$mail->addAddress('kasie_ict_hrd@quick.com');
-			$mail->Subject = $subject;
-			$mail->msgHTML($body);
-	
-			// check error
-			if (!$mail->send()) {
-				echo "Mailer Error: ".$mail->ErrorInfo;
-				exit();
-			}
+
+		//send Email
+		$this->load->library('PHPMailerAutoload');
+		$mail = new PHPMailer();
+		$mail->SMTPDebug = 0;
+		$mail->Debugoutput = 'html';
+
+		// set smtp
+		$mail->isSMTP();
+		$mail->Host = 'm.quick.com';
+		$mail->Port = 465;
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = 'ssl';
+		$mail->SMTPOptions = array(
+			'ssl' => array(
+				'verify_peer' => false,
+				'verify_peer_name' => false,
+				'allow_self_signed' => true
+			)
+		);
+		$mail->Username = 'no-reply';
+		$mail->Password = '123456';
+		$mail->WordWrap = 50;
+
+		// set email content
+		$mail->setFrom('no-reply@quick.com', 'Email Sistem');
+		$mail->addAddress($getnama[0]['email']);
+		$mail->addAddress('kasie_ict_hrd@quick.com');
+		$mail->Subject = $subject;
+		$mail->msgHTML($body);
+
+		// check error
+		if (!$mail->send()) {
+			echo "Mailer Error: " . $mail->ErrorInfo;
+			exit();
+		}
 	}
 }
-
