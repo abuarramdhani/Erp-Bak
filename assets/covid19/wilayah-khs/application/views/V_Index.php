6435509333dd69edccd52d2d9f6e2ebfa598fc82
aji Kurniawan <aji_kurniawan@quick.com>
2020-12-10 08:01:33 +0700

    Update V_Index.php

--- assets/covid19/wilayah-khs/application/views/V_Index.php
+++ assets/covid19/wilayah-khs/application/views/V_Index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Zona Covid CV. KHS</title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Wilayah Covid 19</title>
 	<link rel="icon" href="<?php echo base_url('assets/img/virus.png') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/Bootstrap/3.3.7/css/bootstrap.min.css') ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/datatables/datatables.min.css') ?>">
