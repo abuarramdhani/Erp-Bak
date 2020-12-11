9121cb1d0562d1e2d78017449379c327afade7cf
aji kurniawan <aji_kurniawan@quick.com>
2020-12-11 09:51:10 +0700

    menambah notifikasi hari terakhir isolasi

--- application/views/Covid/V_Index.php
+++ application/views/Covid/V_Index.php
@@ -7,15 +7,33 @@
 						<h1><b><?=$Title ?></b></h1>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-lg-12">
-						<!-- <div class="box box-primary box-solid">
-							<div class="box-header with-border"></div>
-							<div class="box-body"></div>
-						</div> -->
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
 </section>
+<style type="text/css">
+	.swal2-content li, .swal2-content p{
+		text-align: left !important;
+	}
+</style>
+<script type="text/javascript">
+	$(document).on('ready', function(){
+		<?php 
+		$lokasi = "";
+		if (isset($reminder) && !empty($reminder)) {
+			foreach ($reminder as $r) {
+				$lokasi .= "<li>".$r['nama_seksi']." - ".$r['lokasi']."</li>";
+			}
+			?>
+		swal.fire({
+			title: "[REMINDER]",
+			html: "<p>Untuk Area Isolasi: </p><ul><?php echo $lokasi ?></ul><p>Akan Berkhir Masa Isolasi Besok.</p>",
+			type: "warning",
+			confirmButtonText: 'OK',
+			confirmButtonColor: '#fdcb6e',
+		})
+			<?php
+		}
+		?>
+	})
+</script>
