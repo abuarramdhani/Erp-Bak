fd1c106c8c83f425447c001267885e6933db7213
yogi_ghofur <yogi_ghofur@quick.com>
2020-12-19 09:37:10 +0700

    Tambah jenis baju

--- application/views/MasterPekerja/CetakKategori/V_Index.php
+++ application/views/MasterPekerja/CetakKategori/V_Index.php
@@ -309,8 +309,10 @@
                                                 <label> Telkomsel Mygroup</label><br>
                                                 <input name="pidgin" type="checkbox" class="chk_FilterTarikData" value="tp.pidgin_account">
                                                 <label> Akun Pidgin</label><br>
-                                                <input name="uk_baju" type="checkbox" class="chk_FilterTarikData" value="tp.uk_baju, tp.jenis_baju">
+                                                <input name="uk_baju" type="checkbox" class="chk_FilterTarikData" value="tp.uk_baju">
                                                 <label> Ukuran Baju</label><br>
+                                                <input name="jenis_baju" type="checkbox" class="chk_FilterTarikData" value="tp.jenis_baju">
+                                                <label> Jenis Baju</label><br>
                                                 <input name="uk_celana" type="checkbox" class="chk_FilterTarikData" value="tp.uk_celana">
                                                 <label> Ukuran Celana</label><br>
                                                 <input name="uk_sepatu" type="checkbox" class="chk_FilterTarikData" value="tp.uk_sepatu">
