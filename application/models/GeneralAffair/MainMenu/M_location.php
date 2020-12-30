fca31bd05b53929e0a71f724ee9cf4332d42e319
Dicka Ismaji <dicka_ismaji@quick.com>
2020-12-30 14:04:25 +0700

    Update M_location.php

--- application/models/GeneralAffair/MainMenu/M_location.php
+++ application/models/GeneralAffair/MainMenu/M_location.php
@@ -12,7 +12,7 @@ class M_location extends CI_Model
         // 626 adalah userid dr B0825 -> Ibnu -> Sedang dimutasi ke GA Pusat
         // kondisi khusus
         // setelah mutasi selesai, line 15 dapat dihapus
-        if ($userid == '626') return '01';
+        if ($userid == '626') return [['location_code' => '01']];
         // end
     	
     	$textq = "select '226' user_id,(select b.location_code from sys.sys_user a inner join er.er_employee_all b on a.employee_id = b.employee_id where a.user_id = '$userid') location_code;";
