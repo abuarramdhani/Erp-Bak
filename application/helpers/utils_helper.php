04ff4bd813a1e5f67ebc89fa1328771e9dcbcb4e
dicka_ismaji <dicka_ismaji@quick.com>
2021-01-25 21:27:42 +0700

    Add helper

--- application/helpers/utils_helper.php
+++ application/helpers/utils_helper.php
@@ -17,12 +17,52 @@ if (!function_exists('debug')) {
  * laravel style
  */
 if (!function_exists('response')) {
-  function response()
+  /**
+   * Helper Class
+   * Http Response
+   */
+  class Response
   {
-    class Response
+    /**
+     * @var Array
+     * http response status
+     */
+    protected $code = array(
+      100 => 'Continue',
+      200 => 'OK',
+      400 => 'Bad Request',
+      401 => 'Unauthorized',
+      403 => 'Forbidden',
+      404 => 'Not Found',
+      500 => 'Internal Server Error'
+    );
+
+    /**
+     * Send Json response
+     * @param Mixed
+     * @param Integer { Code } | Optional
+     */
+    public static function json($var, $code = 200)
     {
+      header("Content-Type: application/json; charset=utf-8");
+      http_response_code($code);
+      echo json_encode($var);
     }
 
+    /**
+     * Send HTML response
+     * @param String { HTML text }
+     */
+    public static function html($string)
+    {
+      header("Content-Type: text/html; charset=utf-8");
+      http_response_code(200);
+      echo $string;
+    }
+  }
+
+  function response()
+  {
     return new Response;
   }
 }
