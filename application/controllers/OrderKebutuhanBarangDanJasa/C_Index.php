0d4c18c4979041978f5e0130270c7167a7967685
yusuf_taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2021-02-10 12:38:23 +0700

    Perbaikan Direct Masuk ke Okebaja dari Email Error

--- application/controllers/OrderKebutuhanBarangDanJasa/C_Index.php
+++ application/controllers/OrderKebutuhanBarangDanJasa/C_Index.php
@@ -17,10 +17,44 @@ class C_Index extends CI_Controller {
   private function checkSession()
   {
     if (!$this->session->is_logged) {
-      $this->session->set_userdata('last_page', current_url());
-      $this->session->set_userdata('Responsbility', 'some_value');
       redirect();
     }
+  }
+
+  private function decrypt($encrypted_value)
+  {
+    $removed_salt = str_replace(['-', '_', '~'], ['+', '/', '='], $encrypted_value);
+    $decoded_value = $this->encrypt->decode($removed_salt);
+
+    return $decoded_value;
+  }
+
+  private function login($nomor_induk)
+  {
+    $user_detail = $this->index->getDetail($nomor_induk)[0];
+
+    $user_session = [
+      'is_logged' => 1,
+      'userid' => $user_detail->user_id,
+      'user' => $nomor_induk,
+      'employee' => $user_detail->employee_name,
+      'kodesie' => $user_detail->section_code,
+      'kode_lokasi_kerja' => $user_detail->location_code,
+    ];
+
+    $this->session->set_userdata($user_session);
+  }
+
+  private function setActiveResponsibility($user_responsibility)
+  {
+    $responsibility_session = [
+      'responsibility' => $user_responsibility->user_group_menu_name,
+      'responsibility_id' => $user_responsibility->user_group_menu_id,
+      'module_link' => $user_responsibility ->module_link,
+      'org_id' => $user_responsibility->org_id,
+    ];
+
+    $this->session->set_userdata($responsibility_session);
   }
 
   public function index()
@@ -55,7 +89,7 @@ class C_Index extends CI_Controller {
   private function getOrderInformation($unapproved, $judged, $url)
   {
     $total = $unapproved + $judged;
-    $percentage = $judged === 0 && $total === 0 ? 100 : round($judged / $total * 100);
+    $percentage = $judged === 0 && $total === 0 ? 100 : floor($judged / $total * 100);
 
     return (object) [
       'Unapproved' => $unapproved,
@@ -207,138 +241,25 @@ class C_Index extends CI_Controller {
 
   public function directEmail($key)
   {
-    $decrypt = str_replace(array('-', '_', '~'), array('+', '/', '='), $key);
-    $decrypt = $this->encrypt->decode($decrypt);
-    
-    $user = $this->index->getDetail($decrypt);
-      
-    foreach($user as $user_item){
-      $iduser       = $user_item->user_id;
-      $password_default   = $user_item->password_default;
-      $kodesie      = $user_item->section_code;
-      $employee_name     = $user_item->employee_name; 
-      $kode_lokasi_kerja   = $user_item->location_code;
-    }
-    $ses = array(
-            'is_logged'     => 1,
-            'userid'       => $iduser,
-            'user'         => $decrypt,
-            'employee'      => $employee_name,
-            'kodesie'       => $kodesie,
-            'kode_lokasi_kerja'  => $kode_lokasi_kerja,
-          );
-    $this->session->set_userdata($ses);
+    $nomor_induk = $this->decrypt($key);
 
-    $UserResponsibility = $this->user->getUserResponsibility($iduser, 2679);
-    if (count($UserResponsibility)==0) {
-      $UserResponsibility = $this->user->getUserResponsibility($iduser, 2681);
-      
+    $this->login($nomor_induk);
+
+    $approver_responsibility = $this->user->getUserResponsibility($this->session->userid, 2679);
+    $pengelola_responsibility = $this->user->getUserResponsibility($this->session->userid, 2681);
+    $has_approver_responsibility = count($approver_responsibility) > 0;
+    $has_pengelola_responsibility = count($pengelola_responsibility) > 0;
+
+    if ($has_approver_responsibility) {
+      $approver_responsibility = (object) $approver_responsibility[0];
+      $this->setActiveResponsibility($approver_responsibility);
+    } else if ($has_pengelola_responsibility) {
+      $pengelola_responsibility = (object) $pengelola_responsibility[0];
+      $this->setActiveResponsibility($pengelola_responsibility);
+    } else {
+      redirect(base_url());
     }
 
-    foreach($UserResponsibility as $UserResponsibility_item){
-      $this->session->set_userdata('responsibility', $UserResponsibility_item['user_group_menu_name']);
-      // if(empty($UserResponsibility_item['user_group_menu_id'])){
-        // $UserResponsibility_item['user_group_menu_id'] = 0;
-      // }
-      $this->session->set_userdata('responsibility_id', $UserResponsibility_item['user_group_menu_id']);
-      $this->session->set_userdata('module_link', $UserResponsibility_item['module_link']);
-      $this->session->set_userdata('org_id', $UserResponsibility_item['org_id']);
-    }
-
-        $this->checkSession();
-        
-    $user_id = $this->session->userid;
-    
-    $data['Menu'] = 'Dashboard';
-    $data['SubMenuOne'] = '';
-
-    // print_r($this->session->responsibility_id);exit;
-    
-    $data['UserMenu'] = $this->user->getUserMenu($user_id,$this->session->responsibility_id);
-    $data['UserSubMenuOne'] = $this->user->getMenuLv2($user_id,$this->session->responsibility_id);
-    $data['UserSubMenuTwo'] = $this->user->getMenuLv3($user_id,$this->session->responsibility_id);
-
-    if (count($UserResponsibility)==0) {
-      // $resp = $this->session->userdata['responsibility'];
-      echo '<b>ERROR :</b> User ini tidak mempunyai akses ke aplikasi Order Kebutuhan barang Dan Jasa, silahkan tambahkan terlebih  dahulu agar ERP bisa diakses menggunakan akun ini!';
-      exit;
-    }
-    
-    $data['normal'] = array();
-    $data['urgent'] = array();
-    $data['susulan'] = array();
-
-    $noind = $this->session->user;
-    // $noind = '6355';
-		$data['approver'] = $this->requisition->getPersonId($noind);
-
-    $this->load->view('V_Header',$data);
-    $this->load->view('V_Sidemenu',$data);
-    // if ($this->session->responsibility_id == 2664 || $this->session->responsibility_id == 2665) {
-      $allOrder = $this->approver->getListDataOrder();
-      
-      foreach ($allOrder as $key => $order) {
-        $checkOrder = $this->approver->checkOrder($order['ORDER_ID']);
-        // echo'<pre>';
-        // print_r($checkOrder);
-        if (isset($checkOrder[0])) {
-          if ($this->session->responsibility_id == 2681) {
-            if ($checkOrder[0]['APPROVER_ID'] == $data['approver'][0]['PERSON_ID'] && $checkOrder[0]['APPROVER_TYPE'] == 7 ) {
-              $orderSiapTampil = $this->approver->getOrderToApprove($order['ORDER_ID']);
-              if ($orderSiapTampil[0]['ORDER_CLASS'] != '2') {
-                if ($orderSiapTampil[0]['URGENT_FLAG'] == 'N' && $orderSiapTampil[0]['IS_SUSULAN'] =='N') {
-                  array_push($data['normal'], $orderSiapTampil[0]);
-                }elseif ($orderSiapTampil[0]['URGENT_FLAG'] == 'Y' && $orderSiapTampil[0]['IS_SUSULAN'] =='N') {
-                  array_push($data['urgent'], $orderSiapTampil[0]);
-                }elseif ($orderSiapTampil[0]['IS_SUSULAN'] =='Y') {
-                  array_push($data['susulan'], $orderSiapTampil[0]);
-                }
-              }
-            }
-          }else{
-            if ($checkOrder[0]['APPROVER_ID'] == $data['approver'][0]['PERSON_ID'] && $checkOrder[0]['APPROVER_TYPE'] != 7 ) {
-              $orderSiapTampil = $this->approver->getOrderToApprove($order['ORDER_ID']);
-              if ($orderSiapTampil[0]['ORDER_CLASS'] != '2') {
-                if ($orderSiapTampil[0]['URGENT_FLAG'] == 'N' && $orderSiapTampil[0]['IS_SUSULAN'] =='N') {
-                  array_push($data['normal'], $orderSiapTampil[0]);
-                }elseif ($orderSiapTampil[0]['URGENT_FLAG'] == 'Y' && $orderSiapTampil[0]['IS_SUSULAN'] =='N') {
-                  array_push($data['urgent'], $orderSiapTampil[0]);
-                }elseif ($orderSiapTampil[0]['IS_SUSULAN'] =='Y') {
-                  array_push($data['susulan'], $orderSiapTampil[0]);
-                }
-              }
-            }
-          }
-        }
-
-
-      }
-
-      // $data['normalBelum'] = array();
-      // $data['urgentBelum'] = array();
-      // $data['susulanBelum'] = array();
-      // $data['normalSelesai'] = array();
-      // $data['urgentSelesai'] = array();
-      // $data['susulanSelesai'] = array();
-
-      $data['normalOrder'] = $this->approver->getDetailOrderNormalTotal($data['approver'][0]['PERSON_ID']);
-      // foreach ($normalOrder as $key => $normal) {
-      //   if ($normal['JUDGEMENT'] == null) {
-      //     array_push($data['normalBelum'], $normalOrder[$key]);
-      //   }elseif ($normal['JUDGEMENT'] != null) {
-      //     array_push($data['normalSelesai'], $normalOrder[$key]);
-      //   }
-      // }
-      $data['urgentOrder'] = $this->approver->getDetailOrderUrgentTotal($data['approver'][0]['PERSON_ID']);
-      
-      $data['susulanOrder'] = $this->approver->getDetailOrderSusulanTotal($data['approver'][0]['PERSON_ID']);
-      
-      
-      $this->load->view('OrderKebutuhanBarangDanJasa/V_Index',$data);
-    // }else {
-    //   $this->load->view('OrderKebutuhanBarangDanJasa/V_Index2',$data);
-    // }
-        $this->load->view('V_Footer',$data);
-    }
+    redirect(base_url('OrderKebutuhanBarangDanJasa'));
+  }
 }
-
