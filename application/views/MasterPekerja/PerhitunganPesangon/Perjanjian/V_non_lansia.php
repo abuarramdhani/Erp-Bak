64eca1d41e39f04dc6432466e8b63c2ec6987df1
aji kurniawan <aji_kurniawan@quick.com>
2021-02-24 15:02:53 +0700

    update undang-undang cetak pesangon

--- application/views/MasterPekerja/PerhitunganPesangon/Perjanjian/V_non_lansia.php
+++ application/views/MasterPekerja/PerhitunganPesangon/Perjanjian/V_non_lansia.php
@@ -176,7 +176,7 @@
 			<td style="border-right:1px solid black; border-bottom: 1px solid black; width: 200px; font-size: 14px; font-family: times;" valign="top">&nbsp;Kesimpulan</td>
       <td style="border-right:1px solid black; border-bottom: 1px solid black; width:10px; text-align: center font-size: 14px; font-family: times; text-align: center;" valign="top">:</td>
 			<td style="width:5px; border-bottom: 1px solid black;"></td>
-			<td style="border-bottom: 1px solid black; font-size: 14px; font-family: times; text-align: justify;">Kedua belah pihak sepakat untuk mengakhiri hubungan kerjanya secara Bipartit pertanggal <b><?php echo $tgl_keluar.' '.$bln_keluar.' '.$thn_keluar ?></b> dan Perusahaan memberikan kompensasi sesuai UU-RI No.13 tahun 2003<hr width="40%" style="margin-bottom: 0.5em; margin-right: auto;"></td>
+			<td style="border-bottom: 1px solid black; font-size: 14px; font-family: times; text-align: justify;">Kedua belah pihak sepakat untuk mengakhiri hubungan kerjanya secara Bipartit pertanggal <b><?php echo $tgl_keluar.' '.$bln_keluar.' '.$thn_keluar ?></b> dan Perusahaan memberikan kompensasi sesuai PP No. 35 tahun 2021<hr width="40%" style="margin-bottom: 0.5em; margin-right: auto;"></td>
     </tr>
   </table>
 	<br>
