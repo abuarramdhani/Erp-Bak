76a31beb78f4b02cfb4b61deb0cfeb5f98790474
Yusuf Taufiqurrahman <yusuf_taufiqurrahman@quick.com>
2021-03-04 17:59:56 +0700

    Revert "Merge branch 'okebaja-juan' of http://git.quick.com/erp/khs-erp into okebaja-juan"
    This reverts commit 576d482be6834d9026e943d0e9396bc9f9576211

--- application/config/routes.php
+++ application/config/routes.php
@@ -3930,7 +3930,6 @@ $route['OrderKebutuhanBarangDanJasa/Import/(:any)'] = 'OrderKebutuhanBarangDanJa
 $route['OrderKebutuhanBarangDanJasa/SMSApprover'] = 'OrderKebutuhanBarangDanJasa/C_SendSMS';
 $route['OrderKebutuhanBarangDanJasa/SMSPengelola'] = 'OrderKebutuhanBarangDanJasa/C_SendSMS/pengelola';
 $route['OrderKebutuhanBarangDanJasa/SMSPuller'] = 'OrderKebutuhanBarangDanJasa/C_SendSMS/puller';
-$route['OrderKebutuhanBarangDanJasa/SMSPurchasing'] = 'OrderKebutuhanBarangDanJasa/C_SendSMS/purchasing';
 
 //--------------------------------------------------Monitoring Pengiriman Pesanan Luar-------------------------------//
 $route['MonitoringPengirimanPesananLuar'] 	= 'MonitoringPengPesananLuar/C_MonitoringPengPesananLuar/rekapPO';
