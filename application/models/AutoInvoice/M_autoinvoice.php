a6d782c8a715a7f695c46f1d3abf747dd73ee231
Violin Radhiyan <rizki_violin_radhiyan@git.quick.com>
2021-03-30 07:41:00 +0700

    push model lupa prod

--- application/models/AutoInvoice/M_autoinvoice.php
+++ application/models/AutoInvoice/M_autoinvoice.php
@@ -6,7 +6,7 @@ class M_autoinvoice extends CI_Model
     {
         parent::__construct();
         $this->load->database();
-        $this->oracle = $this->load->database('oracle_dev', true);
+        $this->oracle = $this->load->database('oracle', true);
     }
     public function DoReady()
     {
