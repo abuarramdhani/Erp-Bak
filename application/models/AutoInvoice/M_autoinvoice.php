57a6eea06e78297ad001787a6e446746c02a241b
Violin Radhiyan <rizki_violin_radhiyan@git.quick.com>
2021-03-30 10:18:44 +0700

    Push Model Revisi

--- application/models/AutoInvoice/M_autoinvoice.php
+++ application/models/AutoInvoice/M_autoinvoice.php
@@ -97,7 +97,7 @@ class M_autoinvoice extends CI_Model
         AND wdd.SOURCE_HEADER_ID = ooha.HEADER_ID
         AND wdd.ORG_ID = hou.ORGANIZATION_ID
         AND wdd.RELEASED_STATUS = 'Y'
-        AND not exists (select wdd1.BATCH_ID from wsh_delivery_details wdd1 where wdd1.BATCH_ID = wdd.BATCH_ID and wdd1.RELEASED_STATUS <> 'Y')
+        AND not exists (select wdd1.BATCH_ID from wsh_delivery_details wdd1 where wdd1.BATCH_ID = wdd.BATCH_ID and wdd1.RELEASED_STATUS = 'S')
         AND wdd.BATCH_ID = ksw.WDD_BATCH_ID
         AND ksw.TRX_NUMBER is null";
 
