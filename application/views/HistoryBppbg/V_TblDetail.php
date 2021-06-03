be91333186b9e50d9c4efbcdfcad6d3ff1f441ac
Simon Hertoyo <simon_hertoyo@quick.com>
2021-06-03 13:58:30 +0700

    Update V_TblDetail.php

--- application/views/HistoryBppbg/V_TblDetail.php
+++ application/views/HistoryBppbg/V_TblDetail.php
@@ -10,12 +10,10 @@
                 <th style="vertical-align: middle; width: 5%;">No</th>
                 <th style="vertical-align: middle;">Kode Barang</th>
                 <th style="vertical-align: middle;">Deskripsi</th>
-                <!-- <th style="vertical-align: middle; width: 5%;">Account</th> -->
-                <!-- <th style="vertical-align: middle; width: 5%;">Cost Center</th> -->
                 <th style="vertical-align: middle;">Seksi Pengebon</th>
-                <!-- <th style="vertical-align: middle; width: 5%;">Seksi Pemakai</th> -->
                 <th style="vertical-align: middle; width: 8%;">Gudang</th>
                 <th style="vertical-align: middle; width: 5%;">Tanggal Pembuatan Bppbg</th>
+                <th style="vertical-align: middle; width: 14%;">Keterangan</th>
                 <th style="vertical-align: middle; width: 5%;">Status Transact</th>
             </tr>
         </thead>
@@ -44,12 +42,10 @@
                 <td style="width: 5%;"><?= $no; ?></td>
                 <td><?= $i['KODE_BARANG'] ?></td>
                 <td style="text-align:left;"><?= $i['NAMA_BARANG'] ?></td>
-                <!-- <td style="width: 5%;"><?= $i['ACCOUNT'] ?></td> -->
-                <!-- <td style="width: 5%;"><?= $i['COST_CENTER'] ?></td> -->
                 <td><?= $i['SEKSI_BON'] ?></td>
-                <!-- <td style="width: 5%;"><?= $i['PEMAKAI'] ?></td> -->
                 <td style="width: 8%;"><?= $i['TUJUAN_GUDANG'] ?></td>
                 <td style="width: 10%;"><?= $i['TANGGAL'] ?></td>
+                <td style="width: 14%;"><?= $i['KETERANGAN'] ?></td>
                 <td style="width: 5%; <?= $style ?>"><?= $icon ?></td>
             </tr>
             <?php $no++;} ?>
