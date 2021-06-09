31a244a0e8a49256adc87edd486fbf2414f507ef
Simon Hertoyo <simon_hertoyo@quick.com>
2021-06-09 14:20:07 +0700

    Update cek stok

--- assets/js/customMHS.js
+++ assets/js/customMHS.js
@@ -50,46 +50,66 @@ $(document).ready(function(){
   });
 });
 
+
+function checkStock(){
+  var subkon = $('#slcSubStock option:selected').val();
+  var subname = $('#slcSubStock option:selected').html();
+  var handling = $('#slcHanStock option:selected').val();
+
+  ajax = $.ajax({
+    url: baseurl + 'MonitoringHandlingSubkon/Stock/getDataStock',
+    data: {
+      subkon : subkon,
+      subname : subname,
+      handling : handling
+    },
+    type: 'POST',
+    beforeSend: function() {
+      $('#loadingArea2').show();
+      $('div#tb_stock').hide();
+    },
+    success: function(result) {
+      $('#loadingArea2').hide();       
+      $('div#tb_stock').show();
+      $('div#tb_stock').html(result);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.error();
+    }
+  })
+}
+
+
 function cariHS(){
   var subkon = $('#slcJenisSub option:selected').val();
   var subname = $('#slcJenisSub option:selected').html();
   var handling = $('#slcHandling option:selected').val();
   var date = $('#slcDateRange').val();
   var status = $('#slcType option:selected').val();
-  console.log(subkon + ' ' + subname + ' ' + handling);
 
-  if (subkon == '') {
-    Swal.fire(
-      'Oops!',
-      'Nama Subkon tidak boleh kosong!!!',
-      'error'
-    )
-  }
-  else {
-    ajax = $.ajax({
-      url: baseurl + 'MonitoringHandlingSubkon/Monitoring/getDataMonitoring',
-      data: {
-        subkon : subkon,
-        subname : subname,
-        handling : handling,
-        date : date,
-        status : status
-      },
-      type: 'POST',
-      beforeSend: function() {
-        $('#loadingArea').show();
-        $('div#tb_monitoring').hide();
-      },
-      success: function(result) {
-        $('#loadingArea').hide();       
-        $('div#tb_monitoring').show();
-        $('div#tb_monitoring').html(result);
-      },
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        console.error();
-      }
-    })    
-  }
+  ajax = $.ajax({
+    url: baseurl + 'MonitoringHandlingSubkon/Monitoring/getDataMonitoring',
+    data: {
+      subkon : subkon,
+      subname : subname,
+      handling : handling,
+      date : date,
+      status : status
+    },
+    type: 'POST',
+    beforeSend: function() {
+      $('#loadingArea').show();
+      $('div#tb_monitoring').hide();
+    },
+    success: function(result) {
+      $('#loadingArea').hide();       
+      $('div#tb_monitoring').show();
+      $('div#tb_monitoring').html(result);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.error();
+    }
+  })
 }
 
 
@@ -125,4 +145,43 @@ function cetak(){
   else {
     $('#linkCetak').attr('href',baseurl + "MonitoringHandlingSubkon/Cetak/cetakKIS/" + subkon);
   }
+}
+
+
+function editModal(locator,subkon,handling,nama_handling,saldo){
+  $('#editSubkon').html(subkon);
+  $('#editNamaHandling').html('[ ' + handling + ' ] ' + nama_handling);
+  $('#txtEditSaldo').val(saldo);
+  $('#editLoc').val(locator);
+  $('#editHanId').val(handling);
+}
+
+function editSaldo(){
+  var qty = $('#txtEditSaldo').val();
+  var loc = $('#editLoc').val();
+  var handling_code = $('#editHanId').val();
+
+  ajax = $.ajax({
+    url: baseurl + 'MonitoringHandlingSubkon/Stock/editSaldoAwal',
+    data: {
+      qty : qty,
+      loc : loc,
+      handling_code : handling_code
+    },
+    type: 'POST',
+    beforeSend: function() {
+    },
+    success: function(result) {
+      Swal.fire(
+        'Success!',
+        'Data berhasil diedit!!!',
+        'success'
+      );
+      $('#editModal').modal('hide');
+      checkStock();
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      console.error();
+    }
+  })
 }
