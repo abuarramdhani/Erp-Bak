cdee9cc087c4a5856e79f5a80d7806748d7632c8
Meyta <meyta_rizki_nugraheni@quick.com>
2021-06-10 18:50:14 +0700

    target laju laporan produksi

--- application/views/MonitoringJobProduksi/V_PdfLaporan.php
+++ application/views/MonitoringJobProduksi/V_PdfLaporan.php
@@ -55,10 +55,11 @@
         <td style="border-bottom:0px solid black; border-collapse: collapse;text-align:center;font-size: 12px"><?= ($tanggal)?>/<?= ($data['hari'] - 4)?> hari kerja = <?= round((($tanggal)/($data['hari'] - 4)) * 100, 2)?>% </td>
     </tr>
 	<?php
+	$target_laju = ($data['hari'] - 4) - $tanggal == 0 ? 0 : round(($data['ttl_target'] - $data['ttl_real'])/ (($data['hari'] - 4) - $tanggal));
 	$satuan_laju = $data['id_kategori'] == 15 || $data['id_kategori'] == 19 || $data['id_kategori'] == 13 || $data['id_kategori'] == 14 ? 'pcs' : 'unit';
 	?>
     <tr>
-        <td style="border-bottom:0px solid black; border-collapse: collapse;text-align:center;font-size: 12px">Target laju saat ini : <?= round(($data['ttl_target'] - $data['ttl_real'])/ (($data['hari'] - 4) - $tanggal)).' '.$satuan_laju?></td>
+        <td style="border-bottom:0px solid black; border-collapse: collapse;text-align:center;font-size: 12px">Target laju saat ini : <?= $target_laju.' '.$satuan_laju?></td>
     </tr>
 </table>
 </div>
