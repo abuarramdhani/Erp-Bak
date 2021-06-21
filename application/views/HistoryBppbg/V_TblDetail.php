bf2d398b1c5998d0826edd12c01b29e94b63c1ce
Simon Hertoyo <simon_hertoyo@quick.com>
2021-06-21 14:57:54 +0700

    Update V_TblDetail.php

--- application/views/HistoryBppbg/V_TblDetail.php
+++ application/views/HistoryBppbg/V_TblDetail.php
@@ -1,8 +1,20 @@
 <div class="panel-body">
-    <p style="float: left;">
-        <img style="width: 20mm; height: auto;" src="<?php echo base_url('assets/img/bppbgQRCODE/'.$bon.'.png') ?>">
-        <b>SCAN ME!!!</b>
-    </p>
+    <div class="row">
+        <div class="col-md-4">
+            <p style="float: left;">
+                <img style="width: 20mm; height: auto;" src="<?php echo base_url('assets/img/bppbgQRCODE/'.$bon.'.png') ?>">
+                <b>SCAN ME!!!</b>
+            </p>            
+        </div>
+        <div class="col-md-4">
+            <h5><b>Tanggal Pengebonan : </b><br><?= $bppbg[0]['TANGGAL'] ?></h5>
+            <h5><b>Tujuan Gudang : </b><br><?= $bppbg[0]['TUJUAN_GUDANG'] ?></h5>            
+        </div>
+        <div class="col-md-4">
+            <h5><b>Seksi Pengebon : </b><br><?= $bppbg[0]['SEKSI_BON'] ?></h5>
+            <h5><b>Seksi Pemakai : </b><br><?= $bppbg[0]['SEKSI_PEMAKAI'] ?></h5>
+        </div>
+    </div>
     <br>
     <table class="datatable table table-bordered table-hover table-striped text-center" id="myTable" style="width: 100%;">
         <thead style="background-color:#6ACEF0">
@@ -10,11 +22,9 @@
                 <th style="vertical-align: middle; width: 5%;">No</th>
                 <th style="vertical-align: middle;">Kode Barang</th>
                 <th style="vertical-align: middle;">Deskripsi</th>
-                <th style="vertical-align: middle;">Seksi Pengebon</th>
                 <th style="vertical-align: middle; width: 5%;">Permintaan</th>
                 <th style="vertical-align: middle; width: 5%;">Penyerahan</th>
-                <th style="vertical-align: middle; width: 5%;">Satuan</th>
-                <th style="vertical-align: middle; width: 5%;">Tanggal Pembuatan Bppbg</th>
+                <th style="vertical-align: middle; width: 5%;">UOM</th>
                 <th style="vertical-align: middle; width: 15%;">Keterangan</th>
                 <th style="vertical-align: middle; width: 5%;">Transact Android</th>
                 <th style="vertical-align: middle; width: 5%;">Material Transaction</th>
@@ -74,16 +84,12 @@
             <tr style="background-color: <?= $color ?>; <?= $fw ?>">
                 <td style="width: 5%;">
                     <?= $no; ?>
-                        
-                    </td>
+                </td>
                 <td>
                     <?= $i['KODE_BARANG'] ?>
                 </td>
                 <td style="text-align:left;">
                     <?= $i['NAMA_BARANG'] ?>
-                </td>
-                <td>
-                    <?= $i['SEKSI_BON'] ?>
                 </td>
                 <td style="width: 5%;">
                     <?= $i['PERMINTAAN'] ?>
@@ -93,9 +99,6 @@
                 </td>
                 <td style="width: 5%;">
                     <?= $i['SATUAN'] ?>
-                </td>
-                <td style="width: 10%;">
-                    <?= $i['TANGGAL'] ?>
                 </td>
                 <td style="width: 15%;">
                     <?= $i['KETERANGAN'] ?>
