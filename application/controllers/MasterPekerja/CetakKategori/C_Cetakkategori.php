79732a734d0e1e5d81c464c93131f002ec07d258
enggal aldiansyah <enggal_aldiansyah@quick.com>
2021-06-30 11:46:44 +0700

    SMS SIM
    update
    > Pakai GSM 1.2

--- application/controllers/MasterPekerja/CetakKategori/C_Cetakkategori.php
+++ application/controllers/MasterPekerja/CetakKategori/C_Cetakkategori.php
@@ -153,13 +153,10 @@ class C_Cetakkategori extends CI_Controller
                     if ($datvk == 'masa_kerja') {
                         $worksheet->setCellValue($cell[$cellInd] . $rowStart, str_replace(['years', 'mons', 'days'], ['Tahun', 'Bulan', 'Hari'], $datvkv));
                     } else {
-                        $worksheet->setCellValue($cell[$cellInd] . $rowStart, $datvkv);
-                        $worksheet
-                        ->getStyle($cell[$cellInd] . $rowStart)
-                        ->getNumberFormat()
-                        ->setFormatCode(
-                            PHPExcel_Style_NumberFormat::FORMAT_TEXT
-                            );
+                        if($datvk == 'nik' || $datvk == 'no_kk')
+                            $worksheet->setCellValue($cell[$cellInd] . $rowStart, "'".$datvkv);
+                        else
+                            $worksheet->setCellValue($cell[$cellInd] . $rowStart, $datvkv);
                     }
                     $worksheet->getStyle($cell[$cellInd] . $rowStart)->applyFromArray([
                         'borders' => [
