857e2a92eda2ece3c1af2259826042c8b32ea336
Violin Radhiyan <rizki_violin_radhiyan@git.quick.com>
2021-07-16 14:39:23 +0700

    push model prod

--- application/models/KomisiPenjualan/M_komisi.php
+++ application/models/KomisiPenjualan/M_komisi.php
@@ -6,7 +6,7 @@ class M_komisi extends CI_Model
     {
         parent::__construct();
         $this->load->database();
-        $this->oracle = $this->load->database('oracle_dev', true);
+        $this->oracle = $this->load->database('oracle', true);
         $this->personalia = $this->load->database('personalia', true);
     }
     public function getLineId($n)
