895bfc2b365f160f26e17d8759770f714b95a281
kurniawan1209 <kurniawan1564@gmail.com>
2021-08-23 09:59:30 +0700

    Perbaikan validasi pada pipeline LPT

--- application/controllers/LaporanPenjualanTraktor/Pipeline/C_Index.php
+++ application/controllers/LaporanPenjualanTraktor/Pipeline/C_Index.php
@@ -13,24 +13,20 @@ class C_Index extends CI_Controller
 		$this->load->model('LaporanPenjualanTraktor/M_pipeline');
 	}
 
-	// public function index(){
-	// 	print_r('Masih dalam perbaikan');
-	// }
-
 	public function index(){
 
 		$max_req_type = $this->M_pipeline->get_max_request('khs_lpb_type')[0]['REQ_ID'];
 		$max_req_detail = $this->M_pipeline->get_max_request('khs_lpb_penjualan_detail')[0]['REQ_ID'];
 
 		$validate_type = $this->M_pipeline->check_data_availability('khs_lpb_type_hari', 'REQUEST_ID', $max_req_type);
-		$validate_detail = $this->M_pipeline->check_data_availability('khs_lpb_type_hari', 'REQUEST_ID', $max_req_detail);
+		$validate_detail = $this->M_pipeline->check_data_availability('khs_lpb_penjualan_detail', 'REQUEST_ID', $max_req_detail);
 
 		if ($validate_type[0]['count'] == 0){
 			$sales = $this->M_pipeline->get_data_sales(); // DATA PENJUALAN
 			$this->M_pipeline->insert_sales_data('khs_lpb_type_hari', $sales);
 		}
 
-		if ($validate_type[0]['count'] == 0){
+		if ($validate_detail[0]['count'] == 0){
 			$detail = $this->M_pipeline->get_data_sales_detail(); // DATA PENJUALAN DETAIL
 			$this->M_pipeline->insert_sales_data('khs_lpb_penjualan_detail', $detail);
 		}		
@@ -49,6 +45,22 @@ class C_Index extends CI_Controller
 	}
 
 	public function support_data($data, $table, $pk){
+
+		if ($table == 'khs_lpb_skip_date'){
+			$ora_skip = $this->M_pipeline->get_all_skip_date('ORACLE');
+			$ora_skip = array_column($ora_skip, "DATE_ID");
+
+			$pgr_skip = $this->M_pipeline->get_all_skip_date('POSTGRES');
+			$pgr_skip = array_column($pgr_skip, "DATE_ID");
+
+			$diff = array_diff($pgr_skip, $ora_skip);
+
+			if (!(empty($diff))){
+				$key = array_keys($diff)[0];
+				$this->M_pipeline->delete_data($table, "DATE_ID", $diff[$key]);
+			}
+		}
+
 		foreach ($data as $key => $dt) {
 			$validate = $this->M_pipeline->check_data_availability($table, $pk, $dt[$pk]);
 
