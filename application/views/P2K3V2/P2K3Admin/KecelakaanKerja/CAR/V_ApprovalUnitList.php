061698caa6e521370a3641bd073ac1367557265f
akbarsani46 <akbarsani46@gitlab.com>
2021-09-11 11:32:15 +0700

    Enable CAR

--- application/views/P2K3V2/P2K3Admin/KecelakaanKerja/CAR/V_ApprovalUnitList.php
+++ application/views/P2K3V2/P2K3Admin/KecelakaanKerja/CAR/V_ApprovalUnitList.php
@@ -101,7 +101,7 @@
                               <i class="fa fa-file-pdf-o"></i>
                             </a>
                             <?php if ($key['car_is_created'] == 't') : ?>
-                              <a target="_blank" href="<?= base_url("p2k3adm_V2/Admin/Car/Approval/Unit/$key[id_kecelakaan]") ?>" title="<?= $unitCarButtonTitle ?>" class="btn <?= $carButtonClass ?> btn-sm" style="display:none">
+                              <a target="_blank" href="<?= base_url("p2k3adm_V2/Admin/Car/Approval/Unit/$key[id_kecelakaan]") ?>" title="<?= $unitCarButtonTitle ?>" class="btn <?= $carButtonClass ?> btn-sm" style="display:block">
                                 <i class="fa fa-check-circle"></i>
                               </a>
                             <?php else : ?>
